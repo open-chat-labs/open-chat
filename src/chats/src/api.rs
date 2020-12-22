eb4867b812464fd9a6a0002cb9824ffdf12e0f11
megrogan <megrogan@gmail.com>
2020-12-22 12:02:52 +0000

    Added list_chats query

--- src/chats/src/api.rs
+++ src/chats/src/api.rs
@@ -1,13 +1,13 @@
 use ic_cdk_macros::*;
 use ic_types::Principal;
-use crate::domain::chat::Message;
+use crate::domain::chat::{ChatId, ChatSummary, Message};
 use crate::queries::*;
 use crate::updates::*;
-use crate::domain::chat_list::ChatId;
 
-// #[query]
-// fn list_chats() -> Vec<ChatSummary> {
-// }
+#[query]
+fn list_chats() -> Vec<ChatSummary> {
+    list_chats::query()
+}
 
 #[query]
 fn get_messages(chat_id: ChatId, from_index: usize) -> Option<Vec<Message>> {
@@ -15,12 +15,12 @@ fn get_messages(chat_id: ChatId, from_index: usize) -> Option<Vec<Message>> {
 }
 
 #[update]
-fn create_chat(recipient: Principal) -> Option<ChatId> {
-    create_chat::update(recipient)    
+fn create_chat(recipient: Principal, text: String) -> Option<ChatId> {
+    create_chat::update(recipient, text)
 }
 
 #[update]
-fn send_message(chat_id: ChatId, text: String) -> bool {
+fn send_message(chat_id: ChatId, text: String) -> Option<u64> {
     send_message::update(chat_id, text)
 }
 
--- src/chats/src/updates/create_chat.rs
+++ src/chats/src/updates/create_chat.rs
@@ -1,11 +1,12 @@
+use ic_cdk::{api, storage};
 use ic_types::Principal;
-use crate::domain::chat_list::{ChatId, ChatList};
-use ic_cdk::{storage};
+use crate::domain::chat::ChatId;
+use crate::domain::chat_list::ChatList;
 
-pub fn update(recipient: Principal) -> Option<ChatId> {
-
+pub fn update(recipient: Principal, text: String) -> Option<ChatId> {
     let chat_list: &mut ChatList = storage::get_mut();
     let me = ic_cdk::caller();
+    let timestamp = api::time() as u64;
 
-    chat_list.create(me, recipient)
+    chat_list.create(me, recipient, text, timestamp)
 }
