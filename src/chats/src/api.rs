634dce4e9dd4fd97a572766edf60961cb5afe6e3
Hamish Peebles <hamishpeebles@gmail.com>
2020-12-28 15:38:41 +0000

    Add 'to_id' to get_messages so that any slice of messages can be retrieved

--- src/chats/src/api.rs
+++ src/chats/src/api.rs
@@ -40,7 +40,7 @@ fn list_chats() -> Vec<ChatSummary> {
 }
 
 #[query]
-fn get_messages(chat_id: ChatId, from_id: u32) -> Option<Vec<Message>> {
-    get_messages::query(chat_id, from_id)
+fn get_messages(chat_id: ChatId, from_id: u32, to_id: u32) -> Option<Vec<Message>> {
+    get_messages::query(chat_id, from_id, to_id)
 }
 
--- src/chats/src/queries/get_messages.rs
+++ src/chats/src/queries/get_messages.rs
@@ -2,10 +2,10 @@ use ic_cdk::storage;
 use crate::domain::chat_list::ChatList;
 use crate::domain::chat::{Chat, ChatId, Message};
 
-pub fn query(chat_id: ChatId, from_id: u32) -> Option<Vec<Message>> {
+pub fn query(chat_id: ChatId, from_id: u32, to_id: u32) -> Option<Vec<Message>> {
     let chat_list: &ChatList = storage::get();
     let me = shared::user_id::get_current();
     let chat = chat_list.get(chat_id, &me)?;
 
-    Some(chat.get_messages(from_id))
+    Some(chat.get_messages(from_id, to_id))
 }
