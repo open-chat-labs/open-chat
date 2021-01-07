f0c5f0994454439bdfa965407e91642da1561c08
Hamish Peebles <hamishpeebles@gmail.com>
2021-01-07 09:48:32 +0000

    Fix component id

--- src/open_chat_assets/public/components/Main.tsx
+++ src/open_chat_assets/public/components/Main.tsx
@@ -29,7 +29,7 @@ export default function() {
     });
 
     return (
-        <div id="side" style={{ display:"flex", flexDirection:"column", height:"100%" }}>
+        <div id="main" style={{ display:"flex", flexDirection:"column", height:"100%" }}>
             <div>
                 {messages}
             </div>
