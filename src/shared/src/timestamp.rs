6c7a92c8433d38d24ad4e990520812b176b24143
Hamish Peebles <hamishpeebles@gmail.com>
2021-01-07 17:19:59 +0000

    Change Timestamp values to be based on number of milliseconds (#27)

--- src/shared/src/timestamp.rs
+++ src/shared/src/timestamp.rs
@@ -1,5 +1,7 @@
 pub type Timestamp = u64;
 
+const NANOS_PER_MILLISECOND: u64 = 1_000_000;
+
 pub fn now() -> Timestamp {
-    ic_cdk::api::time() as Timestamp
+    (ic_cdk::api::time() as u64 / NANOS_PER_MILLISECOND) as Timestamp
 }
