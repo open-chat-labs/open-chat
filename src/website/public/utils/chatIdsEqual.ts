60fa8e9d2398669431e0121d468b454206c1c58d
megrogan <megrogan@gmail.com>
2021-01-08 16:47:16 +0000

    Deleted some old files

--- src/website/public/utils/chatIdsEqual.ts
+++
@@ -1,6 +0,0 @@
-import { ChatId } from "../model/chats";
-
-export default function(chatId1: ChatId, chatId2: ChatId) : boolean {
-    // TODO: Sort this!
-    return Boolean(chatId1 && chatId2 && chatId1.toString() === chatId2.toString());
-}
