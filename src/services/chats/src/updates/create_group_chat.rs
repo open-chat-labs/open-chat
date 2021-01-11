09445969cbe18eeef238a7909eb3176ba1cd025c
megrogan <megrogan@gmail.com>
2021-01-11 15:47:48 +0000

    Add timestamp to CreateGroupChatResponse

--- src/services/chats/src/updates/create_group_chat.rs
+++ src/services/chats/src/updates/create_group_chat.rs
@@ -1,6 +1,6 @@
 use ic_cdk::export::candid::CandidType;
 use ic_cdk::storage;
-use shared::timestamp;
+use shared::{timestamp, timestamp::Timestamp};
 use shared::user_id::UserId;
 use crate::domain::chat::ChatId;
 use crate::domain::chat_list::ChatList;
@@ -12,13 +12,22 @@ pub fn update(participants: Vec<UserId>, subject: String) -> Response {
     let now = timestamp::now();
 
     match chat_list.create_group_chat(me, participants, subject, now) {
-        Some(chat_id) => Success(chat_id),
+        Some(chat_id) => Success(Result {
+            chat_id,
+            created: now
+        }),
         None => ChatAlreadyExists,
     }
 }
 
 #[derive(CandidType)]
+pub struct Result {
+    chat_id: ChatId,
+    created: Timestamp
+}
+
+#[derive(CandidType)]
 pub enum Response {
-    Success(ChatId),
+    Success(Result),
     ChatAlreadyExists,
 }
