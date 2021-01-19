20cbad793e339d8bdf8526cf3121c0e4f6884e0a
megrogan <megrogan@gmail.com>
2021-01-19 16:06:43 +0000

    Enable BlobStorge upgrade

--- src/services/chats/src/upgrade.rs
+++ src/services/chats/src/upgrade.rs
@@ -1,13 +1,16 @@
 use ic_cdk_macros::*;
 use shared::upgrade;
 use crate::domain::chat_list::ChatList;
+use crate::domain::blob_storage::BlobStorage;
 
 #[pre_upgrade]
 fn pre_upgrade() {
     upgrade::pre_upgrade::<ChatList>();
+    upgrade::pre_upgrade::<BlobStorage>();
 }
 
 #[post_upgrade]
 fn post_upgrade() {
     upgrade::post_upgrade::<ChatList>();
+    upgrade::post_upgrade::<BlobStorage>();
 }
