712bbc7eafe77ef37ec469ecb4cccfcfcef79dc8
Hamish Peebles <hamishpeebles@gmail.com>
2021-01-21 22:53:41 +0000

    Remove the need for each message to be assigned a unique key

--- src/website/public/services/candidConverters/localMessage.ts
+++ src/website/public/services/candidConverters/localMessage.ts
@@ -1,4 +1,3 @@
-import { v1 as uuidv1 } from "uuid";
 import { LocalMessage } from "../../model/messages";
 import { fromCandid as messageContentFromCandid } from "./messageContent";
 import { toDate as timestampToDate } from "../candidConverters/timestamp";
@@ -8,7 +7,7 @@ export function fromCandid(value: any) : LocalMessage {
     return {
         kind: "local",
         id: value.id,
-        key: uuidv1().toString(),
+        key: value.id,
         date: timestampToDate(value.timestamp),
         sender: userIdFromCandid(value.sender),
         content: messageContentFromCandid(value.content)
