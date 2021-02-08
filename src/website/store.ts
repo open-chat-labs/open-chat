222a474ade7e8501867c17e2302215b300db6630
megrogan <megrogan@gmail.com>
2021-02-08 12:48:36 +0000

    Small fix

--- src/website/store.ts
+++ src/website/store.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
