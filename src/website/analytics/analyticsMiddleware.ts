653d0bd0114f878ecb0b342783f7490cf6baae0d
Hamish Peebles <hamishpeebles@gmail.com>
2021-02-19 09:50:01 +0000

    Formatting

--- src/website/analytics/analyticsMiddleware.ts
+++ src/website/analytics/analyticsMiddleware.ts
@@ -1,6 +1,7 @@
 import { Middleware } from "redux";
 import { RootState } from "../reducers";
 import trackEvent from "./eventTracker";
+
 const analyticsMiddleware : Middleware<{}, RootState> = store => next => event => {
     trackEvent(event.type, event.payload);
     return next(event);
