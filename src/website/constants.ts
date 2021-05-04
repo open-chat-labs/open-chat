386a0e06a4f93ef912f699b5234ad139a3d7f1b9
Dimitris Sarlis <dimitris@dfinity.org>
2021-05-04 15:49:37 +0200

    Latest deployment on messaging

--- src/website/constants.ts
+++ src/website/constants.ts
@@ -1,6 +1,6 @@
 export const APP_TITLE = "Open Chat";
 
-export const IDP_URL = 'https://identity.messaging.ic0.app/authorize';
+export const IDP_URL = 'https://identity.messaging.dfinity.network/';
 
 export const PAGE_SIZE = 20;
 export const CHUNK_SIZE_BYTES = 1024 * 500; // 500KB
