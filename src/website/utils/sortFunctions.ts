66cf463c95abe9fb83ec15286bc1667066abf9a2
megrogan <megrogan@gmail.com>
2021-05-24 17:57:05 +0100

    Remove v slow sorting!

--- src/website/utils/sortFunctions.ts
+++ src/website/utils/sortFunctions.ts
@@ -8,13 +8,7 @@ export function compareBy(property: string, caseInsensitive: boolean) {
     return function (a: any, b: any) {
         // this works with strings and numbers
         let a1 = a[property];
-        let b1 = b[property];
-        
-        if (caseInsensitive) {
-            a1 = a1.toString().toLower();
-            b1 = b1.toString().toLower();
-        }
-
+        let b1 = b[property];        
         var result = (a1 < b1) ? -1 : (a1 > b1) ? 1 : 0;
         return result * sortOrder;
     }
