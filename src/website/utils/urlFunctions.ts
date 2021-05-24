c8d69b46848d0d90dd378713c6eb80a5e1e854e0
megrogan <megrogan@gmail.com>
2021-05-24 22:39:20 +0100

    Simplify regex for finding urls in text

--- src/website/utils/urlFunctions.ts
+++ src/website/utils/urlFunctions.ts
@@ -15,12 +15,10 @@ export function extractQueryStringAsObject(): any {
         : {};
 }
 
-export function  wrapURLs(text: string, new_window: boolean) : string {
-    var url_pattern = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}\-\x{ffff}]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig;
+export function wrapURLs(text: string, new_window: boolean) : string {
+    var url_pattern = /(ftp|http|https):\/\/[^ "]+/g;
     var target = (new_window === true || new_window == null) ? '_blank' : '';    
     return text.replace(url_pattern, function (url) {
-        var protocol_pattern = /^(?:(?:https?|ftp):\/\/)/i;
-        var href = protocol_pattern.test(url) ? url : 'http://' + url;
-        return '<a href="' + href + '" target="' + target + '">' + url + '</a>';
+        return '<a href="' + url + '" target="' + target + '">' + url + '</a>';
     });
 };
