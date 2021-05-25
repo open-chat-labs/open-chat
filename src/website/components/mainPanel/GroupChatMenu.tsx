62cbaa1180996df0bdb5471aa8080e32f60e6cd8
Hamish Peebles <hamishpeebles@gmail.com>
2021-05-25 23:28:55 +0100

    Ask users for confirmation before leaving a group

--- src/website/components/mainPanel/GroupChatMenu.tsx
+++ src/website/components/mainPanel/GroupChatMenu.tsx
@@ -27,7 +27,11 @@ function DirectChatMenu(props: Props) {
 
     const menuItems: MenuItem[] = [];
     menuItems.push({ text: "Participants", action: () => dispatch(changeRightPanel(RightPanelType.Particpants)) });
-    menuItems.push({ text: "Leave group", action: () => dispatch(leaveGroup(props.chatId))});
+    menuItems.push({ text: "Leave group", action: () => {
+        if (confirm("Are you sure you want to leave this group?")) {
+            dispatch(leaveGroup(props.chatId));
+        }
+    }});
 
     return <PopOverMenu icon={<MoreVertIcon className={classes.menuIcon} />} menuItems={menuItems} placement="bottom-end" />;
 }
