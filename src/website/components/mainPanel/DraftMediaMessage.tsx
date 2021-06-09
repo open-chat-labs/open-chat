eb8639ca1cf447ddf3508eccdafae3a627efcedf
megrogan <megrogan@gmail.com>
2021-06-09 21:24:30 +0100

    Stop image overflow in DraftMediaMessage

--- src/website/components/mainPanel/DraftMediaMessage.tsx
+++ src/website/components/mainPanel/DraftMediaMessage.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import makeStyles from "@material-ui/styles/makeStyles";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
-import Box from "@material-ui/core/Box";
+import { RootState } from "../../reducers";
+import { ViewMode } from "../../domain/model/viewMode";
 import Image from "../shared/Image";
 import Video from "../shared/Video";
+import Dimensions from "../../utils/Dimensions";
 
 type Props = {
     width: number,
@@ -12,15 +15,20 @@ type Props = {
     blobUrl: string
 }
 
+type StyleProps = {
+    width: number,
+    height: number,
+}
+
 export default React.memo(DraftMediaMessage);
 
-const useStyles = makeStyles<Theme>((theme: Theme) => ({
+const useStyles = makeStyles<Theme, StyleProps>((_: Theme) => ({
     media: {
-        minWidth: 250,
-        maxWidth: 500,
         margin: 14,
         marginBottom: 4,
         borderRadius: 16,
+        width: props => props.width,
+        height: props => props.height,
         "& *": {
             borderRadius: "inherit"
         }
@@ -28,12 +36,21 @@ const useStyles = makeStyles<Theme>((theme: Theme) => ({
 }));
 
 function DraftMediaMessage(props: Props): JSX.Element {
-    const classes = useStyles();
+    const viewMode = useSelector((state: RootState) => state.appState.viewMode);
+    const maxDim = viewMode === ViewMode.Desktop ? 440 : 290;
+    const mediaDimensions = new Dimensions(props.width, props.height);
+    const scaled = mediaDimensions.scaleToFit(new Dimensions(maxDim, maxDim));
+    const styleProps = {
+        width: scaled.width,
+        height: scaled.height
+    };
+    const classes = useStyles(styleProps);
+
     return (
-        <Box className={classes.media}>
+        <div className={classes.media}>
             {props.mimeType.startsWith("image/") 
             ? <Image src={props.blobUrl} /> 
             : <Video src={props.blobUrl} />}
-        </Box>
+        </div>
     );
 }
