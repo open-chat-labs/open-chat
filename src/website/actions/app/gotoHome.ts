1263df557139cbc39993b1c6ec80ce311676a5b6
megrogan <megrogan@gmail.com>
2021-06-10 18:07:31 +0100

    Allow back button on desktop (#309)

--- src/website/actions/app/gotoHome.ts
+++ src/website/actions/app/gotoHome.ts
@@ -1,26 +1,7 @@
-import { Dispatch } from "react";
-import { RootState } from "../../reducers";
-import { ViewMode } from "../../domain/model/viewMode";
-import * as historyFunctions from "../../domain/historyFunctions";
-import { gotoChatByIndex } from "../chats/gotoChat";
-
 export const GOTO_HOME = "GOTO_HOME";
 
 export default function() {
-    return async (dispatch: Dispatch<any>, getState: () => RootState) => {
-        if (getState().appState.viewMode == ViewMode.Desktop) {
-            const selectedChatIndex = getState().chatsState.selectedChatIndex;
-            if (selectedChatIndex != null) {
-                const chats = getState().chatsState.chats;
-                const selectedChat = chats[selectedChatIndex];
-                historyFunctions.pushOrReplaceChat(selectedChat.chatId, false);
-            } else {
-                dispatch(gotoChatByIndex(0));
-            }
-        } else {
-            dispatch({ type: GOTO_HOME });
-        }    
-    }
+    return { type: GOTO_HOME };
 }
 
 export type GotoHomeEvent = {
