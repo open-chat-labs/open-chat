f0870b9a13dd3be2ebc951b33c4eb23a5e1c1252
megrogan <megrogan@gmail.com>
2021-06-30 12:54:39 +0100

    Fixed user_index app candid and re-generated idl and types

---
+++ v2/backend/canisters/user_index/src/canister/sms_service/idl.js
@@ -0,0 +1,24 @@
+export default ({ IDL }) => {
+  const PendingSmsMessagesRequest = IDL.Record({
+    'max_results' : IDL.Nat64,
+    'from_index' : IDL.Nat64,
+  });
+  const SmsNotification = IDL.Record({
+    'message' : IDL.Text,
+    'phone_number' : IDL.Text,
+  });
+  const PendingSmsMessagesResponse = IDL.Variant({
+    'Success' : IDL.Record({
+      'notifications' : IDL.Vec(SmsNotification),
+      'latest_index' : IDL.Nat64,
+    }),
+  });
+  return IDL.Service({
+    'pending_sms_messages' : IDL.Func(
+        [PendingSmsMessagesRequest],
+        [PendingSmsMessagesResponse],
+        ['query'],
+      ),
+  });
+};
+export const init = ({ IDL }) => { return []; };
