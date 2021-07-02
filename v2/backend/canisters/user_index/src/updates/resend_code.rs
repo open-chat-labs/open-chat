78c930d357bf2a41f3c4958f55a30b4531f1b05f
Hamish Peebles <hamishpeebles@gmail.com>
2021-07-02 16:21:41 +0100

    Add tests to 'resend_code' (#359)

--- v2/backend/canisters/user_index/src/updates/resend_code.rs
+++ v2/backend/canisters/user_index/src/updates/resend_code.rs
@@ -33,3 +33,60 @@ pub enum Response {
     AlreadyClaimed,
     UserNotFound,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::model::data::Data;
+    use crate::model::runtime_state::RuntimeState;
+    use crate::model::user::{UnconfirmedUser, ConfirmedUser, CanisterCreationStatus};
+    use crate::test::env::TestEnv;
+    use phonenumber::PhoneNumber;
+    use std::str::FromStr;
+    use super::*;
+
+    #[test]
+    fn unconfirmed_user_returns_success() {
+        let env = TestEnv::default();
+        let mut data = Data::default();
+        data.users.add(User::Unconfirmed(UnconfirmedUser {
+            principal: env.caller,
+            phone_number: PhoneNumber::from_str("+44 1111 111 111").unwrap(),
+            confirmation_code: "123456".to_string(),
+            date_generated: env.now,
+            sms_messages_sent: 1,
+        }));
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let result = update(&mut runtime_state);
+        assert!(matches!(result, Response::Success));
+        assert_eq!(runtime_state.data.sms_queue.len(), 1);
+    }
+
+    #[test]
+    fn confirmed_user_returns_already_claimed() {
+        let env = TestEnv::default();
+        let mut data = Data::default();
+        data.users.add(User::Confirmed(ConfirmedUser {
+            principal: env.caller,
+            phone_number: PhoneNumber::from_str("+44 1111 111 111").unwrap(),
+            user_id: None,
+            username: None,
+            canister_creation_status: CanisterCreationStatus::Pending,
+            date_confirmed: env.now,
+        }));
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let result = update(&mut runtime_state);
+        assert!(matches!(result, Response::AlreadyClaimed));
+    }
+
+    #[test]
+    fn no_user_returns_user_not_found() {
+        let env = TestEnv::default();
+        let data = Data::default();
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let result = update(&mut runtime_state);
+        assert!(matches!(result, Response::UserNotFound));
+    }
+}
