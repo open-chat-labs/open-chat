0e35ae06cadfa66e2e3744bd7a45afeba7b70b2d
Hamish Peebles <hamishpeebles@gmail.com>
2021-07-02 16:21:31 +0100

    Add tests to 'confirm_phone_number' (#358)

--- v2/backend/canisters/user_index/src/updates/confirm_phone_number.rs
+++ v2/backend/canisters/user_index/src/updates/confirm_phone_number.rs
@@ -55,3 +55,110 @@ pub enum Response {
     AlreadyClaimed,
     UserNotFound,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::model::data::Data;
+    use crate::model::runtime_state::RuntimeState;
+    use crate::model::user::UnconfirmedUser;
+    use crate::test::env::TestEnv;
+    use std::str::FromStr;
+    use super::*;
+
+    #[test]
+    fn correct_code_returns_success() {
+        let env = TestEnv::default();
+        let confirmation_code = "123456".to_string();
+        let mut data = Data::default();
+        data.users.add(User::Unconfirmed(UnconfirmedUser {
+            principal: env.caller,
+            phone_number: PhoneNumber::from_str("+44 1111 111 111").unwrap(),
+            confirmation_code: confirmation_code.clone(),
+            date_generated: env.now,
+            sms_messages_sent: 1,
+        }));
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let request = Request {
+            confirmation_code
+        };
+        let result = update(request, &mut runtime_state);
+        assert!(matches!(result, Response::Success));
+    }
+
+    #[test]
+    fn incorrect_code_returns_confirmation_code_incorrect() {
+        let env = TestEnv::default();
+        let mut data = Data::default();
+        data.users.add(User::Unconfirmed(UnconfirmedUser {
+            principal: env.caller,
+            phone_number: PhoneNumber::from_str("+44 1111 111 111").unwrap(),
+            confirmation_code: "123456".to_string(),
+            date_generated: env.now,
+            sms_messages_sent: 1,
+        }));
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let request = Request {
+            confirmation_code: "123457".to_string()
+        };
+        let result = update(request, &mut runtime_state);
+        assert!(matches!(result, Response::ConfirmationCodeIncorrect));
+    }
+
+    #[test]
+    fn code_expired_returns_confirmation_code_expired() {
+        let confirmation_code = "123456".to_string();
+        let mut env = TestEnv::default();
+        let mut data = Data::default();
+        data.users.add(User::Unconfirmed(UnconfirmedUser {
+            principal: env.caller,
+            phone_number: PhoneNumber::from_str("+44 1111 111 111").unwrap(),
+            confirmation_code: confirmation_code.clone(),
+            date_generated: env.now,
+            sms_messages_sent: 1,
+        }));
+        env.now += CONFIRMATION_CODE_EXPIRY_MILLIS + 1;
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let request = Request {
+            confirmation_code
+        };
+        let result = update(request, &mut runtime_state);
+        assert!(matches!(result, Response::ConfirmationCodeExpired));
+    }
+
+    #[test]
+    fn confirmed_user_returns_already_claimed() {
+        let env = TestEnv::default();
+        let mut data = Data::default();
+        data.users.add(User::Confirmed(ConfirmedUser {
+            principal: env.caller,
+            phone_number: PhoneNumber::from_str("+44 1111 111 111").unwrap(),
+            user_id: None,
+            username: None,
+            canister_creation_status: CanisterCreationStatus::Pending,
+            date_confirmed: env.now,
+        }));
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let request = Request {
+            confirmation_code: "123456".to_string()
+        };
+        let result = update(request, &mut runtime_state);
+        assert!(matches!(result, Response::AlreadyClaimed));
+    }
+
+    #[test]
+    fn no_user_returns_user_not_found() {
+        let env = TestEnv::default();
+        let data = Data::default();
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        let request = Request {
+            confirmation_code: "123456".to_string()
+        };
+        let result = update(request, &mut runtime_state);
+        assert!(matches!(result, Response::UserNotFound));
+    }
+}
