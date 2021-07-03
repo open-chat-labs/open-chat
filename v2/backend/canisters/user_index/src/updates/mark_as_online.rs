2d9af46df0f02a2d6c8dd45d755e5d41da24aa34
Hamish Peebles <hamishpeebles@gmail.com>
2021-07-03 17:37:36 +0100

    Add tests to 'mark_as_online'

--- v2/backend/canisters/user_index/src/updates/mark_as_online.rs
+++ v2/backend/canisters/user_index/src/updates/mark_as_online.rs
@@ -9,3 +9,43 @@ pub fn update(runtime_state: &mut RuntimeState) {
 
 #[derive(Deserialize)]
 pub struct Request {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::model::data::Data;
+    use crate::model::user::{CreatedUser, User};
+    use crate::test::env::TestEnv;
+    use phonenumber::PhoneNumber;
+    use std::str::FromStr;
+
+    #[test]
+    fn last_online_is_updated() {
+        let mut env = TestEnv::default();
+        let mut data = Data::default();
+        data.users.add(User::Created(CreatedUser {
+            principal: env.caller,
+            phone_number: PhoneNumber::from_str("+44 1111 111 111").unwrap(),
+            user_id: env.caller,
+            username: "abc".to_string(),
+            date_created: env.now,
+            last_online: env.now,
+        }));
+        env.now += 10000;
+        let mut runtime_state = RuntimeState::new(Box::new(env), data);
+
+        update(&mut runtime_state);
+
+        let user = runtime_state
+            .data
+            .users
+            .get_by_principal(&runtime_state.env.caller())
+            .unwrap();
+
+        if let User::Created(u) = user {
+            assert_eq!(u.last_online, runtime_state.env.now());
+        } else {
+            panic!();
+        }
+    }
+}
