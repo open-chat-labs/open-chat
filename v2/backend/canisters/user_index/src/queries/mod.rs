8d73f91e717247eb52f652ad26e89684193b02f4
megrogan <megrogan@gmail.com>
2021-07-05 17:57:57 +0100

    Added metrics endpoint to user_index

--- v2/backend/canisters/user_index/src/queries/mod.rs
+++ v2/backend/canisters/user_index/src/queries/mod.rs
@@ -1,4 +1,5 @@
 pub mod current_user;
+pub mod metrics;
 pub mod pending_sms_messages;
 pub mod search;
 pub mod user;
---
+++ v2/backend/shared/src/memory.rs
@@ -0,0 +1,12 @@
+pub fn used() -> u64 {
+    #[cfg(target_arch = "wasm32")]
+    {
+        (core::arch::wasm32::memory_size(0) * 65536) as u64
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        // This branch won't actually ever be taken
+        1024 * 1024 * 100 // 100Mb
+    }
+}
