bc15d61874bd1278984c583b1ab120a03ccb701f
Hamish Peebles <hamishpeebles@gmail.com>
2021-07-08 14:57:51 +0100

    Another case of 'id' -> 'index'

--- v2/backend/canisters/user/src/queries/messages.rs
+++ v2/backend/canisters/user/src/queries/messages.rs
@@ -10,7 +10,7 @@ pub fn query(args: Args, runtime_state: &RuntimeState) -> Response {
     if runtime_state.is_caller_owner() {
         let chat_id = DirectChatId::from((&runtime_state.env.owner_user_id(), &args.user_id));
         if let Some(chat) = runtime_state.data.direct_chats.get(&chat_id) {
-            let messages = chat.get_messages(args.from_id, args.to_id);
+            let messages = chat.get_messages(args.from_index, args.to_index);
             Success(SuccessResult { messages })
         } else {
             ChatNotFound
@@ -23,8 +23,8 @@ pub fn query(args: Args, runtime_state: &RuntimeState) -> Response {
 #[derive(Deserialize)]
 pub struct Args {
     user_id: UserId,
-    from_id: MessageIndex,
-    to_id: MessageIndex,
+    from_index: MessageIndex,
+    to_index: MessageIndex,
 }
 
 #[derive(CandidType)]
