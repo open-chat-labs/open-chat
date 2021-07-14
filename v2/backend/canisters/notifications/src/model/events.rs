ced9d9f4a219b60207f8f179cb7abdf10ca39a2f
Hamish Peebles <hamishpeebles@gmail.com>
2021-07-14 11:27:00 +0100

    Add tests

--- v2/backend/canisters/notifications/src/model/events.rs
+++ v2/backend/canisters/notifications/src/model/events.rs
@@ -26,7 +26,7 @@ impl Events {
             let start_index = (from_event_index - earliest_event_index) as usize;
             let end_index = min(start_index + (max_events as usize), self.events.len());
 
-            (start_index..=end_index)
+            (start_index..end_index)
                 .filter_map(|i| self.events.get(i))
                 .cloned()
                 .collect()
@@ -36,7 +36,7 @@ impl Events {
     }
 
     pub fn add(&mut self, event: Event) -> u64 {
-        let event_index = self.events.back().map(|e| e.index).unwrap_or(0) + 1;
+        let event_index = self.events.back().map(|e| e.index + 1).unwrap_or(0);
         self.events.push_back(IndexedEvent {
             index: event_index,
             event,
@@ -49,14 +49,15 @@ impl Events {
         event_index
     }
 
-    pub fn remove(&mut self, up_to_event_index: u64) {
+    pub fn remove(&mut self, up_to_event_index: u64) -> u32 {
         if let Some(earliest_event_index) = self.events.front().map(|e| e.index) {
             if earliest_event_index <= up_to_event_index {
-                let count_to_remove = (up_to_event_index - earliest_event_index) as usize;
+                let count_to_remove = (up_to_event_index + 1 - earliest_event_index) as usize;
 
-                self.events.drain(0..count_to_remove);
+                return self.events.drain(0..count_to_remove).len() as u32
             }
         }
+        0
     }
 }
 
@@ -94,4 +95,149 @@ pub struct GroupMessageNotification {
 pub struct Subscription {
     pub user_id: UserId,
     pub subscription: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use candid::Principal;
+
+    #[test]
+    fn add() {
+        let mut events_collection = Events::default();
+
+        for i in 0..10 {
+            let subscription = Subscription {
+                user_id: Principal::from_slice(&[i]).into(),
+                subscription: i.to_string(),
+            };
+            events_collection.add(Event::Subscription(subscription));
+        }
+
+        assert_eq!(events_collection.events.len(), 10);
+
+        for i in 0..10 {
+            let indexed_event = &events_collection.events[i];
+            assert_eq!(indexed_event.index, i as u64);
+            if let Event::Subscription(subscription) = &indexed_event.event {
+                assert_eq!(subscription.subscription, i.to_string());
+            } else {
+                panic!();
+            }
+        }
+    }
+
+    #[test]
+    fn get_from_start() {
+        let mut events_collection = Events::default();
+
+        for i in 0..10 {
+            let subscription = Subscription {
+                user_id: Principal::from_slice(&[i]).into(),
+                subscription: i.to_string(),
+            };
+            events_collection.add(Event::Subscription(subscription));
+        }
+
+        let events = events_collection.get(0, 5);
+
+        assert_eq!(events.len(), 5);
+
+        for i in 0..5 {
+            let indexed_event = &events[i];
+            assert_eq!(indexed_event.index, i as u64);
+            if let Event::Subscription(subscription) = &indexed_event.event {
+                assert_eq!(subscription.subscription, i.to_string());
+            } else {
+                panic!();
+            }
+        }
+    }
+
+    #[test]
+    fn get_with_offset() {
+        let mut events_collection = Events::default();
+
+        for i in 0..10 {
+            let subscription = Subscription {
+                user_id: Principal::from_slice(&[i]).into(),
+                subscription: i.to_string(),
+            };
+            events_collection.add(Event::Subscription(subscription));
+        }
+
+        let events = events_collection.get(5, 5);
+
+        assert_eq!(events.len(), 5);
+
+        for i in 5..10 {
+            let indexed_event = &events[i - 5];
+            assert_eq!(indexed_event.index, i as u64);
+            if let Event::Subscription(subscription) = &indexed_event.event {
+                assert_eq!(subscription.subscription, i.to_string());
+            } else {
+                panic!();
+            }
+        }
+    }
+
+    #[test]
+    fn get_with_request_exceeding_data_range() {
+        let mut events_collection = Events::default();
+
+        for i in 0..10 {
+            let subscription = Subscription {
+                user_id: Principal::from_slice(&[i]).into(),
+                subscription: i.to_string(),
+            };
+            events_collection.add(Event::Subscription(subscription));
+        }
+
+        let events = events_collection.get(5, 10);
+
+        assert_eq!(events.len(), 5);
+
+        for i in 5..10 {
+            let indexed_event = &events[i - 5];
+            assert_eq!(indexed_event.index, i as u64);
+            if let Event::Subscription(subscription) = &indexed_event.event {
+                assert_eq!(subscription.subscription, i.to_string());
+            } else {
+                panic!();
+            }
+        }
+    }
+
+    #[test]
+    fn remove() {
+        let mut events_collection = Events::default();
+
+        for i in 0..10 {
+            let subscription = Subscription {
+                user_id: Principal::from_slice(&[i]).into(),
+                subscription: i.to_string(),
+            };
+            events_collection.add(Event::Subscription(subscription));
+        }
+
+        assert_eq!(events_collection.remove(4), 5);
+
+        let events = events_collection.get(0, 5);
+
+        assert_eq!(events.len(), 5);
+
+        for i in 5..10 {
+            let indexed_event = &events[i - 5];
+            assert_eq!(indexed_event.index, i as u64);
+            if let Event::Subscription(subscription) = &indexed_event.event {
+                assert_eq!(subscription.subscription, i.to_string());
+            } else {
+                panic!();
+            }
+        }
+
+        assert_eq!(events_collection.remove(4), 0);
+        assert_eq!(events_collection.remove(9), 5);
+        assert!(events_collection.events.is_empty());
+    }
 }
