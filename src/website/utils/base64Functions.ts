a999161a3dd4e607778cdc5171416ebce2542d07
megrogan <megrogan@gmail.com>
2021-07-15 18:16:23 +0100

    Add WebPush subscription WIP

---
+++ src/website/utils/base64Functions.ts
@@ -0,0 +1,14 @@
+export function toUint8Array(base64String: string) : Uint8Array {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
