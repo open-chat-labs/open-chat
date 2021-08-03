41edf60e6acd88896ad0c5ef0bc2e3bd6f987d33
megrogan <megrogan@gmail.com>
2021-08-03 15:10:41 +0100

    Add v1 notification types to notifications canister and service

--- v2/backend/canisters/notifications/api/src/updates/mod.rs
+++ v2/backend/canisters/notifications/api/src/updates/mod.rs
@@ -1,5 +1,6 @@
 pub mod push_direct_message_notification;
 pub mod push_group_message_notification;
 pub mod push_subscription;
+pub mod push_v1direct_message_notification;
 pub mod push_v1group_message_notification;
 pub mod remove_notifications;
