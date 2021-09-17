627cabf5ce5397e7c2e2773eb42544593b5a0f25
Julian Jelfs <julian.jelfs@gmail.com>
2021-09-17 15:46:45 +0100

    messed it up somehow

--- v2/frontend/src/components/Logo.svelte
+++ v2/frontend/src/components/Logo.svelte
@@ -1,5 +1,9 @@
 <div class="logo">
-    <svg viewBox="0 0 500 500">
+    <svg
+        viewBox="0 0 500 500"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        xmlns:bx="https://boxy-svg.com">
         <defs>
             <linearGradient
                 gradientUnits="userSpaceOnUse"
@@ -12,11 +16,12 @@
                 <stop offset="0" style="stop-color: #bada55" />
                 <stop offset="1" style="stop-color: #758d29" />
             </linearGradient>
-            <linearGradient id="orange-yellow">
+            <linearGradient id="gradient-2" bx:pinned="true" />
+            <linearGradient id="gradient-5" bx:pinned="true">
                 <stop style="stop-color: rgb(251, 176, 59);" offset="0" />
                 <stop style="stop-color: rgb(240, 90, 36);" offset="1" />
             </linearGradient>
-            <linearGradient id="purple-pink">
+            <linearGradient id="gradient-6" bx:pinned="true">
                 <stop style="stop-color: rgb(95, 37, 131);" offset="0" />
                 <stop style="stop-color: rgb(237, 30, 121);" offset="1" />
             </linearGradient>
@@ -31,23 +36,23 @@
                 <stop offset="1" style="stop-color: rgba(20, 107, 145, 1)" />
             </linearGradient>
             <linearGradient
-                id="purple-pink-trans"
+                id="gradient-6-1"
                 gradientUnits="userSpaceOnUse"
                 x1="973.216"
                 y1="100.665"
                 x2="973.216"
                 y2="388.077"
                 gradientTransform="matrix(0.974127, -0.22842, 0.310454, 1.352474, -95.300314, 85.515158)"
-                xlink:href="#purple-pink" />
+                xlink:href="#gradient-6" />
             <linearGradient
-                id="orange-yellow-trans"
+                id="gradient-5-0"
                 gradientUnits="userSpaceOnUse"
-                x1="973.216"
-                y1="100.665"
-                x2="973.216"
-                y2="388.077"
-                gradientTransform="matrix(0.974127, -0.22842, 0.310454, 1.352474, -95.300314, 85.515158)"
-                xlink:href="#orange-yellow" />
+                x1="188.919"
+                y1="1.638"
+                x2="188.919"
+                y2="361.638"
+                gradientTransform="matrix(-0.999999, 0.0016, -0.002016, -1.25907, 376.77992, 357.264546)"
+                xlink:href="#gradient-5" />
         </defs>
         <rect
             style="stroke: url(#gradient-1); filter: none; stroke-width: 0px; fill-rule: nonzero; fill: url(#gradient-0);"
@@ -58,15 +63,15 @@
             rx="60"
             ry="60" />
         <path
-            class="left-arc"
             d="M 188.919 181.638 m -180 0 a 180 180 0 1 0 360 0 a 180 180 0 1 0 -360 0 Z M 188.919 181.638 m -100 0 a 100 100 0 0 1 200 0 a 100 100 0 0 1 -200 0 Z"
-            style="fill: url(#orange-yellow-trans);"
-            transform="matrix(1, 0.000074, -0.000074, 1, 61.094496, 68.347628)" />
+            style="fill: url(#gradient-5-0);"
+            transform="matrix(1, 0.000074, -0.000074, 1, 61.094496, 68.347628)"
+            bx:shape="ring 188.919 181.638 100 100 180 180 1@55492482" />
         <path
-            class="right-arc"
-            style="stroke-width: 0px; paint-order: stroke; fill: url(#purple-pink-trans);"
+            style="stroke-width: 0px; paint-order: stroke; fill: url(#gradient-6-1);"
             transform="matrix(1.031731, 0.000001, 0, 1.020801, -634.597412, 0.544884)"
-            d="M 958.327234958 100.664699414 A 175.433 175.433 0 0 1 958.327234958 388.077300586 L 913.296322517 323.766492741 A 96.924 96.924 0 0 0 913.296322517 164.975507259 Z" />
+            d="M 958.327234958 100.664699414 A 175.433 175.433 0 0 1 958.327234958 388.077300586 L 913.296322517 323.766492741 A 96.924 96.924 0 0 0 913.296322517 164.975507259 Z"
+            bx:shape="pie 857.703 244.371 96.924 175.433 35 145 1@99003c38" />
         <circle style="fill: rgb(25, 25, 25);" cx="250" cy="250" r="100" />
     </svg>
 </div>
