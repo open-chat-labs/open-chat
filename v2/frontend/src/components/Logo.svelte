e01ba70b89b2f4b3b12e241b0ceee704362ec18f
Julian Jelfs <julian.jelfs@gmail.com>
2021-09-17 14:36:42 +0100

    add a couple of candidate logos to the site

--- v2/frontend/src/components/Logo.svelte
+++ v2/frontend/src/components/Logo.svelte
@@ -2,12 +2,12 @@
 
 <style type="text/scss">
     .logo {
-        background-image: url("../assets/logo.jpg");
+        background-image: url("../oc-logo2.svg");
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
         width: 80px;
         height: 80px;
-        border-radius: 50%;
+        // border-radius: 50%;
     }
 </style>
