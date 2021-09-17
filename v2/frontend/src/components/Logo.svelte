d878f681c163cd8d1e2985530a28e3210bb82af0
Julian Jelfs <julian.jelfs@gmail.com>
2021-09-17 15:43:48 +0100

    convert to svelte component so we can manipulate easier

--- v2/frontend/src/components/Logo.svelte
+++ v2/frontend/src/components/Logo.svelte
@@ -1,13 +1,79 @@
-<div class="logo" />
+<div class="logo">
+    <svg viewBox="0 0 500 500">
+        <defs>
+            <linearGradient
+                gradientUnits="userSpaceOnUse"
+                x1="230.87"
+                y1="58.869"
+                x2="230.87"
+                y2="385.502"
+                id="gradient-1"
+                gradientTransform="matrix(1.483987, 0, 0, 1.53077, -92.608261, -90.114895)">
+                <stop offset="0" style="stop-color: #bada55" />
+                <stop offset="1" style="stop-color: #758d29" />
+            </linearGradient>
+            <linearGradient id="orange-yellow">
+                <stop style="stop-color: rgb(251, 176, 59);" offset="0" />
+                <stop style="stop-color: rgb(240, 90, 36);" offset="1" />
+            </linearGradient>
+            <linearGradient id="purple-pink">
+                <stop style="stop-color: rgb(95, 37, 131);" offset="0" />
+                <stop style="stop-color: rgb(237, 30, 121);" offset="1" />
+            </linearGradient>
+            <linearGradient
+                gradientUnits="userSpaceOnUse"
+                x1="250"
+                y1="0"
+                x2="250"
+                y2="500"
+                id="gradient-0">
+                <stop offset="0" style="stop-color: rgba(40, 170, 226, 1)" />
+                <stop offset="1" style="stop-color: rgba(20, 107, 145, 1)" />
+            </linearGradient>
+            <linearGradient
+                id="purple-pink-trans"
+                gradientUnits="userSpaceOnUse"
+                x1="973.216"
+                y1="100.665"
+                x2="973.216"
+                y2="388.077"
+                gradientTransform="matrix(0.974127, -0.22842, 0.310454, 1.352474, -95.300314, 85.515158)"
+                xlink:href="#purple-pink" />
+            <linearGradient
+                id="orange-yellow-trans"
+                gradientUnits="userSpaceOnUse"
+                x1="973.216"
+                y1="100.665"
+                x2="973.216"
+                y2="388.077"
+                gradientTransform="matrix(0.974127, -0.22842, 0.310454, 1.352474, -95.300314, 85.515158)"
+                xlink:href="#orange-yellow" />
+        </defs>
+        <rect
+            style="stroke: url(#gradient-1); filter: none; stroke-width: 0px; fill-rule: nonzero; fill: url(#gradient-0);"
+            x="0"
+            y="0"
+            width="500"
+            height="500"
+            rx="60"
+            ry="60" />
+        <path
+            class="left-arc"
+            d="M 188.919 181.638 m -180 0 a 180 180 0 1 0 360 0 a 180 180 0 1 0 -360 0 Z M 188.919 181.638 m -100 0 a 100 100 0 0 1 200 0 a 100 100 0 0 1 -200 0 Z"
+            style="fill: url(#orange-yellow-trans);"
+            transform="matrix(1, 0.000074, -0.000074, 1, 61.094496, 68.347628)" />
+        <path
+            class="right-arc"
+            style="stroke-width: 0px; paint-order: stroke; fill: url(#purple-pink-trans);"
+            transform="matrix(1.031731, 0.000001, 0, 1.020801, -634.597412, 0.544884)"
+            d="M 958.327234958 100.664699414 A 175.433 175.433 0 0 1 958.327234958 388.077300586 L 913.296322517 323.766492741 A 96.924 96.924 0 0 0 913.296322517 164.975507259 Z" />
+        <circle style="fill: rgb(25, 25, 25);" cx="250" cy="250" r="100" />
+    </svg>
+</div>
 
 <style type="text/scss">
     .logo {
-        background-image: url("../oc-logo2.svg");
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
         width: 80px;
         height: 80px;
-        // border-radius: 50%;
     }
 </style>
