42c815254f8c107b50207dfb09500c8e47d7e40e
megrogan <megrogan@gmail.com>
2021-09-23 14:42:26 +0100

    Fix some names

--- v2/backend/canisters/user/impl/src/updates/remove_webrtc_connection_details.rs
+++ v2/backend/canisters/user/impl/src/updates/remove_webrtc_connection_details.rs
@@ -2,6 +2,7 @@ use crate::{RuntimeState, RUNTIME_STATE};
 use cycles_utils::check_cycles_balance;
 use ic_cdk_macros::update;
 use std::collections::HashSet;
+use std::iter::FromIterator;
 use user_canister::remove_webrtc_connection_details::*;
 
 #[update]
@@ -12,10 +13,10 @@ fn remove_webrtc_connection_details(args: Args) -> Response {
 }
 
 fn remove_webrtc_connection_details_impl(args: Args, runtime_state: &mut RuntimeState) -> Response {
-    let user_ids: HashSet<_> = args.ids.into_iter().collect();
+    let ids = HashSet::from_iter(args.ids);
     runtime_state
         .data
         .webrtc_connection_details_map
-        .remove_connection_details(&user_ids);
+        .remove_connection_details(&ids);
     Response::Success
 }
