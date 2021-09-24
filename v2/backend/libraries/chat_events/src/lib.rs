d5dfa575826c0ab9eaf71e1f21b0a4f6e63573f5
Hamish Peebles <hamishpeebles@gmail.com>
2021-09-24 17:20:36 +0100

    Fix tests

--- v2/backend/libraries/chat_events/src/lib.rs
+++ v2/backend/libraries/chat_events/src/lib.rs
@@ -5,6 +5,6 @@ mod chat_events;
 mod direct_chat_events;
 mod group_chat_events;
 
-pub use chat_events::{ChatEventInternal, DeleteMessageResult, PushMessageArgs, ToggleReactionResult};
+pub use crate::chat_events::{ChatEventInternal, DeleteMessageResult, PushMessageArgs, ToggleReactionResult};
 pub use direct_chat_events::DirectChatEvents;
 pub use group_chat_events::GroupChatEvents;
