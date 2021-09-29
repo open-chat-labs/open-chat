09f101f00763252d6df03879409973ddac76f6c8
megrogan <megrogan@gmail.com>
2021-09-29 17:49:00 +0100

    Wire up avatar_id in group index for create case (#635)

--- v2/backend/libraries/types/src/avatar.rs
+++ v2/backend/libraries/types/src/avatar.rs
@@ -10,3 +10,9 @@ pub struct Avatar {
     pub mime_type: String,
     pub data: ByteBuf,
 }
+
+impl Avatar {
+    pub fn id(avatar: &Option<Avatar>) -> Option<u128> {
+        avatar.as_ref().map(|a| a.id)
+    }
+}
