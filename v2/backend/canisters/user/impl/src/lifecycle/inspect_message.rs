fa4f9e629c4212d6db27eb4d6ea5613d288faff1
megrogan <megrogan@gmail.com>
2021-10-06 09:33:47 +0100

    Removed webrtc connection brokering from backend

--- v2/backend/canisters/user/impl/src/lifecycle/inspect_message.rs
+++ v2/backend/canisters/user/impl/src/lifecycle/inspect_message.rs
@@ -1,6 +1,5 @@
 use crate::{RuntimeState, RUNTIME_STATE};
 use ic_cdk_macros::inspect_message;
-use user_canister::add_webrtc_session_details;
 
 #[inspect_message]
 fn inspect_message() {
@@ -12,10 +11,6 @@ fn try_accept(runtime_state: &RuntimeState) {
 
     let acceptable = match method_name.as_str() {
         "deposit_cycles" => true,
-        "add_webrtc_session_details" => {
-            let (args,) = ic_cdk::api::call::arg_data::<(add_webrtc_session_details::Args,)>();
-            runtime_state.data.direct_chats.exists(args.session_details.user_id())
-        }
         _ => runtime_state.is_caller_owner(),
     };
 
