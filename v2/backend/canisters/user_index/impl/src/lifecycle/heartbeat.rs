0df89eb438d4eff2b06f14106d9952ae029cc3f3
megrogan <megrogan@gmail.com>
2021-10-13 10:41:22 +0100

    Use canister_pool for creating users (#714)

---
+++ v2/backend/canisters/user_index/impl/src/lifecycle/heartbeat.rs
@@ -0,0 +1,35 @@
+use ic_cdk_macros::heartbeat;
+
+#[heartbeat]
+fn heartbeat() {
+    topup_canister_pool::run();
+}
+
+mod topup_canister_pool {
+    use crate::{RuntimeState, RUNTIME_STATE, USER_CANISTER_INITIAL_CYCLES_BALANCE};
+    use types::CanisterId;
+    use utils::canister;
+    use utils::consts::CREATE_CANISTER_CYCLES_FEE;
+
+    pub fn run() {
+        let is_full = RUNTIME_STATE.with(|state| is_pool_full(state.borrow().as_ref().unwrap()));
+        if !is_full {
+            ic_cdk::block_on(add_new_canister());
+        }
+    }
+
+    fn is_pool_full(runtime_state: &RuntimeState) -> bool {
+        runtime_state.data.canister_pool.is_full()
+    }
+
+    async fn add_new_canister() {
+        let cycles_required = USER_CANISTER_INITIAL_CYCLES_BALANCE + CREATE_CANISTER_CYCLES_FEE;
+        if let Ok(canister_id) = canister::create(cycles_required).await {
+            RUNTIME_STATE.with(|state| add_canister_to_pool(state.borrow_mut().as_mut().unwrap(), canister_id));
+        }
+    }
+
+    fn add_canister_to_pool(runtime_state: &mut RuntimeState, canister_id: CanisterId) {
+        runtime_state.data.canister_pool.push(canister_id);
+    }
+}
