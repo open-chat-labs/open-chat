bf2c884c9ace7398004cbd8dbe8ebee62462b818
megrogan <megrogan@gmail.com>
2021-10-15 10:11:50 +0100

    toggle_mute -> mute and unmute

--- v2/backend/canisters/user/impl/src/updates/mute_notifications.rs
+++ v2/backend/canisters/user/impl/src/updates/mute_notifications.rs
@@ -3,35 +3,38 @@ use cycles_utils::check_cycles_balance;
 use group_canister::c2c_toggle_mute_notifications;
 use ic_cdk_macros::update;
 use tracing::instrument;
-use types::{CanisterId, Timestamped};
-use user_canister::toggle_mute_notifications::*;
+use types::{CanisterId, ChatId, Timestamped};
+use user_canister::mute_notifications::*;
 
 #[update]
 #[instrument(level = "trace")]
-fn toggle_mute_notifications(args: Args) -> Response {
+fn mute_notifications(args: Args) -> Response {
     check_cycles_balance();
 
-    RUNTIME_STATE.with(|state| toggle_mute_notifications_impl(args, state.borrow_mut().as_mut().unwrap()))
+    RUNTIME_STATE.with(|state| toggle_mute_notifications_impl(args.chat_id, true, state.borrow_mut().as_mut().unwrap()))
 }
 
-fn toggle_mute_notifications_impl(args: Args, runtime_state: &mut RuntimeState) -> Response {
+#[update]
+#[instrument(level = "trace")]
+fn unmute_notifications(args: Args) -> Response {
+    check_cycles_balance();
+
+    RUNTIME_STATE.with(|state| toggle_mute_notifications_impl(args.chat_id, false, state.borrow_mut().as_mut().unwrap()))
+}
+
+fn toggle_mute_notifications_impl(chat_id: ChatId, mute: bool, runtime_state: &mut RuntimeState) -> Response {
     runtime_state.trap_if_caller_not_owner();
 
     let now = runtime_state.env.now();
-    let notifications_muted = Timestamped::new(args.mute, now);
+    let notifications_muted = Timestamped::new(mute, now);
 
-    match runtime_state.data.group_chats.get_mut(&args.chat_id) {
+    match runtime_state.data.group_chats.get_mut(&chat_id) {
         Some(group_chat) => {
             group_chat.notifications_muted = notifications_muted;
-
-            ic_cdk::block_on(toggle_mute_notifications_on_group_canister(
-                group_chat.chat_id.into(),
-                args.mute,
-            ));
-
+            ic_cdk::block_on(toggle_mute_notifications_on_group_canister(group_chat.chat_id.into(), mute));
             Response::Success
         }
-        None => match runtime_state.data.direct_chats.get_mut(&args.chat_id) {
+        None => match runtime_state.data.direct_chats.get_mut(&chat_id) {
             Some(direct_chat) => {
                 direct_chat.notifications_muted = notifications_muted;
                 Response::Success
