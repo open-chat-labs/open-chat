c3be183df86ca19964e3f02f05ed72ae57817b74
Hamish Peebles <hamishpeebles@gmail.com>
2021-10-16 15:16:34 +0100

    Add 'deploy-prod' and 'upgrade-canister-prod' scripts

--- v2/backend/canister_installer/src/main.rs
+++ v2/backend/canister_installer/src/main.rs
@@ -7,6 +7,7 @@ use types::CanisterId;
 #[tokio::main]
 async fn main() {
     let opts = Opts::parse();
+    let test_mode = opts.test_mode != 0;
 
     let canister_ids = CanisterIds {
         user_index: opts.user_index,
@@ -16,13 +17,14 @@ async fn main() {
 
     let identity = get_dfx_identity(&opts.controller);
 
-    install_service_canisters(identity, opts.url, canister_ids).await;
+    install_service_canisters(identity, opts.url, canister_ids, test_mode).await;
 }
 
 #[derive(Clap)]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
     url: String,
+    test_mode: u32,
     controller: String,
     user_index: CanisterId,
     group_index: CanisterId,
--- v2/deploy-prod.sh
+++ v2/deploy-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 IDENTITY=$1
-TEST_MODE=1
+TEST_MODE=$2
 
 # Pass in the dfx identity name
 # eg './deploy-local openchat'
@@ -14,17 +14,13 @@ TEST_MODE=1
 ./compress-wasm.sh group_canister_impl
 ./compress-wasm.sh user_canister_impl
 
-dfx --identity $IDENTITY canister --no-wallet create user_index
-dfx --identity $IDENTITY canister --no-wallet create group_index
-dfx --identity $IDENTITY canister --no-wallet create notifications
-
-USER_INDEX_CANISTER_ID=$(dfx canister --no-wallet id user_index)
-GROUP_INDEX_CANISTER_ID=$(dfx canister --no-wallet id group_index)
-NOTIFICATIONS_INDEX_CANISTER_ID=$(dfx canister --no-wallet id notifications)
+USER_INDEX_CANISTER_ID=$(dfx canister --network ic --no-wallet id user_index)
+GROUP_INDEX_CANISTER_ID=$(dfx canister --network ic --no-wallet id group_index)
+NOTIFICATIONS_INDEX_CANISTER_ID=$(dfx canister --network ic --no-wallet id notifications)
 
 cargo run \
   --manifest-path backend/canister_installer/Cargo.toml \
-  'http://127.0.0.1:8000/' \
+  'https://ic0.app/' \
   $TEST_MODE \
   $IDENTITY \
   $USER_INDEX_CANISTER_ID \
