c001aa7c319652bedb2744353fdbcf8fbf5d7d9e
Julian Jelfs <julian.jelfs@gmail.com>
2021-10-21 12:06:05 +0100

    tweak

--- v2/frontend/src/utils/layout.ts
+++ v2/frontend/src/utils/layout.ts
@@ -7,12 +7,13 @@ const MSG_ACTION_WIDTH = 51.19;
 // position is known *before* the elements are rendered. This is necessary to make scrollToBottom work
 // reliably.
 export function calculateHeight(parentWidth: number, content: ImageContent | VideoContent): number {
+    const halfHeight = window.innerHeight / 2;
     const landscape = content.height < content.width;
     const ratio = content.height / content.width;
     const availWidth = parentWidth - MSG_ACTION_WIDTH;
-    const calculatedWidth = Math.min(availWidth, window.innerHeight / 2);
+    const calculatedWidth = Math.min(availWidth, halfHeight);
     const calculatedHeight = landscape
-        ? Math.min(calculatedWidth * ratio, window.innerHeight / 2)
-        : Math.min(content.height, window.innerHeight / 2);
+        ? Math.min(calculatedWidth * ratio, halfHeight)
+        : Math.min(content.height, halfHeight);
     return calculatedHeight;
 }
