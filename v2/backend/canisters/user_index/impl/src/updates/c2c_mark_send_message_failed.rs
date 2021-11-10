c99dfe1116a9b677dde4b8094e83611be3cea72e
Hamish Peebles <hamishpeebles@gmail.com>
2021-11-10 15:05:03 +0000

    Retry sending messages that fail the first time (#861)

---
+++ v2/backend/canisters/user_index/impl/src/updates/c2c_mark_send_message_failed.rs
@@ -0,0 +1,29 @@
+use crate::{RuntimeState, RUNTIME_STATE};
+use canister_api_macros::trace;
+use ic_cdk_macros::update;
+use user_index_canister::c2c_mark_send_message_failed::{Response::*, *};
+
+// When sending a direct message, the message is first sent from the client's device to their own
+// user canister, their canister then forwards the message on to the intended recipient's canister.
+// That c2c call can fail if the recipient's canister is stopped due to it being in the middle of an
+// upgrade. If that happens the user's canister will instead call c2c_mark_send_message_failed, the
+// user_index will then call back into the user's canister once the recipient's canister has
+// finished upgrading allowing the canister to try sending the message again.
+#[update]
+#[trace]
+fn c2c_mark_send_message_failed(args: Args) -> Response {
+    RUNTIME_STATE.with(|state| c2c_mark_send_message_failed_impl(args, state.borrow_mut().as_mut().unwrap()))
+}
+
+fn c2c_mark_send_message_failed_impl(args: Args, runtime_state: &mut RuntimeState) -> Response {
+    runtime_state.trap_if_caller_not_open_chat_user();
+
+    let sender = runtime_state.env.caller().into();
+    let now = runtime_state.env.now();
+    runtime_state
+        .data
+        .failed_messages_pending_retry
+        .add(sender, args.recipient, now);
+
+    Success
+}
