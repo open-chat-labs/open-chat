b6e640d603cfc8f5712d1676526c5c4e9f7ede86
Hamish Peebles <hamishpeebles@gmail.com>
2021-11-10 14:59:56 +0000

    Bit of a cleanup

--- v2/backend/canisters/user/impl/src/model/failed_messages_pending_retry.rs
+++ v2/backend/canisters/user/impl/src/model/failed_messages_pending_retry.rs
@@ -1,4 +1,4 @@
-use crate::updates::send_message::{send_to_recipients_canister, CyclesTransferDetails};
+use crate::updates::send_message::CyclesTransferDetails;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use types::UserId;
@@ -14,18 +14,7 @@ impl FailedMessagesPendingRetry {
         self.messages.entry(recipient).or_default().push((args, cycles_transfer));
     }
 
-    pub fn retry(&mut self, recipient: &UserId) {
-        if let Some((recipient, messages)) = self.messages.remove_entry(recipient) {
-            ic_cdk::block_on(Self::retry_async(recipient, messages));
-        }
-    }
-
-    async fn retry_async(recipient: UserId, messages: Vec<(c2c_send_message::Args, Option<CyclesTransferDetails>)>) {
-        let futures: Vec<_> = messages
-            .into_iter()
-            .map(|(args, cycles_transfer)| send_to_recipients_canister(recipient, args, cycles_transfer, true))
-            .collect();
-
-        futures::future::join_all(futures).await;
+    pub fn take(&mut self, recipient: &UserId) -> Vec<(c2c_send_message::Args, Option<CyclesTransferDetails>)> {
+        self.messages.remove(recipient).unwrap_or_default()
     }
 }
--- v2/backend/canisters/user/impl/src/updates/c2c_retry_sending_failed_messages.rs
+++ v2/backend/canisters/user/impl/src/updates/c2c_retry_sending_failed_messages.rs
@@ -1,6 +1,8 @@
+use crate::updates::send_message::{send_to_recipients_canister, CyclesTransferDetails};
 use crate::{run_regular_jobs, RuntimeState, RUNTIME_STATE};
 use canister_api_macros::trace;
 use ic_cdk_macros::update;
+use types::UserId;
 use user_canister::c2c_retry_sending_failed_messages::{Response::*, *};
 
 #[update]
@@ -17,6 +19,21 @@ fn c2c_retry_sending_failed_messages_impl(args: Args, runtime_state: &mut Runtim
         panic!("'c2c_retry_sending_failed_messages_impl' can only be called by the user_index");
     }
 
-    runtime_state.data.failed_messages_pending_retry.retry(&args.recipient);
+    let messages_to_retry = runtime_state.data.failed_messages_pending_retry.take(&args.recipient);
+    if !messages_to_retry.is_empty() {
+        ic_cdk::block_on(retry_sending_messages(args.recipient, messages_to_retry));
+    }
     Success
+}
+
+async fn retry_sending_messages(
+    recipient: UserId,
+    messages_to_retry: Vec<(user_canister::c2c_send_message::Args, Option<CyclesTransferDetails>)>,
+) {
+    let futures: Vec<_> = messages_to_retry
+        .into_iter()
+        .map(|(args, cycles_transfer)| send_to_recipients_canister(recipient, args, cycles_transfer, true))
+        .collect();
+
+    futures::future::join_all(futures).await;
 }
