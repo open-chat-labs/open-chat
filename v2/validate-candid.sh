71e697164936d81fadcb8e7d01fce4a0d2378409
Hamish Peebles <hamishpeebles@gmail.com>
2021-11-16 14:05:09 +0000

    Validate candid files as a GitHub check

---
+++ v2/validate-candid.sh
@@ -0,0 +1,6 @@
+didc check ./backend/canisters/group/api/can.did
+didc check ./backend/canisters/group_index/api/can.did
+didc check ./backend/canisters/notifications/api/can.did
+didc check ./backend/canisters/online_users_aggregator/api/can.did
+didc check ./backend/canisters/user/api/can.did
+didc check ./backend/canisters/user_index/api/can.did
