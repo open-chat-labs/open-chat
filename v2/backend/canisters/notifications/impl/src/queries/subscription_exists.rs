b00975c3fee388b090022f4405204ecbe3bbb259
Hamish Peebles <hamishpeebles@gmail.com>
2021-11-18 21:55:26 +0000

    Avoid passing user_id to notifications canister

--- v2/backend/canisters/notifications/impl/src/queries/subscription_exists.rs
+++ v2/backend/canisters/notifications/impl/src/queries/subscription_exists.rs
@@ -8,8 +8,13 @@ fn subscription_exists(args: Args) -> Response {
 }
 
 fn subscription_exists_impl(args: Args, runtime_state: &RuntimeState) -> Response {
-    match runtime_state.data.subscriptions.exists(&args.user_id, args.p256dh_key) {
-        true => Yes,
-        false => No,
+    let caller = runtime_state.env.caller();
+    if let Some(user_id) = runtime_state.data.principal_to_user_id.get(&caller) {
+        match runtime_state.data.subscriptions.exists(user_id, args.p256dh_key) {
+            true => Yes,
+            false => No,
+        }
+    } else {
+        No
     }
 }
