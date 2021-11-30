ca578473c0722b0bd3718be36e99c79bcac646d8
Hamish Peebles <hamishpeebles@gmail.com>
2021-11-30 09:45:19 +0000

    Wrap sending WebRTC messages in a try / catch

--- v2/frontend/src/domain/webrtc/RtcConnectionsManager.ts
+++ v2/frontend/src/domain/webrtc/RtcConnectionsManager.ts
@@ -89,7 +89,9 @@ export class RtcConnectionsManager {
         userIds.forEach((userId) => {
             const conn = this.connections.get(userId);
             if (conn && conn.open) {
-                conn.send(message);
+                try {
+                    conn.send(message);
+                } catch {}
             }
         });
     };
