fc95e75a0678fefe9bb9f8c2338302dc024e1fe0
megrogan <megrogan@gmail.com>
2021-11-30 18:24:59 +0000

    Simplify and fix image/video messages

--- v2/frontend/src/components/home/VideoContent.svelte
+++ v2/frontend/src/components/home/VideoContent.svelte
@@ -1,37 +1,22 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-    import { onMount } from "svelte";
     import { _ } from "svelte-i18n";
     import type { VideoContent } from "../../domain/chat/chat";
-    import { calculateHeight } from "../../utils/layout";
     import Caption from "./Caption.svelte";
 
     export let content: VideoContent;
     export let fill: boolean;
 
-    let landscape = content.height < content.width;
-    let height = 0;
-
-    function recalculateHeight() {
-        height = calculateHeight(
-            document.getElementById("chat-messages")?.offsetWidth ?? 0,
-            content
-        );
-    }
-
-    onMount(recalculateHeight);
+    let withCaption = content.caption !== undefined;
 </script>
-
-<svelte:window on:resize={recalculateHeight} />
 
 <div class="video">
     <video
         preload="none"
-        style={`height: ${height}px`}
         poster={content.imageData.blobUrl}
-        class:landscape
         class:fill
+        class:withCaption
         controls>
         <track kind="captions" />
         {#if content.videoData.blobUrl}
@@ -50,17 +35,16 @@
         cursor: pointer;
 
         video {
-            max-width: none;
-            width: auto;
+            width: 100%;
+            height: 100%;
             display: block;
 
             &:not(.fill) {
                 border-radius: $sp4;
             }
 
-            &.landscape {
-                max-width: calc(var(--vh, 1vh) * 50);
-                width: 100%;
+            &.withCaption {
+                margin-bottom: $sp2;
             }
         }
     }
