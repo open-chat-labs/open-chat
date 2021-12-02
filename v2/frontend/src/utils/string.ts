95e074193be882672f505cac8f1e163fdf53532c
Hamish Peebles <hamishpeebles@gmail.com>
2021-12-02 13:58:20 +0000

    Say 'You' rather than your username in replies

---
+++ v2/frontend/src/utils/string.ts
@@ -0,0 +1,8 @@
+export function toTitleCase(str: string): string {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
