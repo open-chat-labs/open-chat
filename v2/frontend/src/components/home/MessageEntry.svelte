e999b09a5060d7a09b08faf77eed0e7688a15ee5
Hamish Peebles <hamishpeebles@gmail.com>
2021-12-09 11:32:50 +0000

    Fix reverse typing bug (#1016)

--- v2/frontend/src/components/home/MessageEntry.svelte
+++ v2/frontend/src/components/home/MessageEntry.svelte
@@ -45,7 +45,12 @@
                 initialisedEdit = true;
             }
         } else if (inp) {
-            inp.textContent = $textContent ?? "";
+            const text = $textContent ?? "";
+            // Only set the textbox text when required rather than every time, because doing so sets the focus back to
+            // the start of the textbox on some devices.
+            if (inp.textContent !== text) {
+                inp.textContent = text;
+            }
         }
         if ($editingEvent === undefined) {
             initialisedEdit = false;
