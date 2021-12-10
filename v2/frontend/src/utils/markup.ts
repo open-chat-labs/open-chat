07214845f088e387eb34dc80d32b323a61fe10d3
megrogan <megrogan@gmail.com>
2021-12-10 10:01:35 +0000

    Fix url detection regular expression

--- v2/frontend/src/utils/markup.ts
+++ v2/frontend/src/utils/markup.ts
@@ -1,10 +1,12 @@
+/* eslint-disable no-useless-escape */
+
 // Blacklisted completely
 // " < \s
 //
 // Blacklisted from end
 // . , ( ) [ ] { } ! @ + : %
 export function wrapURLsInAnchorTags(text: string, new_window: boolean) : string {
-    const url_pattern = /(ftp|http|https):\/\/[^"<\s]+[^"<\s.,()[]{}!@+:%]/g;
+    const url_pattern = /(ftp|http|https):\/\/[^\"<\s]+[^\"<\s.,\(\)\[\]\{\}!@+:%]/g;
     const target = (new_window === true || new_window == null) ? '_blank' : '';    
     return text.replace(url_pattern, function (url) {
         return '<a href="' + url + '" target="' + target + '">' + url + '</a>';
