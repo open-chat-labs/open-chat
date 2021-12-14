b36458d44bd5a901af3b4b21690bf2999ba2bdc7
Hamish Peebles <hamishpeebles@gmail.com>
2021-12-14 18:01:24 +0000

    Only return latest message in summary if it is visible to user (#1050)

--- v2/backend/canisters/group/impl/src/queries/c2c_summary.rs
+++ v2/backend/canisters/group/impl/src/queries/c2c_summary.rs
@@ -14,7 +14,8 @@ fn c2c_summary_impl(runtime_state: &RuntimeState) -> Response {
     let data = &runtime_state.data;
     if let Some(participant) = data.participants.get(caller) {
         let latest_event = runtime_state.data.events.last();
-        let mentions = get_most_recent_mentions(participant);
+        let min_visible_message_index = participant.min_visible_message_index();
+
         let summary = Summary {
             chat_id: runtime_state.env.canister_id().into(),
             last_updated: latest_event.timestamp,
@@ -23,13 +24,16 @@ fn c2c_summary_impl(runtime_state: &RuntimeState) -> Response {
             avatar_id: Avatar::id(&data.avatar),
             is_public: data.is_public,
             min_visible_event_index: participant.min_visible_event_index(),
-            min_visible_message_index: participant.min_visible_message_index(),
-            latest_message: data.events.latest_message(),
+            min_visible_message_index,
+            latest_message: data
+                .events
+                .latest_message()
+                .filter(|m| m.event.message_index >= min_visible_message_index),
             latest_event_index: latest_event.index,
             joined: participant.date_added,
             participant_count: data.participants.len(),
             role: participant.role,
-            mentions,
+            mentions: get_most_recent_mentions(participant),
             wasm_version: WASM_VERSION.with(|v| **v.borrow()),
         };
         Success(SuccessResult { summary })
