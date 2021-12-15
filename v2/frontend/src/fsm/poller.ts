e2f4818a4bdee6221ef7432708d9c4b146a73aef
Hamish Peebles <hamishpeebles@gmail.com>
2021-12-15 12:05:12 +0000

    Prevent stampede by only starting timer once previous run has completed (#1061)

--- v2/frontend/src/fsm/poller.ts
+++ v2/frontend/src/fsm/poller.ts
@@ -2,11 +2,12 @@ import type { Unsubscriber } from "svelte/store";
 import { background } from "../stores/background";
 
 export class Poller {
-    private intervalId: number | undefined;
+    private timeoutId: number | undefined;
     private unsubscribeBackground: Unsubscriber | undefined;
+    private stopped = false;
 
     constructor(
-        private fn: () => unknown,
+        private fn: () => Promise<void>,
         private interval: number,
         private idleInterval: number = interval
     ) {
@@ -17,18 +18,25 @@ export class Poller {
 
     private start(hidden: boolean): void {
         const interval = hidden ? this.idleInterval : this.interval;
-        if (this.intervalId !== undefined) {
-            window.clearInterval(this.intervalId);
+        if (this.timeoutId !== undefined) {
+            window.clearTimeout(this.timeoutId);
         }
-        this.intervalId = window.setInterval(this.fn, interval);
+
+        const runInLoop = () => {
+            if (this.stopped) return;
+            this.timeoutId = window.setTimeout(() => this.fn().finally(runInLoop), interval);
+        };
+
+        this.timeoutId = window.setTimeout(runInLoop, interval);
     }
 
     stop(): void {
-        if (this.intervalId !== undefined) {
-            window.clearInterval(this.intervalId);
+        if (this.timeoutId !== undefined) {
+            window.clearTimeout(this.timeoutId);
         }
         if (this.unsubscribeBackground) {
             this.unsubscribeBackground();
         }
+        this.stopped = true;
     }
 }
