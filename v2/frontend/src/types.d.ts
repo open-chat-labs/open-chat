8018e524ce7d889f4ee6dcb5a98f8b86957921b8
Julian Jelfs <julian.jelfs@gmail.com>
2021-12-16 18:48:03 +0000

    better phone number selector

--- v2/frontend/src/types.d.ts
+++ v2/frontend/src/types.d.ts
@@ -30,3 +30,5 @@ declare module "svelte-easy-crop" {
     }
     export default class Cropper extends SvelteComponentTyped<CropperProps, CropperEvents> {}
 }
+
+declare module "intl-tel-input/build/js/utils";
