31ccacb5160932af86c703fb8b1be049d328b330
Julian Jelfs <julian.jelfs@gmail.com>
2021-12-16 18:57:32 +0000

    remove unused package

--- v2/frontend/src/types.d.ts
+++ v2/frontend/src/types.d.ts
@@ -1,15 +1,5 @@
 declare module "remount/es5";
 
-declare module "country-telephone-data" {
-    type Country = {
-        name: string;
-        dialCode: string;
-        format: string;
-        iso2: string;
-    };
-    declare const allCountries: Country[];
-}
-
 declare module "uuid" {
     export function v1(): string;
 }
