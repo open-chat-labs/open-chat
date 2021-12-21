4ebe8f70e59b1ca00d6ece8c07be08ecfca78a1d
Julian Jelfs <julian.jelfs@gmail.com>
2021-12-21 18:31:04 +0000

    better

--- v2/frontend/src/stores/time.ts
+++ v2/frontend/src/stores/time.ts
@@ -1,8 +1,14 @@
-import { writable } from "svelte/store";
+import { readable } from "svelte/store";
 
 // this is a fairly arbitrary interval but it seems about right
-setInterval(() => {
-    now.set(Date.now());
-}, 5000);
+const INTERVAL = 5000;
 
-export const now = writable(Date.now());
+export const now = readable(Date.now(), (set) => {
+    const interval = window.setInterval(() => {
+        set(Date.now());
+    }, INTERVAL);
+
+    return function stop() {
+        window.clearInterval(interval);
+    };
+});
