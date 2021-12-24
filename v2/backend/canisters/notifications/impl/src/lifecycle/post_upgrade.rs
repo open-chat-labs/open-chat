563511bd24b8de3e85d78da46aee20e2d70d680f
Hamish Peebles <hamishpeebles@gmail.com>
2021-12-24 13:10:34 +0000

    Set test_mode to false and clear trace messages

--- v2/backend/canisters/notifications/impl/src/lifecycle/post_upgrade.rs
+++ v2/backend/canisters/notifications/impl/src/lifecycle/post_upgrade.rs
@@ -17,8 +17,11 @@ fn post_upgrade(args: Args) {
 
     match version {
         StateVersion::V1 => {
-            let (data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
+            let (mut data, log_messages, _): (Data, Vec<LogMessage>, Vec<LogMessage>) =
                 serializer::deserialize(&bytes).unwrap();
+
+            data.test_mode = false;
+            let trace_messages = Vec::new();
 
             init_logger(data.test_mode);
             init_state(env, data, args.wasm_version);
