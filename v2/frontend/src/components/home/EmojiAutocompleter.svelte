2f98c10e929829ff36fa3b092f6b1d5a43c44d9c
Julian Jelfs <julian.jelfs@gmail.com>
2022-01-04 17:53:02 +0000

    just about there I think

---
+++ v2/frontend/src/components/home/EmojiAutocompleter.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    import MenuItem from "../MenuItem.svelte";
+    import Menu from "../Menu.svelte";
+    import VirtualList from "../VirtualList.svelte";
+    import { createEventDispatcher } from "svelte";
+    import { _ } from "svelte-i18n";
+    import { emojiDatabase } from "../../utils/emojis";
+    import type { NativeEmoji } from "emoji-picker-element/shared";
+
+    const ITEM_HEIGHT = 49.59;
+
+    type EmojiSummary = {
+        unicode: string;
+        code: string;
+    };
+
+    export let query: string | undefined;
+    export let offset: number;
+
+    let index = 0;
+    let matches: EmojiSummary[] = [];
+
+    $: {
+        if (query !== undefined) {
+            emojiDatabase.getPreferredSkinTone().then((tone) => {
+                emojiDatabase.getEmojiBySearchQuery(query!).then((m) => {
+                    matches = (m as NativeEmoji[])
+                        .map((match) => {
+                            return {
+                                unicode:
+                                    match.skins?.find((s) => s.tone === tone)?.unicode ??
+                                    match.unicode,
+                                code: match.shortcodes
+                                    ? match.shortcodes[match.shortcodes.length - 1]
+                                    : match.annotation,
+                            };
+                        })
+                        .filter((match) => !match.unicode.includes("🫢"));
+                });
+            });
+        }
+    }
+
+    $: console.log(matches);
+
+    const dispatch = createEventDispatcher();
+
+    function select(emoji: string) {
+        dispatch("select", emoji);
+    }
+
+    function onKeyDown(ev: KeyboardEvent): void {
+        switch (ev.key) {
+            case "ArrowDown":
+                index = (index + 1) % matches.length;
+                ev.preventDefault();
+                ev.stopPropagation();
+                break;
+            case "ArrowUp":
+                index = index === 0 ? matches.length - 1 : index - 1;
+                ev.preventDefault();
+                ev.stopPropagation();
+                break;
+            case "Escape":
+                dispatch("close");
+                ev.preventDefault();
+                ev.stopPropagation();
+                break;
+            case "Enter":
+                const match = matches[index];
+                if (match) {
+                    select(match.unicode);
+                }
+                ev.preventDefault();
+                ev.stopPropagation();
+                break;
+        }
+    }
+</script>
+
+<div class="picker" style={`bottom: ${offset}px; height: ${matches.length * ITEM_HEIGHT}px`}>
+    <Menu>
+        <VirtualList keyFn={(e) => e.unicode} items={matches} let:item let:itemIndex>
+            <MenuItem selected={itemIndex === index} on:click={() => select(item.unicode)}>
+                <div class="emoji" slot="icon">
+                    {item.unicode}
+                </div>
+                <div slot="text">
+                    :{item.code}:
+                </div>
+            </MenuItem>
+        </VirtualList>
+    </Menu>
+</div>
+
+<svelte:body on:keydown={onKeyDown} />
+
+<style type="text/scss">
+    :global(.picker .menu) {
+        box-shadow: none;
+        position: relative;
+        width: 100%;
+        height: 100%;
+        @include z-index("footer-overlay");
+    }
+
+    .picker {
+        position: absolute;
+        width: 100%;
+        max-height: calc(var(--vh, 1vh) * 50);
+        overflow: auto;
+        box-shadow: var(--menu-inverted-sh);
+    }
+    .emoji {
+        @include font(book, normal, fs-160);
+        margin-right: $sp4;
+    }
+</style>
