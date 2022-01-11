0243a82e20a80307a04038fd0ae562cd1615db44
Hamish Peebles <hamishpeebles@gmail.com>
2022-01-11 16:39:24 +0000

    Rename function

--- v2/backend/canisters/group_index/impl/src/queries/recommended_groups.rs
+++ v2/backend/canisters/group_index/impl/src/queries/recommended_groups.rs
@@ -3,11 +3,11 @@ use group_index_canister::recommended_groups::{Response::*, *};
 use ic_cdk_macros::query;
 
 #[query]
-fn new_popular_and_hot(args: Args) -> Response {
-    read_state(|state| new_popular_and_hot_impl(args, state))
+fn recommended_groups(args: Args) -> Response {
+    read_state(|state| recommended_groups_impl(args, state))
 }
 
-fn new_popular_and_hot_impl(args: Args, runtime_state: &RuntimeState) -> Response {
+fn recommended_groups_impl(args: Args, runtime_state: &RuntimeState) -> Response {
     Success(SuccessResult {
         groups: runtime_state.data.cached_hot_groups.get(args.count as usize),
     })
