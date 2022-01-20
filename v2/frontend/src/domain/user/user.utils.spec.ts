e0187bb2b835dd8331a7be0f3b9c62e935aa6dc7
Julian Jelfs <julian.jelfs@gmail.com>
2022-01-20 15:42:26 +0000

    extract userIds from mentions (#1301)
    * extract userIds from mentions
    
    * extract mention userIds from replies too
    
    Co-authored-by: megrogan <megrogan@gmail.com>

--- v2/frontend/src/domain/user/user.utils.spec.ts
+++ v2/frontend/src/domain/user/user.utils.spec.ts
@@ -1,5 +1,11 @@
 import type { UserLookup } from "./user";
-import { compareUsername, missingUserIds, parseMentions, userIsOnline } from "./user.utils";
+import {
+    compareUsername,
+    extractUserIdsFromMentions,
+    missingUserIds,
+    parseMentions,
+    userIsOnline,
+} from "./user.utils";
 
 const now = Date.now();
 jest.setSystemTime(now);
@@ -24,6 +30,23 @@ const lookup: UserLookup = {
         updated: BigInt(0),
     },
 };
+
+describe("extract user ids from mentions", () => {
+    test("extract a single user id", () => {
+        const parsed = extractUserIdsFromMentions("hello there @UserId(xyz), how are you?");
+        expect(parsed).toEqual(["xyz"]);
+    });
+    test("extract multiple user ids", () => {
+        const parsed = extractUserIdsFromMentions(
+            "hello there @UserId(xyz) and hello @UserId(abc), how are you?"
+        );
+        expect(parsed).toEqual(["xyz", "abc"]);
+    });
+    test("when there are no userIds to extract", () => {
+        const parsed = extractUserIdsFromMentions("this is a string that doesn't have any userIds");
+        expect(parsed).toEqual([]);
+    });
+});
 
 describe("parse mentions", () => {
     test("replace a single mention", () => {
--- v2/frontend/src/domain/user/user.utils.ts
+++ v2/frontend/src/domain/user/user.utils.ts
@@ -79,8 +79,14 @@ export function nullUser(username: string): UserSummary {
     };
 }
 
+const mentionRegex = /@UserId\(([\d\w-]+)\)/g;
+
+export function extractUserIdsFromMentions(text: string): string[] {
+    return [...text.matchAll(mentionRegex)].map((m) => m[1]);
+}
+
 export function parseMentions(userLookup: UserLookup, text: string, unknown: string): string {
-    return text.replace(/@UserId\(([\d\w-]+)\)/g, (_match, p1) => {
+    return text.replace(mentionRegex, (_match, p1) => {
         const username = userLookup[p1]?.username ?? unknown;
         return `**@${username}**`;
     });
