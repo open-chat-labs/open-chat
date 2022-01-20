48ab26f9351ef5dd2fd0c2d66dd6c91928bafe85
Julian Jelfs <julian.jelfs@gmail.com>
2022-01-20 12:55:14 +0000

    reinstate markdown parsing

--- v2/frontend/src/components/home/Markdown.svelte
+++ v2/frontend/src/components/home/Markdown.svelte
@@ -1,40 +1,66 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-    import {encode} from 'html-entities';
-    import { replaceNewlinesWithBrTags, wrapURLsInAnchorTags } from "../../utils/markup";
+    import { wrapURLsInAnchorTags } from "../../utils/markup";
+    import { marked } from "marked";
+    import DOMPurify from "dompurify";
+    import { onMount } from "svelte";
+    import { rollbar } from "../../utils/logging";
 
     export let text: string;
     export let inline: boolean = true;
     export let oneLine: boolean = false;
     export let suppressLinks: boolean = false;
 
-    function renderTextContent(text: string): string {
-        let str = text;
+    let sanitized = "unsafe";
+    const options = {
+        breaks: !oneLine,
+    };
 
-        // HTML encode the text
-        str = encode(str);
-
-        if (!oneLine) {
-            str = replaceNewlinesWithBrTags(str);
+    onMount(() => {
+        let parsed = text;
+        try {
+            parsed = marked.parseInline(renderTextContent(text), options);
+        } catch (err: any) {
+            rollbar.error("Error parsing markdown: ", err);
         }
 
+        try {
+            sanitized = DOMPurify.sanitize(
+                parsed,
+                suppressLinks ? { FORBID_ATTR: ["href"] } : { ALLOWED_ATTR: ["target", "href"] }
+            );
+        } catch (err: any) {
+            rollbar.error("Error sanitzing message content: ", err);
+        }
+    });
+
+    // TODO - we shouldn't need this, we should be able to do it with a custom renderer, but I want to
+    // come back to that.
+    // this will not do exactly what we want for markdown links e.g. [a link](https://www.google.co.uk)
+    function renderTextContent(text: string): string {
+        let str = text;
         if (!suppressLinks) {
             str = wrapURLsInAnchorTags(str, true);
         }
-
         return str;
-    }    
+    }
 </script>
 
 <p class="markdown-wrapper" class:inline class:oneLine>
-    {@html renderTextContent(text)}
+    {@html sanitized}
 </p>
 
 <style type="text/scss">
     :global(.markdown-wrapper a) {
         text-decoration: underline;
         word-break: break-all;
+    }
+
+    :global(.markdown-wrapper code) {
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        background-color: rgba(255, 255, 255, 0.1);
+        padding: 0 $sp2;
     }
 
     .markdown-wrapper {
