bcbe8c8f28dfcef19f31befbf681d4e1622dd8e7
Julian Jelfs <julian.jelfs@gmail.com>
2022-01-31 10:40:02 +0000

    this is going to have to change

--- v2/frontend/src/services/data/data.client.mock.ts
+++ v2/frontend/src/services/data/data.client.mock.ts
@@ -1,13 +1,8 @@
 import type { UserStorage } from "../../domain/user/user";
 import type { MessageContent } from "../../domain/chat/chat";
 import type { IDataClient } from "./data.client.interface";
-import type { StorageStatus } from "../../domain/data/data";
 
 export class DataClientMock implements IDataClient {
-    async storageStatus(): Promise<StorageStatus> {
-        return Promise.resolve({ byteLimit: BigInt(0), bytesUsed: BigInt(0) });
-    }
-
     async uploadData(_content: MessageContent): Promise<boolean> {
         return Promise.resolve(true);
     }
