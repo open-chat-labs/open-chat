f0fbfd3252ce26604b21f5be6d480e0802cf7de8
megrogan <megrogan@gmail.com>
2022-02-21 17:58:54 +0000

    user_index::check_username

---
+++ backend/canisters/user_index/api/src/queries/check_username.rs
@@ -0,0 +1,16 @@
+use candid::CandidType;
+use serde::Deserialize;
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct Args {
+    pub username: String,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum Response {
+    Success,
+    UsernameTaken,
+    UsernameInvalid,
+    UsernameTooShort(u16),
+    UsernameTooLong(u16),
+}
---
+++ backend/canisters/user_index/impl/src/queries/check_username.rs
@@ -0,0 +1,24 @@
+use crate::updates::set_username::{validate_username, UsernameValidationResult};
+use crate::{read_state, RuntimeState};
+use ic_cdk_macros::query;
+use user_index_canister::check_username::{Response::*, *};
+
+#[query]
+fn check_username(args: Args) -> Response {
+    read_state(|state| check_username_impl(args, state))
+}
+
+fn check_username_impl(args: Args, runtime_state: &RuntimeState) -> Response {
+    match validate_username(&args.username) {
+        UsernameValidationResult::TooShort(min_length) => return UsernameTooShort(min_length),
+        UsernameValidationResult::TooLong(max_length) => return UsernameTooLong(max_length),
+        UsernameValidationResult::Invalid => return UsernameInvalid,
+        _ => {}
+    };
+
+    if runtime_state.data.users.does_username_exist(&args.username) {
+        return UsernameTaken;
+    }
+
+    Success
+}
