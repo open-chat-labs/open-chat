12e965c58045f5368529ad907138dca6458ae45d
Hamish Peebles <hamishpeebles@gmail.com>
2022-02-21 13:19:42 +0000

    Remove v1 code (#1447)

--- src/services/user_mgmt/src/queries/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod get_current_user;
-pub mod get_stats;
-pub mod get_user_id;
-pub mod get_users;
-pub mod search_users;
--- v2/generate-wasm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo Building package $1
-cargo build --target wasm32-unknown-unknown --release --package $1
-
-echo Optimising wasm
-wasm-opt target/wasm32-unknown-unknown/release/$1.wasm --strip-debug -Oz -o target/wasm32-unknown-unknown/release/$1-opt.wasm
