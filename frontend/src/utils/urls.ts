7e0ef86afcc978fd0c3dee7d6667b51453827597
Hamish Peebles <hamishpeebles@gmail.com>
2022-03-12 10:17:51 +0000

    formatting

--- frontend/src/utils/urls.ts
+++ frontend/src/utils/urls.ts
@@ -6,5 +6,6 @@ export function isAbsoluteUrl(url: string): boolean {
     return regex.test(url);
 }
 
-export const openChatFriendlyUrl = process.env.DFX_NETWORK === "ic_test" ? "https://test.oc.app" : "https://oc.app";
+export const openChatFriendlyUrl =
+    process.env.DFX_NETWORK === "ic_test" ? "https://test.oc.app" : "https://oc.app";
 export const synonymousUrlRegex = new RegExp(`^(${window.location.origin}|${openChatFriendlyUrl})`);
