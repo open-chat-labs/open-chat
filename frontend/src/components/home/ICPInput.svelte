2f104a0f57cb9156f955ee65cf44fc6b96dc9027
Hamish Peebles <hamishpeebles@gmail.com>
2022-03-17 11:35:21 +0000

    Hide 'stepper' on ICPInput (#1565)

--- frontend/src/components/home/ICPInput.svelte
+++ frontend/src/components/home/ICPInput.svelte
@@ -66,5 +66,18 @@
         text-align: right;
         display: block;
         outline: none;
+        padding: 0 $sp3;
+    }
+
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
     }
 </style>
