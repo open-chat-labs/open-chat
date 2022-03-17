1e7b6cbf1b2f4a9dd55b90b7182fb5fbf06a43c7
Hamish Peebles <hamishpeebles@gmail.com>
2022-03-17 17:32:32 +0000

    Mark sending ICP as done in the roadmap

--- frontend/src/components/RoadmapModal.svelte
+++ frontend/src/components/RoadmapModal.svelte
@@ -18,10 +18,8 @@
             <li><del>Create polls</del></li>
             <li><del>Group permissions including read-only groups</del></li>
             <li><del>Double-click to show images full-screen</del></li>
-            <li>
-                Deposit tokens into your OpenChat wallet, send tokens as messages, withdraw tokens<br />
-                (cycles and ICP first, then Bitcoin and Ether later, plus more)
-            </li>
+            <li><del>Deposit ICP into your OpenChat account, send ICP as messages, withdraw ICP</del></li>
+            <li>Send additional tokens as messages, such as BTC, ETH, Cycles, etc</li>
             <li>Message forwarding</li>
             <li>Improved UI design</li>
             <li>Tag messages (eg star) and search/filter by tag</li>
