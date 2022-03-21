3f2970e08eecd144d619eaf85320c60e93dd628a
Hamish Peebles <hamishpeebles@gmail.com>
2022-03-21 18:46:35 +0000

    We only need the threshold to go up to 0.5

--- frontend/src/components/home/CurrentChatMessages.svelte
+++ frontend/src/components/home/CurrentChatMessages.svelte
@@ -68,7 +68,7 @@
         const options = {
             root: messagesDiv,
             rootMargin: "0px",
-            threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+            threshold: [0.1, 0.2, 0.3, 0.4, 0.5],
         };
 
         observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
