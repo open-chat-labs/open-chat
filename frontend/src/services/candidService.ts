f77ff7ea7365564f305336c183eae5f5a741df98
Julian Jelfs <julian.jelfs@gmail.com>
2022-04-06 18:03:17 +0100

    remove async noise

--- frontend/src/services/candidService.ts
+++ frontend/src/services/candidService.ts
@@ -23,56 +23,46 @@ export abstract class CandidService {
         });
     }
 
-    protected async handleResponse<From, To>(
+    protected handleResponse<From, To>(
         service: Promise<From>,
         mapper: (from: From) => To,
         args?: unknown
     ): Promise<To> {
-        let response: From;
-        try {
-            response = await service;
-        } catch (e) {
-            console.log(e, args);
-            throw toHttpError(e as Error, this.identity);
-        }
-        return mapper(response);
+        return service.then(mapper).catch((err) => {
+            console.log(err, args);
+            throw toHttpError(err as Error, this.identity);
+        });
     }
 
-    protected async handleQueryResponse<From, To>(
+    protected handleQueryResponse<From, To>(
         serviceCall: () => Promise<From>,
         mapper: (from: From) => To,
         args?: unknown,
         retries = 0
     ): Promise<To> {
-        let response: From;
-        try {
-            response = await serviceCall();
-            if (retries !== 0) {
-                debug(`query: Recovered from a temporary query error after ${retries} retries`);
-            }
-        } catch (e) {
-            const httpErr = toHttpError(e as Error, this.identity);
-
-            if (
-                !(httpErr instanceof SessionExpiryError) &&
-                !(httpErr instanceof AuthError) &&
-                retries < MAX_RETRIES
-            ) {
-                const delay = RETRY_DELAY * Math.pow(2, retries);
-                return new Promise((resolve, reject) => {
-                    debug(`query: error occurred, retrying in ${delay}ms`);
-                    window.setTimeout(() => {
-                        this.handleQueryResponse(serviceCall, mapper, args, retries + 1)
-                            .then(resolve)
-                            .catch(reject);
-                    }, delay);
-                });
-            } else {
-                debug(`query: Error performing query request: ${e}, ${args}, ${retries}`);
-                throw httpErr;
-            }
-        }
-        return mapper(response);
+        return serviceCall()
+            .then(mapper)
+            .catch((err) => {
+                const httpErr = toHttpError(err as Error, this.identity);
+                if (
+                    !(httpErr instanceof SessionExpiryError) &&
+                    !(httpErr instanceof AuthError) &&
+                    retries < MAX_RETRIES
+                ) {
+                    const delay = RETRY_DELAY * Math.pow(2, retries);
+                    return new Promise((resolve, reject) => {
+                        debug(`query: error occurred, retrying in ${delay}ms`);
+                        window.setTimeout(() => {
+                            this.handleQueryResponse(serviceCall, mapper, args, retries + 1)
+                                .then(resolve)
+                                .catch(reject);
+                        }, delay);
+                    });
+                } else {
+                    debug(`query: Error performing query request: ${err}, ${args}, ${retries}`);
+                    throw httpErr;
+                }
+            });
     }
 
     constructor(protected identity: Identity) {}
