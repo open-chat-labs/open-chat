d233daebf3920014bde70970d9af099f39a698bf
Julian Jelfs <julian.jelfs@gmail.com>
2022-04-07 09:22:53 +0100

    mechanism to retry query calls if they fail (#1676)
    * retry queries
    
    * remove async noise

--- frontend/src/services/candidService.ts
+++ frontend/src/services/candidService.ts
@@ -1,6 +1,15 @@
 import { Actor, HttpAgent, Identity } from "@dfinity/agent";
 import type { IDL } from "@dfinity/candid";
-import { toHttpError } from "./httpError";
+import { rollbar } from "../utils/logging";
+import { AuthError, SessionExpiryError, toHttpError } from "./httpError";
+
+const MAX_RETRIES = 7;
+const RETRY_DELAY = 100;
+
+function debug(msg: string): void {
+    rollbar.debug(msg);
+    console.log(msg);
+}
 
 export abstract class CandidService {
     protected createServiceClient<T>(factory: IDL.InterfaceFactory, canisterId: string): T {
@@ -14,19 +23,46 @@ export abstract class CandidService {
         });
     }
 
-    protected async handleResponse<From, To>(
+    protected handleResponse<From, To>(
         service: Promise<From>,
         mapper: (from: From) => To,
         args?: unknown
     ): Promise<To> {
-        let response: From;
-        try {
-            response = await service;
-        } catch (e) {
-            console.log(e, args);
-            throw toHttpError(e as Error, this.identity);
-        }
-        return mapper(response);
+        return service.then(mapper).catch((err) => {
+            console.log(err, args);
+            throw toHttpError(err as Error, this.identity);
+        });
+    }
+
+    protected handleQueryResponse<From, To>(
+        serviceCall: () => Promise<From>,
+        mapper: (from: From) => To,
+        args?: unknown,
+        retries = 0
+    ): Promise<To> {
+        return serviceCall()
+            .then(mapper)
+            .catch((err) => {
+                const httpErr = toHttpError(err as Error, this.identity);
+                if (
+                    !(httpErr instanceof SessionExpiryError) &&
+                    !(httpErr instanceof AuthError) &&
+                    retries < MAX_RETRIES
+                ) {
+                    const delay = RETRY_DELAY * Math.pow(2, retries);
+                    return new Promise((resolve, reject) => {
+                        debug(`query: error occurred, retrying in ${delay}ms`);
+                        window.setTimeout(() => {
+                            this.handleQueryResponse(serviceCall, mapper, args, retries + 1)
+                                .then(resolve)
+                                .catch(reject);
+                        }, delay);
+                    });
+                } else {
+                    debug(`query: Error performing query request: ${err}, ${args}, ${retries}`);
+                    throw httpErr;
+                }
+            });
     }
 
     constructor(protected identity: Identity) {}
