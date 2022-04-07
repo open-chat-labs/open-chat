b2d41397a41990889a9ef7de4365c8e99f718e6c
megrogan <megrogan@gmail.com>
2022-04-07 10:10:41 +0100

    Apply padding to specific area

--- frontend/src/components/SectionHeader.svelte
+++ frontend/src/components/SectionHeader.svelte
@@ -26,7 +26,6 @@
         color: var(--section-txt);
         @include z-index("section-header");
         flex: 0 0 60px;
-        gap: $sp2;
 
         &.entry {
             background-color: var(--entry-bg);
