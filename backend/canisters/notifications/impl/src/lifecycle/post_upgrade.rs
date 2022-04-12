87c12be290294bd417f4f4b269e0fb1cf3b0f110
Hamish Peebles <hamishpeebles@gmail.com>
2022-04-12 13:33:59 +0100

    Use `BufferedStableReader` and `BufferedStableWriter` during upgrades

--- backend/canisters/notifications/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/notifications/impl/src/lifecycle/post_upgrade.rs
@@ -1,9 +1,10 @@
-use crate::lifecycle::{init_logger, init_state};
+use crate::lifecycle::{init_logger, init_state, UPGRADE_BUFFER_SIZE};
 use crate::{Data, LOG_MESSAGES};
 use canister_api_macros::trace;
 use canister_logger::{LogMessage, LogMessagesWrapper};
 use ic_cdk_macros::post_upgrade;
 use notifications_canister::post_upgrade::Args;
+use stable_memory::deserialize_from_stable_memory;
 use tracing::info;
 use utils::env::canister::CanisterEnv;
 
@@ -13,10 +14,9 @@ fn post_upgrade(args: Args) {
     ic_cdk::setup();
 
     let env = Box::new(CanisterEnv::new());
-    let bytes = ic_cdk::api::stable::stable_bytes();
 
     let (data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
-        serializer::deserialize(&bytes).unwrap();
+        deserialize_from_stable_memory(UPGRADE_BUFFER_SIZE).unwrap();
 
     init_logger(data.test_mode);
     init_state(env, data, args.wasm_version);
--- backend/canisters/user_index/impl/src/lifecycle/pre_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/pre_upgrade.rs
@@ -1,8 +1,9 @@
+use crate::lifecycle::UPGRADE_BUFFER_SIZE;
 use crate::{take_state, LOG_MESSAGES};
 use canister_api_macros::trace;
 use ic_cdk_macros::pre_upgrade;
+use stable_memory::serialize_to_stable_memory;
 use tracing::info;
-use utils::canister::write_to_stable_memory;
 
 #[pre_upgrade]
 #[trace]
@@ -10,13 +11,12 @@ fn pre_upgrade() {
     info!("Pre-upgrade starting");
 
     let state = take_state();
-    let messages_container = LOG_MESSAGES.with(|l| l.take());
 
+    let messages_container = LOG_MESSAGES.with(|l| l.take());
     let log_messages = messages_container.logs.drain_messages();
     let trace_messages = messages_container.traces.drain_messages();
 
     let stable_state = (state.data, log_messages, trace_messages);
-    let bytes = serializer::serialize(&stable_state).unwrap();
 
-    write_to_stable_memory(0, &bytes);
+    serialize_to_stable_memory(stable_state, UPGRADE_BUFFER_SIZE).unwrap();
 }
