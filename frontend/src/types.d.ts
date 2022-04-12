7a5009ec026aa5364d4f126b8b1ecb6d8c766490
Julian Jelfs <julian.jelfs@gmail.com>
2022-04-12 10:02:08 +0100

    Swipe left to delete (#1698)
    * swipe left to delete chat
    
    * tiny tidy

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "remount/es5";
 
 declare module "uuid" {
@@ -24,3 +26,11 @@ declare module "svelte-easy-crop" {
 declare module "intl-tel-input/build/js/utils";
 
 declare module "svelte-qr";
+
+declare namespace svelte.JSX {
+    interface HTMLAttributes<T> {
+        onleftswipe?: any;
+        onrightswipe?: any;
+        onswiping?: any;
+    }
+}
