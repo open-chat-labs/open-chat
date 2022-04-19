51aabfc4fe7ecc84b082f5835ba01cbd592a2d52
Hamish Peebles <hamishpeebles@gmail.com>
2022-04-19 13:08:29 +0100

    Pass Environment as arg to regular jobs

--- backend/canisters/group/impl/src/regular_jobs.rs
+++ backend/canisters/group/impl/src/regular_jobs.rs
@@ -1,19 +1,20 @@
 use crate::Data;
+use utils::env::Environment;
 use utils::regular_jobs::{RegularJob, RegularJobs};
 use utils::time::MINUTE_IN_MS;
 
 pub(crate) fn build() -> RegularJobs<Data> {
-    let check_cycles_balance = RegularJob::new("Check cycles balance".to_owned(), check_cycles_balance, MINUTE_IN_MS);
-    let retry_deleting_files = RegularJob::new("Retry deleting files".to_string(), retry_deleting_files, MINUTE_IN_MS);
+    let check_cycles_balance = RegularJob::new("Check cycles balance", check_cycles_balance, MINUTE_IN_MS);
+    let retry_deleting_files = RegularJob::new("Retry deleting files", retry_deleting_files, MINUTE_IN_MS);
 
     RegularJobs::new(vec![check_cycles_balance, retry_deleting_files])
 }
 
-fn check_cycles_balance(data: &mut Data) {
+fn check_cycles_balance(_: &dyn Environment, data: &mut Data) {
     let group_index_canister_id = data.group_index_canister_id;
     utils::cycles::check_cycles_balance(0, group_index_canister_id);
 }
 
-fn retry_deleting_files(_: &mut Data) {
+fn retry_deleting_files(_: &dyn Environment, _: &mut Data) {
     open_storage_bucket_client::retry_failed();
 }
