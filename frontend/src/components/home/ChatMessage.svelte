6142b2e2a6f000981b36689ac565d2ee9c7eae8f
Julian Jelfs <julian.jelfs@gmail.com>
2022-04-27 15:42:11 +0100

    belt and braces (#1760)

--- frontend/src/components/home/ChatMessage.svelte
+++ frontend/src/components/home/ChatMessage.svelte
@@ -100,7 +100,7 @@
         if (!me && !readByMe) {
             // todo - leaving this console log here for now just to make sure we are not *over* observing
             console.log("beginning to observe: ", msg.messageIndex);
-            observer.observe(msgElement);
+            observer?.observe(msgElement);
         }
 
         recalculateMediaDimensions();
