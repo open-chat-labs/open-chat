2e4a92431fa4fcc3ad36af1b9bd29b150a8424df
Julian Jelfs <julian.jelfs@gmail.com>
2022-05-03 18:06:54 +0100

    generate faq links (#1792)

--- frontend/src/utils/urls.ts
+++ frontend/src/utils/urls.ts
@@ -9,3 +9,23 @@ export function isAbsoluteUrl(url: string): boolean {
 export const openChatFriendlyUrl =
     process.env.DFX_NETWORK === "ic_test" ? "https://test.oc.app" : "https://oc.app";
 export const synonymousUrlRegex = new RegExp(`^(${window.location.origin}|${openChatFriendlyUrl})`);
+
+function replaceQueryString(qs: URLSearchParams): string {
+    const qsStr = [...qs.keys()].length > 0 ? `?${qs}` : "";
+    const hash = window.location.hash.replace("#", "");
+    const match = hash.match(/.*(\?.*)/);
+    if (match) {
+        return hash.replace(match[1], qsStr);
+    }
+    return hash.startsWith("/") ? `${hash}${qsStr}` : `/${hash}${qsStr}`;
+}
+
+export function addQueryStringParam(qs: URLSearchParams, name: string, val: string): string {
+    qs.set(name, val);
+    return replaceQueryString(qs);
+}
+
+export function removeQueryStringParam(qs: URLSearchParams, name: string): string {
+    qs.delete(name);
+    return replaceQueryString(qs);
+}
