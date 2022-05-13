3916b04b95756b5b622f9831ae707f25d898b801
megrogan <megrogan@gmail.com>
2022-05-13 11:28:23 +0100

    Make update_permissions granular (#1849)

--- backend/canisters/group/api/src/updates/update_permissions.rs
+++ backend/canisters/group/api/src/updates/update_permissions.rs
@@ -1,10 +1,21 @@
 use candid::CandidType;
 use serde::Deserialize;
-use types::GroupPermissions;
+use types::PermissionRole;
 
 #[derive(CandidType, Deserialize, Debug)]
 pub struct Args {
-    pub permissions: GroupPermissions,
+    pub change_permissions: Option<PermissionRole>,
+    pub change_roles: Option<PermissionRole>,
+    pub add_members: Option<PermissionRole>,
+    pub remove_members: Option<PermissionRole>,
+    pub block_users: Option<PermissionRole>,
+    pub delete_messages: Option<PermissionRole>,
+    pub update_group: Option<PermissionRole>,
+    pub pin_messages: Option<PermissionRole>,
+    pub invite_users: Option<PermissionRole>,
+    pub create_polls: Option<PermissionRole>,
+    pub send_messages: Option<PermissionRole>,
+    pub react_to_messages: Option<PermissionRole>,
 }
 
 #[derive(CandidType, Deserialize, Debug)]
