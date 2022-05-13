bb4bacb77ebb3845c6708d07a9efe982319693e1
megrogan <megrogan@gmail.com>
2022-05-13 11:12:56 +0100

    Make update_permissions granular

--- backend/canisters/group/impl/src/updates/update_permissions.rs
+++ backend/canisters/group/impl/src/updates/update_permissions.rs
@@ -4,7 +4,7 @@ use canister_api_macros::trace;
 use chat_events::ChatEventInternal;
 use group_canister::update_permissions::{Response::*, *};
 use ic_cdk_macros::update;
-use types::PermissionsChanged;
+use types::{GroupPermissions, PermissionsChanged};
 
 #[update]
 #[trace]
@@ -22,15 +22,17 @@ fn update_permissions_impl(args: Args, runtime_state: &mut RuntimeState) -> Resp
             return NotAuthorized;
         }
 
-        runtime_state.data.permissions = args.permissions.clone();
+        let old_permissions = runtime_state.data.permissions.clone();
+        let new_permissions = merge_permissions(args, &old_permissions);
+        runtime_state.data.permissions = new_permissions.clone();
 
         let now = runtime_state.env.now();
         let events = &mut runtime_state.data.events;
 
         events.push_event(
             ChatEventInternal::PermissionsChanged(Box::new(PermissionsChanged {
-                old_permissions: runtime_state.data.permissions.clone(),
-                new_permissions: args.permissions,
+                old_permissions,
+                new_permissions,
                 changed_by: participant.user_id,
             })),
             now,
@@ -41,5 +43,22 @@ fn update_permissions_impl(args: Args, runtime_state: &mut RuntimeState) -> Resp
         Success
     } else {
         CallerNotInGroup
+    }
+}
+
+fn merge_permissions(new: Args, old: &GroupPermissions) -> GroupPermissions {
+    GroupPermissions {
+        change_permissions: new.change_permissions.unwrap_or(old.change_permissions),
+        change_roles: new.change_roles.unwrap_or(old.change_roles),
+        add_members: new.add_members.unwrap_or(old.add_members),
+        remove_members: new.remove_members.unwrap_or(old.remove_members),
+        block_users: new.block_users.unwrap_or(old.block_users),
+        delete_messages: new.delete_messages.unwrap_or(old.delete_messages),
+        update_group: new.update_group.unwrap_or(old.update_group),
+        pin_messages: new.pin_messages.unwrap_or(old.pin_messages),
+        invite_users: new.invite_users.unwrap_or(old.invite_users),
+        create_polls: new.create_polls.unwrap_or(old.create_polls),
+        send_messages: new.send_messages.unwrap_or(old.send_messages),
+        react_to_messages: new.react_to_messages.unwrap_or(old.react_to_messages),
     }
 }
