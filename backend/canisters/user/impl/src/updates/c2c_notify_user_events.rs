40a53287e39ba71df8b8284c1a8ae6bd391b938e
megrogan <megrogan@gmail.com>
2022-06-07 14:50:53 +0100

    notify user canister of multiple events at once

--- backend/canisters/user/impl/src/updates/c2c_notify_user_events.rs
+++ backend/canisters/user/impl/src/updates/c2c_notify_user_events.rs
@@ -3,7 +3,7 @@ use crate::{mutate_state, run_regular_jobs, RuntimeState};
 use canister_api_macros::update_candid_and_msgpack;
 use canister_tracing_macros::trace;
 use types::UserEvent;
-use user_canister::c2c_notify_user_event::{Response::*, *};
+use user_canister::c2c_notify_user_events::{Response::*, *};
 
 #[update_candid_and_msgpack(guard = "caller_is_user_index")]
 #[trace]
@@ -14,7 +14,14 @@ fn c2c_notify_user_event(args: Args) -> Response {
 }
 
 fn c2c_notify_user_event_impl(args: Args, runtime_state: &mut RuntimeState) -> Response {
-    match args.event {
+    for event in args.events {
+        process_event(event, runtime_state);
+    }
+    Success
+}
+
+fn process_event(event: UserEvent, runtime_state: &mut RuntimeState) {
+    match event {
         UserEvent::PhoneNumberConfirmed(_) => {
             runtime_state.data.set_user_verified();
         }
@@ -22,5 +29,4 @@ fn c2c_notify_user_event_impl(args: Args, runtime_state: &mut RuntimeState) -> R
             runtime_state.data.set_paid_storage(ev.new_limit);
         }
     }
-    Success
 }
