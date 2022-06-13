2f23768126e100ffd54af6bbc82173773783ca54
Hamish Peebles <hamishpeebles@gmail.com>
2022-06-13 11:06:03 +0100

    Switch to the new crypto message content type

--- backend/canisters/user/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user/impl/src/lifecycle/post_upgrade.rs
@@ -15,10 +15,8 @@ fn post_upgrade(args: Args) {
 
     let env = Box::new(CanisterEnv::new());
 
-    let (mut data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
+    let (data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
         deserialize_from_stable_memory(UPGRADE_BUFFER_SIZE).unwrap();
-
-    data.direct_chats.migrate_to_new_crypto_message_content();
 
     init_logger(data.test_mode);
     init_state(env, data, args.wasm_version);
