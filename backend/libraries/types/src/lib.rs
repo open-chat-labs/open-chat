1f7c98eaffb1d2a197dcbda5beaf5234e77e1ad2
Hamish Peebles <hamishpeebles@gmail.com>
2022-06-15 23:18:05 +0100

    Alphabetise

--- backend/libraries/types/src/lib.rs
+++ backend/libraries/types/src/lib.rs
@@ -90,6 +90,6 @@ pub type ICP = Tokens;
 pub type Milliseconds = u64;
 pub type NeuronId = u64;
 pub type ProposalId = u64;
+pub type Salt = [u8; 32];
 pub type TimestampMillis = u64;
 pub type TimestampNanos = u64;
-pub type Salt = [u8; 32];
