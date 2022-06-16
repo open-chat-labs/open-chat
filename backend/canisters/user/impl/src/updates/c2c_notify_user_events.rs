2e2c58fe64a06ba6d0a0cf89b52cd5b9196fe2a5
megrogan <megrogan@gmail.com>
2022-06-16 15:11:00 +0100

    Enable openchat_bot messages (#1973)

--- backend/canisters/user/impl/src/updates/c2c_notify_user_events.rs
+++ backend/canisters/user/impl/src/updates/c2c_notify_user_events.rs
@@ -1,5 +1,5 @@
 use crate::guards::caller_is_user_index;
-use crate::{mutate_state, RuntimeState, PREMIUM_GROUP_CREATION_LIMIT};
+use crate::{mutate_state, openchat_bot, RuntimeState, PREMIUM_GROUP_CREATION_LIMIT};
 use canister_api_macros::update_msgpack;
 use canister_tracing_macros::trace;
 use types::UserEvent;
@@ -27,18 +27,15 @@ fn process_event(event: UserEvent, runtime_state: &mut RuntimeState) {
             runtime_state.data.phone_is_verified = true;
             runtime_state.data.storage_limit = ev.new_storage_limit;
             runtime_state.data.group_creation_limit = PREMIUM_GROUP_CREATION_LIMIT;
-            //openchat_bot::send_phone_number_confirmed_bot_message(&ev, runtime_state);
+            openchat_bot::send_phone_number_confirmed_bot_message(&ev, runtime_state);
         }
         UserEvent::StorageUpgraded(ev) => {
             runtime_state.data.storage_limit = ev.new_storage_limit;
             runtime_state.data.group_creation_limit = PREMIUM_GROUP_CREATION_LIMIT;
-            //openchat_bot::send_storage_ugraded_bot_message(&ev, runtime_state);
+            openchat_bot::send_storage_ugraded_bot_message(&ev, runtime_state);
         }
-        UserEvent::UserCreated(ev) => {
-            runtime_state.data.user_created = ev.timestamp;
-        }
-        UserEvent::ReferredUserRegistered(_ev) => {
-            //openchat_bot::send_referred_user_joined_message(&ev, runtime_state);
+        UserEvent::ReferredUserRegistered(ev) => {
+            openchat_bot::send_referred_user_joined_message(&ev, runtime_state);
         }
     }
 }
