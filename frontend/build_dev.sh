485825d5ebd22a0589d8df9601ca568e5283bd84
Hamish Peebles <hamishpeebles@gmail.com>
2022-06-17 23:56:55 +0100

    Add the ProposalsBot to the user store

--- frontend/build_dev.sh
+++ frontend/build_dev.sh
@@ -1,13 +1,13 @@
 WATCH=$1
 
-export INTERNET_IDENTITY_URL=http://localhost:8000?canisterId=rwlgt-iiaaa-aaaaa-aaaaa-cai
+export INTERNET_IDENTITY_URL=http://localhost:8000?canisterId=rno2w-sqaaa-aaaaa-aaacq-cai
 export DFX_NETWORK=local
 export DEV_PORT=5001
 export CLIENT_CACHING=true
 export OPEN_STORAGE_INDEX_CANISTER=qjdve-lqaaa-aaaaa-aaaeq-cai
-export LEDGER_CANISTER_ICP=qoctq-giaaa-aaaaa-aaaea-cai
-export LEDGER_CANISTER_BTC=qoctq-giaaa-aaaaa-aaaea-cai
-export LEDGER_CANISTER_CHAT=qoctq-giaaa-aaaaa-aaaea-cai
+export LEDGER_CANISTER_ICP=ryjl3-tyaaa-aaaaa-aaaba-cai
+export LEDGER_CANISTER_BTC=ryjl3-tyaaa-aaaaa-aaaba-cai
+export LEDGER_CANISTER_CHAT=ryjl3-tyaaa-aaaaa-aaaba-cai
 export BLOB_URL_PATTERN=http://{canisterId}.localhost:8000/{blobType}/
 export ENABLE_MULTI_CRYPTO=true
 
