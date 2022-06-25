7ee77c6bc898b9da9e7e96dd60c4dae09041cb38
Hamish Peebles <hamishpeebles@gmail.com>
2022-06-25 12:16:12 +0100

    Handle the fact that some proposals are hidden

--- backend/canisters/proposals_bot/impl/src/lifecycle/heartbeat.rs
+++ backend/canisters/proposals_bot/impl/src/lifecycle/heartbeat.rs
@@ -21,9 +21,9 @@ mod retrieve_proposals {
     pub fn run() {
         if let Some((governance_canister_id, next_proposal_id, is_nns)) = mutate_state(get_next) {
             if is_nns {
-                ic_cdk::spawn(retrieve_nns_proposal(governance_canister_id, next_proposal_id));
+                ic_cdk::spawn(retrieve_nns_proposals(governance_canister_id, next_proposal_id));
             } else {
-                ic_cdk::spawn(retrieve_sns_proposal(governance_canister_id, next_proposal_id));
+                ic_cdk::spawn(retrieve_sns_proposals(governance_canister_id, next_proposal_id));
             }
         }
     }
@@ -35,46 +35,87 @@ mod retrieve_proposals {
         Some((governance_canister_id, proposal_id, is_nns))
     }
 
-    async fn retrieve_nns_proposal(governance_canister_id: CanisterId, next_proposal_id: ProposalId) {
-        let args = ListProposalInfo {
+    async fn retrieve_nns_proposals(governance_canister_id: CanisterId, next_proposal_id: ProposalId) {
+        let latest_proposal_args = ListProposalInfo {
             limit: 1,
-            before_proposal: Some(WrappedProposalId {
-                id: next_proposal_id + 1,
-            }),
+            before_proposal: None,
             exclude_topic: Vec::new(),
             include_reward_status: Vec::new(),
             include_status: Vec::new(),
         };
 
-        let response = governance_clients::nns::list_proposals(governance_canister_id, &args).await;
-        handle_proposal_response(governance_canister_id, next_proposal_id, response);
+        let mut response = governance_clients::nns::list_proposals(governance_canister_id, &latest_proposal_args).await;
+        if let Some(latest_proposal_id) = response
+            .as_ref()
+            .ok()
+            .and_then(|p| p.iter().next())
+            .and_then(|p| p.id.as_ref())
+            .map(|p| p.id)
+        {
+            // Check if we have missed any proposals, if so, try to grab them
+            if latest_proposal_id > next_proposal_id {
+                let proposals_args = ListProposalInfo {
+                    limit: (1 + latest_proposal_id - next_proposal_id) as u32,
+                    before_proposal: Some(WrappedProposalId {
+                        id: latest_proposal_id + 1,
+                    }),
+                    exclude_topic: Vec::new(),
+                    include_reward_status: Vec::new(),
+                    include_status: Vec::new(),
+                };
+                response = governance_clients::nns::list_proposals(governance_canister_id, &proposals_args).await;
+            }
+        }
+
+        handle_proposals_response(governance_canister_id, next_proposal_id, response);
     }
 
-    async fn retrieve_sns_proposal(governance_canister_id: CanisterId, next_proposal_id: ProposalId) {
-        let args = ListProposals {
+    async fn retrieve_sns_proposals(governance_canister_id: CanisterId, next_proposal_id: ProposalId) {
+        let latest_proposal_args = ListProposals {
             limit: 1,
-            before_proposal: Some(WrappedProposalId {
-                id: next_proposal_id + 1,
-            }),
+            before_proposal: None,
             exclude_type: Vec::new(),
             include_reward_status: Vec::new(),
             include_status: Vec::new(),
         };
 
-        let response = governance_clients::sns::list_proposals(governance_canister_id, &args).await;
-        handle_proposal_response(governance_canister_id, next_proposal_id, response);
+        let mut response = governance_clients::sns::list_proposals(governance_canister_id, &latest_proposal_args).await;
+        if let Some(latest_proposal_id) = response
+            .as_ref()
+            .ok()
+            .and_then(|p| p.iter().next())
+            .and_then(|p| p.id.as_ref())
+            .map(|p| p.id)
+        {
+            // Check if we have missed any proposals, if so, try to grab them
+            if latest_proposal_id > next_proposal_id {
+                let proposals_args = ListProposals {
+                    limit: (1 + latest_proposal_id - next_proposal_id) as u32,
+                    before_proposal: Some(WrappedProposalId {
+                        id: latest_proposal_id + 1,
+                    }),
+                    exclude_type: Vec::new(),
+                    include_reward_status: Vec::new(),
+                    include_status: Vec::new(),
+                };
+                response = governance_clients::sns::list_proposals(governance_canister_id, &proposals_args).await;
+            }
+        }
+
+        handle_proposals_response(governance_canister_id, next_proposal_id, response);
     }
 
-    fn handle_proposal_response<R: RawProposal>(
+    fn handle_proposals_response<R: RawProposal>(
         governance_canister_id: CanisterId,
-        next_proposal_id: u64,
+        mut next_proposal_id: u64,
         response: CallResult<Vec<R>>,
     ) {
         match response {
             Ok(response) => {
                 mutate_state(|state| {
-                    if let Some(raw_proposal) = response.into_iter().next() {
-                        if raw_proposal.id() == next_proposal_id {
+                    for raw_proposal in response.into_iter().rev() {
+                        let proposal_id = raw_proposal.id();
+                        if proposal_id >= next_proposal_id {
                             if !raw_proposal.is_excluded() {
                                 match raw_proposal.try_into() {
                                     Ok(proposal) => {
@@ -83,17 +124,17 @@ mod retrieve_proposals {
                                             .nervous_systems
                                             .enqueue_proposal(&governance_canister_id, proposal, false);
                                     }
-                                    Err(error) => {
-                                        error!(error, "Failed to transform proposal");
-                                    }
+                                    Err(error) => error!(error, "Failed to transform proposal"),
                                 }
                             }
-                            state
-                                .data
-                                .nervous_systems
-                                .set_next_proposal_id(&governance_canister_id, next_proposal_id + 1);
+                            next_proposal_id = proposal_id + 1;
                         }
                     }
+
+                    state
+                        .data
+                        .nervous_systems
+                        .set_next_proposal_id(&governance_canister_id, next_proposal_id);
 
                     let now = state.env.now();
                     state
