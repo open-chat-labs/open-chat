85256af8fac5b92cd5941834664e50716fdce074
Hamish Peebles <hamishpeebles@gmail.com>
2022-07-15 09:29:59 +0100

    Add script for deploying website

---
+++ scripts/deploy-website-prod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+IDENTITY=$1
+VERSION=$2
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+if ! cargo install --list | grep -Fq "icx-asset"
+then
+  cargo install icx-asset
+fi
+
+OPENCHAT_WEBSITE_VERSION=$VERSION npm run --prefix frontend build:prod
+
+dfx --identity $IDENTITY deploy --network ic --no-wallet website
+icx-asset --pem ~/.config/dfx/identity/$IDENTITY/identity.pem --replica https://ic0.app/ upload 6hsbt-vqaaa-aaaaf-aaafq-cai /.well-known/ii-alternative-origins=frontend/build/.well-known/ii-alternative-origins
+
