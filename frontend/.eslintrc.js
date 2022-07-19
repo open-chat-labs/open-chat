02970c8417294e5f8aab4f43b8ae3d2ef5dd2e95
Julian Jelfs <julian.jelfs@gmail.com>
2022-07-19 11:43:53 +0100

    just use eslintignore instead

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint", "prettier"],
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-    ignorePatters: ["public/*", "**/candid/*.ts"],
     rules: {
         "@typescript-eslint/no-explicit-any": ["error"],
         "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
