37f8482221961dc5de100ae8f6c46814b39272ae
Julian Jelfs <julian.jelfs@gmail.com>
2022-07-19 11:32:39 +0100

    ignore candid

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint", "prettier"],
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-    ignorePatterns: ["./public/*", "**/candid/*"],
+    ignorePatters: ["public/*", "**/candid/*.ts"],
     rules: {
         "@typescript-eslint/no-explicit-any": ["error"],
         "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
