108c164a641251b7a68cdd66c8bd5f9f54cfdea1
Hamish Peebles <hamishpeebles@gmail.com>
2022-07-19 10:40:03 +0100

    Use deterministic MessageId

--- backend/canisters/proposals_bot/impl/src/lifecycle/heartbeat.rs
+++ backend/canisters/proposals_bot/impl/src/lifecycle/heartbeat.rs
@@ -153,7 +153,7 @@ mod push_proposals {
     async fn push_proposal(governance_canister_id: CanisterId, chat_id: ChatId, proposal: Proposal) {
         let message_id = generate_message_id(governance_canister_id, proposal.id());
         let send_message_args = group_canister::send_message::Args {
-            message_id: mutate_state(|state| MessageId::generate(|| state.env.random_u32())),
+            message_id,
             thread_root_message_index: None,
             content: MessageContent::GovernanceProposal(ProposalContent {
                 governance_canister_id,
