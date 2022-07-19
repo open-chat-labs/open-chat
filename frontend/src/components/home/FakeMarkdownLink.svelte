3c8c710ca7e14934c760c2a82e98a45ac0e07197
Julian Jelfs <julian.jelfs@gmail.com>
2022-07-19 10:29:59 +0100

    change the approach to markdown parsing

---
+++ frontend/src/components/home/FakeMarkdownLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let title: string | null;
+    export let text: string;
+</script>
+
+<span class="fake-link" {title}>{text}</span>
+
+<style type="text/scss">
+    .fake-link {
+        text-decoration: none;
+    }
+</style>
--- frontend/src/components/home/Markdown.svelte
+++ frontend/src/components/home/Markdown.svelte
@@ -1,110 +1,56 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-    import { marked } from "marked";
-    import DOMPurify from "dompurify";
-    import { afterUpdate } from "svelte";
-    import { rollbar } from "../../utils/logging";
     import { userStore } from "../../stores/user";
     import { _ } from "svelte-i18n";
-    import { isAbsoluteUrl, synonymousUrlRegex } from "../../utils/urls";
+    import MarkdownLink from "./MarkdownLink.svelte";
+    import FakeMarkdownLink from "./FakeMarkdownLink.svelte";
+    import SvelteMarkdown from "svelte-markdown";
 
     export let text: string;
     export let inline: boolean = true;
     export let oneLine: boolean = false;
     export let suppressLinks: boolean = false;
 
-    let sanitized = "unsafe";
-    const options = {
-        breaks: !oneLine,
-    };
+    $: parsed = replaceUserIds(text);
 
-    type MentionToken = {
-        type: "mention";
-        raw: string;
-        username: string;
-        userId: string;
-    };
-
-    const mention = {
-        name: "mention",
-        level: "inline" as "inline",
-        start: (src: string) => {
-            return src.match(/@/)?.index ?? -1;
-        },
-        tokenizer: (src: string) => {
-            const rule = /^@UserId\(([\d\w-]+)\)/;
-            const match = rule.exec(src);
-            if (match) {
-                const username = $userStore[match[1]]?.username ?? $_("unknown");
-                return {
-                    type: "mention",
-                    raw: match[0],
-                    username,
-                    userId: match[1],
-                };
+    function replaceUserIds(text: string): string {
+        return text.replace(/@UserId\(([\d\w-]+)\)/g, (match, p1) => {
+            const u = $userStore[p1];
+            if (u !== undefined) {
+                return `**[@${u.username}](#/${u.userId}?type=direct)**`;
             }
-        },
-        renderer: (token: MentionToken) => {
-            return `<a href="#/${token.userId}?type=direct"><strong>@${token.username}</strong></a>`;
-        },
-        childTokens: ["strong"],
-    };
-
-    const renderer = {
-        link(href: string | null, title: string | null, text: string) {
-            if (suppressLinks || href === null) {
-                return `<span class="fake-link" ${title && `title=${title}`}>${text}</span>`;
-            } else {
-                let target = "";
-                // Check if the link is to a synonymous url (eg. https://oc.app), if so, convert it to a relative link
-                if (synonymousUrlRegex.test(href)) {
-                    href = href.replace(synonymousUrlRegex, "");
-                    if (href === "" || href === "/") {
-                        href = "/#";
-                    }
-                } else if (isAbsoluteUrl(href)) {
-                    target = 'target="_blank"';
-                }
-
-                return `<a href=${href} ${title && `title=${title}`} ${target}>${text}</a>`;
-            }
-        },
-    };
-
-    marked.use({ renderer, extensions: [mention] });
-
-    function render() {
-        let parsed = text;
-        try {
-            console.log("Rendering markdown: ", oneLine, suppressLinks, text)
-            parsed = marked.parseInline(text, options);
-        } catch (err: any) {
-            rollbar.error("Error parsing markdown: ", err);
-        }
-
-        try {
-            sanitized = DOMPurify.sanitize(parsed, {
-                ALLOWED_ATTR: ["target", "href", "class"],
-            });
-        } catch (err: any) {
-            rollbar.error("Error sanitzing message content: ", err);
-        }
+            return match;
+        });
     }
-
-    afterUpdate(render);
 </script>
 
 <p class="markdown-wrapper" class:inline class:oneLine>
-    {@html sanitized}
+    {#if suppressLinks}
+        <SvelteMarkdown
+            renderers={{
+                link: FakeMarkdownLink,
+            }}
+            isInline={true}
+            source={parsed}
+            options={{
+                breaks: !oneLine,
+            }} />
+    {:else}
+        <SvelteMarkdown
+            renderers={{
+                link: MarkdownLink,
+            }}
+            isInline={true}
+            source={parsed}
+            options={{
+                breaks: !oneLine,
+            }} />
+    {/if}
 </p>
 
 <style type="text/scss">
     :global(.markdown-wrapper a) {
-        text-decoration: underline;
-    }
-
-    :global(.markdown-wrapper .fake-link) {
         text-decoration: underline;
     }
 
