a22c6571bb9fac8100eeae101003e0c137ab99c2
megrogan <megrogan@gmail.com>
2022-07-26 08:30:16 +0100

    De-dup time store

--- frontend/src/stores/time.ts
+++ frontend/src/stores/time.ts
@@ -1,25 +1,19 @@
 import { readable } from "svelte/store";
 
+function intervalStore(duration: number) {
+    return readable(Date.now(), (set) => {
+        const interval = window.setInterval(() => {
+            set(Date.now());
+        }, duration);
+
+        return function stop() {
+            window.clearInterval(interval);
+        };
+    });
+}
+
 // this is a fairly arbitrary interval but it seems about right
-const INTERVAL = 5000;
-
-export const now = readable(Date.now(), (set) => {
-    const interval = window.setInterval(() => {
-        set(Date.now());
-    }, INTERVAL);
-
-    return function stop() {
-        window.clearInterval(interval);
-    };
-});
+export const now = intervalStore(5000);
 
 // a more fine-grained "now" used for updating a seconds counter
-export const now500 = readable(Date.now(), (set) => {
-    const interval = window.setInterval(() => {
-        set(Date.now());
-    }, 500);
-
-    return function stop() {
-        window.clearInterval(interval);
-    };
-});
+export const now500 = intervalStore(500);
