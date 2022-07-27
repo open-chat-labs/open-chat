559cb4b1e0d0939df73686dda368538a57304398
Hamish Peebles <hamishpeebles@gmail.com>
2022-07-27 15:10:52 +0100

    Log whenever `max_concurrent_canister_upgrades` is updated

--- backend/canisters/group_index/impl/src/updates/set_max_concurrent_canister_upgrades.rs
+++ backend/canisters/group_index/impl/src/updates/set_max_concurrent_canister_upgrades.rs
@@ -3,6 +3,7 @@ use crate::{mutate_state, RuntimeState};
 use canister_tracing_macros::trace;
 use group_index_canister::set_max_concurrent_canister_upgrades::{Response::*, *};
 use ic_cdk_macros::update;
+use tracing::info;
 
 #[update(guard = "caller_is_controller")]
 #[trace]
@@ -12,5 +13,6 @@ async fn set_max_concurrent_canister_upgrades(args: Args) -> Response {
 
 fn set_max_concurrent_canister_upgrades_impl(args: Args, runtime_state: &mut RuntimeState) -> Response {
     runtime_state.data.max_concurrent_canister_upgrades = args.value;
+    info!("Max concurrent canister upgrades set to {}", args.value);
     Success
 }
