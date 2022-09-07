b04c4b58d554334f6d0b7247735baeba42bb69a5
Hamish Peebles <hamishpeebles@gmail.com>
2022-09-07 11:31:02 +0100

    Add `latest_client_event_index` to `thread_previews`

--- backend/canisters/group/api/src/queries/thread_previews.rs
+++ backend/canisters/group/api/src/queries/thread_previews.rs
@@ -1,21 +1,24 @@
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
-use types::{EventWrapper, Message, MessageIndex};
+use types::{EventIndex, EventWrapper, Message, MessageIndex};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub threads: Vec<MessageIndex>,
+    pub latest_client_event_index: Option<EventIndex>,
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub enum Response {
     Success(SuccessResult),
     CallerNotInGroup,
+    ReplicaNotUpToDate(EventIndex),
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct SuccessResult {
     pub threads: Vec<ThreadPreview>,
+    pub latest_event_index: EventIndex,
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
