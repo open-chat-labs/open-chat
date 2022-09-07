454c99f47c72e30290ec344e2b8c57e95e1b346d
Hamish Peebles <hamishpeebles@gmail.com>
2022-09-07 14:39:25 +0100

    Bump cache version

--- frontend/src/utils/caching.ts
+++ frontend/src/utils/caching.ts
@@ -21,7 +21,7 @@ import type { UserSummary } from "../domain/user/user";
 import { rollbar } from "./logging";
 import { UnsupportedValueError } from "./error";
 
-const CACHE_VERSION = 37;
+const CACHE_VERSION = 38;
 
 export type Database = Promise<IDBPDatabase<ChatSchema>>;
 
