beacdbf7a6b576f2e21f233d4666c5602b0a4d03
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-02 21:37:36 +0100

    Update dfx to version 0.12.0-beta.3

--- frontend/build_dev.sh
+++ frontend/build_dev.sh
@@ -1,12 +1,12 @@
 WATCH=$1
 
-# export INTERNET_IDENTITY_URL=http://localhost:8080?canisterId=qaa6y-5yaaa-aaaaa-aaafa-cai
-export INTERNET_IDENTITY_URL=http://127.0.0.1:8080?canisterId=qaa6y-5yaaa-aaaaa-aaafa-cai
-export NFID_URL=http://localhost:8080?canisterId=qaa6y-5yaaa-aaaaa-aaafa-cai
+# export INTERNET_IDENTITY_URL=http://localhost:8080?canisterId=qhbym-qaaaa-aaaaa-aaafq-cai
+export INTERNET_IDENTITY_URL=http://127.0.0.1:8080?canisterId=qhbym-qaaaa-aaaaa-aaafq-cai
+export NFID_URL=http://localhost:8080?canisterId=qhbym-qaaaa-aaaaa-aaafq-cai
 export DFX_NETWORK=local
 export DEV_PORT=5001
 export CLIENT_CACHING=true
-export OPEN_STORAGE_INDEX_CANISTER=si2b5-pyaaa-aaaaa-aaaja-cai
+export OPEN_STORAGE_INDEX_CANISTER=sp3hj-caaaa-aaaaa-aaajq-cai
 export LEDGER_CANISTER_ICP=ryjl3-tyaaa-aaaaa-aaaba-cai
 export LEDGER_CANISTER_BTC=ryjl3-tyaaa-aaaaa-aaaba-cai
 export LEDGER_CANISTER_CHAT=ryjl3-tyaaa-aaaaa-aaaba-cai
