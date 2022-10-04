3a99b3270a616fcfe180c94dbbfa3209a03c68c7
megrogan <megrogan@gmail.com>
2022-10-04 15:21:49 +0100

    Comment change

--- backend/libraries/utils/src/case_insensitive_hash_map.rs
+++ backend/libraries/utils/src/case_insensitive_hash_map.rs
@@ -36,7 +36,7 @@ impl<V> CaseInsensitiveHashMap<V> {
         self.len() == 0
     }
 
-    // Returns an iterator over a tuple with the matching item and whether it is a prefix match
+    // Returns an iterator containing a tuple with the matching item and whether it is a prefix match
     pub fn search(&self, term: &str) -> impl Iterator<Item = (&V, bool)> {
         let term_uppercase = term.to_uppercase();
         self.map
