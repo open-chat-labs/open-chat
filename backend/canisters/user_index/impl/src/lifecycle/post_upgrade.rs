dffe01339dcf6f3571db4ada6bda885e94996eb4
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-05 14:06:24 +0100

    One time job to register the ProposalsBot

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,5 +1,5 @@
 use crate::lifecycle::{init_logger, init_state, UPGRADE_BUFFER_SIZE};
-use crate::{Data, LOG_MESSAGES};
+use crate::{Data, Principal, LOG_MESSAGES};
 use canister_logger::{LogMessage, LogMessagesWrapper};
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
@@ -7,6 +7,7 @@ use stable_memory::deserialize_from_stable_memory;
 use tracing::info;
 use user_index_canister::post_upgrade::Args;
 use utils::env::canister::CanisterEnv;
+use utils::env::Environment;
 
 #[post_upgrade]
 #[trace]
@@ -19,6 +20,18 @@ fn post_upgrade(args: Args) {
         deserialize_from_stable_memory(UPGRADE_BUFFER_SIZE).unwrap();
 
     data.users.rehydrate();
+
+    let proposals_bot = Principal::from_text("iywa7-ayaaa-aaaaf-aemga-cai").unwrap();
+
+    data.users.register(
+        proposals_bot,
+        proposals_bot.into(),
+        data.user_canister_wasm.version,
+        "ProposalsBot".to_string(),
+        env.now(),
+        None,
+        true,
+    );
 
     init_logger(data.test_mode);
     init_state(env, data, args.wasm_version);
