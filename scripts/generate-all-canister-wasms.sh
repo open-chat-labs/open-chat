ae700bbf912635b67b74cb83b4f5c628595ee174
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-18 17:30:24 +0100

    Bring `integration_tests` into the main workspace (#2551)

---
+++ scripts/generate-all-canister-wasms.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+./scripts/generate-wasm.sh callback_canister_impl
+./scripts/generate-wasm.sh group_canister_impl
+./scripts/generate-wasm.sh group_index_canister_impl
+./scripts/generate-wasm.sh notifications_canister_impl
+./scripts/generate-wasm.sh online_users_aggregator_canister_impl
+./scripts/generate-wasm.sh proposals_bot_canister_impl
+./scripts/generate-wasm.sh user_canister_impl
+./scripts/generate-wasm.sh user_index_canister_impl
