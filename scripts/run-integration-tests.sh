529ca7448bbe0e862935ecbd5412d29b652b7282
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-18 17:07:10 +0100

    Allow passing in the number of test threads

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -5,10 +5,11 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 cd $SCRIPT_DIR/..
 
 BUILD_WASMS=${1:-true}
+TEST_THREADS=${2:-2}
 
 if [ $BUILD_WASMS = true ]
 then
     ./scripts/generate-all-canister-wasms.sh
 fi
 
-cargo test --release --package integration_tests -- --test-threads 2
+cargo test --release --package integration_tests -- --test-threads $TEST_THREADS
