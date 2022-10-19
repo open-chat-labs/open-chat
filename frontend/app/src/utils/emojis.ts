7d6ab49c30af45a60e3824aa5cb7513d0e232cea
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-19 18:23:58 +0100

    Exit early for empty strings (#2563)

--- frontend/app/src/utils/emojis.ts
+++ frontend/app/src/utils/emojis.ts
@@ -3,5 +3,5 @@ import { Database } from "emoji-picker-element";
 export const emojiDatabase = new Database();
 
 export async function isSingleEmoji(text: string): Promise<boolean> {
-    return (await emojiDatabase.getEmojiByUnicodeOrName(text)) !== null;
+    return text.length > 0 && (await emojiDatabase.getEmojiByUnicodeOrName(text)) !== null;
 }
