9b6957f41193511894fd830f45a072d31889bdea
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-19 15:53:42 +0100

    Exclude single digits from being counted as emojis

--- frontend/app/src/utils/emojis.ts
+++ frontend/app/src/utils/emojis.ts
@@ -1,9 +1,10 @@
 import { Database } from "emoji-picker-element";
 
 const emojiRegex = /^\p{Emoji}$/u;
+const digitRegex = /^\d$/;
 
 export const emojiDatabase = new Database();
 
 export function isSingleEmoji(text: string): boolean {
-    return emojiRegex.test(text);
+    return emojiRegex.test(text) && !digitRegex.test(text);
 }
