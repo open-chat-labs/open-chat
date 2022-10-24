d86b08c45b93c2b25f0be9ebd9f29c30d7b9254a
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-24 19:12:50 +0100

    Add scripts for deploying to testnets

---
+++ scripts/deploy-testnet.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+NETWORK=$1
+IC_URL=$2
+IDENTITY=${3:-default}
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+LEDGER_CANISTER_ID=ryjl3-tyaaa-aaaaa-aaaba-cai
+
+# Create the OpenChat canisters
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 user_index
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 group_index
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 notifications
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 online_users_aggregator
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 callback
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 proposals_bot
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 cycles_dispenser
+dfx --identity $IDENTITY canister create --network $NETWORK --no-wallet --with-cycles 100000000000000 open_storage_index
+
+# Install the OpenChat canisters
+./scripts/deploy.sh $NETWORK $IC_URL $IDENTITY $LEDGER_CANISTER_ID true
--- scripts/deploy-website-web-test.sh
+++ scripts/deploy-website-web-test.sh
@@ -4,12 +4,12 @@
 # eg './deploy-website-web-test.sh openchat 2.0.305'
 
 IDENTITY=$1
-VERSION=$2
+export OPENCHAT_WEBSITE_VERSION=$2
 
 SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 cd $SCRIPT_DIR/..
 
-OPENCHAT_WEBSITE_VERSION=$VERSION npm run --prefix frontend build:prod
+npm run --prefix frontend build:prod
 
 dfx --identity $IDENTITY deploy --network web_test --no-wallet website
