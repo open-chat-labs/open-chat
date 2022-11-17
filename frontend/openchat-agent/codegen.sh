53aa18691d1c2ebb84c1a646aeea55897e51a32c
Hamish Peebles <hamishpeebles@gmail.com>
2022-11-17 15:58:51 +0000

    Fix `codegen.sh` (#2683)

--- frontend/openchat-agent/codegen.sh
+++ frontend/openchat-agent/codegen.sh
@@ -1,20 +1,20 @@
-didc bind ../backend/canisters/user_index/api/can.did -t ts > ./src/services/userIndex/candid/types.d.ts
-didc bind ../backend/canisters/user_index/api/can.did -t js > ./src/services/userIndex/candid/idl.js
+didc bind ../../backend/canisters/user_index/api/can.did -t ts > ./src/services/userIndex/candid/types.d.ts
+didc bind ../../backend/canisters/user_index/api/can.did -t js > ./src/services/userIndex/candid/idl.js
 
-didc bind ../backend/canisters/user/api/can.did -t ts > ./src/services/user/candid/types.d.ts
-didc bind ../backend/canisters/user/api/can.did -t js > ./src/services/user/candid/idl.js
+didc bind ../../backend/canisters/user/api/can.did -t ts > ./src/services/user/candid/types.d.ts
+didc bind ../../backend/canisters/user/api/can.did -t js > ./src/services/user/candid/idl.js
 
-didc bind ../backend/canisters/group/api/can.did -t ts > ./src/services/group/candid/types.d.ts
-didc bind ../backend/canisters/group/api/can.did -t js > ./src/services/group/candid/idl.js
+didc bind ../../backend/canisters/group/api/can.did -t ts > ./src/services/group/candid/types.d.ts
+didc bind ../../backend/canisters/group/api/can.did -t js > ./src/services/group/candid/idl.js
 
-didc bind ../backend/canisters/group_index/api/can.did -t ts > ./src/services/groupIndex/candid/types.d.ts
-didc bind ../backend/canisters/group_index/api/can.did -t js > ./src/services/groupIndex/candid/idl.js
+didc bind ../../backend/canisters/group_index/api/can.did -t ts > ./src/services/groupIndex/candid/types.d.ts
+didc bind ../../backend/canisters/group_index/api/can.did -t js > ./src/services/groupIndex/candid/idl.js
 
-didc bind ../backend/canisters/notifications/api/can.did -t ts > ./src/services/notifications/candid/types.d.ts
-didc bind ../backend/canisters/notifications/api/can.did -t js > ./src/services/notifications/candid/idl.js
+didc bind ../../backend/canisters/notifications/api/can.did -t ts > ./src/services/notifications/candid/types.d.ts
+didc bind ../../backend/canisters/notifications/api/can.did -t js > ./src/services/notifications/candid/idl.js
 
-didc bind ../backend/canisters/online_users_aggregator/api/can.did -t ts > ./src/services/online/candid/types.d.ts
-didc bind ../backend/canisters/online_users_aggregator/api/can.did -t js > ./src/services/online/candid/idl.js
+didc bind ../../backend/canisters/online_users_aggregator/api/can.did -t ts > ./src/services/online/candid/types.d.ts
+didc bind ../../backend/canisters/online_users_aggregator/api/can.did -t js > ./src/services/online/candid/idl.js
 
 didc bind ./src/services/ledger/candid/can.did -t ts > ./src/services/ledger/candid/types.d.ts
 didc bind ./src/services/ledger/candid/can.did -t js > ./src/services/ledger/candid/idl.js
