ac23b3631e2d8431ac3c2ae858b2240f7d71e93a
Hamish Peebles <hamishpeebles@gmail.com>
2022-11-22 17:03:27 +0000

    Revert "Enable new joiner rewards for the ICP Party group" (#2705)

--- backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
@@ -1,17 +1,13 @@
 use crate::lifecycle::{init_logger, init_state, UPGRADE_BUFFER_SIZE};
-use crate::{Data, NewJoinerRewards, LOG_MESSAGES};
-use candid::Principal;
+use crate::{Data, LOG_MESSAGES};
 use canister_logger::{LogMessage, LogMessagesWrapper};
 use canister_tracing_macros::trace;
 use group_canister::post_upgrade::Args;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::deserialize_from_stable_memory;
 use tracing::info;
-use types::ICP;
 use utils::env::canister::CanisterEnv;
 use utils::env::Environment;
-
-const E8S_PER_TOKEN: u64 = 100_000_000;
 
 #[post_upgrade]
 #[trace]
@@ -22,17 +18,6 @@ fn post_upgrade(args: Args) {
 
     let (mut data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
         deserialize_from_stable_memory(UPGRADE_BUFFER_SIZE).unwrap();
-
-    let icp_party_private_group = Principal::from_text("5akog-wqaaa-aaaaf-avpjq-cai").unwrap();
-    if env.canister_id() == icp_party_private_group && data.new_joiner_rewards.is_none() {
-        let start = 1668018600000; // 9th November at 16:30 GMT
-        data.new_joiner_rewards = Some(NewJoinerRewards::new(
-            100,
-            ICP::from_e8s(5 * E8S_PER_TOKEN),
-            Some(start),
-            None,
-        ));
-    }
 
     data.events.end_overdue_polls(env.now());
 
