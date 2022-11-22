e33dff9185b068d98e5934d0d64a46bad2316c1a
Hamish Peebles <hamishpeebles@gmail.com>
2022-11-22 14:09:58 +0000

    Revert "Append "SCAM!" to usernames of known scammers" (#2694)

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,11 +1,10 @@
 use crate::lifecycle::{init_logger, init_state, UPGRADE_BUFFER_SIZE};
-use crate::{mutate_state, Data, LOG_MESSAGES};
+use crate::{Data, LOG_MESSAGES};
 use canister_logger::{LogMessage, LogMessagesWrapper};
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::deserialize_from_stable_memory;
 use tracing::info;
-use types::{UserEvent, UsernameChanged};
 use user_index_canister::post_upgrade::Args;
 use utils::consts::MIN_CYCLES_BALANCE;
 use utils::env::canister::CanisterEnv;
@@ -35,28 +34,6 @@ fn post_upgrade(args: Args) {
 
     cycles_dispenser_client::init_from_bytes(&cycles_dispenser_client_state);
     cycles_dispenser_client::set_min_cycles_balance(3 * MIN_CYCLES_BALANCE / 2);
-
-    mutate_state(|state| {
-        let scam_usernames = ["SNS1Bot", "SNS1_B0T"];
-        let now = state.env.now();
-        for username in scam_usernames {
-            if let Some(user) = state.data.users.get_by_username(username) {
-                info!(?user, "Updating user");
-                let user_id = user.user_id;
-
-                let mut clone = user.clone();
-                let new_username = clone.username + "_(SCAM!)";
-                clone.username = new_username.clone();
-                clone.date_updated = now;
-
-                state.data.users.update(clone);
-                state.data.user_event_sync_queue.push(
-                    user_id,
-                    UserEvent::UsernameChanged(UsernameChanged { username: new_username }),
-                );
-            }
-        }
-    });
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
 }
