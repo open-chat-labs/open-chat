b6ae91bdce58319624223cd44fae060fbc5ff4e8
Matt Grogan <megrogan@gmail.com>
2022-11-23 16:00:05 +0000

    Return Message from undelete_message (#2709)

--- backend/canisters/group/api/src/updates/undelete_messages.rs
+++ backend/canisters/group/api/src/updates/undelete_messages.rs
@@ -1,6 +1,6 @@
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
-use types::{MessageId, MessageIndex};
+use types::{Message, MessageId, MessageIndex};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
@@ -11,9 +11,14 @@ pub struct Args {
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub enum Response {
-    Success,
+    Success(SuccessResult),
     CallerNotInGroup,
     MessageNotFound,
     UserSuspended,
     ChatFrozen,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct SuccessResult {
+    pub messages: Vec<Message>,
 }
