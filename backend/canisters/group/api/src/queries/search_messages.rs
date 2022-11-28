a3117bc70f254f8c6eb46cb44ce1bfd692868c01
Matt Grogan <megrogan@gmail.com>
2022-11-28 15:12:39 +0000

    Support searching chat messages by sender (#2713)

--- backend/canisters/group/api/src/queries/search_messages.rs
+++ backend/canisters/group/api/src/queries/search_messages.rs
@@ -1,11 +1,12 @@
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
-use types::MessageMatch;
+use types::{MessageMatch, UserId};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub search_term: String,
     pub max_results: u8,
+    pub users: Option<Vec<UserId>>,
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
@@ -14,6 +15,7 @@ pub enum Response {
     InvalidTerm,
     TermTooLong(u8),
     TermTooShort(u8),
+    TooManyUsers(u8),
     CallerNotInGroup,
 }
 
