c894455dad586fa6c3e55c822ad7902933084411
Julian Jelfs <julian.jelfs@gmail.com>
2022-11-29 13:41:08 +0000

    don't use hex with alpha notation

--- frontend/app/src/theme/themes.ts
+++ frontend/app/src/theme/themes.ts
@@ -13,6 +13,18 @@ import { derived, readable, writable } from "svelte/store";
 // const purpleTo = "#5f2583";
 // const purpleGradient = `linear-gradient(${purpleFrom}, ${purpleTo})`;
 
+export function hexPercent(hex: string, alpha: number | undefined): string {
+    const r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha !== undefined) {
+        return `rgba(${r}, ${g}, ${b}, ${alpha / 100})`;
+    } else {
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+}
+
 export interface Theme {
     name: string;
     label: string;
