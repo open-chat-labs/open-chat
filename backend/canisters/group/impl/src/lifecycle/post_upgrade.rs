90cd8f7c90188a26daf8bed69ff2f9906d542827
Hamish Peebles <hamishpeebles@gmail.com>
2022-11-29 16:38:18 +0000

    Revert "Recalculate number of messages deleted"

--- backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
@@ -19,7 +19,6 @@ fn post_upgrade(args: Args) {
     let (mut data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
         deserialize_from_stable_memory(UPGRADE_BUFFER_SIZE).unwrap();
 
-    data.events.recalculate_messages_deleted();
     data.events.end_overdue_polls(env.now());
 
     init_logger(data.test_mode);
