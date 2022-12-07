a5f41a56f047f9bf8a5b805032a1d8d3d25f22f2
Hamish Peebles <hamishpeebles@gmail.com>
2022-12-07 10:35:25 +0000

    clippy

--- backend/canisters/group/impl/src/regular_jobs.rs
+++ backend/canisters/group/impl/src/regular_jobs.rs
@@ -1,7 +1,7 @@
 use crate::Data;
 use utils::env::Environment;
 use utils::regular_jobs::{RegularJob, RegularJobs};
-use utils::time::{HOUR_IN_MS, MINUTE_IN_MS};
+use utils::time::MINUTE_IN_MS;
 
 pub(crate) fn build() -> RegularJobs<Data> {
     let check_cycles_balance = RegularJob::new("Check cycles balance", check_cycles_balance, 30 * MINUTE_IN_MS);
