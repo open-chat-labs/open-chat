bd1563e40d189faeac676d78b28291468e721618
megrogan <megrogan@gmail.com>
2022-12-12 16:04:16 +0000

    Typo in comment

--- backend/canisters/local_group_index/api/src/lifecycle/init.rs
+++ backend/canisters/local_group_index/api/src/lifecycle/init.rs
@@ -4,7 +4,7 @@ use types::{CanisterId, CanisterWasm, Version};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
-    // The wasm module for creating user canisters
+    // The wasm module for creating group canisters
     pub group_canister_wasm: CanisterWasm,
 
     // The wasm version running on this canister
