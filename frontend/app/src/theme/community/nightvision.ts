dcbef8c5a01404c540a37398a39e4cb10a79b344
Julian Jelfs <julian.jelfs@gmail.com>
2022-12-12 09:34:05 +0000

    Theme fixes (#2787)

--- frontend/app/src/theme/community/nightvision.ts
+++ frontend/app/src/theme/community/nightvision.ts
@@ -4,6 +4,7 @@ import { hexPercent } from "../themes";
 const green = "#0d8329";
 const txtLight = "#0d8329ba";
 const txt = green;
+const componentBg = "#151f19";
 
 export function getTheme(base: Theme): Theme {
     // it's ok to mutate the theme passed in because it's a clone
@@ -15,6 +16,10 @@ export function getTheme(base: Theme): Theme {
     base.txt = txt;
     base["txt-light"] = txtLight;
     base.icon.txt = green;
+    base.button.bg = "#084a16";
+    base.button.hv = "#094a17";
+    base.button.disabled = componentBg;
+    base.button.txt = "rgba(255 255 255 / 80%)";
     base.collapsible.closed.header.txt = txt;
     base.collapsible.open.header.arrow = green;
     base.accent = "#a9be54";
@@ -24,21 +29,17 @@ export function getTheme(base: Theme): Theme {
     base.modal.bg = base.panel.right.modal;
     base.chatSummary["bg-selected"] = "rgb(39 65 39 / 40%)";
     base.chatSummary.hv = "rgb(39 65 39 / 30%)";
+    base.chatSummary.del = base.button.bg;
     base.members.hv = base.chatSummary.hv;
     base.placeholder = txtLight;
-    base.entry.input.bg = "#151f19";
-    base.input.bg = "#151f19";
-    base.chatSearch.bg = "#151f19";
-    base.button.bg = "#084a16";
-    base.button.hv = "#094a17";
-    base.button.disabled = "#151f19";
-    base.button.txt = "rgba(255 255 255 / 80%)";
+    base.entry.input.bg = componentBg;
+    base.input.bg = componentBg;
+    base.chatSearch.bg = componentBg;
     base.currentChat.msg.bg = "#243427";
     base.currentChat.msg.me.bg = hexPercent(base.button.bg, 70);
     base.currentChat.msg.txt = "rgba(255 255 255 / 50%)";
     base.currentChat.date.bg = base.currentChat.msg.bg;
     base.currentChat.msg.inert = base.currentChat.msg.bg;
-    base.currentChat.msg.me.bg = hexPercent(base.button.bg, 70);
     base.notificationBar.bg = "#04621f";
     base.notificationBar.txt = "rgba(255 255 255 / 50%)";
     base.timeline.txt = base.notificationBar.txt;
@@ -52,6 +53,7 @@ export function getTheme(base: Theme): Theme {
     base.primary = green;
     base.link.underline = base.primary;
     base.scrollbar.bg = base.button.hv;
+    base.toast.success.bg = base.notificationBar.bg;
 
     return base;
 }
--- frontend/app/src/theme/community/submarine.ts
+++ frontend/app/src/theme/community/submarine.ts
@@ -4,6 +4,7 @@ import { hexPercent } from "../themes";
 const red100 = "#ae1111";
 const txtLight = "#922222";
 const txt = red100;
+const componentBg = "#2a1e1e";
 
 export function getTheme(base: Theme): Theme {
     // it's ok to mutate the theme passed in because it's a clone
@@ -15,6 +16,10 @@ export function getTheme(base: Theme): Theme {
     base.txt = txt;
     base["txt-light"] = txtLight;
     base.icon.txt = red100;
+    base.button.bg = "#7c0a0a";
+    base.button.hv = "#5b0505";
+    base.button.txt = "rgba(255 255 255 / 80%)";
+    base.button.disabled = componentBg;
     base.collapsible.closed.header.txt = txt;
     base.collapsible.open.header.arrow = red100;
     base.accent = "#87441c";
@@ -24,15 +29,12 @@ export function getTheme(base: Theme): Theme {
     base.modal.bg = base.panel.right.modal;
     base.chatSummary["bg-selected"] = "rgb(64 55 55 / 40%)";
     base.chatSummary.hv = "rgb(64 55 55 / 30%)";
+    base.chatSummary.del = base.button.bg;
     base.members.hv = base.chatSummary.hv;
     base.placeholder = txtLight;
-    base.entry.input.bg = "#2a1e1e";
-    base.input.bg = "#2a1e1e";
-    base.chatSearch.bg = "#2a1e1e";
-    base.button.bg = "#7c0a0a";
-    base.button.hv = "#5b0505";
-    base.button.txt = "rgba(255 255 255 / 80%)";
-    base.button.disabled = "#2a1e1e";
+    base.entry.input.bg = componentBg;
+    base.input.bg = componentBg;
+    base.chatSearch.bg = componentBg;
     base.currentChat.msg.bg = "#342424";
     base.currentChat.msg.me.bg = hexPercent(base.button.bg, 70);
     base.currentChat.msg.txt = "rgba(255 255 255 / 50%)";
@@ -51,6 +53,7 @@ export function getTheme(base: Theme): Theme {
     base.primary = red100;
     base.link.underline = base.primary;
     base.scrollbar.bg = base.button.hv;
+    base.toast.success.bg = base.notificationBar.bg;
 
     return base;
 }
