87b60ee0a1b3a9c3799dc5ff2bafa469a837446a
Hamish Peebles <hamishpeebles@gmail.com>
2022-12-23 15:58:48 +0000

    Use local user index as the authorizer

--- backend/canisters/user/impl/src/lib.rs
+++ backend/canisters/user/impl/src/lib.rs
@@ -85,7 +85,7 @@ impl RuntimeState {
         if let Some(canister_id) = get_random_item(&self.data.notifications_canister_ids, random) {
             let args = c2c_push_notification::Args {
                 recipients,
-                authorizer: Some(self.data.user_index_canister_id),
+                authorizer: Some(self.data.local_user_index_canister_id),
                 notification_bytes: candid::encode_one(notification).unwrap(),
             };
             ic_cdk::spawn(push_notification_inner(*canister_id, args));
