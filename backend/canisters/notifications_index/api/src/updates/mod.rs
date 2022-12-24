638574a1b38d7c72d89f5a29e439b222b2366221
Hamish Peebles <hamishpeebles@gmail.com>
2022-12-24 11:05:28 +0000

    Add notifications functionality to `notifications_index`

--- backend/canisters/notifications_index/api/src/updates/mod.rs
+++ backend/canisters/notifications_index/api/src/updates/mod.rs
@@ -1,6 +1,8 @@
 pub mod add_notifications_canister;
+pub mod c2c_push_notification_v2;
 pub mod c2c_update_user_principal;
 pub mod push_subscription;
+pub mod remove_notifications;
 pub mod remove_subscription;
 pub mod remove_subscriptions;
 pub mod remove_subscriptions_for_user;
