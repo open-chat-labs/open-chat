e00b9dbe2ad5753ed9814d420677902702b97b59
megrogan <megrogan@gmail.com>
2022-12-28 13:53:35 +0000

    Remove proposals_bot user from local_user_index

--- backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,11 +1,13 @@
 use crate::lifecycle::{init_logger, init_state, UPGRADE_BUFFER_SIZE};
 use crate::{Data, LOG_MESSAGES};
+use candid::Principal;
 use canister_logger::{LogMessage, LogMessagesWrapper};
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use local_user_index_canister::post_upgrade::Args;
 use stable_memory::deserialize_from_stable_memory;
 use tracing::info;
+use types::UserId;
 use utils::consts::OPENCHAT_BOT_USER_ID;
 use utils::cycles::init_cycles_dispenser_client;
 use utils::env::canister::CanisterEnv;
@@ -20,7 +22,7 @@ fn post_upgrade(args: Args) {
     let (mut data, log_messages, trace_messages): (Data, Vec<LogMessage>, Vec<LogMessage>) =
         deserialize_from_stable_memory(UPGRADE_BUFFER_SIZE).unwrap();
 
-    remove_openchat_bot(&mut data);
+    remove_bot_users(&mut data);
 
     init_logger(data.test_mode);
     init_cycles_dispenser_client(data.cycles_dispenser_canister_id);
@@ -33,9 +35,13 @@ fn post_upgrade(args: Args) {
     info!(version = %args.wasm_version, "Post-upgrade complete");
 }
 
-fn remove_openchat_bot(data: &mut Data) {
+fn remove_bot_users(data: &mut Data) {
     data.local_users.remove(&OPENCHAT_BOT_USER_ID);
     data.global_users.set_bot(OPENCHAT_BOT_USER_ID);
+
+    let proposals_bot_user_id: UserId = Principal::from_text("iywa7-ayaaa-aaaaf-aemga-cai").unwrap().into();
+    data.local_users.remove(&proposals_bot_user_id);
+    data.global_users.set_bot(proposals_bot_user_id);
 }
 
 fn rehydrate_log_messages(
