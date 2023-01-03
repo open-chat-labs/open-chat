c52b550d4200c66e1f3c2033e89ff459a67a0c18
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-03 11:53:46 +0000

    Use `management_canister` from the cdk

--- backend/canisters/local_user_index/impl/src/updates/c2c_notify_low_balance.rs
+++ backend/canisters/local_user_index/impl/src/updates/c2c_notify_low_balance.rs
@@ -3,8 +3,9 @@ use crate::{mutate_state, read_state, RuntimeState, USER_CANISTER_TOP_UP_AMOUNT}
 use canister_api_macros::update_msgpack;
 use canister_tracing_macros::trace;
 use types::{CyclesTopUp, NotifyLowBalanceArgs, NotifyLowBalanceResponse, UserId};
+use utils::canister::deposit_cycles;
 use utils::consts::MIN_CYCLES_BALANCE;
-use utils::cycles::{can_spend_cycles, top_up_canister};
+use utils::cycles::can_spend_cycles;
 
 #[update_msgpack(guard = "caller_is_local_user_canister")]
 #[trace]
@@ -15,7 +16,7 @@ async fn c2c_notify_low_balance(_args: NotifyLowBalanceArgs) -> NotifyLowBalance
     };
     let amount = prepare_ok.top_up.amount;
 
-    if top_up_canister(prepare_ok.user_id.into(), amount).await.is_ok() {
+    if deposit_cycles(prepare_ok.user_id.into(), amount).await.is_ok() {
         mutate_state(|state| commit(prepare_ok.user_id, prepare_ok.top_up, state));
         NotifyLowBalanceResponse::Success(amount)
     } else {
--- backend/libraries/utils/src/canister/stop.rs
+++ backend/libraries/utils/src/canister/stop.rs
@@ -1,26 +1,18 @@
-use crate::canister;
-use candid::{CandidType, Principal};
-use ic_cdk::api;
-use serde::{Deserialize, Serialize};
+use ic_cdk::api::call::CallResult;
+use ic_cdk::api::management_canister;
+use ic_cdk::api::management_canister::main::CanisterIdRecord;
 use tracing::error;
 use types::CanisterId;
 
-pub async fn stop(canister_id: CanisterId) -> Result<(), canister::Error> {
-    #[derive(CandidType, Serialize, Deserialize)]
-    struct StopArgs {
-        canister_id: Principal,
-    }
-
-    let stop_args = StopArgs { canister_id };
-
-    if let Err((code, msg)) = api::call::call::<_, ()>(Principal::management_canister(), "stop_canister", (stop_args,)).await {
-        error!(
-            error_code = code as u8,
-            error_message = msg.as_str(),
-            "Error calling stop_canister"
-        );
-        return Err(canister::Error { code, msg });
-    };
-
-    Ok(())
+pub async fn stop(canister_id: CanisterId) -> CallResult<()> {
+    management_canister::main::stop_canister(CanisterIdRecord { canister_id })
+        .await
+        .map_err(|(code, msg)| {
+            error!(
+                error_code = code as u8,
+                error_message = msg.as_str(),
+                "Error calling stop_canister"
+            );
+            (code, msg)
+        })
 }
