e92788ab68512a2d2f2f5c15bff823e720c3fe3f
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-06 17:07:24 +0000

    Remove code to retry existing failed messages

--- backend/canisters/user/impl/src/model/failed_messages_pending_retry.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use types::UserId;
-use user_canister::c2c_send_message;
-
-#[derive(Serialize, Deserialize, Default)]
-pub struct FailedMessagesPendingRetry {
-    pub messages: HashMap<UserId, Vec<c2c_send_message::Args>>,
-}
