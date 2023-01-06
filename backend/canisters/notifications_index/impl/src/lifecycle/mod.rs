67ed0ddaee67af768afca344dbe7e63ea4039c5d
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-06 16:15:12 +0000

    Start timer jobs after delay to ensure canister is restarted

--- backend/canisters/notifications_index/impl/src/lifecycle/mod.rs
+++ backend/canisters/notifications_index/impl/src/lifecycle/mod.rs
@@ -1,4 +1,5 @@
-use crate::{init_state as set_state, Data, RuntimeState, LOG_MESSAGES, WASM_VERSION};
+use crate::{init_state as set_state, read_state, Data, RuntimeState, LOG_MESSAGES, WASM_VERSION};
+use std::time::Duration;
 use types::{Timestamped, Version};
 use utils::env::Environment;
 
@@ -18,7 +19,7 @@ fn init_state(env: Box<dyn Environment>, data: Data, wasm_version: Version) {
     let now = env.now();
     let runtime_state = RuntimeState::new(env, data);
 
-    // crate::jobs::start(&runtime_state);
+    ic_cdk::timer::set_timer(Duration::from_secs(120), read_state(crate::jobs::start));
 
     set_state(runtime_state);
     WASM_VERSION.with(|v| *v.borrow_mut() = Timestamped::new(wasm_version, now));
