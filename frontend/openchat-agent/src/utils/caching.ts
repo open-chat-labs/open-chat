726aa7cac363fef1699dae689e1287183c2e6062
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-09 15:18:07 +0000

    Bump cache version

--- frontend/openchat-agent/src/utils/caching.ts
+++ frontend/openchat-agent/src/utils/caching.ts
@@ -19,7 +19,7 @@ import {
 } from "openchat-shared";
 import type { Principal } from "@dfinity/principal";
 
-const CACHE_VERSION = 53;
+const CACHE_VERSION = 54;
 
 export type Database = Promise<IDBPDatabase<ChatSchema>>;
 
