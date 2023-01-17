ddb4125dc5155dbbddbd17c9b0c3a8732cd247cc
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-17 15:53:05 +0000

    Update OpenStorage to latest version

--- backend/libraries/canister_client/src/lib.rs
+++ backend/libraries/canister_client/src/lib.rs
@@ -116,6 +116,13 @@ pub struct CyclesDispenserInitArgs {
 pub struct OpenStorageInitArgs {
     pub service_principals: Vec<Principal>,
     pub bucket_canister_wasm: CanisterWasm,
+    pub cycles_dispenser_config: Option<CyclesDispenserConfig>,
     pub wasm_version: Version,
     pub test_mode: bool,
+}
+
+#[derive(CandidType, Debug)]
+pub struct CyclesDispenserConfig {
+    pub canister_id: CanisterId,
+    pub min_cycles_balance: Cycles,
 }
