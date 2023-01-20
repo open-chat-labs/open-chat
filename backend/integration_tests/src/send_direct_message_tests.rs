e83b938884cf0d49e819138513d05a880255cb75
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-20 09:57:12 +0000

    Ensure direct messages arrive in order they were sent to first canister (#2986)

--- backend/integration_tests/src/send_direct_message_tests.rs
+++ backend/integration_tests/src/send_direct_message_tests.rs
@@ -3,7 +3,7 @@ use crate::client::{start_canister, stop_canister};
 use crate::rng::random_message_id;
 use crate::setup::{return_env, setup_env, TestEnv};
 use std::time::Duration;
-use types::{ChatEvent, MessageContent, TextContent};
+use types::{ChatEvent, EventIndex, MessageContent, TextContent};
 
 #[test]
 fn send_message_succeeds() {
@@ -135,4 +135,52 @@ fn send_message_retries_if_fails() {
         canister_ids,
         controller,
     });
+}
+
+#[test]
+fn messages_arrive_in_order_even_if_some_fail_originally() {
+    let TestEnv {
+        mut env,
+        canister_ids,
+        controller,
+    } = setup_env();
+
+    let user1 = client::user_index::happy_path::register_user(&mut env, canister_ids.user_index);
+    let user2 = client::user_index::happy_path::register_user(&mut env, canister_ids.user_index);
+
+    stop_canister(&mut env, canister_ids.local_user_index, user2.user_id.into());
+
+    client::user::happy_path::send_text_message(&mut env, &user1, user2.user_id, "1", None);
+    client::user::happy_path::send_text_message(&mut env, &user1, user2.user_id, "2", None);
+
+    start_canister(&mut env, canister_ids.local_user_index, user2.user_id.into());
+
+    client::user::happy_path::send_text_message(&mut env, &user1, user2.user_id, "3", None);
+
+    let events_response = client::user::happy_path::events(&env, &user2, user1.user_id, EventIndex::default(), true, 100, 100);
+
+    let first = &events_response.events[events_response.events.len() - 3].event;
+    let second = &events_response.events[events_response.events.len() - 2].event;
+    let third = &events_response.events[events_response.events.len() - 1].event;
+
+    validate_event_is_message_with_text(first, "1");
+    validate_event_is_message_with_text(second, "2");
+    validate_event_is_message_with_text(third, "3");
+
+    return_env(TestEnv {
+        env,
+        canister_ids,
+        controller,
+    });
+}
+
+fn validate_event_is_message_with_text(event: &ChatEvent, text: &str) {
+    if let ChatEvent::Message(m) = &event {
+        if let MessageContent::Text(t) = &m.content {
+            if t.text == text {
+                return;
+            }
+        }
+    }
+    panic!("Event does not match. {event:?}. {text}");
 }
