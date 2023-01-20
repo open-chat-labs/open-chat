e2c1c5c8be083b0257211bed75ed415966b0862f
Julian Jelfs <julian.jelfs@gmail.com>
2023-01-20 13:06:12 +0000

    conditional alert

--- frontend/app/src/components/home/Home.svelte
+++ frontend/app/src/components/home/Home.svelte
@@ -144,8 +144,9 @@
             closeThread();
         } else if (ev instanceof SendMessageFailed) {
             // This can occur either for chat messages or thread messages so we'll just handle it here
-            // TODO - not sure whether we need this now
-            // toastStore.showFailureToast("errorSendingMessage");
+            if (ev.detail) {
+                toastStore.showFailureToast("errorSendingMessage");
+            }
         } else if (ev instanceof ChatsUpdated) {
             closeNotifications((notification: Notification) => {
                 if (
