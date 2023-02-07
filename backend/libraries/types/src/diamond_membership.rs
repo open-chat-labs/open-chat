7514dcc08fe4b7bfbd66360b03fd8f789ddd940e
Hamish Peebles <hamishpeebles@gmail.com>
2023-02-07 15:41:54 +0000

    Candid serialize `DiamondMembershipPlanDuration` as a `nat8`

--- backend/libraries/types/src/diamond_membership.rs
+++ backend/libraries/types/src/diamond_membership.rs
@@ -1,7 +1,7 @@
 use crate::{Milliseconds, TimestampMillis};
+use candid::types::{Serializer, Type};
 use candid::CandidType;
-use serde::{Deserialize, Serialize};
-use serde_repr::{Deserialize_repr, Serialize_repr};
+use serde::{Deserialize, Deserializer, Serialize};
 
 #[derive(CandidType, Serialize, Deserialize, Debug, Default)]
 pub struct DiamondMembershipDetails {
@@ -9,7 +9,7 @@ pub struct DiamondMembershipDetails {
     pub recurring: Option<DiamondMembershipPlanDuration>,
 }
 
-#[derive(CandidType, Serialize_repr, Deserialize_repr, Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug)]
 #[repr(u8)]
 pub enum DiamondMembershipPlanDuration {
     OneMonth = 1,
@@ -26,6 +26,52 @@ impl DiamondMembershipPlanDuration {
             Self::OneMonth => Self::MONTH_IN_MS,
             Self::ThreeMonths => 3 * Self::MONTH_IN_MS,
             Self::OneYear => 12 * Self::MONTH_IN_MS,
+        }
+    }
+
+    const fn months(&self) -> u8 {
+        match self {
+            DiamondMembershipPlanDuration::OneMonth => 1,
+            DiamondMembershipPlanDuration::ThreeMonths => 3,
+            DiamondMembershipPlanDuration::OneYear => 12,
+        }
+    }
+}
+
+impl CandidType for DiamondMembershipPlanDuration {
+    fn _ty() -> Type {
+        u8::_ty()
+    }
+
+    fn idl_serialize<S>(&self, serializer: S) -> Result<(), S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_nat8(self.months())
+    }
+}
+
+impl Serialize for DiamondMembershipPlanDuration {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_u8(self.months())
+    }
+}
+
+impl<'de> Deserialize<'de> for DiamondMembershipPlanDuration {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let months = u8::deserialize(deserializer)?;
+
+        match months {
+            1 => Ok(DiamondMembershipPlanDuration::OneMonth),
+            3 => Ok(DiamondMembershipPlanDuration::ThreeMonths),
+            12 => Ok(DiamondMembershipPlanDuration::OneYear),
+            _ => Err(serde::de::Error::custom(format!("invalid value: {months}"))),
         }
     }
 }
