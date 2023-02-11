2bbf9773ee3a1d50356a5e9f374926e363eaf2cb
Hamish Peebles <hamishpeebles@gmail.com>
2023-02-11 20:51:12 +0000

    Temporarily stop using MemoryManager during `pre_upgrade`

--- backend/canisters/user/impl/src/lifecycle/pre_upgrade.rs
+++ backend/canisters/user/impl/src/lifecycle/pre_upgrade.rs
@@ -1,9 +1,8 @@
 use crate::lifecycle::UPGRADE_BUFFER_SIZE;
-use crate::memory::get_upgrades_memory;
 use crate::take_state;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::pre_upgrade;
-use ic_stable_structures::writer::{BufferedWriter, Writer};
+use stable_memory::serialize_to_stable_memory;
 use tracing::info;
 
 #[pre_upgrade]
@@ -17,8 +16,5 @@ fn pre_upgrade() {
 
     let stable_state = (state.data, logs, traces);
 
-    let mut memory = get_upgrades_memory();
-    let writer = BufferedWriter::new(UPGRADE_BUFFER_SIZE, Writer::new(&mut memory, 0));
-
-    serializer::serialize(stable_state, writer).unwrap();
+    serialize_to_stable_memory(stable_state, UPGRADE_BUFFER_SIZE).unwrap();
 }
