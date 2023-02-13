f238d56d885813709dc4e62668bd56c85b17d7a5
Hamish Peebles <hamishpeebles@gmail.com>
2023-02-13 01:17:15 +0000

    Register the local group indexes as super admins

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,11 +1,14 @@
 use crate::lifecycle::{init_env, init_state, UPGRADE_BUFFER_SIZE};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{mutate_state, Data};
+use candid::Principal;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use ic_stable_structures::reader::{BufferedReader, Reader};
+use local_user_index_canister::{Event, SuperAdminStatusChanged, UserRegistered};
 use tracing::info;
+use types::Version;
 use user_index_canister::post_upgrade::Args;
 use utils::cycles::init_cycles_dispenser_client;
 
@@ -25,4 +28,46 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    let local_group_indexes: Vec<_> = ["suaf3-hqaaa-aaaaf-bfyoa-cai", "ainth-qaaaa-aaaar-aaaba-cai"]
+        .into_iter()
+        .map(|str| Principal::from_text(str).unwrap())
+        .collect();
+
+    // Register the local group indexes as super admins
+    mutate_state(|state| {
+        let now = state.env.now();
+
+        for (index, canister_id) in local_group_indexes.into_iter().enumerate() {
+            let username = format!("GroupUpgradeBot{index}");
+            state.data.users.register(
+                canister_id,
+                canister_id.into(),
+                Version::default(),
+                username.clone(),
+                now,
+                None,
+                true,
+            );
+
+            state.data.push_event_to_all_local_user_indexes(
+                Event::UserRegistered(UserRegistered {
+                    user_id: canister_id.into(),
+                    user_principal: canister_id,
+                    username,
+                    is_bot: true,
+                    referred_by: None,
+                }),
+                None,
+            );
+
+            state.data.push_event_to_all_local_user_indexes(
+                Event::SuperAdminStatusChanged(SuperAdminStatusChanged {
+                    user_id: canister_id.into(),
+                    is_super_admin: true,
+                }),
+                None,
+            );
+        }
+    })
 }
