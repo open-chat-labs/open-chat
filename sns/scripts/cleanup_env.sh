11a84d9952ec1cdde6fcfc7694f1ae6773ef8466
megrogan <megrogan@gmail.com>
2023-02-17 09:22:19 +0000

    Replace .sh with .ini

--- sns/scripts/cleanup_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Remove the sns_canister_ids.json
-FILE_NAME=''"$NETWORK"'_sns_canister_ids.json'
-rm -f $FILE_NAME
--- sns/scripts/remove_direct_dev_access.sh
+++ sns/scripts/remove_direct_dev_access.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-# Set current directory to the directory this script is in
-SCRIPT=$(readlink -f "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT")
-cd $SCRIPT_DIR
-
 # Set env variables based on .env file
-set -o allexport
-source .env
-set +o allexport
+set -o allexport; source .env; set +o allexport
+./utils/setup_env.sh
 
-./access_control/remove-dev-principal-from-canisters-acl.sh
+SNS_GOVERNANCE_CANISTER_ID=$(dfx canister --network $NETWORK id sns_governance)
+
+CANDID="(record { principals = vec {principal \"$SNS_GOVERNANCE_CANISTER_ID\"}})"
+
+dfx -qq --identity $IDENTITY canister --network $NETWORK call user_index set_governance_principals $CANDID
+dfx -qq --identity $IDENTITY canister --network $NETWORK call group_index set_governance_principals $CANDID
+dfx -qq --identity $IDENTITY canister --network $NETWORK call notifications_index set_governance_principals $CANDID
+dfx -qq --identity $IDENTITY canister --network $NETWORK call proposals_bot set_governance_principals $CANDID
+dfx -qq --identity $IDENTITY canister --network $NETWORK call open_storage_index set_governance_principals $CANDID
+dfx -qq --identity $IDENTITY canister --network $NETWORK call cycles_dispenser set_governance_principals $CANDID
+
+./utils/cleanup_env.sh
