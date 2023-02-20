19ce743310c38090663cc5b3449da75648d755d2
Matt Grogan <megrogan@gmail.com>
2023-02-20 09:18:16 +0000

    Some sns script fixes (#3168)

--- sns/scripts/add_canisters_governance_principal.sh
+++ sns/scripts/add_canisters_governance_principal.sh
@@ -9,11 +9,11 @@ OC_DFX_PRINCIPAL=$(dfx --identity $IDENTITY identity get-principal)
 
 CANDID="(record { principals = vec {principal \"$SNS_GOVERNANCE_CANISTER_ID\"; principal \"$OC_DFX_PRINCIPAL\"}})"
 
-dfx -qq --identity $IDENTITY canister --network $NETWORK call user_index set_governance_principals $CANDID
-dfx -qq --identity $IDENTITY canister --network $NETWORK call group_index set_governance_principals $CANDID
-dfx -qq --identity $IDENTITY canister --network $NETWORK call notifications_index set_governance_principals $CANDID
-dfx -qq --identity $IDENTITY canister --network $NETWORK call proposals_bot set_governance_principals $CANDID
-dfx -qq --identity $IDENTITY canister --network $NETWORK call open_storage_index set_governance_principals $CANDID
-dfx -qq --identity $IDENTITY canister --network $NETWORK call cycles_dispenser set_governance_principals $CANDID
+dfx -qq --identity $IDENTITY canister --network $NETWORK call user_index set_governance_principals "$CANDID"
+dfx -qq --identity $IDENTITY canister --network $NETWORK call group_index set_governance_principals "$CANDID"
+dfx -qq --identity $IDENTITY canister --network $NETWORK call notifications_index set_governance_principals "$CANDID"
+dfx -qq --identity $IDENTITY canister --network $NETWORK call proposals_bot set_governance_principals "$CANDID"
+dfx -qq --identity $IDENTITY canister --network $NETWORK call open_storage_index set_governance_principals "$CANDID"
+dfx -qq --identity $IDENTITY canister --network $NETWORK call cycles_dispenser set_governance_principals "$CANDID"
 
 ./utils/cleanup_env.sh
