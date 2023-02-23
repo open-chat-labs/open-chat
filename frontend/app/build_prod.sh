dc833b054a45f9ef7546e2114c64e0f26b18bd47
Hamish Peebles <hamishpeebles@gmail.com>
2023-02-23 15:47:20 +0000

    Remove trailing slash

--- frontend/app/build_prod.sh
+++ frontend/app/build_prod.sh
@@ -2,7 +2,7 @@ export INTERNET_IDENTITY_URL=https://identity.ic0.app
 export NFID_URL=https://nfid.one/authenticate/?applicationName=OpenChat
 export DFX_NETWORK=ic
 export DEV_PORT=5001
-export IC_URL=https://icp-api.io/
+export IC_URL=https://icp-api.io
 export II_DERIVATION_ORIGIN=https://6hsbt-vqaaa-aaaaf-aaafq-cai.ic0.app
 export II_ALTERNATIVE_ORIGINS=https://oc.app,https://webtest.oc.app,https://xp7uu-xyaaa-aaaaf-aoa6a-cai.ic0.app
 export LEDGER_CANISTER_ICP=ryjl3-tyaaa-aaaaa-aaaba-cai
