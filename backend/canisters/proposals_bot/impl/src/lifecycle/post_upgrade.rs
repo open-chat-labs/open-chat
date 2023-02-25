f8a4beee75d2cb45f684f42af23a39ed71dccc40
Hamish Peebles <hamishpeebles@gmail.com>
2023-02-25 22:56:04 +0000

    One time job to add payloads for existing OpenChat proposal messages

--- backend/canisters/proposals_bot/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/proposals_bot/impl/src/lifecycle/post_upgrade.rs
@@ -1,12 +1,15 @@
+use crate::governance_clients::sns::ListProposals;
 use crate::lifecycle::{init_env, init_state, UPGRADE_BUFFER_SIZE};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{governance_clients, read_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use ic_stable_structures::reader::{BufferedReader, Reader};
 use proposals_bot_canister::post_upgrade::Args;
+use std::time::Duration;
 use tracing::info;
+use types::{CanisterId, MessageContent, Proposal, ProposalContent};
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -25,4 +28,48 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    ic_cdk_timers::set_timer(Duration::default(), || {
+        ic_cdk::spawn(get_openchat_proposal_payloads());
+    });
+}
+
+async fn get_openchat_proposal_payloads() {
+    let governance_canister_id = CanisterId::from_text("2jvtu-yqaaa-aaaaq-aaama-cai").unwrap();
+    let group_id = read_state(|state| state.data.nervous_systems.get_chat_id(&governance_canister_id)).unwrap();
+    let proposals_response = governance_clients::sns::list_proposals(
+        governance_canister_id,
+        &ListProposals {
+            limit: 30,
+            ..Default::default()
+        },
+    )
+    .await;
+
+    if let Ok(raw_proposals) = proposals_response {
+        let proposals: Vec<Proposal> = raw_proposals.into_iter().filter_map(|p| p.try_into().ok()).collect();
+        let args: Vec<_> = read_state(|state| {
+            let nervous_system = state.data.nervous_systems.get(&governance_canister_id).unwrap();
+
+            proposals
+                .into_iter()
+                .map(|p| group_canister::edit_message::Args {
+                    thread_root_message_index: None,
+                    message_id: nervous_system.active_proposals.get(&p.id()).unwrap().1,
+                    content: MessageContent::GovernanceProposal(ProposalContent {
+                        governance_canister_id,
+                        proposal: p.clone(),
+                        my_vote: None,
+                    }),
+                    correlation_id: 0,
+                })
+                .collect()
+        });
+
+        futures::future::join_all(
+            args.iter()
+                .map(|a| group_canister_c2c_client::edit_message(group_id.into(), a)),
+        )
+        .await;
+    }
 }
