5159fe7681aad5d950021e793847768a0bb76788
megrogan <megrogan@gmail.com>
2023-03-01 16:00:50 +0000

    Remove/add platform moderator custom functions

---
+++ scripts/proposals/remove_custom_sns_function.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Extract the args or use defaults
+TITLE=$1
+URL=$2
+SUMMARY=$3
+FUNCTION_ID=$4
+
+# Set current directory to the OC root
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/../..
+
+# Set env variables based on .env file
+set -o allexport; source .env; set +o allexport
+./sns/scripts/utils/setup_env.sh
+
+# Build the proposal candid
+PROPOSAL="(record { title=\"$TITLE\"; url=\"$URL\"; summary=\"$SUMMARY\"; action=opt variant {RemoveGenericNervousSystemFunction = $FUNCTION_ID:nat64}})"
+
+# Submit the proposal
+./sns/scripts/utils/submit_proposal.sh "$PROPOSAL"
+
+# Cleanup
+./sns/scripts/utils/cleanup_env.sh
