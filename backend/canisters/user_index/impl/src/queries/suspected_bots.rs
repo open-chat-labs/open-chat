1eda2dfab97ce29607dfe5ed947e252f48bf1bd1
megrogan <megrogan@gmail.com>
2023-03-01 15:23:15 +0000

    Add serde aliases & rename guard

--- backend/canisters/user_index/impl/src/queries/suspected_bots.rs
+++ backend/canisters/user_index/impl/src/queries/suspected_bots.rs
@@ -1,9 +1,9 @@
-use crate::guards::caller_is_super_admin;
+use crate::guards::caller_is_platform_moderator;
 use crate::{read_state, RuntimeState};
 use ic_cdk_macros::query;
 use user_index_canister::suspected_bots::{Response::*, *};
 
-#[query(guard = "caller_is_super_admin")]
+#[query(guard = "caller_is_platform_moderator")]
 fn suspected_bots(args: Args) -> Response {
     read_state(|state| suspected_bots_impl(args, state))
 }
