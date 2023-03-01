554ff5213a2d42509d40cd752de65d65533ce05f
megrogan <megrogan@gmail.com>
2023-03-01 17:45:55 +0000

    Fixes

--- backend/canisters/group_index/impl/src/updates/add_hot_group_exclusion.rs
+++ backend/canisters/group_index/impl/src/updates/add_hot_group_exclusion.rs
@@ -1,13 +1,15 @@
 use crate::{mutate_state, read_state, validate_user_is_super_admin, RuntimeState, ValidationError};
 use candid::Principal;
 use canister_tracing_macros::trace;
-use group_index_canister::add_hot_group_exclusion::{Response::*, *};
+use group_index_canister::{add_hot_group_exclusion, remove_hot_group_exclusion};
 use ic_cdk_macros::update;
 use types::{CanisterId, ChatId};
 
 #[update]
 #[trace]
-async fn add_hot_group_exclusion(args: Args) -> Response {
+async fn add_hot_group_exclusion(args: add_hot_group_exclusion::Args) -> add_hot_group_exclusion::Response {
+    use group_index_canister::add_hot_group_exclusion::Response::*;
+
     let PrepareResult {
         caller,
         user_index_canister_id,
@@ -24,21 +26,24 @@ async fn add_hot_group_exclusion(args: Args) -> Response {
         Err(ValidationError::InternalError(error)) => return InternalError(error),
     };
 
-    mutate_state(|state| commit(&args.chat_id, true, state));
-
-    Success
+    match mutate_state(|state| commit(&args.chat_id, true, state)) {
+        false => ChatNotFound,
+        true => Success,
+    }
 }
 
 #[update]
 #[trace]
-async fn remove_hot_group_exclusion(args: Args) -> Response {
+async fn remove_hot_group_exclusion(args: remove_hot_group_exclusion::Args) -> remove_hot_group_exclusion::Response {
+    use group_index_canister::remove_hot_group_exclusion::Response::*;
+
     let PrepareResult {
         caller,
         user_index_canister_id,
         ..
     } = match read_state(|state| prepare(&args.chat_id, state)) {
         Ok(ok) if !ok.is_excluded => ok,
-        Ok(_) => return ChatAlreadyExcluded,
+        Ok(_) => return ChatNotExcluded,
         Err(_) => return ChatNotFound,
     };
 
@@ -48,9 +53,10 @@ async fn remove_hot_group_exclusion(args: Args) -> Response {
         Err(ValidationError::InternalError(error)) => return InternalError(error),
     };
 
-    mutate_state(|state| commit(&args.chat_id, false, state));
-
-    Success
+    match mutate_state(|state| commit(&args.chat_id, false, state)) {
+        false => ChatNotFound,
+        true => Success,
+    }
 }
 
 struct PrepareResult {
@@ -72,10 +78,12 @@ fn prepare(chat_id: &ChatId, runtime_state: &RuntimeState) -> Result<PrepareResu
     }
 }
 
-fn commit(chat_id: &ChatId, exclude: bool, runtime_state: &mut RuntimeState) {
+fn commit(chat_id: &ChatId, exclude: bool, runtime_state: &mut RuntimeState) -> bool {
     if let Some(group) = runtime_state.data.public_groups.get_mut(chat_id) {
         group.set_excluded_from_hotlist(exclude);
+        true
     } else {
-        unreachable!();
+        // ChatNotFound
+        false
     }
 }
