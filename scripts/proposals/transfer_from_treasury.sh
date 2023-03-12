82bd65eadf6accd26aef0562e1c5f713d7ac8697
Hamish Peebles <hamishpeebles@gmail.com>
2023-03-12 09:19:59 +0000

    Add `transfer_from_treasury` script (#3303)

---
+++ scripts/proposals/transfer_from_treasury.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Extract the args
+TITLE=$1
+URL=$2
+SUMMARY=$3
+
+TOKEN=$4
+TO_PRINCIPAL=$5
+TO_SUBACCOUNT=$6
+MEMO=$7
+AMOUNT_E8S=$8
+
+if [ $TOKEN = "ICP" ]
+then
+    TREASURY=1
+elif [ $TOKEN = "CHAT" ]
+    TREASURY=2
+else
+    echo "TOKEN not set"
+    exit 1
+fi
+
+# Set current directory to the OC root
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/../..
+
+# Set env variables based on .env file
+set -o allexport; source .env; set +o allexport
+./sns/scripts/utils/setup_env.sh
+
+# Build the proposal candid
+PROPOSAL="(record { title=\"$TITLE\"; url=\"$URL\"; summary=\"$SUMMARY\"; action=opt variant {
+TransferSnsTreasuryFunds = record {
+    from_treasury=$TREASURY:int32;
+    to_principal=opt principal \"$TO_PRINCIPAL\": opt principal;
+    to_subaccount=opt record { subaccount = vec { $TO_SUBACCOUNT };
+    memo=$MEMO: opt nat64;
+    amount_e8s=$AMOUNT_E8S: nat64
+}})"
+
+# Submit the proposal
+./sns/scripts/utils/submit_proposal.sh "$PROPOSAL"
+
+# Cleanup
+./sns/scripts/utils/cleanup_env.sh
