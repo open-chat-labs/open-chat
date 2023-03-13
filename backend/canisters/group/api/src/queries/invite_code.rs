f9f7e40e4a5491b5210bc078fc6b0f4a3dfd1e38
Hamish Peebles <hamishpeebles@gmail.com>
2023-03-13 12:36:00 +0000

    Remove duplicate code by using shared `Empty` struct

--- backend/canisters/group/api/src/queries/invite_code.rs
+++ backend/canisters/group/api/src/queries/invite_code.rs
@@ -1,8 +1,8 @@
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
+use types::Empty;
 
-#[derive(CandidType, Serialize, Deserialize, Debug)]
-pub struct Args {}
+pub type Args = Empty;
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub enum Response {
--- backend/canisters/group/api/src/queries/summary.rs
+++ backend/canisters/group/api/src/queries/summary.rs
@@ -1,9 +1,8 @@
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
-use types::GroupCanisterGroupChatSummary;
+use types::{Empty, GroupCanisterGroupChatSummary};
 
-#[derive(CandidType, Serialize, Deserialize, Debug)]
-pub struct Args {}
+pub type Args = Empty;
 
 // Allow the large size difference because essentially all responses are the large variant anyway
 #[allow(clippy::large_enum_variant)]
