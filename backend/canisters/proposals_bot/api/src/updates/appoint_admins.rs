6a094d965489b70f1c065842fb59c0d3e95f20c6
megrogan <megrogan@gmail.com>
2023-03-17 14:51:13 +0000

    Added appoint_admins endpoint to proposals_bot

---
+++ backend/canisters/proposals_bot/api/src/updates/appoint_admins.rs
@@ -0,0 +1,37 @@
+use candid::CandidType;
+use human_readable::{HumanReadablePrincipal, ToHumanReadable};
+use serde::{Deserialize, Serialize};
+use types::{CanisterId, UserId};
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct Args {
+    pub governance_canister_id: CanisterId,
+    pub users: Vec<UserId>,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub enum Response {
+    Success,
+    NotFound,
+}
+
+#[derive(Serialize)]
+pub struct HumanReadableArgs {
+    pub governance_canister_id: HumanReadablePrincipal,
+    pub users: Vec<HumanReadablePrincipal>,
+}
+
+impl ToHumanReadable for Args {
+    type Target = HumanReadableArgs;
+
+    fn to_human_readable(&self) -> Self::Target {
+        HumanReadableArgs {
+            governance_canister_id: self.governance_canister_id.into(),
+            users: self
+                .users
+                .iter()
+                .map(|u| HumanReadablePrincipal::from(<UserId as Into<CanisterId>>::into(*u)))
+                .collect(),
+        }
+    }
+}
