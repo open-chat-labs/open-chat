37dd48d1f7f254add3676aa6b7a584bd0980bee4
Hamish Peebles <hamishpeebles@gmail.com>
2023-03-23 15:47:35 +0000

    Add scripts to create SNS functions calling into InfinitySwap

--- sns/scripts/proposals/create_custom_sns_function.sh
+++ sns/scripts/proposals/create_custom_sns_function.sh
@@ -11,15 +11,12 @@ SUMMARY=$3
 FUNCTION_ID=$4
 FUNCTION_NAME=$5
 FUNCTION_DESC=$6
-TARGET_CANISTER=$7
+TARGET_CANISTER_ID=$7
 TARGET_NAME=$8
-VALIDATOR_CANISTER=$9
-VALIDATOR_NAME=''"$TARGET_NAME"'_validate'
+VALIDATOR_CANISTER_ID=$9
+VALIDATOR_NAME=${10}
 
 echo "Create custom SNS function $FUNCTION_ID"
-
-TARGET_CANISTER_ID=$(dfx -qq canister --network $NETWORK id $TARGET_CANISTER)
-VALIDATOR_CANISTER_ID=$(dfx -qq canister --network $NETWORK id $VALIDATOR_CANISTER)
 
 PROPOSAL="(record { title=\"$TITLE\"; url=\"$URL\"; summary=\"$SUMMARY\"; action=opt variant {AddGenericNervousSystemFunction = record {id=($FUNCTION_ID:nat64); name=\"$FUNCTION_NAME\"; description=opt\"$FUNCTION_DESC\"; function_type=opt variant {GenericNervousSystemFunction=record{validator_canister_id=opt principal\"$VALIDATOR_CANISTER_ID\"; target_canister_id=opt principal\"$TARGET_CANISTER_ID\"; validator_method_name=opt\"$VALIDATOR_NAME\"; target_method_name=opt\"$TARGET_NAME\"}}}}})"
 
