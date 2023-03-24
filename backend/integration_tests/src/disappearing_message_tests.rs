a627eacc46ca5bd7e887776b393dc71131805e7b
Hamish Peebles <hamishpeebles@gmail.com>
2023-03-24 13:47:22 +0000

    Improve sharing of TestEnv between tests (#3361)

--- backend/integration_tests/src/disappearing_message_tests.rs
+++ backend/integration_tests/src/disappearing_message_tests.rs
@@ -1,23 +1,21 @@
+use crate::env::ENV;
 use crate::rng::random_string;
-use crate::setup::{return_env, setup_env, TestEnv};
-use crate::{client, User};
+use crate::{client, TestEnv, User};
 use ic_test_state_machine_client::StateMachine;
 use itertools::Itertools;
+use std::ops::Deref;
 use std::time::Duration;
 use types::{CanisterId, ChatId, OptionUpdate};
 
 #[test]
 fn disappearing_messages_in_group_chats() {
-    let TestEnv {
-        mut env,
-        canister_ids,
-        controller,
-    } = setup_env();
+    let mut wrapper = ENV.deref().get();
+    let TestEnv { env, canister_ids, .. } = wrapper.env();
 
-    let TestData { user1, group_id } = init_test_data(&mut env, canister_ids.user_index, true);
+    let TestData { user1, group_id } = init_test_data(env, canister_ids.user_index, true);
 
     client::group::update_group_v2(
-        &mut env,
+        env,
         user1.principal,
         group_id.into(),
         &group_canister::update_group_v2::Args {
@@ -26,10 +24,10 @@ fn disappearing_messages_in_group_chats() {
         },
     );
 
-    let send_message_response1 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "abc", None);
+    let send_message_response1 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "abc", None);
 
     assert!(
-        client::group::happy_path::events_by_index(&env, &user1, group_id, vec![send_message_response1.event_index])
+        client::group::happy_path::events_by_index(env, &user1, group_id, vec![send_message_response1.event_index])
             .events
             .first()
             .is_some()
@@ -39,14 +37,14 @@ fn disappearing_messages_in_group_chats() {
     env.tick();
 
     assert!(
-        client::group::happy_path::events_by_index(&env, &user1, group_id, vec![send_message_response1.event_index])
+        client::group::happy_path::events_by_index(env, &user1, group_id, vec![send_message_response1.event_index])
             .events
             .first()
             .is_none()
     );
 
     client::group::update_group_v2(
-        &mut env,
+        env,
         user1.principal,
         group_id.into(),
         &group_canister::update_group_v2::Args {
@@ -55,37 +53,28 @@ fn disappearing_messages_in_group_chats() {
         },
     );
 
-    let send_message_response2 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "xyz", None);
+    let send_message_response2 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "xyz", None);
 
     env.advance_time(Duration::from_secs(100000));
     env.tick();
 
     assert!(
-        client::group::happy_path::events_by_index(&env, &user1, group_id, vec![send_message_response2.event_index])
+        client::group::happy_path::events_by_index(env, &user1, group_id, vec![send_message_response2.event_index])
             .events
             .first()
             .is_some()
     );
-
-    return_env(TestEnv {
-        env,
-        canister_ids,
-        controller,
-    });
 }
 
 #[test]
 fn group_chat_summary_contains_expired_messages() {
-    let TestEnv {
-        mut env,
-        canister_ids,
-        controller,
-    } = setup_env();
+    let mut wrapper = ENV.deref().get();
+    let TestEnv { env, canister_ids, .. } = wrapper.env();
 
-    let TestData { user1, group_id } = init_test_data(&mut env, canister_ids.user_index, true);
+    let TestData { user1, group_id } = init_test_data(env, canister_ids.user_index, true);
 
     client::group::update_group_v2(
-        &mut env,
+        env,
         user1.principal,
         group_id.into(),
         &group_canister::update_group_v2::Args {
@@ -94,19 +83,19 @@ fn group_chat_summary_contains_expired_messages() {
         },
     );
 
-    let send_message_response1 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "abc", None);
+    let send_message_response1 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "abc", None);
     env.advance_time(Duration::from_millis(400));
-    let send_message_response2 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "def", None);
+    let send_message_response2 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "def", None);
     env.advance_time(Duration::from_millis(400));
-    let send_message_response3 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "ghi", None);
+    let send_message_response3 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "ghi", None);
     env.advance_time(Duration::from_millis(400));
-    let send_message_response4 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "jkl", None);
+    let send_message_response4 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "jkl", None);
     env.advance_time(Duration::from_millis(400));
-    let send_message_response5 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "mno", None);
+    let send_message_response5 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "mno", None);
     env.advance_time(Duration::from_millis(400));
-    let send_message_response6 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "mno", None);
+    let send_message_response6 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "mno", None);
 
-    let summary = client::group::happy_path::summary(&env, &user1, group_id);
+    let summary = client::group::happy_path::summary(env, &user1, group_id);
     let summary_timestamp = send_message_response6.timestamp;
 
     assert_eq!(summary.events_ttl, Some(1000));
@@ -123,7 +112,7 @@ fn group_chat_summary_contains_expired_messages() {
     env.advance_time(Duration::from_millis(2000));
 
     client::group::update_group_v2(
-        &mut env,
+        env,
         user1.principal,
         group_id.into(),
         &group_canister::update_group_v2::Args {
@@ -131,8 +120,8 @@ fn group_chat_summary_contains_expired_messages() {
             ..Default::default()
         },
     );
-    let send_message_response7 = client::group::happy_path::send_text_message(&mut env, &user1, group_id, None, "pqr", None);
-    let summary_updates = client::group::happy_path::summary_updates(&env, &user1, group_id, summary_timestamp).unwrap();
+    let send_message_response7 = client::group::happy_path::send_text_message(env, &user1, group_id, None, "pqr", None);
+    let summary_updates = client::group::happy_path::summary_updates(env, &user1, group_id, summary_timestamp).unwrap();
 
     assert_eq!(summary_updates.events_ttl.expand(), Some(Some(2000)));
     assert_eq!(
@@ -147,12 +136,6 @@ fn group_chat_summary_contains_expired_messages() {
         summary_updates.next_message_expiry.expand(),
         Some(send_message_response7.expires_at)
     );
-
-    return_env(TestEnv {
-        env,
-        canister_ids,
-        controller,
-    });
 }
 
 fn init_test_data(env: &mut StateMachine, user_index: CanisterId, public: bool) -> TestData {
--- backend/integration_tests/src/freeze_group_tests.rs
+++ backend/integration_tests/src/freeze_group_tests.rs
@@ -1,26 +1,28 @@
+use crate::env::ENV;
 use crate::rng::random_string;
-use crate::setup::{return_env, setup_env, TestEnv};
 use crate::utils::tick_many;
-use crate::{client, User};
+use crate::{client, TestEnv, User};
 use candid::Principal;
 use group_index_canister::freeze_group::SuspensionDetails;
 use ic_test_state_machine_client::StateMachine;
+use std::ops::Deref;
 use std::time::Duration;
 use types::{CanisterId, ChatId};
 use utils::time::DAY_IN_MS;
 
 #[test]
 fn freeze_then_unfreeze() {
+    let mut wrapper = ENV.deref().get();
     let TestEnv {
-        mut env,
+        env,
         canister_ids,
         controller,
-    } = setup_env();
+    } = wrapper.env();
 
-    let TestData { user1, group_id, .. } = init_test_data(&mut env, canister_ids.user_index, controller);
+    let TestData { user1, group_id, .. } = init_test_data(env, canister_ids.user_index, *controller);
 
     client::group_index::freeze_group(
-        &mut env,
+        env,
         user1.principal,
         canister_ids.group_index,
         &group_index_canister::freeze_group::Args {
@@ -33,7 +35,7 @@ fn freeze_then_unfreeze() {
     let summary_args = group_canister::public_summary::Args { invite_code: None };
 
     if let group_canister::public_summary::Response::Success(res) =
-        client::group::public_summary(&env, user1.principal, group_id.into(), &summary_args)
+        client::group::public_summary(env, user1.principal, group_id.into(), &summary_args)
     {
         assert!(res.summary.frozen.is_some());
     } else {
@@ -41,38 +43,33 @@ fn freeze_then_unfreeze() {
     }
 
     client::group_index::unfreeze_group(
-        &mut env,
+        env,
         user1.principal,
         canister_ids.group_index,
         &group_index_canister::unfreeze_group::Args { chat_id: group_id },
     );
 
     if let group_canister::public_summary::Response::Success(res) =
-        client::group::public_summary(&env, user1.principal, group_id.into(), &summary_args)
+        client::group::public_summary(env, user1.principal, group_id.into(), &summary_args)
     {
         assert!(res.summary.frozen.is_none());
     } else {
         panic!()
     }
-
-    return_env(TestEnv {
-        env,
-        canister_ids,
-        controller,
-    });
 }
 
 #[test]
 fn can_only_be_called_by_platform_moderator() {
+    let mut wrapper = ENV.deref().get();
     let TestEnv {
-        mut env,
+        env,
         canister_ids,
         controller,
-    } = setup_env();
+    } = wrapper.env();
 
     let TestData {
         user1, user2, group_id, ..
-    } = init_test_data(&mut env, canister_ids.user_index, controller);
+    } = init_test_data(env, canister_ids.user_index, *controller);
 
     let freeze_args = group_index_canister::freeze_group::Args {
         chat_id: group_id,
@@ -80,13 +77,13 @@ fn can_only_be_called_by_platform_moderator() {
         suspend_members: None,
     };
 
-    let response1 = client::group_index::freeze_group(&mut env, user2.principal, canister_ids.group_index, &freeze_args);
+    let response1 = client::group_index::freeze_group(env, user2.principal, canister_ids.group_index, &freeze_args);
     assert!(matches!(
         response1,
         group_index_canister::freeze_group::Response::NotAuthorized
     ));
 
-    let response2 = client::group_index::freeze_group(&mut env, user1.principal, canister_ids.group_index, &freeze_args);
+    let response2 = client::group_index::freeze_group(env, user1.principal, canister_ids.group_index, &freeze_args);
     assert!(
         matches!(response2, group_index_canister::freeze_group::Response::Success(_)),
         "{response2:#?}",
@@ -94,39 +91,34 @@ fn can_only_be_called_by_platform_moderator() {
 
     let unfreeze_args = group_index_canister::unfreeze_group::Args { chat_id: group_id };
 
-    let response3 = client::group_index::unfreeze_group(&mut env, user2.principal, canister_ids.group_index, &unfreeze_args);
+    let response3 = client::group_index::unfreeze_group(env, user2.principal, canister_ids.group_index, &unfreeze_args);
     assert!(matches!(
         response3,
         group_index_canister::unfreeze_group::Response::NotAuthorized
     ));
 
-    let response4 = client::group_index::unfreeze_group(&mut env, user1.principal, canister_ids.group_index, &unfreeze_args);
+    let response4 = client::group_index::unfreeze_group(env, user1.principal, canister_ids.group_index, &unfreeze_args);
     assert!(matches!(
         response4,
         group_index_canister::unfreeze_group::Response::Success(_)
     ));
-
-    return_env(TestEnv {
-        env,
-        canister_ids,
-        controller,
-    });
 }
 
 #[test]
 fn search_excludes_frozen_groups() {
+    let mut wrapper = ENV.deref().get();
     let TestEnv {
-        mut env,
+        env,
         canister_ids,
         controller,
-    } = setup_env();
+    } = wrapper.env();
 
     let TestData {
         user1,
         user2,
         group_id,
         group_name,
-    } = init_test_data(&mut env, canister_ids.user_index, controller);
+    } = init_test_data(env, canister_ids.user_index, *controller);
 
     let search_args = group_index_canister::search::Args {
         search_term: group_name,
@@ -134,7 +126,7 @@ fn search_excludes_frozen_groups() {
     };
 
     if let group_index_canister::search::Response::Success(res) =
-        client::group_index::search(&env, user2.principal, canister_ids.group_index, &search_args)
+        client::group_index::search(env, user2.principal, canister_ids.group_index, &search_args)
     {
         assert_eq!(res.matches.len(), 1);
     } else {
@@ -142,7 +134,7 @@ fn search_excludes_frozen_groups() {
     }
 
     client::group_index::freeze_group(
-        &mut env,
+        env,
         user1.principal,
         canister_ids.group_index,
         &group_index_canister::freeze_group::Args {
@@ -153,34 +145,29 @@ fn search_excludes_frozen_groups() {
     );
 
     if let group_index_canister::search::Response::Success(res) =
-        client::group_index::search(&env, user2.principal, canister_ids.group_index, &search_args)
+        client::group_index::search(env, user2.principal, canister_ids.group_index, &search_args)
     {
         assert!(res.matches.is_empty());
     } else {
         panic!()
     }
-
-    return_env(TestEnv {
-        env,
-        canister_ids,
-        controller,
-    });
 }
 
 #[test]
 fn freeze_and_suspend_users() {
+    let mut wrapper = ENV.deref().get();
     let TestEnv {
-        mut env,
+        env,
         canister_ids,
         controller,
-    } = setup_env();
+    } = wrapper.env();
 
     let TestData {
         user1, user2, group_id, ..
-    } = init_test_data(&mut env, canister_ids.user_index, controller);
+    } = init_test_data(env, canister_ids.user_index, *controller);
 
     client::group_index::freeze_group(
-        &mut env,
+        env,
         user1.principal,
         canister_ids.group_index,
         &group_index_canister::freeze_group::Args {
@@ -195,29 +182,24 @@ fn freeze_and_suspend_users() {
 
     env.tick();
 
-    let user = client::user_index::happy_path::current_user(&env, user2.principal, canister_ids.user_index);
+    let user = client::user_index::happy_path::current_user(env, user2.principal, canister_ids.user_index);
 
     assert!(user.suspension_details.is_some());
-
-    return_env(TestEnv {
-        env,
-        canister_ids,
-        controller,
-    });
 }
 
 #[test]
 fn delete_frozen_group() {
+    let mut wrapper = ENV.deref().get();
     let TestEnv {
-        mut env,
+        env,
         canister_ids,
         controller,
-    } = setup_env();
+    } = wrapper.env();
 
-    let TestData { user1, group_id, .. } = init_test_data(&mut env, canister_ids.user_index, controller);
+    let TestData { user1, group_id, .. } = init_test_data(env, canister_ids.user_index, *controller);
 
     client::group_index::freeze_group(
-        &mut env,
+        env,
         user1.principal,
         canister_ids.group_index,
         &group_index_canister::freeze_group::Args {
@@ -230,7 +212,7 @@ fn delete_frozen_group() {
     env.advance_time(Duration::from_millis(7 * DAY_IN_MS));
 
     let delete_group_response1 = client::group_index::delete_frozen_group(
-        &mut env,
+        env,
         user1.principal,
         canister_ids.group_index,
         &group_index_canister::delete_frozen_group::Args { chat_id: group_id },
@@ -246,7 +228,7 @@ fn delete_frozen_group() {
     env.advance_time(Duration::from_millis(1));
 
     let delete_group_response2 = client::group_index::delete_frozen_group(
-        &mut env,
+        env,
         user1.principal,
         canister_ids.group_index,
         &group_index_canister::delete_frozen_group::Args { chat_id: group_id },
@@ -259,15 +241,9 @@ fn delete_frozen_group() {
         "{delete_group_response2:?}"
     );
 
-    tick_many(&mut env, 5);
+    tick_many(env, 5);
 
     assert!(!env.canister_exists(Principal::from(group_id).as_slice().try_into().unwrap()));
-
-    return_env(TestEnv {
-        env,
-        canister_ids,
-        controller,
-    });
 }
 
 fn init_test_data(env: &mut StateMachine, user_index: CanisterId, controller: Principal) -> TestData {
