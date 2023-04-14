3ac8b7f4c16fd364014c9964cd9186331f3b83c7
Hamish Peebles <hamishpeebles@gmail.com>
2023-04-14 16:42:05 +0100

    Remove transfer limits from the backend (#3457)

--- backend/canisters/group/impl/src/updates/send_message_v2.rs
+++ backend/canisters/group/impl/src/updates/send_message_v2.rs
@@ -52,7 +52,7 @@ fn send_message_impl(args: Args, runtime_state: &mut RuntimeState) -> Response {
                 ContentValidationError::Empty => MessageEmpty,
                 ContentValidationError::TextTooLong(max_length) => TextTooLong(max_length),
                 ContentValidationError::InvalidPoll(reason) => InvalidPoll(reason),
-                ContentValidationError::TransferCannotBeZero | ContentValidationError::TransferLimitExceeded(_) => {
+                ContentValidationError::TransferCannotBeZero => {
                     unreachable!()
                 }
                 ContentValidationError::InvalidTypeForForwarding => {
