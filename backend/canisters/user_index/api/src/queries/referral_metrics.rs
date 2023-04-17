2533323a92050cf4c46ce15cafce7524b7372bbb
megrogan <megrogan@gmail.com>
2023-04-17 09:27:43 +0100

    Fix referral_metrics

--- backend/canisters/user_index/api/src/queries/referral_metrics.rs
+++ backend/canisters/user_index/api/src/queries/referral_metrics.rs
@@ -11,11 +11,12 @@ pub enum Response {
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct ReferralMetrics {
-    pub users_who_referred: u32,
     pub users_who_referred_paid_diamond: u32,
     pub users_who_referred_unpaid_diamond: u32,
+    pub users_who_referred_overall: u32,
     pub users_who_referred_90_percent_unpaid_diamond: u32,
     pub referrals_of_paid_diamond: u32,
     pub referrals_of_unpaid_diamond: u32,
+    pub referrals_other: u32,
     pub icp_raised_by_referrals_to_paid_diamond: u32,
 }
--- backend/canisters/user_index/impl/src/queries/referral_metrics.rs
+++ backend/canisters/user_index/impl/src/queries/referral_metrics.rs
@@ -13,11 +13,13 @@ fn referral_metrics(_args: Args) -> Response {
 struct ReferralData {
     paid_diamond: u32,
     unpaid_diamond: u32,
+    other: u32,
     icp_raised_for_paid_diamond_e8s: u64,
 }
 
 fn referral_metrics_impl(runtime_state: &RuntimeState) -> Response {
     let mut user_referrals_map: HashMap<UserId, ReferralData> = HashMap::new();
+    let now = runtime_state.env.now();
 
     for user in runtime_state.data.users.iter() {
         if let Some(referred_by) = user.referred_by {
@@ -27,8 +29,10 @@ fn referral_metrics_impl(runtime_state: &RuntimeState) -> Response {
             if icp_raised_for_paid_diamond > 0 {
                 data.paid_diamond += 1;
                 data.icp_raised_for_paid_diamond_e8s += icp_raised_for_paid_diamond;
-            } else {
+            } else if user.diamond_membership_details.is_active(now) {
                 data.unpaid_diamond += 1;
+            } else {
+                data.other += 1;
             }
         }
     }
@@ -36,15 +40,15 @@ fn referral_metrics_impl(runtime_state: &RuntimeState) -> Response {
     let mut user_referrals: Vec<ReferralData> = user_referrals_map.into_values().collect();
     user_referrals.sort_unstable_by_key(|u| Reverse(u.unpaid_diamond));
 
-    let mut users_who_referred: u32 = 0;
     let mut users_who_referred_paid_diamond: u32 = 0;
     let mut users_who_referred_unpaid_diamond: u32 = 0;
+    let mut users_who_referred_overall: u32= 0;
     let mut referrals_of_paid_diamond: u32 = 0;
     let mut referrals_of_unpaid_diamond: u32 = 0;
+    let mut referrals_other: u32 = 0;
     let mut icp_raised_by_referrals_to_paid_diamond_e8s: u64 = 0;
 
     for data in user_referrals.iter() {
-        users_who_referred += 1;
         icp_raised_by_referrals_to_paid_diamond_e8s += data.icp_raised_for_paid_diamond_e8s;
         if data.paid_diamond > 0 {
             users_who_referred_paid_diamond += 1;
@@ -54,6 +58,10 @@ fn referral_metrics_impl(runtime_state: &RuntimeState) -> Response {
             users_who_referred_unpaid_diamond += 1;
             referrals_of_unpaid_diamond += data.unpaid_diamond;
         }
+        if data.other > 0 {
+            referrals_other += data.other;
+        }
+        users_who_referred_overall += 1;
     }
 
     let threshold = (referrals_of_unpaid_diamond as f32 * 0.9) as u32;
@@ -70,12 +78,13 @@ fn referral_metrics_impl(runtime_state: &RuntimeState) -> Response {
     }
 
     Success(ReferralMetrics {
-        users_who_referred,
         users_who_referred_paid_diamond,
         users_who_referred_unpaid_diamond,
+        users_who_referred_overall,
         users_who_referred_90_percent_unpaid_diamond,
         referrals_of_paid_diamond,
         referrals_of_unpaid_diamond,
+        referrals_other,
         icp_raised_by_referrals_to_paid_diamond: (icp_raised_by_referrals_to_paid_diamond_e8s / 100_000_000) as u32,
     })
 }
