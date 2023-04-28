4a079fa2e1e45e13a9aa3839864837c671eb3e43
Julian Jelfs <julian.jelfs@gmail.com>
2023-04-28 10:02:23 +0100

    mobile done

--- frontend/app/src/components/landingpages/Miami.svelte
+++ frontend/app/src/components/landingpages/Miami.svelte
@@ -22,7 +22,40 @@
             lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.
         </p>
 
-        <Launch text="Claim" rootPath={`/${OPENCHAT_BOT_USER_ID}`} on:login />
+        <div class="launch">
+            <Launch text="Claim" rootPath={`/${OPENCHAT_BOT_USER_ID}`} on:login />
+        </div>
+
+        <div class="details">
+            <h4>Details</h4>
+            <p class="blurb">
+                Before claiming your prize, you must create an Internet Identity on the Internet
+                Computer Protocol (ICP) network. Creating an Internet Identity is an easy one time
+                process that will allow you to securely access any dApp in the Internet Computer
+                ecosystem without the hassle of usernames and passwords.
+            </p>
+
+            <h5>What to expect</h5>
+            <div class="list">
+                <div class="num">1.</div>
+                <div class="blurb">
+                    Create a new Internet Identity: Open the ICP wallet and click on "Create a new
+                    Internet Identity." You will be prompted to choose a unique name for your
+                    identity.
+                </div>
+                <div class="num">2.</div>
+                <div class="blurb">
+                    Set up your identity: Once you have chosen a name for your identity, you will be
+                    asked to set up your profile. You can add your photo, contact information, and
+                    other details as desired.
+                </div>
+                <div class="num">3.</div>
+                <div class="blurb">
+                    Use your Internet Identity: Once your identity has been verified, you can use it
+                    to access various services and applications built on the ICP protocol.
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -63,25 +96,38 @@
     .miami {
         .text {
             @include font(bold, normal, fs-200, 50);
-            padding: $sp6 0;
+            padding: $sp5 0;
             margin-top: $sp7;
             margin-bottom: $sp3;
         }
 
+        .details {
+            h4 {
+                @include font(bold, normal, fs-160, 38);
+                margin-bottom: $sp3;
+            }
+
+            .list {
+                display: grid;
+                grid-template-columns: auto auto;
+                align-items: start;
+                column-gap: $sp3;
+
+                .blurb {
+                    @include font(book, normal, fs-80, 20);
+                }
+            }
+        }
+
         .blurb {
-            color: var(--txt-light);
+            color: var(--landing-txt-light);
             @include font(book, normal, fs-60, 18);
             margin-bottom: $sp5;
         }
 
-        // .sub-one {
-        //     @include font(bold, normal, fs-160);
-        // }
-
-        // .sub-two {
-        //     @include font(bold, normal, fs-260);
-        //     font-family: "Bebas Neue", cursive;
-        // }
+        .launch {
+            margin-bottom: $sp6;
+        }
 
         @include mobile() {
             margin-top: 0;
