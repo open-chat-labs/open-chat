f884068938dcc861acd47c6cf1bbf0b23ecf4ab9
Julian Jelfs <julian.jelfs@gmail.com>
2023-05-04 14:13:54 +0100

    increment cache version

--- frontend/openchat-agent/src/utils/caching.ts
+++ frontend/openchat-agent/src/utils/caching.ts
@@ -19,7 +19,7 @@ import type {
 import type { Principal } from "@dfinity/principal";
 import { toRecord } from "./list";
 
-const CACHE_VERSION = 65;
+const CACHE_VERSION = 66;
 
 export type Database = Promise<IDBPDatabase<ChatSchema>>;
 
