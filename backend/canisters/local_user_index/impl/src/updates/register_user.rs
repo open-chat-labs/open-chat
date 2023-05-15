1d5022942a43f547c1893b5efffab09688ae9340
Hamish Peebles <hamishpeebles@gmail.com>
2023-05-15 10:54:20 +0100

    BTC Miami text updates

--- backend/canisters/local_user_index/impl/src/updates/register_user.rs
+++ backend/canisters/local_user_index/impl/src/updates/register_user.rs
@@ -114,10 +114,10 @@ fn prepare(args: &Args, runtime_state: &mut RuntimeState) -> Result<PrepareOk, R
     {
         vec![
             MessageContent::Text(TextContent {
-                text: "Congratulations!!".to_string(),
+                text: "Welcome to OpenChat!!".to_string(),
             }),
             MessageContent::Text(TextContent {
-                text: format!("Wait here {}, your SATS are coming below 👇", args.username),
+                text: format!("Wait a moment {}, your SATS are coming below 👇", args.username),
             }),
         ]
     } else {
