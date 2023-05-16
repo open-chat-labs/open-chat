180b151f98296b59cb6f34adf4a152ef88399fc1
Julian Jelfs <julian.jelfs@gmail.com>
2023-05-16 16:47:06 +0100

    user a simple select list for the permission selection

--- frontend/app/src/components/home/SelectPermissionRole.svelte
+++ frontend/app/src/components/home/SelectPermissionRole.svelte
@@ -10,63 +10,17 @@
     let roles: PermissionRole[] = ["owner", "admins", "moderators", "members"];
 </script>
 
-<Legend label={`${$_("group.permissions.whoCan")} ${label}`} />
-<Select bind:value={rolePermission}>
-    {#each roles as r, _i (r)}
-        <option value={r}>{$_(`group.role.${r}`)}</option>
-    {/each}
-</Select>
-
-<!-- <li>
+<div class="group-permission">
     <Legend label={`${$_("group.permissions.whoCan")} ${label}`} />
-    <div class="roles">
+    <Select bind:value={rolePermission}>
         {#each roles as r, _i (r)}
-            <div
-                class="role"
-                on:click={() => (rolePermission = r)}
-                class:selected={roles.indexOf(rolePermission) >= roles.indexOf(r)}>
-                {$_(`group.role.${r}`)}
-            </div>
-            <div class="arrow">></div>
+            <option value={r}>{$_(`group.role.${r}`)}</option>
         {/each}
-    </div>
-</li> -->
+    </Select>
+</div>
 
 <style type="text/scss">
-    li {
-        @include font(book, normal, fs-90);
-
-        margin-bottom: $sp4;
-
-        &:last-child {
-            margin-bottom: 0;
-        }
-    }
-
-    .roles {
-        display: flex;
-        gap: $sp3;
-        align-items: center;
-        border-bottom: 1px solid var(--bd);
-        padding: $sp3 0 $sp4 0;
-
-        .arrow:last-child {
-            display: none;
-        }
-
-        .role {
-            border-radius: 22px;
-            border: 1px solid var(--accent);
-            @include font(book, normal, fs-80);
-            text-transform: lowercase;
-            padding: 6px 10px;
-            text-align: center;
-            cursor: pointer;
-
-            &.selected {
-                background-color: var(--accent);
-                color: #fff;
-            }
-        }
+    .group-permission {
+        max-width: 300px;
     }
 </style>
