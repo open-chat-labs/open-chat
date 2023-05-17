bf409f77bc42deb18577601d629604923ac207ed
megrogan <megrogan@gmail.com>
2023-05-17 17:54:08 +0100

    Fix community inspect_message

--- backend/canisters/community/impl/src/lifecycle/inspect_message.rs
+++ backend/canisters/community/impl/src/lifecycle/inspect_message.rs
@@ -6,18 +6,19 @@ fn inspect_message() {
     read_state(accept_if_valid);
 }
 
-fn accept_if_valid(_runtime_state: &RuntimeState) {
+fn accept_if_valid(state: &RuntimeState) {
     let method_name = ic_cdk::api::call::method_name();
 
     // 'inspect_message' only applies to ingress messages so calls to c2c methods should be rejected
     let is_c2c_method = method_name.starts_with("c2c") || method_name == "wallet_receive";
-    if is_c2c_method {
+    let is_frozen = state.data.frozen.value.is_some();
+
+    if is_c2c_method || is_frozen {
         return;
     }
 
-    let is_valid = match method_name.as_str() {
-        _ => false,
-    };
+    let caller = state.env.caller();
+    let is_valid = state.data.members.get(caller).is_some();
 
     if is_valid {
         ic_cdk::api::call::accept_message();
