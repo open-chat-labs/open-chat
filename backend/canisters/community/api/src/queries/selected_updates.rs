8adf0535704f0b36c6af079cc30feaac85f46ea1
megrogan <megrogan@gmail.com>
2023-05-22 10:09:27 +0100

    clippy

--- backend/canisters/community/api/src/queries/selected_updates.rs
+++ backend/canisters/community/api/src/queries/selected_updates.rs
@@ -1,6 +1,6 @@
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
-use types::{CommunityMember, EventIndex, GroupRules, TimestampMillis, UserId};
+use types::{CommunityMember, GroupRules, TimestampMillis, UserId};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
