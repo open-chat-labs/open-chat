c262571d0636ec5856baf456c3f93d8f865da815
Matt Grogan <megrogan@gmail.com>
2023-05-23 11:01:24 +0100

    Added community::c2c_toggle_mute_notifications (#3644)

---
+++ backend/canisters/community/impl/src/updates/c2c_toggle_mute_notifications.rs
@@ -0,0 +1,23 @@
+use crate::{mutate_state, RuntimeState};
+use canister_api_macros::update_msgpack;
+use canister_tracing_macros::trace;
+use community_canister::c2c_toggle_mute_notifications::{Response::*, *};
+use types::Timestamped;
+
+#[update_msgpack]
+#[trace]
+fn c2c_toggle_mute_notifications(args: Args) -> Response {
+    mutate_state(|state| c2c_toggle_mute_notifications_impl(args, state))
+}
+
+fn c2c_toggle_mute_notifications_impl(args: Args, state: &mut RuntimeState) -> Response {
+    let caller = state.env.caller();
+    let now = state.env.now();
+    match state.data.members.get_mut(caller) {
+        Some(member) => {
+            member.notifications_muted = Timestamped::new(args.mute, now);
+            Success
+        }
+        None => CallerNotInCommunity,
+    }
+}
