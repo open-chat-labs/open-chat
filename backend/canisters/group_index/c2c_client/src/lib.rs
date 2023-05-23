86d4090abddb150e6c451520aa8a094fddc09744
Matt Grogan <megrogan@gmail.com>
2023-05-23 10:55:08 +0100

    Added community::c2c_delete_community (#3642)

--- backend/canisters/group_index/c2c_client/src/lib.rs
+++ backend/canisters/group_index/c2c_client/src/lib.rs
@@ -6,6 +6,7 @@ generate_c2c_call!(c2c_filter_groups);
 
 // Updates
 generate_c2c_call!(c2c_create_group);
+generate_c2c_call!(c2c_delete_community);
 generate_c2c_call!(c2c_delete_group);
 generate_c2c_call!(c2c_make_private);
 generate_c2c_call!(c2c_mark_active);
