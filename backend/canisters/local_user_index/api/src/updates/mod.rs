0ddcb030e8dc43cf831ffe2fbead6e8279a3c29f
Hamish Peebles <hamishpeebles@gmail.com>
2023-05-25 17:56:57 +0100

    Implement `invite_users_to_community`

--- backend/canisters/local_user_index/api/src/updates/mod.rs
+++ backend/canisters/local_user_index/api/src/updates/mod.rs
@@ -3,6 +3,7 @@ pub mod c2c_notify_events;
 pub mod c2c_notify_low_balance;
 pub mod c2c_notify_user_index_events;
 pub mod c2c_upgrade_user_canister_wasm;
+pub mod invite_users_to_community;
 pub mod invite_users_to_group;
 pub mod join_community;
 pub mod join_group;
