a1df9226ef77bc3e911b0e51093a1500ff9129c8
Hamish Peebles <hamishpeebles@gmail.com>
2023-05-25 18:26:07 +0100

    Implement `invite_users_to_community` (#3669)

--- backend/canisters/community/c2c_client/src/lib.rs
+++ backend/canisters/community/c2c_client/src/lib.rs
@@ -6,6 +6,7 @@ use community_canister::*;
 // Updates
 generate_c2c_call!(c2c_delete_community);
 generate_c2c_call!(c2c_freeze_community);
+generate_c2c_call!(c2c_invite_users);
 generate_c2c_call!(c2c_join_community);
 generate_c2c_call!(c2c_leave_community);
 generate_c2c_call!(c2c_unfreeze_community);
