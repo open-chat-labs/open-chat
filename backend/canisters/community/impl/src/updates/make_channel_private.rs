2b6fac5ed33abd032e8d0c654b8494c5ed6a70a6
Matt Grogan <megrogan@gmail.com>
2023-05-26 11:07:01 +0100

    Implement community::make_channel_private (#3674)

---
+++ backend/canisters/community/impl/src/updates/make_channel_private.rs
@@ -0,0 +1,37 @@
+use crate::{mutate_state, RuntimeState};
+use canister_tracing_macros::trace;
+use community_canister::make_channel_private::{Response::*, *};
+use group_chat_core::MakePrivateResult;
+use ic_cdk_macros::update;
+
+#[update]
+#[trace]
+fn make_channel_private(args: Args) -> Response {
+    mutate_state(|state| make_channel_private_impl(args, state))
+}
+
+fn make_channel_private_impl(args: Args, state: &mut RuntimeState) -> Response {
+    if state.data.is_frozen() {
+        return CommunityFrozen;
+    }
+
+    if let Some(member) = state.data.members.get(state.env.caller()) {
+        if member.suspended.value {
+            return UserSuspended;
+        }
+
+        if let Some(channel) = state.data.channels.get_mut(&args.channel_id) {
+            match channel.chat.make_private(member.user_id, state.env.now()) {
+                MakePrivateResult::Success => Success,
+                MakePrivateResult::UserSuspended => UserSuspended,
+                MakePrivateResult::UserNotInGroup => UserNotInChannel,
+                MakePrivateResult::NotAuthorized => NotAuthorized,
+                MakePrivateResult::AlreadyPrivate => AlreadyPrivate,
+            }
+        } else {
+            ChannelNotFound
+        }
+    } else {
+        UserNotInCommunity
+    }
+}
