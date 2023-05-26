9edaa9d1c88c916da088891a74cebaef73aa3e73
megrogan <megrogan@gmail.com>
2023-05-26 15:12:18 +0100

    Implement community::add_members_to_channel

---
+++ backend/canisters/community/api/src/updates/add_members_to_channel.rs
@@ -0,0 +1,51 @@
+use candid::CandidType;
+use serde::{Deserialize, Serialize};
+use types::{ChannelId, GateCheckFailedReason, UserId};
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct Args {
+    pub channel_id: ChannelId,
+    pub user_ids: Vec<UserId>,
+    pub added_by_name: String,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub enum Response {
+    Success,
+    PartialSuccess(PartialSuccessResult),
+    Failed(FailedResult),
+    CommunityFrozen,
+    UserSuspended,
+    UserNotInCommunity,
+    UserNotInChannel,
+    ChannelNotFound,
+    UserLimitReached(u32),
+    NotAuthorized,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct PartialSuccessResult {
+    pub users_added: Vec<UserId>,
+    pub users_already_in_channel: Vec<UserId>,
+    pub users_failed_gate_check: Vec<UserFailedGateCheck>,
+    pub users_failed_with_error: Vec<UserFailedError>,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct FailedResult {
+    pub users_already_in_channel: Vec<UserId>,
+    pub users_failed_gate_check: Vec<UserFailedGateCheck>,
+    pub users_failed_with_error: Vec<UserFailedError>,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct UserFailedGateCheck {
+    pub user_id: UserId,
+    pub reason: GateCheckFailedReason,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct UserFailedError {
+    pub user_id: UserId,
+    pub error: String,
+}
