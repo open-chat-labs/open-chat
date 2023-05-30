8dbd303de646ca9c7ac8e4cbd784b6386b52df29
megrogan <megrogan@gmail.com>
2023-05-30 12:07:43 +0100

    Implement community::invite_code

--- backend/canisters/community/api/src/queries/mod.rs
+++ backend/canisters/community/api/src/queries/mod.rs
@@ -2,6 +2,7 @@ pub mod c2c_summary;
 pub mod events;
 pub mod events_by_index;
 pub mod events_window;
+pub mod invite_code;
 pub mod messages_by_message_index;
 pub mod rules;
 pub mod selected_initial;
