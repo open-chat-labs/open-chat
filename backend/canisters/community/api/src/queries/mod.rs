8d294b8d7e3f0de684a8fb6ff2b86596c2592bd4
megrogan <megrogan@gmail.com>
2023-05-30 12:04:14 +0100

    Implement community::rules

--- backend/canisters/community/api/src/queries/mod.rs
+++ backend/canisters/community/api/src/queries/mod.rs
@@ -3,6 +3,7 @@ pub mod events;
 pub mod events_by_index;
 pub mod events_window;
 pub mod messages_by_message_index;
+pub mod rules;
 pub mod selected_initial;
 pub mod selected_updates;
 pub mod summary;
--- backend/canisters/community/impl/src/queries/mod.rs
+++ backend/canisters/community/impl/src/queries/mod.rs
@@ -3,6 +3,7 @@ mod events_by_index;
 mod events_window;
 mod http_request;
 mod messages_by_message_index;
+mod rules;
 mod selected_initial;
 mod selected_updates;
 mod summary;
