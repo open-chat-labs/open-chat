e06c9f6788c362368e79e821a571651f04867f51
megrogan <megrogan@gmail.com>
2023-05-31 12:45:29 +0100

    Improve search by splitting "terms" into "tokens"

--- backend/libraries/search/src/document.rs
+++ backend/libraries/search/src/document.rs
@@ -1,10 +1,39 @@
-use crate::query::*;
+use std::{cmp::max_by, collections::HashSet};
+use types::UserId;
 
 type Milliseconds = u64;
 
+pub struct Query {
+    pub tokens: Vec<Token>,
+    pub users: HashSet<UserId>,
+}
+
+pub struct Token {
+    pub value: String,
+    pub value_lower: String,
+}
+
+impl Query {
+    pub fn parse(free_text: &str) -> Query {
+        Query {
+            tokens: parse_tokens(free_text),
+            users: HashSet::new(),
+        }
+    }
+}
+
 pub struct Field {
-    value: String,
+    tokens: Vec<Token>,
     weight: f32,
+}
+
+impl Field {
+    fn new(free_text: &str, weight: f32) -> Field {
+        Field {
+            tokens: parse_tokens(free_text),
+            weight,
+        }
+    }
 }
 
 #[derive(Default)]
@@ -15,7 +44,7 @@ pub struct Document {
 
 impl Document {
     pub fn add_field(&mut self, value: String, weight: f32) -> &mut Document {
-        self.fields.push(Field { value, weight });
+        self.fields.push(Field::new(&value, weight));
         self
     }
 
@@ -64,28 +93,39 @@ fn score_field(query: &Query, field: &Field) -> f32 {
     let mut total = 0.0;
 
     for token in &query.tokens {
-        total += score_field_for_token(token, &field.value);
+        total += score_field_for_token(token, field);
     }
 
     // Average of token matches
     let score = total / (query.tokens.len() as f32);
 
     if score > 0.0 {
-        score * calculate_length_boost(field.value.len() as f32) * field.weight
+        score * field.weight
     } else {
         0.0
     }
 }
 
-fn score_field_for_token(token: &Token, field: &str) -> f32 {
-    let field_lower = field.to_lowercase();
-    if field.starts_with(&token.value) {
+fn score_field_for_token(search_token: &Token, field: &Field) -> f32 {
+    let mut max_score: f32 = 0.0;
+
+    // Max of token matches
+    for field_token in &field.tokens {
+        let score = score_token_match(search_token, field_token) * calculate_length_boost(field_token.value.len() as f32);
+        max_score = max_by(max_score, score, |a, b| a.partial_cmp(b).unwrap());
+    }
+
+    max_score
+}
+
+fn score_token_match(search_token: &Token, field_token: &Token) -> f32 {
+    if field_token.value.starts_with(&search_token.value) {
         5.0
-    } else if field_lower.starts_with(&token.value_lower) {
+    } else if field_token.value_lower.starts_with(&search_token.value_lower) {
         3.0
-    } else if field.contains(&token.value) {
+    } else if field_token.value.contains(&search_token.value) {
         2.0
-    } else if field_lower.contains(&token.value_lower) {
+    } else if field_token.value_lower.contains(&search_token.value_lower) {
         1.0
     } else {
         0.0
@@ -99,4 +139,13 @@ fn calculate_length_boost(x: f32) -> f32 {
 fn calculate_age_boost(age: Milliseconds) -> f32 {
     let age_in_days = (age / (1000 * 60 * 60 * 24)) as f32;
     1.0 + (-age_in_days / 20.0).exp()
+}
+
+fn parse_tokens(text: &str) -> Vec<Token> {
+    text.split_whitespace()
+        .map(|word| Token {
+            value: word.to_string(),
+            value_lower: word.to_lowercase(),
+        })
+        .collect()
 }
