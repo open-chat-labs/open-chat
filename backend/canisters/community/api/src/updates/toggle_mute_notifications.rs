190e1ec2f7eaf1fbff92ca049958142f4b3f23c9
Matt Grogan <megrogan@gmail.com>
2023-06-01 11:52:45 +0100

    Implement community::toggle_mute_notifications (#3702)

---
+++ backend/canisters/community/api/src/updates/toggle_mute_notifications.rs
@@ -0,0 +1,15 @@
+use candid::CandidType;
+use serde::{Deserialize, Serialize};
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct Args {
+    pub mute: bool,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub enum Response {
+    Success,
+    CommunityFrozen,
+    UserNotInCommunity,
+    UserSuspended,
+}
