e47a184163f28b41566fa084b50266c62a244686
Matt Grogan <megrogan@gmail.com>
2023-06-02 14:13:15 +0100

    Move invite_users logic into GroupChatCore (#3716)

--- backend/canisters/group/impl/src/updates/c2c_invite_users.rs
+++ backend/canisters/group/impl/src/updates/c2c_invite_users.rs
@@ -3,12 +3,8 @@ use crate::guards::caller_is_user_index_or_local_user_index;
 use crate::{mutate_state, run_regular_jobs, RuntimeState};
 use canister_api_macros::update_msgpack;
 use canister_tracing_macros::trace;
-use chat_events::ChatEventInternal;
 use group_canister::c2c_invite_users::{Response::*, *};
-use group_chat_core::UserInvitation;
-use types::{EventIndex, MessageIndex, UsersInvited};
-
-const MAX_INVITES: usize = 100;
+use group_chat_core::InvitedUsersResult;
 
 #[update_msgpack(guard = "caller_is_user_index_or_local_user_index")]
 #[trace]
@@ -25,75 +21,17 @@ fn c2c_invite_users_impl(args: Args, state: &mut RuntimeState) -> Response {
 
     let now = state.env.now();
 
-    if let Some(member) = state.data.chat.members.get(&args.caller) {
-        // The original caller must be authorized to invite other users
-        if member.suspended.value || (!state.data.chat.is_public && !member.role.can_invite_users(&state.data.chat.permissions))
-        {
-            return NotAuthorized;
-        }
-
-        // Filter out users who are already members and those who have already been invited
-        let invited_users: Vec<_> = args
-            .users
-            .iter()
-            .filter(|(user_id, principal)| {
-                state.data.chat.members.get(user_id).is_none() && !state.data.chat.invited_users.contains(principal)
-            })
-            .copied()
-            .collect();
-
-        let user_ids: Vec<_> = invited_users.iter().map(|(user_id, _)| user_id).copied().collect();
-
-        if !state.data.chat.is_public {
-            // Check the max invite limit will not be exceeded
-            if state.data.chat.invited_users.len() + invited_users.len() > MAX_INVITES {
-                return TooManyInvites(MAX_INVITES as u32);
-            }
-
-            // Find the latest event and message that the invited users are allowed to see
-            let mut min_visible_event_index = EventIndex::default();
-            let mut min_visible_message_index = MessageIndex::default();
-            if !state.data.chat.history_visible_to_new_joiners {
-                // If there is only an initial "group created" event then allow these users
-                // to see the "group created" event by starting min_visible_* at zero
-                let events_reader = state.data.chat.events.main_events_reader(now);
-                if events_reader.len() > 1 {
-                    min_visible_event_index = events_reader.next_event_index();
-                    min_visible_message_index = events_reader.next_message_index();
-                }
-            };
-
-            // Add new invites
-            for (user_id, principal) in invited_users.iter() {
-                state.data.chat.invited_users.add(
-                    *principal,
-                    UserInvitation {
-                        invited: *user_id,
-                        invited_by: member.user_id,
-                        timestamp: now,
-                        min_visible_event_index,
-                        min_visible_message_index,
-                    },
-                );
-            }
-
-            // Push a UsersInvited event
-            state.data.chat.events.push_main_event(
-                ChatEventInternal::UsersInvited(Box::new(UsersInvited {
-                    user_ids: user_ids.clone(),
-                    invited_by: member.user_id,
-                })),
-                args.correlation_id,
-                now,
-            );
+    match state.data.chat.invite_users(args.caller, args.users, now) {
+        InvitedUsersResult::Success(r) => {
             handle_activity_notification(state);
+            Success(SuccessResult {
+                invited_users: r.invited_users,
+                group_name: r.group_name,
+            })
         }
-
-        Success(SuccessResult {
-            invited_users: user_ids,
-            group_name: state.data.chat.name.clone(),
-        })
-    } else {
-        CallerNotInGroup
+        InvitedUsersResult::UserNotInGroup => CallerNotInGroup,
+        InvitedUsersResult::NotAuthorized => NotAuthorized,
+        InvitedUsersResult::UserSuspended => NotAuthorized,
+        InvitedUsersResult::TooManyInvites(v) => TooManyInvites(v),
     }
 }
