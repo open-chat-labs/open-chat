dcdda297416d6856449a6d156e1d9775f050c901
Hamish Peebles <hamishpeebles@gmail.com>
2023-06-05 10:20:48 +0100

    Implement `community::summary` + store channels against each member (#3725)

--- backend/libraries/types/src/channel_summary.rs
+++ backend/libraries/types/src/channel_summary.rs
@@ -1,19 +1,36 @@
-use crate::{AccessGate, ChannelId, EventIndex, GroupPermissions, GroupRole, TimestampMillis};
+use crate::{
+    AccessGate, ChannelId, ChatMetrics, EventIndex, EventWrapper, GroupCanisterThreadDetails, GroupPermissions, GroupRole,
+    GroupSubtype, Mention, Message, MessageIndex, Milliseconds, RangeSet, TimestampMillis,
+};
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
 
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
-pub struct ChannelSummary {
+pub struct CommunityCanisterChannelSummary {
     pub channel_id: ChannelId,
+    pub last_updated: TimestampMillis,
     pub name: String,
     pub description: String,
+    pub subtype: Option<GroupSubtype>,
     pub avatar_id: Option<u128>,
     pub is_public: bool,
+    pub history_visible_to_new_joiners: bool,
+    pub min_visible_event_index: EventIndex,
+    pub min_visible_message_index: MessageIndex,
+    pub latest_message: Option<EventWrapper<Message>>,
+    pub latest_event_index: EventIndex,
     pub joined: TimestampMillis,
     pub member_count: u32,
     pub role: GroupRole,
+    pub mentions: Vec<Mention>,
     pub permissions: GroupPermissions,
+    pub notifications_muted: bool,
+    pub metrics: ChatMetrics,
+    pub my_metrics: ChatMetrics,
+    pub latest_threads: Vec<GroupCanisterThreadDetails>,
+    pub date_last_pinned: Option<TimestampMillis>,
+    pub events_ttl: Option<Milliseconds>,
+    pub expired_messages: RangeSet<MessageIndex>,
+    pub next_message_expiry: Option<TimestampMillis>,
     pub gate: Option<AccessGate>,
-    pub last_updated: TimestampMillis,
-    pub latest_event_index: EventIndex,
 }
