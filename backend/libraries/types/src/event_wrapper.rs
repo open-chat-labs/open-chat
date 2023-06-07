e1a4a6f880c5b2d4d436fdc3df2c3e8d1f57b3aa
Hamish Peebles <hamishpeebles@gmail.com>
2023-06-07 13:15:50 +0100

    First step towards removing `correlation_id`

--- backend/libraries/types/src/event_wrapper.rs
+++ backend/libraries/types/src/event_wrapper.rs
@@ -7,7 +7,9 @@ use std::fmt::Debug;
 pub struct EventWrapper<T: CandidType + Clone + Debug> {
     pub index: EventIndex,
     pub timestamp: TimestampMillis,
+    #[serde(default)]
     pub correlation_id: u64,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub expires_at: Option<TimestampMillis>,
     pub event: T,
 }
