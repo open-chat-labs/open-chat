ab32e3a2b60d714f70e9807527a3722883aa8598
Hamish Peebles <hamishpeebles@gmail.com>
2023-06-07 15:50:00 +0100

    Add `ic-domains` file to assets canister (#3528)

--- frontend/openchat-sw/build_prod.sh
+++ frontend/openchat-sw/build_prod.sh
@@ -1,7 +1,5 @@
 export INTERNET_IDENTITY_URL=https://identity.ic0.app
 export NFID_URL=https://nfid.one/authenticate/?applicationName=OpenChat
-export II_DERIVATION_ORIGIN=https://6hsbt-vqaaa-aaaaf-aaafq-cai.ic0.app
-export II_ALTERNATIVE_ORIGINS=https://oc.app,https://webtest.oc.app,https://xp7uu-xyaaa-aaaaf-aoa6a-cai.ic0.app
 
 npm i
 npx rollup -c
