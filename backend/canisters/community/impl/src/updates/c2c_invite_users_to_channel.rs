cab757cf9dc1b507c47606a7db63fad507d739d5
megrogan <megrogan@gmail.com>
2023-06-07 14:35:18 +0100

    Restrict guard

--- backend/canisters/community/impl/src/updates/c2c_invite_users_to_channel.rs
+++ backend/canisters/community/impl/src/updates/c2c_invite_users_to_channel.rs
@@ -1,11 +1,11 @@
-use crate::guards::caller_is_user_index_or_local_user_index;
+use crate::guards::caller_is_local_user_index;
 use crate::{mutate_state, RuntimeState};
 use canister_api_macros::update_msgpack;
 use canister_tracing_macros::trace;
 use community_canister::c2c_invite_users_to_channel::{Response::*, *};
 use group_chat_core::InvitedUsersResult;
 
-#[update_msgpack(guard = "caller_is_user_index_or_local_user_index")]
+#[update_msgpack(guard = "caller_is_local_user_index")]
 #[trace]
 fn c2c_invite_users_to_channel(args: Args) -> Response {
     mutate_state(|state| c2c_invite_users_to_channel_impl(args, state))
@@ -63,36 +63,3 @@ fn c2c_invite_users_to_channel_impl(args: Args, state: &mut RuntimeState) -> Res
         UserNotInCommunity
     }
 }
-
-// #[allow(clippy::too_many_arguments)]
-// fn send_oc_bot_message_to_each_user(
-//     invited_by: UserId,
-//     user_ids: Vec<UserId>,
-//     community_id: CommunityId,
-//     channel_id: ChannelId,
-//     community_name: String,
-//     channel_name: String,
-//     local_user_index: CanisterId,
-//     fire_and_forget_handler: &mut FireAndForgetHandler,
-// ) {
-//     let text = format!("You have been invited to the channel [{channel_name}](/community/{community_id}/channel/{channel_id}) in the community [{community_name}](/community/{community_id}) by @UserId({invited_by}).");
-//     let message = MessageContent::Text(TextContent { text });
-
-//     let args = c2c_notify_events::Args {
-//         events: user_ids
-//             .into_iter()
-//             .map(|user_id| {
-//                 Event::OpenChatBotMessage(OpenChatBotMessage {
-//                     user_id,
-//                     message: message.clone(),
-//                 })
-//             })
-//             .collect(),
-//     };
-
-//     fire_and_forget_handler.send(
-//         local_user_index,
-//         "c2c_notify_events_msgpack".to_string(),
-//         serialize_then_unwrap(args),
-//     );
-// }
