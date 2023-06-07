467ad20129693e9af5b65d7cbe546ab9b125c290
megrogan <megrogan@gmail.com>
2023-06-07 18:05:23 +0100

    fmt

--- backend/integration_tests/src/communities/send_message_tests.rs
+++ backend/integration_tests/src/communities/send_message_tests.rs
@@ -4,7 +4,7 @@ use crate::{client, CanisterIds, TestEnv, User};
 use candid::Principal;
 use ic_test_state_machine_client::StateMachine;
 use std::ops::Deref;
-use types::{CommunityId, ChannelId, MessageContent, ChatEvent};
+use types::{ChannelId, ChatEvent, CommunityId, MessageContent};
 
 #[test]
 fn send_text_in_channel() {
@@ -22,23 +22,11 @@ fn send_text_in_channel() {
         channel_id,
     } = init_test_data(env, canister_ids, *controller);
 
-    let result = client::community::happy_path::send_text_message(
-        env,
-        &user1,
-        community_id,
-        channel_id,
-        None,
-        "Hello, world!",
-        None,
-    );
+    let result =
+        client::community::happy_path::send_text_message(env, &user1, community_id, channel_id, None, "Hello, world!", None);
 
-    let events_response = client::community::happy_path::events_by_index(
-        env,
-        &user2,
-        community_id,
-        channel_id,
-        vec![result.event_index]
-    );
+    let events_response =
+        client::community::happy_path::events_by_index(env, &user2, community_id, channel_id, vec![result.event_index]);
 
     if let ChatEvent::Message(message) = &events_response.events[0].event {
         if let MessageContent::Text(content) = &message.content {
@@ -51,14 +39,11 @@ fn send_text_in_channel() {
     }
 }
 
-fn init_test_data(
-    env: &mut StateMachine,
-    canister_ids: &CanisterIds,
-    controller: Principal
-) -> TestData {
+fn init_test_data(env: &mut StateMachine, canister_ids: &CanisterIds, controller: Principal) -> TestData {
     let user1 = client::register_diamond_user(env, canister_ids, controller);
     let user2 = client::local_user_index::happy_path::register_user(env, canister_ids.local_user_index);
-    let community_id = client::user::happy_path::create_community(env, &user1, &random_string(), true, vec!["general".to_string()]);
+    let community_id =
+        client::user::happy_path::create_community(env, &user1, &random_string(), true, vec!["general".to_string()]);
     client::local_user_index::happy_path::join_community(env, user2.principal, canister_ids.local_user_index, community_id);
 
     env.tick();
@@ -66,7 +51,12 @@ fn init_test_data(
     let summary = client::community::happy_path::summary(env, &user2, community_id);
     let channel_id = summary.channels.iter().find(|c| c.name == "general").unwrap().channel_id;
 
-    TestData { user1, user2, community_id, channel_id }
+    TestData {
+        user1,
+        user2,
+        community_id,
+        channel_id,
+    }
 }
 
 #[allow(dead_code)]
