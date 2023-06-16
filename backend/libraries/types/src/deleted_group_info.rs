353b85e875079f2af71891eb1310ec65ecf5b01d
Hamish Peebles <hamishpeebles@gmail.com>
2023-06-16 11:36:43 +0100

    Populate new field during deserialization (#3803)

--- backend/libraries/types/src/deleted_group_info.rs
+++ backend/libraries/types/src/deleted_group_info.rs
@@ -3,6 +3,7 @@ use candid::CandidType;
 use serde::{Deserialize, Serialize};
 
 #[derive(CandidType, Serialize, Deserialize, Debug, Clone)]
+#[serde(from = "DeletedGroupInfoPrevious")]
 pub struct DeletedGroupInfo {
     pub id: ChatId,
     pub timestamp: TimestampMillis,
@@ -13,10 +14,32 @@ pub struct DeletedGroupInfo {
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug, Clone)]
+pub struct DeletedGroupInfoPrevious {
+    pub id: ChatId,
+    pub timestamp: TimestampMillis,
+    pub deleted_by: UserId,
+    pub group_name: String,
+    pub public: bool,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug, Clone)]
 pub struct DeletedCommunityInfo {
     pub id: CommunityId,
     pub timestamp: TimestampMillis,
     pub deleted_by: UserId,
     pub name: String,
     pub public: bool,
+}
+
+impl From<DeletedGroupInfoPrevious> for DeletedGroupInfo {
+    fn from(value: DeletedGroupInfoPrevious) -> Self {
+        DeletedGroupInfo {
+            id: value.id,
+            timestamp: value.timestamp,
+            deleted_by: value.deleted_by,
+            group_name: value.group_name.clone(),
+            name: value.group_name,
+            public: value.public,
+        }
+    }
 }
