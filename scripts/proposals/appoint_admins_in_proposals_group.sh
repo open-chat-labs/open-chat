6baba9332113d7a0e8aa63ea08dd073babea9719
Hamish Peebles <hamishpeebles@gmail.com>
2023-06-19 11:47:06 +0100

    Update `appoint_admins_in_proposals_group` script (#3808)

--- scripts/proposals/appoint_admins_in_proposals_group.sh
+++ scripts/proposals/appoint_admins_in_proposals_group.sh
@@ -5,16 +5,14 @@ SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 cd $SCRIPT_DIR/..
 
-TITLE="Appoint @Hamish, @julian_jelfs, and @Matt as admins of the OpenChat Proposals group"
-SUMMARY="For each of these users, the proposals_bot will call into the group canister corresponding to the given governance canister to set the user's role within the group to admin.\n\nThis will allow them to update the group description, set the group avatar, and moderate messages if necessary."
+TITLE="Appoint @kinic_developer_org as admin of the Kinic Proposals group"
+SUMMARY="For each given user, the proposals_bot will call into the group canister corresponding to the given governance canister, to set the user's role within the group to admin.\n\nThis will allow them to update the group description, set the group avatar, and moderate messages if necessary."
 
-GOVERNANCE_CANISTER_ID="2jvtu-yqaaa-aaaaq-aaama-cai"
-HAMISH_USER_ID="3skqk-iqaaa-aaaaf-aaa3q-cai"
-JULIAN_USER_ID="2yfsq-kaaaa-aaaaf-aaa4q-cai"
-MATT_USER_ID="27eue-hyaaa-aaaaf-aaa4a-cai"
+GOVERNANCE_CANISTER_ID=74ncn-fqaaa-aaaaq-aaasa-cai
+USER_ID=b2vtn-faaaa-aaaar-aok7q-cai
 
 # add_platform_operator args
-ARGS="(record { governance_canister_id = principal \"$GOVERNANCE_CANISTER_ID\"; users = vec {principal \"$HAMISH_USER_ID\"; principal \"$JULIAN_USER_ID\"; principal \"$MATT_USER_ID\"} })"
+ARGS="(record { governance_canister_id = principal \"$GOVERNANCE_CANISTER_ID\"; users = vec {principal \"$USER_ID\"; } })"
 FUNCTION_ID=4002
 
 # Submit the proposal
