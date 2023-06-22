a786c1e0f9eb0df1d31ac4ec3f94d1baa7bb5cee
Julian Jelfs <julian.jelfs@gmail.com>
2023-06-22 18:17:43 +0100

    nearly there

--- frontend/openchat-agent/src/utils/community.ts
+++ frontend/openchat-agent/src/utils/community.ts
@@ -7,6 +7,7 @@ import {
     type UserCanisterCommunitySummary,
     CommunitySummaryUpdatesResponse,
 } from "openchat-shared";
+import { applyOptionUpdate, mapOptionUpdate } from "./mapping";
 
 export function mergeCommunities(
     userCanisterCommunities: UserCanisterCommunitySummary[],
@@ -25,11 +26,64 @@ export function mergeCommunities(
 
 export function mergeCommunityUpdates(
     communities: CommunitySummary[],
-    _userCanisterUpdates: UserCanisterCommunitySummaryUpdates[],
-    _communityCanisterUpdates: CommunityCanisterCommunitySummaryUpdates[]
+    userCanisterUpdates: UserCanisterCommunitySummaryUpdates[],
+    communityCanisterUpdates: CommunityCanisterCommunitySummaryUpdates[]
 ): CommunitySummary[] {
-    // TODO - implement
-    return communities;
+    const userLookup = CommunityMap.fromList(userCanisterUpdates);
+    const communityLookup = CommunityMap.fromList(communityCanisterUpdates);
+
+    return communities.map((community) => {
+        const u = userLookup.get(community.id);
+        const c = communityLookup.get(community.id);
+
+        const avatarUpdate = mapOptionUpdate(c?.avatarId, (avatarId) => ({
+            blobId: avatarId,
+            canisterId: community.id.communityId,
+        }));
+
+        const bannerUpdate = mapOptionUpdate(c?.bannerId, (bannerId) => ({
+            blobId: bannerId,
+            canisterId: community.id.communityId,
+        }));
+
+        const channelsRemoved = new Set(
+            (c?.membership?.channelsRemoved ?? []).map((c) => c.channelId)
+        );
+
+        const channelsAdded = c?.channelsAdded ?? [];
+
+        const currentChannels = community.channels
+            .filter((c) => !channelsRemoved.has(c.id.channelId))
+            .concat(channelsAdded);
+
+        const channelsUpdated = c?.channelsUpdated ?? [];
+
+        // TOOD we have to somehow merge the currentChannels, the user updates and the community updates
+        // all together - actually I think it's just a copy of mergeGroupChatUpdates called
+        // mergeChannelUpdates - but that's a tomorrow job
+
+        return {
+            id: community.id,
+            name: c?.name ?? community?.name,
+            latestEventIndex: c?.latestEventIndex ?? community.latestEventIndex,
+            lastUpdated: c?.lastUpdated ?? community.lastUpdated,
+            description: c?.description ?? community.description,
+            memberCount: c?.memberCount ?? community.memberCount,
+            avatar: {
+                ...community.avatar,
+                blobReference: applyOptionUpdate(community.avatar.blobReference, avatarUpdate),
+            },
+            banner: {
+                ...community.banner,
+                blobReference: applyOptionUpdate(community.banner.blobReference, bannerUpdate),
+            },
+            membership: {
+                ...community.membership,
+                role: c?.membership?.role ?? community.membership.role,
+            },
+            channels: community.channels, // TODO
+        };
+    });
 }
 
 export function isSuccessfulCommunitySummaryResponse(
