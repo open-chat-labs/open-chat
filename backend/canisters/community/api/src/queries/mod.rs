28a093bb5e5be015fa85601b4b52b514a83baa23
megrogan <megrogan@gmail.com>
2023-06-23 11:17:33 +0100

    Remove unneeded community::rules endpoint

--- backend/canisters/community/api/src/queries/mod.rs
+++ backend/canisters/community/api/src/queries/mod.rs
@@ -8,7 +8,6 @@ pub mod events_window;
 pub mod invite_code;
 pub mod local_user_index;
 pub mod messages_by_message_index;
-pub mod rules;
 pub mod search_channel;
 pub mod selected_channel_initial;
 pub mod selected_channel_updates;
