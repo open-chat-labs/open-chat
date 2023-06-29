460c3e7c2d2d77847102f2ac091f3eb71b6c37de
megrogan <megrogan@gmail.com>
2023-06-29 12:14:45 +0100

    Revert change to group data is_accessible

--- backend/canisters/group/impl/src/lib.rs
+++ backend/canisters/group/impl/src/lib.rs
@@ -378,8 +378,10 @@ impl Data {
     }
 
     pub fn is_accessible(&self, caller: Principal, invite_code: Option<u64>) -> bool {
-        let user_id = self.lookup_user_id(caller);
-        self.chat.is_accessible(user_id) || self.is_invite_code_valid(invite_code)
+        self.chat.is_public
+            || self.get_member(caller).is_some()
+            || self.get_invitation(caller).is_some()
+            || self.is_invite_code_valid(invite_code)
     }
 
     pub fn get_invitation(&self, caller: Principal) -> Option<&UserInvitation> {
