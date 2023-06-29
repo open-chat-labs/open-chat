f36557b260fd2c8f2977e7ad27274b622559eacb
Hamish Peebles <hamishpeebles@gmail.com>
2023-06-29 12:52:11 +0100

    Fix

--- backend/libraries/canister_tracing_macros/src/lib.rs
+++ backend/libraries/canister_tracing_macros/src/lib.rs
@@ -8,7 +8,8 @@ pub fn trace(_: TokenStream, item: TokenStream) -> TokenStream {
     let mut inner = parse_macro_input!(item as ItemFn);
 
     // We will wrap the original fn in a new fn whose signature matches the original fn
-    let wrapper_sig = inner.sig;
+    #[allow(clippy::redundant_clone)] // clippy doesn't realise that this is used in the macro
+    let wrapper_sig = inner.sig.clone();
 
     // Change the name of the inner fn so that it doesn't clash with the wrapper fn
     let inner_method_name = format_ident!("{}_inner_", inner.sig.ident);
