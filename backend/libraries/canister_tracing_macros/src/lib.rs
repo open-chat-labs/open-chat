502b064a519122851aa085daf8b92128fa775955
Hamish Peebles <hamishpeebles@gmail.com>
2023-06-29 12:44:27 +0100

    clippy

--- backend/libraries/canister_tracing_macros/src/lib.rs
+++ backend/libraries/canister_tracing_macros/src/lib.rs
@@ -8,7 +8,7 @@ pub fn trace(_: TokenStream, item: TokenStream) -> TokenStream {
     let mut inner = parse_macro_input!(item as ItemFn);
 
     // We will wrap the original fn in a new fn whose signature matches the original fn
-    let wrapper_sig = inner.sig.clone();
+    let wrapper_sig = inner.sig;
 
     // Change the name of the inner fn so that it doesn't clash with the wrapper fn
     let inner_method_name = format_ident!("{}_inner_", inner.sig.ident);
