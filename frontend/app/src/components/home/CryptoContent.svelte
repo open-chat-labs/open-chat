5d916b14695d090e9d1486b2e730a10e66ad6a87
Julian Jelfs <julian.jelfs@gmail.com>
2023-07-20 16:17:53 +0100

    add icons for kinic and hotornot

--- frontend/app/src/components/home/CryptoContent.svelte
+++ frontend/app/src/components/home/CryptoContent.svelte
@@ -7,7 +7,6 @@
     import { getContext } from "svelte";
     import Wallet from "svelte-material-icons/WalletOutline.svelte";
     import { cryptoLookup } from "openchat-client";
-    import page from "page";
 
     const client = getContext<OpenChat>("client");
     const user = client.user;
@@ -91,16 +90,23 @@
             linear-gradient(hsla(0, 0%, 100%, 0), hsla(0, 0%, 100%, 0) 45%, hsla(0, 0%, 100%, 0.2));
 
         &.icp {
-            background-image: url("../assets/icp_token.svg");
+            background-image: url("/assets/icp_token.svg");
         }
         &.sns1 {
-            background-image: url("../assets/sns1_medium.png");
+            background-image: url("/assets/sns1_medium.png");
         }
         &.ckbtc {
-            background-image: url("../assets/ckbtc_nobackground.svg");
+            background-image: url("/assets/ckbtc_nobackground.svg");
         }
         &.chat {
-            background-image: url("../assets/spinner.svg");
+            background-image: url("/assets/spinner.svg");
+        }
+        &.kinic {
+            background-image: url("/assets/kinic_token.png");
+            border-radius: 50%;
+        }
+        &.hot {
+            background-image: url("/assets/hot_token.svg");
         }
     }
 
