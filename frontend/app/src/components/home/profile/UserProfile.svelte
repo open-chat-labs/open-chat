d142ea172dae21ad4c5881f02d2f87f1f38b7d58
Julian Jelfs <julian.jelfs@gmail.com>
2023-07-26 09:28:42 +0100

    only show failure toast for avatar update

--- frontend/app/src/components/home/profile/UserProfile.svelte
+++ frontend/app/src/components/home/profile/UserProfile.svelte
@@ -148,9 +148,7 @@
 
     function userAvatarSelected(ev: CustomEvent<{ url: string; data: Uint8Array }>): void {
         client.setUserAvatar(ev.detail.data, ev.detail.url).then((success) => {
-            if (success) {
-                toastStore.showSuccessToast("avatarUpdated");
-            } else {
+            if (!success) {
                 toastStore.showFailureToast("avatarUpdateFailed");
             }
         });
