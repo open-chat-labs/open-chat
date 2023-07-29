b6bacc0cfa71f261148393a0e3be61db5803207f
megrogan <megrogan@gmail.com>
2023-07-29 13:07:52 +0100

    Add group_index::lookup_channel_by_group_id

--- backend/canisters/group_index/impl/src/queries/mod.rs
+++ backend/canisters/group_index/impl/src/queries/mod.rs
@@ -3,5 +3,6 @@ pub mod explore_communities;
 pub mod explore_groups;
 pub mod filter_groups;
 pub mod http_request;
+pub mod lookup_channel_by_group_id;
 pub mod recommended_groups;
 pub mod search;
