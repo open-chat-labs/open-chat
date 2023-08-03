8a1f900a07e3e3e02e6d9158f21feaf9f7cf55b3
megrogan <megrogan@gmail.com>
2023-08-03 14:18:28 +0100

    Simplify candid check

--- backend/canisters/notifications/api/src/main.rs
+++ backend/canisters/notifications/api/src/main.rs
@@ -2,8 +2,7 @@ use candid_gen::generate_candid_method;
 
 #[allow(deprecated)]
 fn main() {
-    generate_candid_method!(notifications, notifications, query);
-    generate_candid_method!(notifications, notifications_v1, query);
+    generate_candid_method!(notifications, notification_candid_check, query);
 
     candid::export_service!();
     std::print!("{}", __export_service());
