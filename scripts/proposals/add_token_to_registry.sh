d0eb03380ddfa211919411051a73b4ef40bc7d40
Hamish Peebles <hamishpeebles@gmail.com>
2023-08-10 11:27:51 +0100

    Add scripts to add/update tokens in the Registry

---
+++ scripts/proposals/add_token_to_registry.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Extract the args or use defaults
+TOKEN_NAME=$1
+LEDGER_CANISTER_ID=$2
+TOKEN_STANDARD=$3
+INFO_URL=$4
+HOW_TO_BUY_URL=$5
+TRANSACTION_URL_FORMAT=$6
+
+# Set current directory to the scripts root
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+TITLE="Add $TOKEN_NAME token to the Registry"
+SUMMARY="The Registry is not currently in use. But once it is enabled, only those tokens stored in the Registry will be able to be transferred within OpenChat."
+
+# add_token args
+ARGS="(record {
+    ledger_canister_id=principal \"$LEDGER_CANISTER_ID\";
+    token_standard=variant { \"$TOKEN_STANDARD\" };
+    info_url=\"$INFO_URL\";
+    how_to_buy_url=\"$HOW_TO_BUY_URL\";
+    transaction_url_format=\"$TRANSACTION_URL_FORMAT\";
+})"
+
+FUNCTION_ID=7000
+
+# Submit the proposal
+./make_custom_function_proposal.sh $FUNCTION_ID "$TITLE" "$SUMMARY" "$URL" "$ARGS"
