41876efd82640311006ab12aeabf011bc666f25d
Hamish Peebles <hamishpeebles@gmail.com>
2023-08-14 15:54:56 +0100

    Optimise `selected_updates` for query caching (#4185)

--- backend/canisters/community/impl/src/queries/selected_channel_updates.rs
+++ backend/canisters/community/impl/src/queries/selected_channel_updates.rs
@@ -13,29 +13,23 @@ fn selected_channel_updates_v2(args: Args) -> Response {
 }
 
 fn selected_channel_updates_impl(args: Args, state: &RuntimeState) -> Response {
-    let caller = state.env.caller();
-
-    if !state.data.is_accessible(caller, None) {
-        return PrivateCommunity;
-    }
-
     if let Some(channel) = state.data.channels.get(&args.channel_id) {
-        let user_id = state.data.members.lookup_user_id(caller);
-
-        // Short circuit prior to calling `ic0.time()` so that query caching works effectively.
+        // Short circuit prior to calling `ic0.caller()` and to maximise query caching.
         let latest_event_timestamp = channel.chat.events.latest_event_timestamp().unwrap_or_default();
         if latest_event_timestamp <= args.updates_since {
-            return SuccessNoUpdates(args.updates_since);
+            return SuccessNoUpdates(latest_event_timestamp);
         }
 
-        let now = state.env.now();
+        let caller = state.env.caller();
+        if !state.data.is_accessible(caller, None) {
+            return PrivateCommunity;
+        }
 
-        match channel
-            .chat
-            .selected_group_updates_from_events(args.updates_since, user_id, now)
-        {
+        let user_id = state.data.members.lookup_user_id(caller);
+
+        match channel.chat.selected_group_updates_from_events(args.updates_since, user_id) {
             Some(updates) if updates.has_updates() => Success(updates),
-            Some(_) => SuccessNoUpdates(now),
+            Some(_) => SuccessNoUpdates(latest_event_timestamp),
             None => PrivateChannel,
         }
     } else {
--- backend/canisters/group/impl/src/queries/selected_updates.rs
+++ backend/canisters/group/impl/src/queries/selected_updates.rs
@@ -8,29 +8,27 @@ fn selected_updates_v2(args: Args) -> Response {
 }
 
 fn selected_updates_impl(args: Args, state: &RuntimeState) -> Response {
+    // Short circuit prior to calling `ic0.caller()` to maximise query caching.
+    let latest_event_timestamp = state.data.chat.events.latest_event_timestamp().unwrap_or_default();
+    if latest_event_timestamp <= args.updates_since {
+        return SuccessNoUpdates(latest_event_timestamp);
+    }
+
     let caller = state.env.caller();
     let user_id = match state.data.lookup_user_id(caller) {
         Some(id) => id,
         None => return CallerNotInGroup,
     };
 
-    // Short circuit prior to calling `ic0.time()` so that query caching works effectively.
-    let latest_event_timestamp = state.data.chat.events.latest_event_timestamp().unwrap_or_default();
-    if latest_event_timestamp <= args.updates_since {
-        return SuccessNoUpdates(args.updates_since);
-    }
-
-    let now = state.env.now();
-
     let updates = state
         .data
         .chat
-        .selected_group_updates_from_events(args.updates_since, Some(user_id), now)
+        .selected_group_updates_from_events(args.updates_since, Some(user_id))
         .unwrap();
 
     if updates.has_updates() {
         Success(updates)
     } else {
-        SuccessNoUpdates(now)
+        SuccessNoUpdates(latest_event_timestamp)
     }
 }
