bc242fbf0daf5f9502b17036d6033df99b5e9ded
Julian Jelfs <julian.jelfs@gmail.com>
2023-08-24 12:00:57 +0100

    report readiness

--- frontend/app/src/stores/xframe.ts
+++ frontend/app/src/stores/xframe.ts
@@ -4,7 +4,7 @@ import page from "page";
 import { setModifiedTheme } from "../theme/themes";
 import { routerReady } from "../routes";
 
-type XFrameMessage = UpdateTheme | ChangeRoute | OpenChatReady;
+type XFrameMessage = UpdateTheme | ChangeRoute;
 
 type UpdateTheme = {
     kind: "update_theme";
@@ -18,10 +18,6 @@ type ChangeRoute = {
     path: string;
 };
 
-type OpenChatReady = {
-    kind: "openchat_ready";
-};
-
 export const framed = writable(false);
 
 export function init() {
@@ -30,7 +26,7 @@ export function init() {
         window.addEventListener("message", externalMessage);
         if (window.top) {
             console.debug("XFRAME_TARGET: sending ready message");
-            window.top.postMessage("openchat_ready");
+            window.top.postMessage("openchat_ready", "*");
         }
     }
 }
