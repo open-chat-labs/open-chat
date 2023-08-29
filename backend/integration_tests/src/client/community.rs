7efcf7843690a7fe3530a076ad6ee38e3466f0df
Hamish Peebles <hamishpeebles@gmail.com>
2023-08-29 16:51:27 +0100

    Support changing casing of public groups / communities

--- backend/integration_tests/src/client/community.rs
+++ backend/integration_tests/src/client/community.rs
@@ -121,11 +121,11 @@ pub mod happy_path {
 
     pub fn update_community(
         env: &mut StateMachine,
-        sender: &User,
+        sender: Principal,
         community_id: CommunityId,
         args: &community_canister::update_community::Args,
     ) {
-        let response = super::update_community(env, sender.principal, community_id.into(), args);
+        let response = super::update_community(env, sender, community_id.into(), args);
 
         match response {
             community_canister::update_community::Response::Success => {}
