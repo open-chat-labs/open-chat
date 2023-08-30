b4ba6f61248a3efba7a25beb23dcbd8aadb695bf
Julian Jelfs <julian.jelfs@gmail.com>
2023-08-30 11:31:14 +0100

    Fix height on safari iOS (#4264)

--- frontend/app/src/components/App.svelte
+++ frontend/app/src/components/App.svelte
@@ -550,6 +550,7 @@
             display: flex;
             height: 100vh;
             height: calc(var(--vh, 1vh) * 100);
+            height: 100dvh; // firefox will ignore this
 
             @include size-below(lg) {
                 padding: $sp3;
