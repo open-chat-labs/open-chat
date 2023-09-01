281ce1ec5a1fc70d29f38a9b7b37bb27ab5fba55
megrogan <megrogan@gmail.com>
2023-09-01 22:02:16 +0100

    Remove serde defaults post release

--- backend/canisters/local_user_index/api/src/updates/register_user.rs
+++ backend/canisters/local_user_index/api/src/updates/register_user.rs
@@ -6,7 +6,6 @@ use types::UserId;
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub username: String,
-    #[serde(default)]
     pub display_name: Option<String>,
     pub referral_code: Option<String>,
     pub public_key: Vec<u8>,
