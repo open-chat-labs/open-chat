955dc71d1375eaa88f72998041880eb826f41beb
Julian Jelfs <julian.jelfs@gmail.com>
2023-09-02 13:20:57 +0100

    add frame ancestors export to build scripts

--- frontend/app/build_testnet.sh
+++ frontend/app/build_testnet.sh
@@ -18,5 +18,6 @@ export LEDGER_CANISTER_KINIC=73mez-iiaaa-aaaaq-aaasq-cai
 export LEDGER_CANISTER_HOTORNOT=6rdgd-kyaaa-aaaaq-aaavq-cai
 export LEDGER_CANISTER_GHOST=4c4fd-caaaa-aaaaq-aaa3a-cai
 export BLOB_URL_PATTERN=https://{canisterId}.raw.$DFX_NETWORK.testnet.dfinity.network/{blobType}/
+export FRAME_ANCESTORS=https://ht7v7-iaaaa-aaaak-qakga-cai,https://mdocx-gyaaa-aaaak-qcbsq-cai,https://calm-pasca-49d7be.netlify.app,http://localhost:5173,https://zexzi-jyaaa-aaaam-abj3q-cai.icp0.io,https://xw4dq-4yaaa-aaaam-abeuq-cai.ic0.app
 
 npx rollup -c
