7629427572519a8766a1b6a22bd462a79e66e81f
Julian Jelfs <julian.jelfs@gmail.com>
2023-09-07 09:58:45 +0100

    another attempt to fix the tweet glitching (#4335)
    Co-authored-by: Hamish Peebles <hamishpeebles@gmail.com>

--- frontend/app/src/components/home/Tweet.svelte
+++ frontend/app/src/components/home/Tweet.svelte
@@ -12,8 +12,9 @@
     export let tweetId: string;
 
     let tweetWrapper: HTMLDivElement | undefined;
-    let tweetRendered = false;
     let supported = false;
+
+    let rendering: Promise<any> | undefined = undefined;
 
     onMount(() => {
         supported = (<any>window).twttr !== undefined;
@@ -24,21 +25,23 @@
             intersecting &&
             !$eventListScrolling &&
             tweetWrapper !== undefined &&
-            !tweetRendered &&
+            !rendering &&
             supported
         ) {
             tweetWrapper.innerHTML = "";
-            (<any>window).twttr?.widgets
-                .createTweet(tweetId, tweetWrapper, {
-                    conversation: "none",
-                    theme: $themeStore.name,
-                })
+
+            rendering = (<any>window).twttr?.widgets.createTweet(tweetId, tweetWrapper, {
+                conversation: "none",
+                theme: $themeStore.name,
+            }) as Promise<any>;
+
+            rendering
                 .then(() => {
-                    tweetRendered = true;
                     dispatch("rendered", tweetWrapper);
                 })
                 .catch((err: any) => {
                     console.log("Failed to render tweet: ", err);
+                    rendering = undefined;
                 });
         }
     }
