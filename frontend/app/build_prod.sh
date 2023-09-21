66ca50c4b0aee7af47a4839cfcef6a3fec95fe83
Julian Jelfs <julian.jelfs@gmail.com>
2023-09-21 11:32:53 +0100

    Ancestors (#4412)

--- frontend/app/build_prod.sh
+++ frontend/app/build_prod.sh
@@ -6,6 +6,5 @@ export IC_URL=https://icp-api.io
 export II_DERIVATION_ORIGIN=https://6hsbt-vqaaa-aaaaf-aaafq-cai.ic0.app
 export CUSTOM_DOMAINS=oc.app,webtest.oc.app
 export BLOB_URL_PATTERN=https://{canisterId}.raw.icp0.io/{blobType}/
-export FRAME_ANCESTORS=https://ht7v7-iaaaa-aaaak-qakga-cai,https://mdocx-gyaaa-aaaak-qcbsq-cai,https://calm-pasca-49d7be.netlify.app,http://localhost:5173,https://zexzi-jyaaa-aaaam-abj3q-cai.icp0.io,https://xw4dq-4yaaa-aaaam-abeuq-cai.ic0.app
 
 npx rollup -c
--- frontend/app/build_testnet.sh
+++ frontend/app/build_testnet.sh
@@ -11,6 +11,5 @@ export CLIENT_CACHING=true
 export IC_URL=https://$DFX_NETWORK.testnet.dfinity.network/
 export STORAGE_INDEX_CANISTER=s24we-diaaa-aaaaa-aaaka-cai
 export BLOB_URL_PATTERN=https://{canisterId}.raw.$DFX_NETWORK.testnet.dfinity.network/{blobType}/
-export FRAME_ANCESTORS=https://ht7v7-iaaaa-aaaak-qakga-cai,https://mdocx-gyaaa-aaaak-qcbsq-cai,https://calm-pasca-49d7be.netlify.app,http://localhost:5173,https://zexzi-jyaaa-aaaam-abj3q-cai.icp0.io,https://xw4dq-4yaaa-aaaam-abeuq-cai.ic0.app
 
 npx rollup -c
