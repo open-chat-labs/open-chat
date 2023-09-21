cada723beea2f5dabf458aaff7b6279ce18b7e08
Hamish Peebles <hamishpeebles@gmail.com>
2023-09-21 10:21:44 +0100

    Tidy up handling of environment variables

--- frontend/app/build_dev.sh
+++ frontend/app/build_dev.sh
@@ -1,17 +1,10 @@
 WATCH=$1
 
-# export INTERNET_IDENTITY_URL=http://localhost:8080?canisterId=qhbym-qaaaa-aaaaa-aaafq-cai
+export BUILD_ENV=development
 export INTERNET_IDENTITY_URL=http://127.0.0.1:8080?canisterId=qhbym-qaaaa-aaaaa-aaafq-cai
 export NFID_URL=http://localhost:8080?canisterId=qhbym-qaaaa-aaaaa-aaafq-cai
 export DFX_NETWORK=local
 export DEV_PORT=5001
-export LEDGER_CANISTER_ICP=ryjl3-tyaaa-aaaaa-aaaba-cai
-export LEDGER_CANISTER_SNS1=zfcdd-tqaaa-aaaaq-aaaga-cai
-export LEDGER_CANISTER_BTC=mxzaz-hqaaa-aaaar-qaada-cai
-export LEDGER_CANISTER_CHAT=2ouva-viaaa-aaaaq-aaamq-cai
-export LEDGER_CANISTER_KINIC=73mez-iiaaa-aaaaq-aaasq-cai
-export LEDGER_CANISTER_HOTORNOT=6rdgd-kyaaa-aaaaq-aaavq-cai
-export LEDGER_CANISTER_GHOST=4c4fd-caaaa-aaaaq-aaa3a-cai
 export BLOB_URL_PATTERN=http://{canisterId}.localhost:8080/{blobType}/
 export FRAME_ANCESTORS=https://ht7v7-iaaaa-aaaak-qakga-cai,https://mdocx-gyaaa-aaaak-qcbsq-cai,https://calm-pasca-49d7be.netlify.app,http://localhost:5173,https://zexzi-jyaaa-aaaam-abj3q-cai.icp0.io,https://xw4dq-4yaaa-aaaam-abeuq-cai.ic0.app
 
--- frontend/app/build_prod_test.sh
+++ frontend/app/build_prod_test.sh
@@ -1,17 +1,10 @@
+export BUILD_ENV=prod_test
 export INTERNET_IDENTITY_URL=https://identity.ic0.app
 export NFID_URL=https://nfid.one/authenticate/?applicationName=OpenChatTest
 export DFX_NETWORK=ic_test
-export DEV_PORT=5001
 export IC_URL=https://icp-api.io
 export II_DERIVATION_ORIGIN=https://pfs7b-iqaaa-aaaaf-abs7q-cai.ic0.app
 export CUSTOM_DOMAINS=test.oc.app
-export LEDGER_CANISTER_ICP=ryjl3-tyaaa-aaaaa-aaaba-cai
-export LEDGER_CANISTER_SNS1=zfcdd-tqaaa-aaaaq-aaaga-cai
-export LEDGER_CANISTER_BTC=mxzaz-hqaaa-aaaar-qaada-cai
-export LEDGER_CANISTER_CHAT=2ouva-viaaa-aaaaq-aaamq-cai
-export LEDGER_CANISTER_KINIC=73mez-iiaaa-aaaaq-aaasq-cai
-export LEDGER_CANISTER_HOTORNOT=6rdgd-kyaaa-aaaaq-aaavq-cai
-export LEDGER_CANISTER_GHOST=4c4fd-caaaa-aaaaq-aaa3a-cai
 export BLOB_URL_PATTERN=https://{canisterId}.raw.icp0.io/{blobType}/
 export FRAME_ANCESTORS=https://ht7v7-iaaaa-aaaak-qakga-cai,https://mdocx-gyaaa-aaaak-qcbsq-cai,https://calm-pasca-49d7be.netlify.app,http://localhost:5173,https://zexzi-jyaaa-aaaam-abj3q-cai.icp0.io,https://xw4dq-4yaaa-aaaam-abeuq-cai.ic0.app
 
