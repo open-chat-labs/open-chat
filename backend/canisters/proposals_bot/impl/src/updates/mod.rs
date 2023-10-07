bd2ad017e0ada27e312a0b2bbae51d08afa9fc3e
Hamish Peebles <hamishpeebles@gmail.com>
2023-10-08 00:40:52 +0100

    Remove `add_governance_canister` since it is now automated

--- backend/canisters/proposals_bot/impl/src/updates/mod.rs
+++ backend/canisters/proposals_bot/impl/src/updates/mod.rs
@@ -1,7 +1,5 @@
-pub mod add_governance_canister;
 pub mod appoint_admins;
 pub mod c2c_submit_proposal;
 pub mod import_proposals_group_into_community;
-pub mod remove_governance_canister;
 pub mod stake_neuron_for_submitting_proposals;
 pub mod wallet_receive;
