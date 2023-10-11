4ef8be2215f18bdeea6b35b6f7ef10c4940e4650
Hamish Peebles <hamishpeebles@gmail.com>
2023-10-11 12:27:01 +0100

    Simplify

--- backend/external_canisters/sns_governance/api/src/queries/get_nervous_system_parameters.rs
+++ backend/external_canisters/sns_governance/api/src/queries/get_nervous_system_parameters.rs
@@ -1,12 +1,5 @@
-use candid::CandidType;
-use serde::{Deserialize, Serialize};
 use types::Empty;
 
 pub type Args = Empty;
 
-#[derive(CandidType, Deserialize, Serialize, Clone, Debug)]
-pub struct Response {
-    pub neuron_minimum_stake_e8s: Option<u64>,
-    pub neuron_minimum_dissolve_delay_to_vote_seconds: Option<u64>,
-    pub reject_cost_e8s: Option<u64>,
-}
+pub type Response = crate::types::NervousSystemParameters;
