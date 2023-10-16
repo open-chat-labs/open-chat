829a5998a1233596b0b3fb1700ef218e73203bd7
megrogan <megrogan@gmail.com>
2023-10-16 15:57:35 +0100

    Better spot for "show more" link

--- frontend/app/src/components/home/profile/AccountsModal.svelte
+++ frontend/app/src/components/home/profile/AccountsModal.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import { mobileWidth } from "../../../stores/screenDimensions";
     import ModalContent from "../../ModalContent.svelte";
     import Wallet from "svelte-material-icons/WalletOutline.svelte";
     import { _ } from "svelte-i18n";
     import Accounts from "./Accounts.svelte";
+    import Button from "../../Button.svelte";
+    import LinkButton from "../../LinkButton.svelte";
+
+    const dispatch = createEventDispatcher();
+
+    let showZeroBalance = false;
+    let zeroCount = 0;
 </script>
 
 <ModalContent closeIcon on:close>
@@ -11,7 +20,27 @@
         {$_("wallet")}
     </div>
     <div slot="body">
-        <Accounts />
+        <Accounts bind:showZeroBalance bind:zeroCount />
+    </div>
+    <div slot="footer">
+        <div class="footer">
+            <div class="show-more">
+                {#if zeroCount > 0}
+                    <LinkButton
+                        light
+                        underline={"hover"}
+                        on:click={() => (showZeroBalance = !showZeroBalance)}
+                        >{$_(
+                            showZeroBalance
+                                ? "cryptoAccount.hideZeroBalance"
+                                : "cryptoAccount.showZeroBalance"
+                        )}</LinkButton>
+                {/if}
+            </div>
+            <Button on:click={() => dispatch("close")} small={!$mobileWidth} tiny={$mobileWidth}>
+                {$_("close")}
+            </Button>
+        </div>
     </div>
 </ModalContent>
 
@@ -20,5 +49,16 @@
         display: flex;
         align-items: center;
         gap: $sp3;
+    }
+
+    .footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+
+        .show-more {
+            @include font(light, normal, fs-70);
+            margin-top: $sp4;
+        }
     }
 </style>
