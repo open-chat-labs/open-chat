9da336478f22ce626f6be14825255bd1eac56b30
Hamish Peebles <hamishpeebles@gmail.com>
2023-10-23 16:04:11 +0100

    Fix test again

--- backend/libraries/group_chat_core/src/members.rs
+++ backend/libraries/group_chat_core/src/members.rs
@@ -519,7 +519,7 @@ mod tests {
         let member_bytes = msgpack::serialize_then_unwrap(&member);
         let member_bytes_len = member_bytes.len();
 
-        assert_eq!(member_bytes_len, 120);
+        assert_eq!(member_bytes_len, 121);
 
         let _deserialized: GroupMemberInternal = msgpack::deserialize_then_unwrap(&member_bytes);
     }
