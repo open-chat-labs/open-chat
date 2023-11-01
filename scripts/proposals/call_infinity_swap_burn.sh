53cef876454f4ba555b8c2e19bc65ccae0daa222
Hamish Peebles <hamishpeebles@gmail.com>
2023-11-01 11:30:59 +0000

    Add scripts to submit proposals

---
+++ scripts/proposals/call_infinity_swap_burn.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Set current directory to the scripts root
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+TITLE="Instruct InfinitySwap's CHAT/ICP swap canister to 'burn' liquidity"
+SUMMARY="This will remove the CHAT and ICP liquidity previously added by the OpenChat SNS, making it available to withdraw."
+URL="https://app.infinityswap.one/pools/stats/vahkw-kaaaa-aaaal-acaza-cai"
+ARGS="()"
+FUNCTION_ID=100002
+
+# Submit the proposal
+./make_custom_function_proposal.sh $FUNCTION_ID "$TITLE" "$SUMMARY" "$URL" "$ARGS"
---
+++ scripts/proposals/call_infinity_swap_refund_transfer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Set current directory to the scripts root
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+TITLE="Instruct InfinitySwap's CHAT/ICP swap canister to refund CHAT and ICP"
+SUMMARY="This will instruct InfinitySwap's canister to refund the CHAT and ICP previously deposited by the OpenChat SNS.
+
+The transfers will go to the OpenChat SNS governance canister's default account for both ICP and CHAT, this means that the CHAT will be burned since this is the minting account.
+
+That is not a problem though, since more CHAT can be minted in the future if decided by the DAO."
+URL="https://app.infinityswap.one/pools/stats/vahkw-kaaaa-aaaal-acaza-cai"
+ARGS="()"
+FUNCTION_ID=100003
+
+# Submit the proposal
+./make_custom_function_proposal.sh $FUNCTION_ID "$TITLE" "$SUMMARY" "$URL" "$ARGS"
