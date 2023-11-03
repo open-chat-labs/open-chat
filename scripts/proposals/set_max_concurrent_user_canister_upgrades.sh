2d0f0d52666e8c19372d68eefb8e18891861b29f
Hamish Peebles <hamishpeebles@gmail.com>
2023-11-03 17:26:20 +0000

    Add script to set max user canister upgrade concurrency

---
+++ scripts/proposals/set_max_concurrent_user_canister_upgrades.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TITLE="Increase max concurrent user canister upgrades to 50 per subnet"
+SUMMARY="It currently takes 1.5 days for all user canisters to be upgraded.
+
+By increasing the number of canisters that can be upgraded concurrently these upgrades should speed up."
+
+# Set current directory to the scripts root
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+ARGS="(record { value=50:nat32 })"
+FUNCTION_ID=1004
+
+# Submit the proposal
+./make_custom_function_proposal.sh $FUNCTION_ID "$TITLE" "$SUMMARY" "" "$ARGS"
