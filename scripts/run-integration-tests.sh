d1738da17901fe70c266015e1e5f9f65e30ac8a3
Hamish Peebles <hamishpeebles@gmail.com>
2023-11-14 10:18:59 +0000

    Link entire directory rather than individual files (#4764)

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -24,13 +24,13 @@ then
     ./scripts/generate-all-canister-wasms.sh
 fi
 
-cd backend/integration_tests/local-bin
+cd backend/integration_tests
 echo "PocketIC download starting"
 curl -sO https://download.dfinity.systems/ic/307d5847c1d2fe1f5e19181c7d0fcec23f4658b3/binaries/x86_64-$PLATFORM/pocket-ic.gz || exit 1
 gzip -df pocket-ic.gz
 chmod +x pocket-ic
 echo "PocketIC download completed"
-cd ../../..
+cd ../..
 
 ./scripts/download-nns-canister-wasm.sh icp_ledger ledger-canister_notify-method
 ./scripts/download-nns-canister-wasm.sh cycles_minting_canister cycles-minting-canister
