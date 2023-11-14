9a563ad775224e738347619d1f12c31f26bf04ca
Julian Jelfs <julian.jelfs@gmail.com>
2023-11-14 11:46:17 +0000

    Add new config option to polls (#4765)

--- frontend/openchat-agent/src/services/community/candid/idl.js
+++ frontend/openchat-agent/src/services/community/candid/idl.js
@@ -284,6 +284,7 @@ export const idlFactory = ({ IDL }) => {
     'show_votes_before_end_date' : IDL.Bool,
     'end_date' : IDL.Opt(TimestampMillis),
     'anonymous' : IDL.Bool,
+    'allow_user_to_change_vote' : IDL.Bool,
     'options' : IDL.Vec(IDL.Text),
   });
   const PollContent = IDL.Record({
@@ -1152,6 +1153,7 @@ export const idlFactory = ({ IDL }) => {
     'message_index' : MessageIndex,
   });
   const RegisterPollVoteResponse = IDL.Variant({
+    'UserCannotChangeVote' : IDL.Null,
     'UserNotInChannel' : IDL.Null,
     'ChannelNotFound' : IDL.Null,
     'PollEnded' : IDL.Null,
