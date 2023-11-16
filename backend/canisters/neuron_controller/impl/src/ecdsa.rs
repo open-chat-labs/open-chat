a97a2825abd0420bf8d05dee82706bd3a31b2197
Hamish Peebles <hamishpeebles@gmail.com>
2023-11-16 11:35:23 +0000

    Send `signable` rather than raw requestId

--- backend/canisters/neuron_controller/impl/src/ecdsa.rs
+++ backend/canisters/neuron_controller/impl/src/ecdsa.rs
@@ -2,7 +2,8 @@ use ic_cdk::api::call::CallResult;
 use ic_cdk::api::management_canister::ecdsa::{EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyArgument, SignWithEcdsaArgument};
 use ic_transport_types::{to_request_id, EnvelopeContent};
 use serde::Serialize;
-use tracing::error;
+use sha256::sha256;
+use tracing::{error, info};
 
 pub fn get_key_id(is_local_dev_mode: bool) -> EcdsaKeyId {
     let key_name = if is_local_dev_mode { "dfx_test_key" } else { "key_1" };
@@ -32,7 +33,7 @@ pub async fn get_public_key(key_id: EcdsaKeyId) -> CallResult<Vec<u8>> {
 pub async fn sign_envelope(content: EnvelopeContent, public_key: Vec<u8>, key_id: EcdsaKeyId) -> CallResult<Vec<u8>> {
     let request_id = to_request_id(&content).unwrap();
 
-    let signature = sign(key_id, *request_id).await?;
+    let signature = sign(key_id, &request_id.signable()).await?;
 
     let envelope = Envelope {
         content: content.clone(),
@@ -45,10 +46,18 @@ pub async fn sign_envelope(content: EnvelopeContent, public_key: Vec<u8>, key_id
     serializer.self_describe().unwrap();
     envelope.serialize(&mut serializer).unwrap();
 
+    info!(
+        request_id = String::from(request_id),
+        signature = hex::encode(signature),
+        "Signed envelope"
+    );
+
     Ok(serialized_bytes)
 }
 
-async fn sign(key_id: EcdsaKeyId, message_hash: [u8; 32]) -> CallResult<Vec<u8>> {
+async fn sign(key_id: EcdsaKeyId, message: &[u8]) -> CallResult<Vec<u8>> {
+    let message_hash = sha256(message);
+
     match ic_cdk::api::management_canister::ecdsa::sign_with_ecdsa(SignWithEcdsaArgument {
         message_hash: message_hash.to_vec(),
         derivation_path: Vec::new(),
