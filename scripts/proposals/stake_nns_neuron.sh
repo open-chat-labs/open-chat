0b3628aca8dd3e8c849d7592d6b98c4e5592a537
Hamish Peebles <hamishpeebles@gmail.com>
2023-11-18 23:40:07 +0000

    Add scripts to `stake_nns_neuron` and `manage_nns_neuron`

---
+++ scripts/proposals/stake_nns_neuron.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Set current directory to the scripts root
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
+
+TITLE="Instruct the NeuronController canister to stake a new NNS neuron"
+SUMMARY="This will instruct the NeuronController canister to stake a new neuron with a stake of 1 ICP."
+URL=""
+ARGS="(record {})"
+FUNCTION_ID=8000
+
+# Submit the proposal
+./make_custom_function_proposal.sh $FUNCTION_ID "$TITLE" "$SUMMARY" "$URL" "$ARGS"
