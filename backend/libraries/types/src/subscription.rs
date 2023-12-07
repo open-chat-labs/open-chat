698b8393bec03833bf8d9894c1eefb18aed699bb
Hamish Peebles <hamishpeebles@gmail.com>
2023-12-07 18:10:05 +0000

    Fix

--- backend/libraries/types/src/subscription.rs
+++ backend/libraries/types/src/subscription.rs
@@ -3,28 +3,24 @@ use serde::{Deserialize, Serialize};
 
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct SubscriptionInfo {
-    #[serde(rename = "e", alias = "endpoint")]
     pub endpoint: String,
-    #[serde(rename = "k", alias = "keys")]
     pub keys: SubscriptionKeys,
 }
 
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct SubscriptionKeys {
-    #[serde(rename = "p", alias = "p256h")]
     pub p256dh: String,
-    #[serde(rename = "e", alias = "auth")]
     pub auth: String,
 }
 
 impl SubscriptionInfo {
     pub fn approx_size(&self) -> usize {
-        self.endpoint.len() + self.keys.approx_size() + 2
+        self.endpoint.len() + self.keys.approx_size() + 32
     }
 }
 
 impl SubscriptionKeys {
     pub fn approx_size(&self) -> usize {
-        self.p256dh.len() + self.auth.len() + 2
+        self.p256dh.len() + self.auth.len() + 32
     }
 }
