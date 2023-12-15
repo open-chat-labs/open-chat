f5098c706c59ab0b91c1f4c3c0e8b6ad1ef85fa7
Hamish Peebles <hamishpeebles@gmail.com>
2023-12-15 16:44:36 +0000

    Reset scroll position when changing scope (#5018)

--- frontend/app/src/stores/scrollPos.ts
+++ frontend/app/src/stores/scrollPos.ts
@@ -1,8 +1,5 @@
 import { writable } from "svelte/store";
 import { mobileOperatingSystem } from "../utils/devices";
-import { createMapStore } from "openchat-client";
-
-export const chatListScroll = createMapStore(writable(new Map<string, number>()));
 
 export const eventListScrollTop = writable<number | undefined>(undefined);
 
