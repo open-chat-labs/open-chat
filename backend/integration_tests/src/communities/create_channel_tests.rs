71d31a61620a5a928b3ed8483e05703ec2bb3dda
Hamish Peebles <hamishpeebles@gmail.com>
2023-12-18 12:49:57 +0000

    clippy

--- backend/integration_tests/src/communities/create_channel_tests.rs
+++ backend/integration_tests/src/communities/create_channel_tests.rs
@@ -2,7 +2,6 @@ use crate::env::ENV;
 use crate::rng::random_string;
 use crate::{client, CanisterIds, TestEnv, User};
 use candid::Principal;
-use itertools::Itertools;
 use pocket_ic::PocketIc;
 use std::ops::Deref;
 use test_case::test_case;
