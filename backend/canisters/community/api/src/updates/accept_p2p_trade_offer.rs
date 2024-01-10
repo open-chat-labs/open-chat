e3f0c2820ae6e6131344d5def06a4b3c4b99e468
megrogan <megrogan@gmail.com>
2024-01-10 16:37:22 +0000

    Use `message_id` in `accept_p2p_trade_offer` args

--- backend/canisters/community/api/src/updates/accept_p2p_trade_offer.rs
+++ backend/canisters/community/api/src/updates/accept_p2p_trade_offer.rs
@@ -1,12 +1,12 @@
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
-use types::{ChannelId, MessageIndex};
+use types::{ChannelId, MessageId, MessageIndex};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub channel_id: ChannelId,
     pub thread_root_message_index: Option<MessageIndex>,
-    pub message_index: MessageIndex,
+    pub message_id: MessageId,
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
