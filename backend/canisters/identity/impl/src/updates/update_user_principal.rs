c1b51172833e6a2065da2ee6510a400df4f55873
Hamish Peebles <hamishpeebles@gmail.com>
2024-01-19 18:19:16 +0000

    Fix

--- backend/canisters/identity/impl/src/updates/update_user_principal.rs
+++ backend/canisters/identity/impl/src/updates/update_user_principal.rs
@@ -6,7 +6,7 @@ use identity_canister::update_user_principal::{Response::*, *};
 
 #[update(guard = "caller_is_governance_principal")]
 #[trace]
-async fn appoint_admins(args: Args) -> Response {
+async fn update_user_principal(args: Args) -> Response {
     let user_index_canister_id = read_state(|state| state.data.user_index_canister_id);
 
     match user_index_canister_c2c_client::c2c_update_user_principal(
