5a36a57abaa79883eeaf7ec865fa30d9cf5e0cad
Julian Jelfs <julian.jelfs@gmail.com>
2024-01-20 10:39:42 +0000

    Get user's country code on start up (#5219)

--- frontend/openchat-client/src/openchat.ts
+++ frontend/openchat-client/src/openchat.ts
@@ -451,6 +451,7 @@ import {
     stripLinkDisabledMarker,
 } from "./utils/linkPreviews";
 import { applyTranslationCorrection } from "./stores/i18n";
+import { getUserCountryCode } from "./utils/location";
 
 const UPGRADE_POLL_INTERVAL = 1000;
 const MARK_ONLINE_INTERVAL = 61 * 1000;
@@ -508,6 +509,14 @@ export class OpenChat extends OpenChatAgentWorker {
         localStorage.removeItem("ic-delegation");
         localStorage.removeItem("ic-identity");
         initialiseTracking(config);
+
+        getUserCountryCode()
+            .then((country) => {
+                console.debug("GEO: User's country location is: ", country);
+            })
+            .catch((err) => {
+                console.warn("GEO: Unable to determine user's country location", err);
+            });
 
         this._authClient = AuthClient.create({
             idleOptions: {
