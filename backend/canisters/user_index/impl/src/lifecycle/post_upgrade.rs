533e93e18f4520e7553044c42cc27aff98d6e841
Hamish Peebles <hamishpeebles@gmail.com>
2024-01-23 16:52:26 +0000

    Unsuspend user @Hugh_Mungus who was only partially unsuspended due to bug

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,10 +1,13 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
+use crate::updates::unsuspend_user::unsuspend_user_impl;
 use crate::Data;
+use candid::Principal;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
+use std::time::Duration;
 use tracing::info;
 use user_index_canister::post_upgrade::Args;
 use utils::cycles::init_cycles_dispenser_client;
@@ -24,4 +27,12 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    // Unsuspend user @Hugh_Mungus who wasn't unsuspended from communities due to a bug
+    ic_cdk_timers::set_timer(Duration::ZERO, || {
+        let hugh_mungus_user_id = Principal::from_text("ne6gg-dqaaa-aaaaf-agzwa-cai").unwrap().into();
+        ic_cdk::spawn(async move {
+            unsuspend_user_impl(hugh_mungus_user_id).await;
+        })
+    });
 }
