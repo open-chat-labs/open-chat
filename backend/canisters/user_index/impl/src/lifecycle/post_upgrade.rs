c362f75690c5eeca63dae194bc1fcbc2af3e0ff8
Hamish Peebles <hamishpeebles@gmail.com>
2024-01-24 23:10:02 +0000

    Remove one time code to unsuspend a few users

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,15 +1,11 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::updates::unsuspend_user::unsuspend_user_impl;
 use crate::Data;
-use candid::Principal;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
-use std::time::Duration;
 use tracing::info;
-use types::UserId;
 use user_index_canister::post_upgrade::Args;
 use utils::cycles::init_cycles_dispenser_client;
 
@@ -28,22 +24,4 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
-
-    let users_to_unsuspend: Vec<UserId> = [
-        "qyzts-uiaaa-aaaar-aw4ca-cai",
-        "zjayq-ryaaa-aaaar-axhrq-cai",
-        "o6334-oyaaa-aaaar-awgxa-cai",
-        "s4i57-nqaaa-aaaar-abzcq-cai",
-        "vecg6-eqaaa-aaaar-a23rq-cai",
-        "ne6gg-dqaaa-aaaaf-agzwa-cai",
-    ]
-    .iter()
-    .map(|str| Principal::from_text(str).unwrap().into())
-    .collect();
-
-    ic_cdk_timers::set_timer(Duration::ZERO, || {
-        ic_cdk::spawn(async move {
-            futures::future::join_all(users_to_unsuspend.into_iter().map(unsuspend_user_impl)).await;
-        })
-    });
 }
