404c9e2635cf0fe5a8f2712705dede1702c0fb54
Hamish Peebles <hamishpeebles@gmail.com>
2024-01-24 15:12:47 +0000

    Call into CyclesDispenser using Candid

--- backend/canisters/cycles_dispenser/c2c_client/src/lib.rs
+++ backend/canisters/cycles_dispenser/c2c_client/src/lib.rs
@@ -1,7 +1,7 @@
-use canister_client::generate_c2c_call;
+use canister_client::generate_candid_c2c_call;
 use cycles_dispenser_canister::*;
 
 // Queries
 
 // Updates
-generate_c2c_call!(c2c_request_cycles);
+generate_candid_c2c_call!(c2c_request_cycles);
