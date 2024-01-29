c4517bd77c4204417298dabef93bf743b336a44b
Matt Grogan <megrogan@gmail.com>
2024-01-29 13:32:29 +0000

    Disable registration and diamond upgrade (#5282)

--- frontend/app/src/components/home/upgrade/Features.svelte
+++ frontend/app/src/components/home/upgrade/Features.svelte
@@ -16,6 +16,8 @@
     export let canExtend: boolean;
     export let landing = false;
 
+    let disabled = true;
+
     function cancel() {
         dispatch("cancel");
     }
@@ -232,12 +234,14 @@
         <Footer>
             <Button tiny={$mobileWidth} small={!$mobileWidth} secondary on:click={cancel}
                 ><Translatable resourceKey={i18nKey(isDiamond ? "close" : "cancel")} /></Button>
-            {#if !isDiamond}
-                <Button on:click={upgrade} tiny={$mobileWidth} small={!$mobileWidth}
-                    ><Translatable resourceKey={i18nKey("upgrade.button")} /></Button>
-            {:else if canExtend}
-                <Button on:click={upgrade} tiny={$mobileWidth} small={!$mobileWidth}
-                    ><Translatable resourceKey={i18nKey("upgrade.extendShort")} /></Button>
+            {#if !disabled}
+                {#if !isDiamond}
+                    <Button on:click={upgrade} tiny={$mobileWidth} small={!$mobileWidth}
+                        ><Translatable resourceKey={i18nKey("upgrade.button")} /></Button>
+                {:else if canExtend}
+                    <Button on:click={upgrade} tiny={$mobileWidth} small={!$mobileWidth}
+                        ><Translatable resourceKey={i18nKey("upgrade.extendShort")} /></Button>
+                {/if}
             {/if}
         </Footer>
     </div>
