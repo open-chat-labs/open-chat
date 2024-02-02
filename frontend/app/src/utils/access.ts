c9854472b9d5cbd0ac7cc32d1161551ac793be88
Julian Jelfs <julian.jelfs@gmail.com>
2024-02-02 14:52:41 +0000

    different issuer

--- frontend/app/src/utils/access.ts
+++ frontend/app/src/utils/access.ts
@@ -112,7 +112,7 @@ export const credentialIssuers: CredentialIssuer[] = [
     {
         name: "Employment Info Ltd",
         // origin: "https://v2yvn-myaaa-aaaad-aad4q-cai.icp0.io",
-        origin: "https://vu2yf-xiaaa-aaaad-aad5q-cai.icp0.io/",
+        origin: "https://vu2yf-xiaaa-aaaad-aad5q-cai.icp0.io",
         credentials: [
             { value: "VerifiedEmployee", name: "Is verified employee" },
             // { value: "SomeOther", name: "Some other thing" },
