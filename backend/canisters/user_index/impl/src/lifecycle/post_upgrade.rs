df9a79ea915a9eab9cd19835fdfb059ed604a3c4
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-05 10:13:36 +0000

    Push `DiamondMembershipPaymentReceived` events to all LocalUserIndexes

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,17 +1,13 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::model::pending_payments_queue::{PendingPayment, PendingPaymentReason};
 use crate::{mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
-use icrc_ledger_types::icrc1::account::Account;
-use rand::Rng;
+use local_user_index_canister::Event;
 use stable_memory::get_reader;
 use tracing::info;
-use types::Cryptocurrency;
 use user_index_canister::post_upgrade::Args;
-use utils::consts::SNS_GOVERNANCE_CANISTER_ID;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -42,15 +38,20 @@ fn post_upgrade(args: Args) {
             crate::jobs::sync_legacy_user_principals::start_job_if_required(state);
         }
 
-        // Transfer previously raised funds to the treasury
-        state.data.pending_payments_queue.push(PendingPayment {
-            amount: 365285570000,
-            currency: Cryptocurrency::InternetComputer,
-            timestamp: state.env.now_nanos(),
-            recipient_account: Account::from(SNS_GOVERNANCE_CANISTER_ID),
-            memo: state.env.rng().gen(),
-            reason: PendingPaymentReason::Treasury,
-        });
-        crate::jobs::make_pending_payments::start_job_if_required(state);
+        // Post release - remove this
+        let now = state.env.now();
+        for user in state.data.users.iter() {
+            if let Some(expires_at) = user.diamond_membership_details.expires_at() {
+                if expires_at > now {
+                    for local_user_index_canister_id in state.data.local_index_map.canisters() {
+                        state.data.user_index_event_sync_queue.push(
+                            *local_user_index_canister_id,
+                            Event::DiamondMembershipExpiryDate(user.user_id, expires_at),
+                        );
+                    }
+                }
+            }
+        }
+        crate::jobs::sync_events_to_local_user_index_canisters::start_job_if_required(state);
     });
 }
