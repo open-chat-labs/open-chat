2c6ff06146ac696e01a10ea78e491b742893288a
Matt Grogan <megrogan@gmail.com>
2024-02-08 09:54:32 +0000

    Implement `mark-latest-translations-deployed.sh` (#5335)

--- backend/translation_tool/src/main.rs
+++ backend/translation_tool/src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 use std::process;
-use translation_merger::merge;
-use translation_merger::Config;
+use translation_tool::run;
+use translation_tool::Config;
 
 #[tokio::main]
 async fn main() {
     let config = Config::parse();
 
-    if let Err(e) = merge(config).await {
+    if let Err(e) = run(config).await {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
