3561864e8158c30775c681df475767a15cfe51de
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-12 15:19:31 +0000

    Refund 2 more failed SNEED p2p swaps

--- backend/canisters/escrow/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/escrow/impl/src/lifecycle/post_upgrade.rs
@@ -1,12 +1,15 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::model::pending_payments_queue::{PendingPayment, PendingPaymentReason};
+use crate::{mutate_state, Data};
+use candid::Principal;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use escrow_canister::post_upgrade::Args;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
 use tracing::info;
+use types::UserId;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -24,4 +27,28 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    mutate_state(|state| {
+        let now = state.env.now();
+        if let Some(swap) = state.data.swaps.get_mut(107) {
+            state.data.pending_payments_queue.push(PendingPayment {
+                user_id: UserId::from(Principal::from_text("vfhvn-qyaaa-aaaaf-adoxa-cai").unwrap()),
+                timestamp: now,
+                token_info: swap.token1.clone(),
+                amount: swap.amount1 - swap.token1.fee,
+                swap_id: swap.id,
+                reason: PendingPaymentReason::Refund,
+            });
+        }
+        if let Some(swap) = state.data.swaps.get_mut(148) {
+            state.data.pending_payments_queue.push(PendingPayment {
+                user_id: UserId::from(Principal::from_text("mzhg4-fqaaa-aaaar-ay7iq-cai").unwrap()),
+                timestamp: now,
+                token_info: swap.token1.clone(),
+                amount: swap.amount1 - swap.token1.fee,
+                swap_id: swap.id,
+                reason: PendingPaymentReason::Refund,
+            });
+        }
+    });
 }
