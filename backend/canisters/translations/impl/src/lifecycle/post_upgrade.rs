67264c9bf23052bdcd9f03ba475183e0df8cd5df
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-16 17:57:25 +0000

    Use slightly different timestamps to avoid duplicates

--- backend/canisters/translations/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/translations/impl/src/lifecycle/post_upgrade.rs
@@ -1,12 +1,16 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::model::pending_payments_queue::{PendingPayment, PendingPaymentReason};
+use crate::model::translations::TranslationStatus;
+use crate::{mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
+use std::time::Duration;
 use tracing::info;
 use translations_canister::post_upgrade::Args;
+use types::Cryptocurrency;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -24,4 +28,30 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    ic_cdk_timers::set_timer(Duration::ZERO, || ic_cdk::spawn(retry_failed_payments()));
+}
+
+async fn retry_failed_payments() {
+    mutate_state(|state| {
+        let now = state.env.now();
+
+        for (index, translation) in state
+            .data
+            .translations
+            .iter()
+            .filter(|t| matches!(&t.status, TranslationStatus::Approved(_)))
+            .enumerate()
+        {
+            state.data.pending_payments_queue.push(PendingPayment {
+                recipient_account: translation.proposed.who.into(),
+                timestamp: now - index,
+                currency: Cryptocurrency::CHAT,
+                amount: 100_000_000, // 1 CHAT
+                reason: PendingPaymentReason::Approval,
+            });
+        }
+
+        crate::jobs::make_pending_payments::start_job_if_required(state);
+    });
 }
