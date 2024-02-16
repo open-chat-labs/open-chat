3f84db080866d55ca9eaa8210effcb68a5546d10
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-16 08:42:43 +0000

    Update UserIndex and Escrow canisters post release

--- backend/canisters/user_index/CHANGELOG.md
+++ backend/canisters/user_index/CHANGELOG.md
@@ -5,6 +5,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [unreleased]
 
+### Changed
+
+- Propagate video call operators ids for guarding ([#5374](https://github.com/open-chat-labs/open-chat/pull/5374))
+
+## [[2.0.1061](https://github.com/open-chat-labs/open-chat/releases/tag/v2.0.1061-user_index)] - 2024-02-14
+
 ### Added
 
 - Push backdated `diamond_membership_payment` events ([#5372](https://github.com/open-chat-labs/open-chat/pull/5372))
@@ -12,7 +18,6 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 ### Changed
 
 - Revert job start / stop messages back to `trace` level ([#5370](https://github.com/open-chat-labs/open-chat/pull/5370))
-- Propagate video call operators ids for guarding ([#5374](https://github.com/open-chat-labs/open-chat/pull/5374))
 
 ### Fixed
 
--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,13 +1,10 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::updates::pay_for_diamond_membership::PayForDiamondMembershipEventPayload;
 use crate::{mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
-use event_sink_client::Event;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
-use std::time::Duration;
 use tracing::info;
 use user_index_canister::post_upgrade::Args;
 use utils::cycles::init_cycles_dispenser_client;
@@ -39,37 +36,5 @@ fn post_upgrade(args: Args) {
 
             crate::jobs::sync_legacy_user_principals::start_job_if_required(state);
         }
-    });
-
-    ic_cdk_timers::set_timer(Duration::ZERO, push_diamond_membership_payment_events);
-}
-
-fn push_diamond_membership_payment_events() {
-    mutate_state(|state| {
-        let source = Some(state.env.canister_id().to_text());
-
-        let previous_upgrade = 1707844764000;
-        let events = state.data.users.iter().flat_map(|u| {
-            u.diamond_membership_details
-                .payments()
-                .iter()
-                .filter(|p| p.timestamp < previous_upgrade)
-                .map(|p| Event {
-                    name: "diamond_membership_payment".to_string(),
-                    timestamp: p.timestamp,
-                    user: Some(u.user_id.to_string()),
-                    source: source.clone(),
-                    payload: serde_json::to_vec(&PayForDiamondMembershipEventPayload {
-                        token: p.token.token_symbol().to_string(),
-                        amount: p.amount_e8s,
-                        duration: p.duration.to_string(),
-                        recurring: u.diamond_membership_details.is_recurring(),
-                        manual_payment: p.manual_payment,
-                    })
-                    .unwrap(),
-                })
-        });
-
-        state.data.event_sink_client.push_many(events);
     });
 }
