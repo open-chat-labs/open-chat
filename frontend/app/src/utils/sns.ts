38ecb30091d9dcfe979d33df61e19be5af0fd4fa
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-19 08:45:09 +0000

    Support submitting 'Update Token' proposals (#5396)

--- frontend/app/src/utils/sns.ts
+++ frontend/app/src/utils/sns.ts
@@ -35,3 +35,40 @@ export function createAddTokenPayload(
         ),
     );
 }
+
+export function createUpdateTokenPayload(
+    ledgerCanisterId: string,
+    name: string | undefined,
+    symbol: string | undefined,
+    infoUrl: string | undefined,
+    howToBuyUrl: string | undefined,
+    transactionUrlFormat: string | undefined,
+    logo: string | undefined,
+): Uint8Array {
+    return new Uint8Array(
+        IDL.encode(
+            [
+                IDL.Record({
+                    how_to_buy_url: IDL.Opt(IDL.Text),
+                    info_url: IDL.Opt(IDL.Text),
+                    logo: IDL.Opt(IDL.Text),
+                    name: IDL.Opt(IDL.Text),
+                    ledger_canister_id: IDL.Principal,
+                    symbol: IDL.Opt(IDL.Text),
+                    transaction_url_format: IDL.Opt(IDL.Text),
+                }),
+            ],
+            [
+                {
+                    how_to_buy_url: howToBuyUrl,
+                    info_url: infoUrl,
+                    logo: logo !== undefined && logo.length > 0 ? [logo] : [],
+                    name: name !== undefined && name.length > 0 ? [name] : [],
+                    ledger_canister_id: Principal.fromText(ledgerCanisterId),
+                    symbol: symbol !== undefined && symbol.length > 0 ? [symbol] : [],
+                    transaction_url_format: transactionUrlFormat,
+                },
+            ],
+        ),
+    );
+}
