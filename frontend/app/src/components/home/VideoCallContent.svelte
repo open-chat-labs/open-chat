c5b75fea95117f14bc1a35f5a35055f5e2dd291b
Julian Jelfs <julian.jelfs@gmail.com>
2024-02-19 17:25:50 +0000

    starting to sketch out the video call content component

--- frontend/app/src/components/home/VideoCallContent.svelte
+++ frontend/app/src/components/home/VideoCallContent.svelte
@@ -1,12 +1,78 @@
 <script lang="ts">
-    import type { VideoCallContent } from "openchat-client";
+    import { _ } from "svelte-i18n";
+    import { AvatarSize, OpenChat, type VideoCallContent } from "openchat-client";
+    import Avatar from "../Avatar.svelte";
+    import { getContext } from "svelte";
+    import Translatable from "../Translatable.svelte";
+    import { i18nKey } from "../../i18n/i18n";
+    import Button from "../Button.svelte";
+
+    const client = getContext<OpenChat>("client");
+
+    $: communityMembers = client.currentCommunityMembers;
+    $: userStore = client.userStore;
+    $: initiator = content.participants[0];
+    $: displayName = initiator
+        ? client.getDisplayNameById(initiator.userId, $communityMembers)
+        : $_("unknownUser");
+    $: incall = true;
 
     export let content: VideoCallContent;
 </script>
 
 <div class="video-call">
-    <h1>Video call with: {content.participants}</h1>
+    <p class="initiator">
+        <Translatable resourceKey={i18nKey("videoCall.startedBy", { username: displayName })} />
+    </p>
+    <div class="avatars">
+        {#each [...content.participants].slice(0, 5) as participantId}
+            <Avatar
+                url={client.userAvatarUrl($userStore[participantId.userId])}
+                userId={participantId.userId}
+                size={AvatarSize.Small} />
+        {/each}
+        {#if content.participants.length > 5}
+            <div class="extra">
+                {`+${content.participants.length - 5}`}
+            </div>
+        {/if}
+    </div>
+    {#if incall}
+        <Button fill disabled={content.ended !== undefined}>
+            <Translatable
+                resourceKey={i18nKey(content.ended ? "videoCall.ended" : "videoCall.leave")} />
+        </Button>
+    {:else}
+        <Button fill disabled={content.ended !== undefined}>
+            <Translatable
+                resourceKey={i18nKey(content.ended ? "videoCall.ended" : "videoCall.join")} />
+        </Button>
+    {/if}
 </div>
 
 <style lang="scss">
+    .video-call {
+        padding: $sp3 0 0 0;
+    }
+
+    .initiator {
+        margin-bottom: $sp3;
+    }
+
+    .avatars {
+        display: inline-flex;
+        gap: $sp2;
+        margin-bottom: $sp3;
+    }
+
+    .extra {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        width: toRem(25);
+        height: toRem(25);
+        @include font-size(fs-60);
+        background-color: rgba(0, 0, 0, 0.15);
+    }
 </style>
