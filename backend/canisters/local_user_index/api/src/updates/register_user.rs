3652a5f86349ef2e50fded5d01f9187c4b237b4e
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-20 12:01:42 +0000

    Add `is_from_identity_canister` to `UserRegistered` events

--- backend/canisters/local_user_index/api/src/updates/register_user.rs
+++ backend/canisters/local_user_index/api/src/updates/register_user.rs
@@ -1,13 +1,14 @@
 use candid::CandidType;
 use ic_ledger_types::AccountIdentifier;
 use serde::{Deserialize, Serialize};
+use serde_bytes::ByteBuf;
 use types::UserId;
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub username: String,
     pub referral_code: Option<String>,
-    pub public_key: Vec<u8>,
+    pub public_key: ByteBuf,
 }
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
