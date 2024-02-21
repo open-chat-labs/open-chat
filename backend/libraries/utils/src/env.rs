3a309a8a1902097bb6194fbfeb2f6740b72bcb41
megrogan <megrogan@gmail.com>
2024-02-21 14:44:16 +0000

    Add `new_rng` method to Environment trait

--- backend/libraries/utils/src/env.rs
+++ backend/libraries/utils/src/env.rs
@@ -1,5 +1,8 @@
 use candid::Principal;
 use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
+use sha256::sha256;
 use types::{CanisterId, Cycles, TimestampMillis, TimestampNanos};
 
 pub mod canister;
@@ -14,5 +17,15 @@ pub trait Environment {
 
     fn now(&self) -> TimestampMillis {
         self.now_nanos() / 1_000_000
+    }
+
+    fn new_rng(&mut self) -> StdRng {
+        let mut seed = Vec::new();
+
+        seed.extend(self.rng().gen::<[u8; 32]>());
+        seed.extend(self.caller().as_slice());
+        seed.extend(self.now().to_ne_bytes());
+
+        StdRng::from_seed(sha256(&seed))
     }
 }
