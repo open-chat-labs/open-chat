b24b700a6d480fce9de685c318bdb6c26748df1e
megrogan <megrogan@gmail.com>
2024-02-21 09:48:14 +0000

    Add `user::local_user_index` endpoint

--- backend/canisters/user/api/src/queries/mod.rs
+++ backend/canisters/user/api/src/queries/mod.rs
@@ -6,6 +6,7 @@ pub mod events_by_index;
 pub mod events_window;
 pub mod hot_group_exclusions;
 pub mod initial_state;
+pub mod local_user_index;
 pub mod messages_by_message_index;
 pub mod public_profile;
 pub mod saved_crypto_accounts;
