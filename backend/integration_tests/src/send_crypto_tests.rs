8f4db657829fe8badb34ec87735ddf8483184387
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-22 13:48:11 +0000

    clippy

--- backend/integration_tests/src/send_crypto_tests.rs
+++ backend/integration_tests/src/send_crypto_tests.rs
@@ -3,16 +3,11 @@ use crate::env::ENV;
 use crate::rng::{random_message_id, random_string};
 use crate::utils::{now_nanos, tick_many};
 use crate::{client, TestEnv};
-use candid::Principal;
-use icrc_ledger_types::icrc1::account::Account;
 use ledger_utils::create_pending_transaction;
 use std::ops::Deref;
 use std::time::Duration;
 use test_case::test_case;
-use types::{
-    ChatEvent, CompletedCryptoTransaction, CryptoContent, CryptoTransaction, Cryptocurrency, MessageContent,
-    MessageContentInitial, PendingCryptoTransaction,
-};
+use types::{ChatEvent, CryptoContent, CryptoTransaction, Cryptocurrency, MessageContent, MessageContentInitial};
 
 #[test_case(false)]
 #[test_case(true)]
