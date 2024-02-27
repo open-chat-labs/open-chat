71fff87a267b8fbaa28a495a71735d2d11370852
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-27 12:54:52 +0000

    Add `c2c_vote_on_nns_proposal` to vote on NNS proposals via SNS proposal

--- backend/canisters/neuron_controller/api/src/lifecycle/init.rs
+++ backend/canisters/neuron_controller/api/src/lifecycle/init.rs
@@ -7,6 +7,7 @@ pub struct Args {
     pub governance_principals: Vec<Principal>,
     pub nns_governance_canister_id: CanisterId,
     pub nns_ledger_canister_id: CanisterId,
+    pub proposals_bot_canister_id: CanisterId,
     pub cycles_minting_canister_id: CanisterId,
     pub cycles_dispenser_canister_id: CanisterId,
     pub wasm_version: BuildVersion,
---
+++ backend/canisters/neuron_controller/c2c_client/src/lib.rs
@@ -0,0 +1,7 @@
+use canister_client::generate_candid_c2c_call;
+use neuron_controller_canister::*;
+
+// Queries
+
+// Updates
+generate_candid_c2c_call!(c2c_vote_on_nns_proposal);
