4a9ba230139e6b88588f6a428b3a043a89fe4ed9
Julian Jelfs <julian.jelfs@gmail.com>
2024-03-06 15:24:52 +0000

    include all subdomains for vault bet

--- frontend/app/src/stores/xframe.ts
+++ frontend/app/src/stores/xframe.ts
@@ -13,6 +13,7 @@ const FRAME_ANCESTORS = [
     "https://signalsicp.com",
     "https://gooble.app",
     "https://www.vault-bet.com",
+    "https://vault-bet.com",
     "https://calm-pasca-49d7be.netlify.app", // betbase proof of concept
     "https://221bravo.app",
     "https://ht7v7-iaaaa-aaaak-qakga-cai.icp0.io", //221bravo
