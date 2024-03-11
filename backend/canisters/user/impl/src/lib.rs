784eb9e4c34b1454ee4708b4565e04f5b6cf01e7
Hamish Peebles <hamishpeebles@gmail.com>
2024-03-11 15:44:44 +0000

    Revert "Reset `video_call_operators`" (#5518)

--- backend/canisters/user/impl/src/lib.rs
+++ backend/canisters/user/impl/src/lib.rs
@@ -163,7 +163,6 @@ impl RuntimeState {
                 escrow: self.data.escrow_canister_id,
                 icp_ledger: Cryptocurrency::InternetComputer.ledger_canister_id().unwrap(),
             },
-            video_call_operators: self.data.video_call_operators.clone(),
         }
     }
 }
@@ -202,15 +201,10 @@ struct Data {
     pub token_swaps: TokenSwaps,
     pub p2p_swaps: P2PSwaps,
     pub user_canister_events_queue: CanisterEventSyncQueue<UserCanisterEvent>,
-    #[serde(skip_deserializing, default = "video_call_operators")]
     pub video_call_operators: Vec<Principal>,
     #[serde(default = "event_sink_client")]
     pub event_sink_client: EventSinkClient<CdkRuntime>,
     pub rng_seed: [u8; 32],
-}
-
-fn video_call_operators() -> Vec<Principal> {
-    vec![Principal::from_text("wp3oc-ig6b4-6xvef-yoj27-qt3kw-u2xmp-qbvuv-2grco-s2ndy-wv3ud-7qe").unwrap()]
 }
 
 fn event_sink_client() -> EventSinkClient<CdkRuntime> {
@@ -331,7 +325,6 @@ pub struct Metrics {
     pub direct_chat_metrics: ChatMetrics,
     pub event_sink_client_info: EventSinkClientInfo,
     pub canister_ids: CanisterIds,
-    pub video_call_operators: Vec<Principal>,
 }
 
 fn run_regular_jobs() {
