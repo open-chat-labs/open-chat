441a77230338b8c78c8579cbf0b9e9f00342553f
Hamish Peebles <hamishpeebles@gmail.com>
2024-03-14 00:25:19 +0000

    Retry failed DKP swaps with correct transaction fee

--- backend/canisters/user/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user/impl/src/lifecycle/post_upgrade.rs
@@ -1,11 +1,13 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::timer_job_types::{ProcessTokenSwapJob, TimerJob};
+use crate::{mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
 use tracing::info;
+use types::CanisterId;
 use user_canister::post_upgrade::Args;
 
 #[post_upgrade]
@@ -22,4 +24,24 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    mutate_state(|state| {
+        let dragginz_ledger = CanisterId::from_text("zfcdd-tqaaa-aaaaq-aaaga-cai").unwrap();
+        for swap in state.data.token_swaps.iter_mut().filter(|s| {
+            s.args.output_token.ledger == dragginz_ledger
+                && s.args.output_token.fee == 1000
+                && matches!(s.withdrawn_from_dex_at, Some(Err(_)))
+        }) {
+            let now = state.env.now();
+            swap.args.output_token.fee = 100000;
+            state.data.timer_jobs.enqueue_job(
+                TimerJob::ProcessTokenSwap(Box::new(ProcessTokenSwapJob {
+                    token_swap: swap.clone(),
+                    attempt: 0,
+                })),
+                now,
+                now,
+            );
+        }
+    })
 }
