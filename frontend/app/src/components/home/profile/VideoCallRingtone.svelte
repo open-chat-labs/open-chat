b236d38e96c3701ba452f3e3baa9dd4f8b9cb541
Julian Jelfs <julian.jelfs@gmail.com>
2024-03-14 18:57:57 +0000

    add a waveform for each ringtone

--- frontend/app/src/components/home/profile/VideoCallRingtone.svelte
+++ frontend/app/src/components/home/profile/VideoCallRingtone.svelte
@@ -5,21 +5,35 @@
     import { iconSize } from "../../../stores/iconSize";
     import { createEventDispatcher, onMount } from "svelte";
     import { Ringtone, selectedRingtone } from "../../../stores/video";
+    import WaveSurfer from "wavesurfer.js";
+    import { currentTheme } from "../../../theme/themes";
 
     const dispatch = createEventDispatcher();
 
     export let ringtone: Ringtone;
 
-    let canvas: HTMLCanvasElement;
-    let ctx: CanvasRenderingContext2D | null;
+    let waveform: HTMLDivElement;
 
     $: checked = $selectedRingtone === ringtone.key;
 
     onMount(() => {
-        ctx = canvas.getContext("2d");
-        setTimeout(() => {
-            drawWaveform();
-        }, 2000);
+        const wavesurfer = WaveSurfer.create({
+            height: 30,
+            cursorWidth: 0,
+            barWidth: 2,
+            barRadius: 4,
+            barGap: 2,
+            container: waveform,
+            waveColor: $currentTheme["txt-light"],
+            progressColor: $currentTheme.accent,
+            media: ringtone.audio,
+        });
+
+        wavesurfer.on("click", () => {
+            if (!ringtone.playing) {
+                togglePlay();
+            }
+        });
     });
 
     function togglePlay() {
@@ -29,76 +43,30 @@
     function selectRingtone() {
         selectedRingtone.set(ringtone.key);
     }
-
-    function drawWaveform() {
-        if (!ctx) return;
-
-        const audioContext = new AudioContext();
-        const source = audioContext.createMediaElementSource(ringtone.audio);
-        const analyser = audioContext.createAnalyser();
-        source.connect(analyser);
-        analyser.connect(audioContext.destination);
-        analyser.fftSize = 2048;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-
-        // Clear canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Draw waveform
-        function draw() {
-            if (!ctx) return;
-
-            const sliceWidth = (canvas.width * 1.0) / bufferLength;
-            let x = 0;
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "#00f";
-
-            analyser.getByteTimeDomainData(dataArray);
-
-            ctx.beginPath();
-            for (let i = 0; i < bufferLength; i++) {
-                const v = dataArray[i] / 128.0;
-                const y = (v * canvas.height) / 2;
-                if (i === 0) {
-                    ctx.moveTo(x, y);
-                } else {
-                    ctx.lineTo(x, y);
-                }
-                x += sliceWidth;
-            }
-            ctx.lineTo(canvas.width, canvas.height / 2);
-            ctx.stroke();
-        }
-
-        // Render the waveform
-        function render() {
-            draw();
-            // requestAnimationFrame(render);
-        }
-
-        render();
-    }
 </script>
 
-<Radio on:change={selectRingtone} {checked} id={ringtone.name} group="video-ringtone">
-    <div class="ringtone">
-        <div class="name">{ringtone.name}</div>
-        <div on:click|preventDefault={togglePlay} class="play">
-            {#if ringtone.playing}
-                <PauseCircleOutline size={$iconSize} color={"var(--icon-selected)"} />
-            {:else}
-                <PlayCircleOutline size={$iconSize} color={"var(--icon-txt)"} />
-            {/if}
+<div class="wrapper">
+    <Radio on:change={selectRingtone} {checked} id={ringtone.name} group="video-ringtone">
+        <div class="ringtone">
+            <div class="name">{ringtone.name}</div>
+            <div on:click|preventDefault={togglePlay} class="play">
+                {#if ringtone.playing}
+                    <PauseCircleOutline size={$iconSize} color={"var(--icon-selected)"} />
+                {:else}
+                    <PlayCircleOutline size={$iconSize} color={"var(--icon-txt)"} />
+                {/if}
+            </div>
         </div>
-    </div>
-</Radio>
-
-<canvas bind:this={canvas} />
+    </Radio>
+    <div bind:this={waveform} class="waveform"></div>
+</div>
 
 <style lang="scss">
-    canvas {
-        height: 50px;
+    .wrapper {
+        border: var(--bw) solid var(--bd);
+        border-radius: var(--rd);
+        padding: $sp3;
+        margin-bottom: $sp3;
     }
 
     .ringtone {
