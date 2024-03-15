260ac81f4c5f3b1c654af886e3a63ba860637c7e
megrogan <megrogan@gmail.com>
2024-03-15 08:53:09 +0000

    Join video calls for free

--- frontend/app/src/components/home/upgrade/Features.svelte
+++ frontend/app/src/components/home/upgrade/Features.svelte
@@ -189,6 +189,16 @@
     </Feature>
 
     <Feature {landing}>
+        <div slot="title"><Translatable resourceKey={i18nKey("upgrade.joinVideoCalls")} /></div>
+        <div slot="free">
+            <Check size={"1em"} color={"limegreen"} />
+        </div>
+        <div slot="diamond">
+            <Check size={"1em"} color={"limegreen"} />
+        </div>
+    </Feature>
+
+    <Feature {landing}>
         <div slot="title"><Translatable resourceKey={i18nKey("upgrade.p2pSwap")} /></div>
         <div slot="free">
             <Minus size={"1em"} color={"var(--menu-warn)"} />
@@ -226,7 +236,7 @@
         <div slot="diamond"><Translatable resourceKey={i18nKey("upgrade.eligible")} /></div>
     </Feature>
 
-    <Feature {landing} comingSoon>
+    <!-- <Feature {landing} comingSoon>
         <div slot="title"><Translatable resourceKey={i18nKey("upgrade.customThemes")} /></div>
         <div slot="free">
             <Minus size={"1em"} color={"var(--menu-warn)"} />
@@ -244,7 +254,7 @@
         <div slot="diamond">
             <Check size={"1em"} color={"limegreen"} />
         </div>
-    </Feature>
+    </Feature> -->
 </div>
 
 {#if !landing}
