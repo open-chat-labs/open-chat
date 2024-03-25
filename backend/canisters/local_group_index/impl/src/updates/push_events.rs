cf5e041911f77e19393dccbe25cc5d2baeb95597
Hamish Peebles <hamishpeebles@gmail.com>
2024-03-25 14:48:03 +0000

    Update `event_store` packages to latest version

--- backend/canisters/local_group_index/impl/src/updates/push_events.rs
+++ backend/canisters/local_group_index/impl/src/updates/push_events.rs
@@ -1,34 +1,13 @@
 use crate::guards::caller_is_local_group_or_community_canister;
 use crate::{mutate_state, RuntimeState};
 use canister_tracing_macros::trace;
-use event_store_canister::{Anonymizable, IdempotentEvent};
 use ic_cdk_macros::update;
-use local_group_index_canister::push_events::Args as ArgsPrevious;
-use local_group_index_canister::push_events_v2::Args;
+use local_group_index_canister::push_events::Args;
 
 #[update(guard = "caller_is_local_group_or_community_canister")]
 #[trace]
-fn push_events(args: ArgsPrevious) {
-    mutate_state(|state| {
-        push_events_impl(
-            Args {
-                events: args
-                    .events
-                    .into_iter()
-                    .filter(|e| e.name != "message_sent")
-                    .map(|e| IdempotentEvent {
-                        idempotency_key: e.idempotency_key,
-                        name: e.name,
-                        timestamp: e.timestamp,
-                        user: e.user.map(|u| Anonymizable::new(u, true)),
-                        source: e.source.map(|s| Anonymizable::new(s, true)),
-                        payload: e.payload,
-                    })
-                    .collect(),
-            },
-            state,
-        )
-    })
+fn push_events(args: Args) {
+    mutate_state(|state| push_events_impl(args, state))
 }
 
 #[update(guard = "caller_is_local_group_or_community_canister")]
