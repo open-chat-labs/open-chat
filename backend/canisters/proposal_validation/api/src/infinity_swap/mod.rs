a842dd3417c0ba6f3c94f3d582081db2df45b22b
Hamish Peebles <hamishpeebles@gmail.com>
2024-03-26 08:58:53 +0000

    Add validation function for `manage_neuron` proposals (#5595)

--- backend/canisters/proposal_validation/api/src/infinity_swap/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod burn;
-pub mod mint;
-pub mod receive_tokens;
-pub mod refund_transfer;
--- backend/canisters/proposal_validation/impl/src/lib.rs
+++ backend/canisters/proposal_validation/impl/src/lib.rs
@@ -1,7 +1,4 @@
 use canister_api_macros::proposal_validation;
 use proposal_validation_canister::*;
 
-proposal_validation!(infinity_swap, burn);
-proposal_validation!(infinity_swap, mint);
-proposal_validation!(infinity_swap, receive_tokens);
-proposal_validation!(infinity_swap, refund_transfer);
+proposal_validation!(nns_governance, manage_neuron, false);
