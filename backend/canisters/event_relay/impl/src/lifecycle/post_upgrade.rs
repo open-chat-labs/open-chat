7f06a2f6137ca7b00974067ee8abfb7716f56a9b
Hamish Peebles <hamishpeebles@gmail.com>
2024-03-27 11:23:12 +0000

    Remove code to anonymize events since that now happens in the EventStore

--- backend/canisters/event_relay/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/event_relay/impl/src/lifecycle/post_upgrade.rs
@@ -1,13 +1,12 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::{read_state, Data};
+use crate::Data;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use event_relay_canister::post_upgrade::Args;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
-use std::time::Duration;
-use tracing::{error, info};
+use tracing::info;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -25,21 +24,4 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
-
-    ic_cdk_timers::set_timer(Duration::ZERO, || ic_cdk::spawn(sync_salt_to_event_store()));
-}
-
-async fn sync_salt_to_event_store() {
-    let (event_store_canister_id, salt) = read_state(|state| {
-        (
-            state.data.event_store_client.info().event_store_canister_id,
-            state.data.salt.get(),
-        )
-    });
-
-    if let Err(error) = ic_cdk::call::<_, ()>(event_store_canister_id, "set_salt", (salt,)).await {
-        error!("Failed to sync salt to event store. Error: {error:?}")
-    } else {
-        info!("Salt synced to event store");
-    }
 }
