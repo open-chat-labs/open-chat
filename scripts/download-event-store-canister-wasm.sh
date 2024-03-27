fb6758f3a7d74ea173786d7670d05387e2f94d47
Hamish Peebles <hamishpeebles@gmail.com>
2024-03-27 22:50:21 +0000

    Avoid need for DFX in script

--- scripts/download-event-store-canister-wasm.sh
+++ scripts/download-event-store-canister-wasm.sh
@@ -4,5 +4,18 @@ SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 cd $SCRIPT_DIR/..
 
-dfx build --ic event_store
-cp .dfx/ic/canisters/event_store/event_store.wasm.gz wasms
+URL=$(jq '.canisters.event_store.wasm' dfx.json)
+URL=$(echo "$URL" | tr -d '"')
+echo $URL
+
+mkdir -p wasms
+cd wasms
+
+HTTP_CODE=$(curl -sOL $URL --write-out "%{http_code}")
+
+if [[ ${HTTP_CODE} -ne 200 ]] ; then
+    echo "Failed to download wasm. Response code: ${HTTP_CODE}"
+    exit 1
+fi
+
+echo "Wasm downloaded"
