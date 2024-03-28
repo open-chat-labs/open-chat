4ca87035843eb6f155db6ad86ec7aa1bf8ea648a
Hamish Peebles <hamishpeebles@gmail.com>
2024-03-28 13:21:53 +0000

    Refactor `c2c_can_issue_access_token`

--- backend/canisters/community/impl/src/queries/c2c_can_issue_access_token_for_channel.rs
+++ backend/canisters/community/impl/src/queries/c2c_can_issue_access_token_for_channel.rs
@@ -11,9 +11,6 @@ fn c2c_can_issue_access_token_for_channel(args: Args) -> Response {
 }
 
 fn c2c_can_issue_access_token_for_channel_impl(args: Args, state: &RuntimeState) -> bool {
-    let joining = matches!(args.access_type, AccessTokenType::JoinVideoCall);
-    let ending = matches!(args.access_type, AccessTokenType::MarkVideoCallAsEnded);
-
     let Some(channel) = state.data.channels.get(&args.channel_id) else {
         return false;
     };
@@ -22,5 +19,10 @@ fn c2c_can_issue_access_token_for_channel_impl(args: Args, state: &RuntimeState)
         return false;
     };
 
-    ending || joining || (args.is_diamond && member.role.is_permitted(channel.chat.permissions.start_video_call))
+    match args.access_type {
+        AccessTokenType::StartVideoCall => {
+            args.is_diamond && member.role.is_permitted(channel.chat.permissions.start_video_call)
+        }
+        AccessTokenType::JoinVideoCall | AccessTokenType::MarkVideoCallAsEnded => true,
+    }
 }
--- backend/canisters/group/impl/src/queries/c2c_can_issue_access_token.rs
+++ backend/canisters/group/impl/src/queries/c2c_can_issue_access_token.rs
@@ -11,12 +11,14 @@ fn c2c_can_issue_access_token(args: Args) -> Response {
 }
 
 fn c2c_can_issue_access_token_impl(args: Args, state: &RuntimeState) -> bool {
-    let joining = matches!(args.access_type, AccessTokenType::JoinVideoCall);
-    let ending = matches!(args.access_type, AccessTokenType::MarkVideoCallAsEnded);
-
     let Some(member) = state.data.chat.members.get(&args.user_id) else {
         return false;
     };
 
-    ending || joining || (args.is_diamond && member.role.is_permitted(state.data.chat.permissions.start_video_call))
+    match args.access_type {
+        AccessTokenType::StartVideoCall => {
+            args.is_diamond && member.role.is_permitted(state.data.chat.permissions.start_video_call)
+        }
+        AccessTokenType::JoinVideoCall | AccessTokenType::MarkVideoCallAsEnded => true,
+    }
 }
