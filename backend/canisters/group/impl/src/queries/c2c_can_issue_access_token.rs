e21a9f80d3bee3e0ba2bb82ab8058f1bf09516ea
Julian Jelfs <julian.jelfs@gmail.com>
2024-03-28 12:38:51 +0000

    Add new access token type for marking a video call as ended (#5611)
    Co-authored-by: Hamish Peebles <hamishpeebles@gmail.com>

--- backend/canisters/group/impl/src/queries/c2c_can_issue_access_token.rs
+++ backend/canisters/group/impl/src/queries/c2c_can_issue_access_token.rs
@@ -12,10 +12,11 @@ fn c2c_can_issue_access_token(args: Args) -> Response {
 
 fn c2c_can_issue_access_token_impl(args: Args, state: &RuntimeState) -> bool {
     let joining = matches!(args.access_type, AccessTokenType::JoinVideoCall);
+    let ending = matches!(args.access_type, AccessTokenType::MarkVideoCallAsEnded);
 
     let Some(member) = state.data.chat.members.get(&args.user_id) else {
         return false;
     };
 
-    joining || (args.is_diamond && member.role.is_permitted(state.data.chat.permissions.start_video_call))
+    ending || joining || (args.is_diamond && member.role.is_permitted(state.data.chat.permissions.start_video_call))
 }
