b9f2676742cdcc74438f24eea28b96c6964771de
Hamish Peebles <hamishpeebles@gmail.com>
2024-04-02 21:10:01 +0100

    Fix candid

--- backend/canisters/cycles_dispenser/impl/src/jobs/top_up_sns_canisters.rs
+++ backend/canisters/cycles_dispenser/impl/src/jobs/top_up_sns_canisters.rs
@@ -29,8 +29,8 @@ async fn run_async(canister_id: CanisterId) {
         ]
         .into_iter()
         .flatten()
-        .chain(response.archives)
         .chain(response.dapps)
+        .chain(response.archives)
         .filter(requires_top_up)
         .map(|s| s.canister_id.unwrap())
         .collect();
