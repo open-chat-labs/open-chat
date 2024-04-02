8aef4960c16be57065cdde54d251c984d5336f94
Hamish Peebles <hamishpeebles@gmail.com>
2024-04-02 12:30:30 +0100

    Fix name

--- backend/canisters/user/impl/src/queries/get_cached_btc_address.rs
+++ backend/canisters/user/impl/src/queries/get_cached_btc_address.rs
@@ -4,7 +4,7 @@ use ic_cdk_macros::query;
 use user_canister::get_cached_btc_address::{Response::*, *};
 
 #[query(guard = "caller_is_owner")]
-async fn get_btc_address(_args: Args) -> Response {
+async fn get_cached_btc_address(_args: Args) -> Response {
     match read_state(|state| state.data.btc_address.clone()) {
         Some(btc_address) => Success(btc_address),
         None => NotFound,
