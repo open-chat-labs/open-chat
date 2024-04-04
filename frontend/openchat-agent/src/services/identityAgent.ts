8a962c05f9de2b7be06d7b288ce515b0a34f0213
Hamish Peebles <hamishpeebles@gmail.com>
2024-04-04 16:30:11 +0100

    Finish implementing `identityAgent` (#5623)

--- frontend/openchat-agent/src/services/identityAgent.ts
+++ frontend/openchat-agent/src/services/identityAgent.ts
@@ -1,16 +1,12 @@
 import { IdentityClient } from "./identity/identity.client";
-import type { DerEncodedPublicKey, SignIdentity } from "@dfinity/agent";
+import type { DerEncodedPublicKey, Identity, SignIdentity } from "@dfinity/agent";
 import { DelegationChain, DelegationIdentity } from "@dfinity/identity";
 import type { CheckAuthPrincipalResponse, MigrateLegacyPrincipalResponse } from "openchat-shared";
 
 export class IdentityAgent {
     private _identityClient: IdentityClient;
 
-    constructor(
-        private identity: SignIdentity,
-        identityCanister: string,
-        icUrl: string,
-    ) {
+    constructor(identity: Identity, identityCanister: string, icUrl: string) {
         this._identityClient = IdentityClient.create(identity, identityCanister, icUrl);
     }
 
@@ -22,15 +18,46 @@ export class IdentityAgent {
         return this._identityClient.migrateLegacyPrincipal();
     }
 
-    async getOpenChatIdentity(sessionKey: Uint8Array): Promise<DelegationIdentity | undefined> {
-        const prepareDelegationResponse = await this._identityClient.prepareDelegation(sessionKey);
-        if (prepareDelegationResponse.kind === "not_found") {
-            return undefined;
-        }
+    async createOpenChatIdentity(
+        sessionKey: SignIdentity,
+    ): Promise<DelegationIdentity | undefined> {
+        const sessionKeyDer = toDer(sessionKey);
+        const createIdentityResponse = await this._identityClient.createIdentity(sessionKeyDer);
 
+        return createIdentityResponse.kind === "success"
+            ? this.getDelegation(
+                  createIdentityResponse.userKey,
+                  sessionKey,
+                  sessionKeyDer,
+                  createIdentityResponse.expiration,
+              )
+            : undefined;
+    }
+
+    async getOpenChatIdentity(sessionKey: SignIdentity): Promise<DelegationIdentity | undefined> {
+        const sessionKeyDer = toDer(sessionKey);
+        const prepareDelegationResponse =
+            await this._identityClient.prepareDelegation(sessionKeyDer);
+
+        return prepareDelegationResponse.kind === "success"
+            ? this.getDelegation(
+                  prepareDelegationResponse.userKey,
+                  sessionKey,
+                  sessionKeyDer,
+                  prepareDelegationResponse.expiration,
+              )
+            : undefined;
+    }
+
+    private async getDelegation(
+        userKey: Uint8Array,
+        sessionKey: SignIdentity,
+        sessionKeyDer: Uint8Array,
+        expiration: bigint,
+    ): Promise<DelegationIdentity | undefined> {
         const getDelegationResponse = await this._identityClient.getDelegation(
-            sessionKey,
-            prepareDelegationResponse.expiration,
+            sessionKeyDer,
+            expiration,
         );
 
         if (getDelegationResponse.kind === "not_found") {
@@ -46,9 +73,13 @@ export class IdentityAgent {
 
         const delegationChain = DelegationChain.fromDelegations(
             delegations,
-            prepareDelegationResponse.userKey.buffer as DerEncodedPublicKey,
+            userKey.buffer as DerEncodedPublicKey,
         );
 
-        return DelegationIdentity.fromDelegation(this.identity, delegationChain);
+        return DelegationIdentity.fromDelegation(sessionKey, delegationChain);
     }
+}
+
+function toDer(key: SignIdentity): Uint8Array {
+    return new Uint8Array(key.getPublicKey().toDer() as ArrayBuffer);
 }
