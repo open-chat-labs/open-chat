057aa4ece5a113bb70b48fe0d84df33a4bcf3ddd
Hamish Peebles <hamishpeebles@gmail.com>
2024-04-04 12:56:34 +0100

    Revert `identityStorage`

--- frontend/openchat-shared/src/utils/identityStorage.ts
+++ frontend/openchat-shared/src/utils/identityStorage.ts
@@ -1,16 +1,49 @@
-import { type AuthClientStorage, IdbStorage } from "@dfinity/auth-client";
+import { IdbKeyVal } from "@dfinity/auth-client";
 import { DelegationChain, DelegationIdentity, ECDSAKeyIdentity } from "@dfinity/identity";
 
+const DB_VERSION = 1;
 const KEY_STORAGE_KEY = "identity";
 const KEY_STORAGE_DELEGATION = "delegation";
 
+class IdbStorage {
+    // Initializes a KeyVal on first request
+    private initializedDb: IdbKeyVal | undefined = undefined;
+    get _db(): Promise<IdbKeyVal> {
+        return new Promise((resolve) => {
+            if (this.initializedDb) {
+                resolve(this.initializedDb);
+                return;
+            }
+            IdbKeyVal.create({ version: DB_VERSION, dbName: "oc-auth-db" }).then((db) => {
+                this.initializedDb = db;
+                resolve(db);
+            });
+        });
+    }
+
+    public async get<T = string>(key: string): Promise<T | null> {
+        const db = await this._db;
+        return await db.get(key);
+    }
+
+    public async set<T>(key: string, value: T): Promise<void> {
+        const db = await this._db;
+        await db.set(key, value);
+    }
+
+    public async remove(key: string): Promise<void> {
+        const db = await this._db;
+        await db.remove(key);
+    }
+}
+
 export class IdentityStorage {
-    private storage: AuthClientStorage = new IdbStorage({ dbName: "oc-auth-db" });
+    private storage = new IdbStorage();
 
     async get(): Promise<DelegationIdentity | undefined> {
-        const key = (await this.storage.get(KEY_STORAGE_KEY)) as CryptoKeyPair;
+        const key = await this.storage.get<CryptoKeyPair>(KEY_STORAGE_KEY);
         if (key == null) return undefined;
-        const chain = (await this.storage.get(KEY_STORAGE_DELEGATION)) as string;
+        const chain = await this.storage.get(KEY_STORAGE_DELEGATION);
         if (chain == null) return undefined;
 
         const id = await ECDSAKeyIdentity.fromKeyPair(key);
@@ -20,7 +53,7 @@ export class IdentityStorage {
 
     async set(key: ECDSAKeyIdentity, chain: DelegationChain): Promise<void> {
         await this.storage.set(KEY_STORAGE_KEY, key.getKeyPair());
-        await this.storage.set(KEY_STORAGE_DELEGATION, JSON.stringify(chain.toJSON()));
+        await this.storage.set(KEY_STORAGE_DELEGATION, chain.toJSON());
     }
 
     async remove(): Promise<void> {
