3e4e6efd9e3da8f96fcce315e79166341a759a74
megrogan <megrogan@gmail.com>
2024-04-08 14:56:40 +0100

    Slight text adjustment

--- scripts/proposals/add_platform_moderator.sh
+++ scripts/proposals/add_platform_moderator.sh
@@ -3,7 +3,7 @@
 # Extract the args or use defaults
 USER_ID=$1
 USER_NAME=$2
-SUMMARY=${3:-"Platform moderators can perform some privileged actions to help moderate OpenChat in response to breaches of the [platform rules](https://oc.app/guidelines?section=3).\n\nThey can delete messages and suspend users. In the case of rogue owners they can also assume ownership of groups/communities that they are members of."}
+SUMMARY=${3:-"Platform moderators can perform some privileged actions to help moderate OpenChat in response to breaches of the [platform rules](https://oc.app/guidelines?section=3).\n\nThey can delete messages and suspend users. In the case of rogue owners they can also assume ownership of groups/communities they are members of."}
 
 # Build the title
 TITLE="Add user $USER_NAME ($USER_ID) to the list of platform moderators"
