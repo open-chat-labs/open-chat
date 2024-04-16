8990df242a993287a36311535154ae7c711c3695
Hamish Peebles <hamishpeebles@gmail.com>
2024-04-16 11:31:04 +0100

    Set to true after cutoff

--- backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
@@ -1,7 +1,7 @@
 use crate::jobs::import_groups::finalize_group_import;
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::{read_state, Data};
+use crate::{mutate_state, read_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use community_canister::post_upgrade::Args;
@@ -36,5 +36,11 @@ fn post_upgrade(args: Args) {
         state
             .data
             .record_instructions_count(InstructionCountFunctionId::PostUpgrade, now)
+    });
+
+    mutate_state(|state| {
+        for channel in state.data.channels.iter_mut() {
+            channel.chat.events.set_block_level_markdown(1710152259000);
+        }
     });
 }
