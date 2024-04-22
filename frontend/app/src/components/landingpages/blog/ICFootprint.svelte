c86faec487bb545f2364d16dc596b78601c32f3c
Julian Jelfs <julian.jelfs@gmail.com>
2024-04-22 13:07:44 +0100

    plumbing for ic footprint post

---
+++ frontend/app/src/components/landingpages/blog/ICFootprint.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<section>
+    <p>
+        Setting a new benchmark of environmental responsibility in the tech industry. OpenChat
+        becomes the first DAO to integrate IC Footprint 2.0 — a sustainability protocol that
+        harnesses the power of ICP smart contracts. This real-time system tracks and records key
+        environmental metrics like electricity consumption and carbon emissions directly onto an
+        immutable on-chain ledger. Raising consumer awareness about climate issues and increasingly
+        stringent environmental regulations are fuelling a growing demand for eco-conscious
+        technology especially in crypto.
+    </p>
+
+    <p>
+        Moreover, OpenChat commits to actively decarbonising by acquiring renewable energy
+        certificates and carbon credits. Future developments include embedding OpenChat’s
+        zero-emissions commitment into a custom ESG smart contract, ensuring automatic compliance
+        with its sustainability goals. This innovation underscores OpenChat’s role as a pioneer in
+        Web3 and clean crypto.
+    </p>
+
+    <p>
+        OpenChat's dedication to sustainability strengthens its competitive edge. By focusing on
+        eco-friendly practices, OpenChat attracts more eco-conscious users and stands out from its
+        Web2 competitors like Discord, WhatsApp, and Slack.
+    </p>
+
+    <p>
+        As OpenChat advances its sustainable practices, it serves as a model for other digital
+        platforms, showing that technological innovation and environmental responsibility can
+        coexist.
+    </p>
+
+    <p>Join OpenChat and our movement toward a sustainable Web3 today.</p>
+</section>
