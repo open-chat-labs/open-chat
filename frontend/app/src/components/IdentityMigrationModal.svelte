25e90fe2216c89e37e6eaa0e9d6ffcf6bcf446c3
megrogan <megrogan@gmail.com>
2024-04-24 11:20:03 +0100

    Poll for identity migration progress

--- frontend/app/src/components/IdentityMigrationModal.svelte
+++ frontend/app/src/components/IdentityMigrationModal.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
-    import { getContext } from "svelte";
+    import { getContext, onDestroy } from "svelte";
     import Translatable from "./Translatable.svelte";
     import { i18nKey } from "../i18n/i18n";
     import ModalContent from "./ModalContent.svelte";
     import Progress from "./Progress.svelte";
-    import type { OpenChat } from "openchat-client";
+    import { Poller, type OpenChat } from "openchat-client";
 
     const client = getContext<OpenChat>("client");
+
+    const POLL_INTERVAL = 2000;
+    let poller = new Poller(getProgress, POLL_INTERVAL);
+
+    onDestroy(() => poller.stop());
 
     $: user = client.user;
     $: percent =
         $user.principalUpdates !== undefined
             ? (100 * $user.principalUpdates[0]) / $user.principalUpdates[1]
             : 100;
+
+    function getProgress(): Promise<void> {
+        return client.getIdentityMigrationProgress().then((progress) => {
+            user.update((user) => ({
+                ...user,
+                principalUpdates: progress,
+            }));
+        });
+    }
 </script>
 
 <ModalContent>
--- frontend/openchat-client/src/openchat.ts
+++ frontend/openchat-client/src/openchat.ts
@@ -4138,6 +4138,10 @@ export class OpenChat extends OpenChatAgentWorker {
         });
     }
 
+    getIdentityMigrationProgress(): Promise<[number, number] | undefined> {
+        return this.sendRequest({ kind: "getIdentityMigrationProgress" });
+    }
+
     getDisplayNameById(userId: string, communityMembers?: Map<string, Member>): string {
         const user = this._liveState.userStore[userId];
         return this.getDisplayName(user, communityMembers);
