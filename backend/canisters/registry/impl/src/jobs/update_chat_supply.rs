599c780a13c180ca84d9c7b0585be900d4dd0004
Hamish Peebles <hamishpeebles@gmail.com>
2024-05-14 16:04:05 +0100

    Simplify

--- backend/canisters/registry/impl/src/jobs/update_chat_supply.rs
+++ backend/canisters/registry/impl/src/jobs/update_chat_supply.rs
@@ -1,5 +1,10 @@
-use crate::{mutate_state, read_state};
+use crate::mutate_state;
+use candid::Principal;
+use ic_cdk::api::call::CallResult;
 use icrc_ledger_types::icrc1::account::Account;
+use sha256::sha256;
+use sns_governance_canister::types::NeuronId;
+use std::collections::HashSet;
 use std::time::Duration;
 use tracing::info;
 use types::Timestamped;
@@ -12,63 +17,97 @@ pub fn start_job() {
 }
 
 fn run() {
-    ic_cdk::spawn(run_async());
+    ic_cdk::spawn(async {
+        let _ = run_async().await;
+    });
 }
 
-async fn run_async() {
-    let Ok(total_supply) = icrc_ledger_canister_c2c_client::icrc1_total_supply(SNS_LEDGER_CANISTER_ID)
+async fn run_async() -> CallResult<()> {
+    let total_supply = icrc_ledger_canister_c2c_client::icrc1_total_supply(SNS_LEDGER_CANISTER_ID)
         .await
-        .map(|s| u128::try_from(s.0).unwrap())
-    else {
-        return;
-    };
+        .map(|s| u128::try_from(s.0).unwrap())?;
 
-    let reserved_accounts: Vec<_> = [
-        "d1216f443ead88f8f98a80b2ea59697726f18dffaa58d0a0156d0c605a01b672",
-        "bf51477b537b8fc97cff67c13546b05c863cd989fc765b8a88bc8e8e0e9f5716",
-        "33188d099055359d6db7e0d3fda880688bbf492fcff54bf87bd4842db8715c42",
-        "616cf7d21245ffbbd5bf48a92aa924794c47a5dc2e5d5b2994cd7b03e5c3d18d",
-        "0c9288198b6dc21da28113a842f1c6cb4248eb54273f1b2702c12afbeb5b416f",
-        "a216f4012a24998c037db6b334642406640c2cedbb84a80ad8f54dbaa0ebeab1",
-        "1b750f0a5bbed846a5f59824daf3eaeb4ae2a4a1ceb81ad9f36a1c67b0ffa0e0",
-        "005ef4ffe50d9d4d73f08665d7831d3495e3f988a66d6d22f9563a64aa2a7a91",
-        "92f7588ca294b88948d99bb322f392ae3345de2e0ae72f6250e51660e55e5a99",
-        "56b8c3adaf4ecfc9bb142d6b769bb502b50fb35430274684f0f07feac4a08229",
-        "4b0602348f89b48142205d893a71bd33d8f682c296e9a1f3d947e19f8191a790",
-        "8aad81c1369fe9644621de21e2eb6eea6e3cce6a4b8b8e11831f41418d7a36f0",
-        "6066ff1f82af224a526316857dd8ec44f261b49f7ab44343fe4966a08c734be7",
-        "c0e287b65453b7c155de604fd063d0f6da1feacb0bc1ce48fc0d525760fd777f",
-        "66b576a92b187684f22b6816697ec19e01b0b328eaf14dd137838a463d49b729",
-        "a8b30777cc4ae5eeaaece833ec7dc178a515d4cd433c955d679c504daeb6f226",
-        "ae2eb3446bb2fc227260a0b49a0f362d6d0f2eb8f2e6db09530c13606c36120f",
+    let insiders: Vec<_> = [
+        "zeu5w-lsfxj-o4f5b-xl4sg-enqva-ez4wa-2nlbb-ufigi-fwnmv-zrfok-uae", // Hamish
+        "liskq-yvpvq-eo6av-3gaz6-izybe-x74ku-t42w5-si5dq-eitcy-sih4g-lqe", // Matt
+        "4s36z-lcydz-e4ts5-nuzlo-h3ksp-uu4fe-xndls-pq6ah-mmv5k-x5rjw-hqe", // Julian
+        "n2xex-iyaaa-aaaar-qaaeq-cai",                                     // Dfinity
     ]
     .into_iter()
-    .map(|sa| Account {
-        owner: SNS_GOVERNANCE_CANISTER_ID,
-        subaccount: Some(hex::decode(sa).unwrap().try_into().unwrap()),
-    })
+    .map(|s| Principal::from_text(s).unwrap())
     .collect();
 
+    let mut reserved_subaccounts: HashSet<[u8; 32]> = HashSet::new();
+    reserved_subaccounts.insert(compute_treasury_subaccount());
+    for principal in insiders {
+        for neuron_id in neuron_ids_by_principal(principal).await? {
+            reserved_subaccounts.insert(neuron_id.id.try_into().unwrap());
+        }
+    }
+
     let mut reserved = 0;
-    for account in reserved_accounts {
-        let Ok(balance) = icrc_ledger_canister_c2c_client::icrc1_balance_of(SNS_LEDGER_CANISTER_ID, &account)
-            .await
-            .map(|b| u128::try_from(b.0).unwrap())
-        else {
-            return;
-        };
+    for subaccount in reserved_subaccounts {
+        let balance = icrc_ledger_canister_c2c_client::icrc1_balance_of(
+            SNS_LEDGER_CANISTER_ID,
+            &Account {
+                owner: SNS_GOVERNANCE_CANISTER_ID,
+                subaccount: Some(subaccount),
+            },
+        )
+        .await
+        .map(|b| u128::try_from(b.0).unwrap())?;
 
         reserved += balance;
     }
 
-    let now = read_state(|state| state.env.now());
-
     let circulating_supply = total_supply.saturating_sub(reserved);
 
     mutate_state(|state| {
+        let now = state.env.now();
         state.data.total_supply = Timestamped::new(total_supply, now);
         state.data.circulating_supply = Timestamped::new(circulating_supply, now);
     });
 
     info!(total_supply, circulating_supply, "CHAT supply updated");
+    Ok(())
+}
+
+async fn neuron_ids_by_principal(principal: Principal) -> CallResult<Vec<NeuronId>> {
+    let mut neuron_ids = Vec::new();
+
+    loop {
+        let response = sns_governance_canister_c2c_client::list_neurons(
+            SNS_GOVERNANCE_CANISTER_ID,
+            &sns_governance_canister::list_neurons::Args {
+                of_principal: Some(principal),
+                limit: 20,
+                start_page_at: neuron_ids.last().cloned(),
+            },
+        )
+        .await?;
+
+        let count = response.neurons.len();
+        neuron_ids.extend(response.neurons.into_iter().map(|n| n.id.unwrap()));
+
+        if count < 20 {
+            break;
+        }
+    }
+
+    Ok(neuron_ids)
+}
+
+fn compute_treasury_subaccount() -> [u8; 32] {
+    const DOMAIN: &[u8] = b"token-distribution";
+    const DOMAIN_LENGTH: [u8; 1] = [0x12];
+
+    let principal = SNS_GOVERNANCE_CANISTER_ID;
+    let nonce = 0u64;
+
+    let mut bytes = Vec::new();
+    bytes.extend_from_slice(&DOMAIN_LENGTH);
+    bytes.extend_from_slice(DOMAIN);
+    bytes.extend_from_slice(principal.as_slice());
+    bytes.extend_from_slice(&nonce.to_be_bytes());
+    sha256(&bytes)
 }
