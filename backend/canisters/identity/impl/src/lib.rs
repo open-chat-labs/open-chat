155a5d0a3fadd822f86b348ad6e36b4aaae4d922
Hamish Peebles <hamishpeebles@gmail.com>
2024-05-24 11:57:01 +0100

    Expose count of auth principals per originating canister in metrics (#5851)

--- backend/canisters/identity/impl/src/lib.rs
+++ backend/canisters/identity/impl/src/lib.rs
@@ -65,6 +65,7 @@ impl RuntimeState {
             git_commit_id: utils::git::git_commit_id().to_string(),
             user_principals: self.data.user_principals.user_principals_count(),
             auth_principals: self.data.user_principals.auth_principals_count(),
+            originating_canisters: self.data.user_principals.originating_canisters().clone(),
             canister_ids: CanisterIds {
                 user_index: self.data.user_index_canister_id,
                 cycles_dispenser: self.data.cycles_dispenser_canister_id,
@@ -155,6 +156,7 @@ pub struct Metrics {
     pub git_commit_id: String,
     pub user_principals: u32,
     pub auth_principals: u32,
+    pub originating_canisters: HashMap<CanisterId, u32>,
     pub canister_ids: CanisterIds,
 }
 
