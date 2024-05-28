34a8cf6a409c5867286bdf0bed0970a607ee29cf
Julian Jelfs <julian.jelfs@gmail.com>
2024-05-28 08:57:18 +0100

    Allow us to define custom verified credential gates (#5853)
    Co-authored-by: Hamish Peebles <hamishpeebles@gmail.com>

--- frontend/openchat-agent/src/services/group/candid/types.d.ts
+++ frontend/openchat-agent/src/services/group/candid/types.d.ts
@@ -1949,6 +1949,7 @@ export interface VerifiedCredentialGate {
   >,
   'issuer_origin' : string,
   'issuer_canister_id' : CanisterId,
+  'credential_name' : string,
   'credential_type' : string,
 }
 export interface VerifiedCredentialGateArgs {
--- frontend/openchat-agent/src/services/storageBucket/candid/types.d.ts
+++ frontend/openchat-agent/src/services/storageBucket/candid/types.d.ts
@@ -1532,6 +1532,7 @@ export interface VerifiedCredentialGate {
   >,
   'issuer_origin' : string,
   'issuer_canister_id' : CanisterId,
+  'credential_name' : string,
   'credential_type' : string,
 }
 export interface VerifiedCredentialGateArgs {
