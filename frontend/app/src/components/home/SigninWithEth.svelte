f13de85ea9e50112e3de6c554fa9b3c8f4601501
Hamish Peebles <hamishpeebles@gmail.com>
2024-05-31 07:49:02 +0100

    This is better

--- frontend/app/src/components/home/SigninWithEth.svelte
+++ frontend/app/src/components/home/SigninWithEth.svelte
@@ -20,8 +20,15 @@
         transports: {
             [mainnet.id]: http(),
         },
-        multiInjectedProviderDiscovery: false,
     });
+
+    const connectorNames = new Set<string>();
+    const uniqueConnectors = wagmiConfig.connectors
+        .filter((c) => {
+            if (connectorNames.has(c.name)) return false;
+            connectorNames.add(c.name);
+            return true;
+        });
 
     async function connectWith(connector: Connector) {
         try {
@@ -55,7 +62,7 @@
     };
 </script>
 
-{#each wagmiConfig.connectors as connector}
+{#each uniqueConnectors as connector}
     <div class="auth-option">
         <div class={`icon center ${connecting === connector ? "connecting" : ""}`}>
             {#if icons[connector.id] ?? connector.icon}
