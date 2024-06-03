217652c562af879f6b9fbd302b82132d7e846ab8
megrogan <megrogan@gmail.com>
2024-06-03 13:13:18 +0100

    Test streaks

--- backend/integration_tests/src/chit_tests.rs
+++ backend/integration_tests/src/chit_tests.rs
@@ -3,7 +3,9 @@ use types::ChitEarnedReason;
 use crate::env::ENV;
 use crate::{client, TestEnv};
 use std::ops::Deref;
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
+
+const DAY_IN_SECS: u64 = 60 * 60 * 24;
 
 #[test]
 fn claim_daily_chit_logged_in_user_canister() {
@@ -32,4 +34,36 @@ fn claim_daily_chit_logged_in_user_canister() {
     assert_eq!(events.events.len(), 1);
     assert_eq!(events.events[0].amount, 200);
     assert!(matches!(events.events[0].reason, ChitEarnedReason::DailyClaim));
+}
+
+#[test]
+fn chit_streak_gained_and_lost_as_expected() {
+    let mut wrapper = ENV.deref().get();
+    let TestEnv { env, canister_ids, .. } = wrapper.env();
+
+    let user = client::register_user(env, canister_ids);
+    env.set_time(SystemTime::now());
+
+    let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
+    assert_eq!(result.chit_balance, 200);
+    assert_eq!(result.streak, 1);
+
+    env.advance_time(Duration::from_secs(DAY_IN_SECS));
+    let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
+    assert_eq!(result.chit_balance, 400);
+    assert_eq!(result.streak, 2);
+
+    env.advance_time(Duration::from_secs(DAY_IN_SECS));
+    let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
+    assert_eq!(result.chit_balance, 700);
+    assert_eq!(result.streak, 3);
+
+    env.advance_time(Duration::from_secs(DAY_IN_SECS));
+    let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
+    assert_eq!(result.chit_balance, 1000);
+    assert_eq!(result.streak, 4);
+
+    env.advance_time(2 * Duration::from_secs(DAY_IN_SECS));
+    let current_user = client::user_index::happy_path::current_user(env, user.principal, canister_ids.user_index);
+    assert_eq!(current_user.streak, 0);
 }
