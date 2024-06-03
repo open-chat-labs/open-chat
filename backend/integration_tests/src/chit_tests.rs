aacf5f53d46984ee2dd0980b633bcb834c388ef5
megrogan <megrogan@gmail.com>
2024-06-03 17:04:07 +0100

    Another text fix!

--- backend/integration_tests/src/chit_tests.rs
+++ backend/integration_tests/src/chit_tests.rs
@@ -1,11 +1,14 @@
-use types::ChitEarnedReason;
+use pocket_ic::PocketIc;
+use types::{ChitEarnedReason, Milliseconds, TimestampMillis};
 
 use crate::env::ENV;
+use crate::utils::now_millis;
 use crate::{client, TestEnv};
 use std::ops::Deref;
 use std::time::{Duration, SystemTime};
 
-const DAY_IN_SECS: u64 = 60 * 60 * 24;
+const DAY_ZERO: TimestampMillis = 1704067200000; // Mon Jan 01 2024 00:00:00 GMT+0000
+const MS_IN_DAY: Milliseconds = 1000 * 60 * 60 * 24;
 
 #[test]
 fn claim_daily_chit_logged_in_user_canister() {
@@ -13,7 +16,7 @@ fn claim_daily_chit_logged_in_user_canister() {
     let TestEnv { env, canister_ids, .. } = wrapper.env();
 
     let user = client::register_user(env, canister_ids);
-    env.set_time(SystemTime::now());
+    ensure_time_at_least_day0(env);
 
     let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
 
@@ -42,30 +45,36 @@ fn chit_streak_gained_and_lost_as_expected() {
     let TestEnv { env, canister_ids, .. } = wrapper.env();
 
     let user = client::register_user(env, canister_ids);
-    env.set_time(SystemTime::now());
+    ensure_time_at_least_day0(env);
 
     let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
     assert_eq!(result.chit_balance, 200);
     assert_eq!(result.streak, 1);
 
-    env.advance_time(Duration::from_secs(DAY_IN_SECS));
+    env.advance_time(Duration::from_millis(MS_IN_DAY));
     let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
     assert_eq!(result.chit_balance, 400);
     assert_eq!(result.streak, 2);
 
-    env.advance_time(Duration::from_secs(DAY_IN_SECS));
+    env.advance_time(Duration::from_millis(MS_IN_DAY));
     let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
     assert_eq!(result.chit_balance, 700);
     assert_eq!(result.streak, 3);
 
-    env.advance_time(Duration::from_secs(DAY_IN_SECS));
+    env.advance_time(Duration::from_millis(MS_IN_DAY));
     let result = client::user_index::happy_path::claim_daily_chit(env, user.principal, canister_ids.user_index);
     assert_eq!(result.chit_balance, 1000);
     assert_eq!(result.streak, 4);
 
-    env.advance_time(Duration::from_secs(2 * DAY_IN_SECS));
+    env.advance_time(Duration::from_millis(2 * MS_IN_DAY));
     env.tick();
     let current_user = client::user_index::happy_path::current_user(env, user.principal, canister_ids.user_index);
     assert_eq!(result.chit_balance, 1000);
     assert_eq!(current_user.streak, 0);
+}
+
+fn ensure_time_at_least_day0(env: &mut PocketIc) {
+    if now_millis(env) < DAY_ZERO {
+        env.set_time(SystemTime::now());
+    }
 }
