4de09c0d7e307334fd2059189251d3eed0c6439f
Julian Jelfs <julian.jelfs@gmail.com>
2024-06-05 11:26:54 +0100

    delete some commented css

--- frontend/app/src/components/landingpages/ShopPage.svelte
+++ frontend/app/src/components/landingpages/ShopPage.svelte
@@ -44,27 +44,4 @@
             padding: 0;
         }
     }
-
-    // // These are the theme variables - it does not seem officially supported to override them
-    // // so probably safer to go with a light touch.
-
-    // // --sprd-main1: #000000;
-    // // --sprd-main2: #1e90ff;
-    // // --sprd-main3: #ffffff;
-    // // --sprd-sub2: #000000;
-    // // --sprd-sub3: #ffffff;
-    // // --sprd-main3-dec: 255, 255, 255;
-    // // --sprd-cta-text-color: #000000;
-    // // --sprd-main1-shifted: #808080;
-    // // --sprd-main2-shifted: #8fc8ff;
-    // // --sprd-main3-shifted: #cccccc;
-    // // --sprd-sub2-shifted: #808080;
-    // // --sprd-sub3-shifted: #cccccc;
-    // // --sprd-sub2-shifted-2: #bfbfbf;
-    // // --sprd-main2-shifted-3: #4ba6ff;
-
-    // // We will just override a couple of bits - any more is a bit risky
-    // --sprd-cta-text-color: var(--button-txt);
-    // --sprd-main2: var(--button-bg);
-    // --sprd-main2-shifted-3: var(--button-hv);
 </style>
