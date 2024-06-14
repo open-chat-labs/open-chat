9bd8cfab15ab2ed83529108c24eba0e3223aa7e8
Julian Jelfs <julian.jelfs@gmail.com>
2024-06-14 11:43:36 +0100

    fix chat state caching problem

--- frontend/openchat-agent/src/utils/caching.ts
+++ frontend/openchat-agent/src/utils/caching.ts
@@ -48,7 +48,7 @@ import type { CryptocurrencyContent } from "openchat-shared";
 import type { PrizeContent } from "openchat-shared";
 import type { P2PSwapContent } from "openchat-shared";
 
-const CACHE_VERSION = 103;
+const CACHE_VERSION = 104;
 const MAX_INDEX = 9999999999;
 
 export type Database = Promise<IDBPDatabase<ChatSchema>>;
