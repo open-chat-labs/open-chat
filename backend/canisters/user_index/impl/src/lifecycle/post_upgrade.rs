47ab83d81015300509408791c4ae41f3b393c650
Hamish Peebles <hamishpeebles@gmail.com>
2024-07-11 15:02:00 +0100

    Update UserIndex post release

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,10 +1,9 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::{mutate_state, Data};
+use crate::Data;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
-use local_user_index_canister::{DeleteUser, Event};
 use stable_memory::get_reader;
 use tracing::info;
 use user_index_canister::post_upgrade::Args;
@@ -25,19 +24,4 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
-
-    mutate_state(|state| {
-        for user_id in std::mem::take(&mut state.data.empty_users) {
-            if let Some(canister_id) = state.data.local_index_map.get_index_canister(&user_id) {
-                state.data.user_index_event_sync_queue.push(
-                    canister_id,
-                    Event::DeleteUser(DeleteUser {
-                        user_id,
-                        triggered_by_user: false,
-                    }),
-                );
-            }
-        }
-        crate::jobs::sync_events_to_local_user_index_canisters::start_job_if_required(state);
-    })
 }
