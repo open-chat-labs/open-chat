85e01b79dbca851b0c798a132a69c359f11d8341
Hamish Peebles <hamishpeebles@gmail.com>
2024-07-18 08:30:41 +0100

    Add delay so we can inspect existing data

--- backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
@@ -6,6 +6,7 @@ use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
 use local_user_index_canister::post_upgrade::Args;
 use stable_memory::get_reader;
+use std::time::Duration;
 use tracing::info;
 use utils::cycles::init_cycles_dispenser_client;
 
@@ -25,7 +26,9 @@ fn post_upgrade(args: Args) {
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
 
-    mutate_state(|state| {
-        state.data.referral_codes.set_expired(state.env.now());
+    ic_cdk_timers::set_timer(Duration::from_secs(600), || {
+        mutate_state(|state| {
+            state.data.referral_codes.set_expired(state.env.now());
+        });
     });
 }
