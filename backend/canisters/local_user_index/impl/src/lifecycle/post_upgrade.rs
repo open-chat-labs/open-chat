9ed6706630febeb9114d94a72b27423e511f5603
Hamish Peebles <hamishpeebles@gmail.com>
2024-07-18 08:51:13 +0100

    Expire old BTC Miami referral codes (#6053)

--- backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,11 +1,12 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
 use local_user_index_canister::post_upgrade::Args;
 use stable_memory::get_reader;
+use std::time::Duration;
 use tracing::info;
 use utils::cycles::init_cycles_dispenser_client;
 
@@ -24,4 +25,10 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    ic_cdk_timers::set_timer(Duration::from_secs(600), || {
+        mutate_state(|state| {
+            state.data.referral_codes.set_expired(state.env.now());
+        });
+    });
 }
