5f770f31ec89fed354a805b8da1c5dcb87313e19
megrogan <megrogan@gmail.com>
2024-07-23 17:28:10 +0100

    cargo clippy --tests

--- backend/integration_tests/src/airdrop_bot_tests.rs
+++ backend/integration_tests/src/airdrop_bot_tests.rs
@@ -1,30 +1,30 @@
-use crate::env::ENV;
-use crate::{client, CanisterIds, TestEnv, User};
-use candid::Principal;
-use pocket_ic::PocketIc;
-use std::ops::Deref;
-use testing::rng::random_string;
-use types::{ChatId, GroupRole};
+// use crate::env::ENV;
+// use crate::{client, CanisterIds, TestEnv, User};
+// use candid::Principal;
+// use pocket_ic::PocketIc;
+// use std::ops::Deref;
+// use testing::rng::random_string;
+// use types::{ChatId, GroupRole};
 
-#[test]
-fn airdrop_end_to_end() {
-    let mut wrapper = ENV.deref().get();
-    let TestEnv {
-        env,
-        canister_ids,
-        controller,
-        ..
-    } = wrapper.env();
+// #[test]
+// fn airdrop_end_to_end() {
+//     let mut wrapper = ENV.deref().get();
+//     let TestEnv {
+//         env,
+//         canister_ids,
+//         controller,
+//         ..
+//     } = wrapper.env();
 
-    // - Mint CHAT tokens
-    // - Transfer them to Airdrop bot
-    // - Create 3 users
-    // - Claim daily chit for each user
-    // - Create community
-    // - Create public channel
-    // - Promote airdrop bot to channel owner
-    // - Call set_airdrop
-    // - Check the channel now has AccessGate::Locked
-    // - Check the channel now has exactly 3 prize messages
-    // - Check a user has a DM with expected prize
-}
+//     // - Mint CHAT tokens
+//     // - Transfer them to Airdrop bot
+//     // - Create 3 users
+//     // - Claim daily chit for each user
+//     // - Create community
+//     // - Create public channel
+//     // - Promote airdrop bot to channel owner
+//     // - Call set_airdrop
+//     // - Check the channel now has AccessGate::Locked
+//     // - Check the channel now has exactly 3 prize messages
+//     // - Check a user has a DM with expected prize
+// }
