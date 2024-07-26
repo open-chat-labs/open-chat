4d63cfc2de7fc6ba2bc992369ef0c5af6fe10c9d
megrogan <megrogan@gmail.com>
2024-07-26 14:30:46 +0100

    Hardcode airdrop_bot username

--- backend/canisters/airdrop_bot/impl/src/updates/handle_direct_message.rs
+++ backend/canisters/airdrop_bot/impl/src/updates/handle_direct_message.rs
@@ -1,4 +1,4 @@
-use crate::{mutate_state, RuntimeState};
+use crate::USERNAME;
 use airdrop_bot_canister::handle_direct_message::*;
 use canister_api_macros::update;
 use canister_tracing_macros::trace;
@@ -7,13 +7,9 @@ use types::{BotMessage, MessageContentInitial, TextContent};
 #[update(msgpack = true)]
 #[trace]
 fn handle_direct_message(_args: Args) -> Response {
-    mutate_state(handle_message)
-}
-
-fn handle_message(state: &mut RuntimeState) -> Response {
     let text = "Hi, I am the bot which conducts the CHIT for CHAT airdrops. For information about CHIT and the aridrops please read [this blog post](https://oc.app/blog/chit).".to_string();
     Success(SuccessResult {
-        bot_name: state.data.username.clone(),
+        bot_name: USERNAME.to_string(),
         bot_display_name: None,
         messages: vec![BotMessage {
             thread_root_message_id: None,
