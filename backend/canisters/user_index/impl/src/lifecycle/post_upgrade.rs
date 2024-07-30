84aaebe94c0302ba96550c215bdf9699aa89a0cc
Hamish Peebles <hamishpeebles@gmail.com>
2024-07-30 11:51:46 +0100

    Ensure `date_updated` is non zero for all users

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,6 +1,6 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
@@ -24,4 +24,8 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    mutate_state(|state| {
+        state.data.users.ensure_date_updated_non_zero(env.now());
+    });
 }
