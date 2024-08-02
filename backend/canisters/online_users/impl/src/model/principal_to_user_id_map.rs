ae77f4cbd577c6d91df07f2c8be28470bbbb1fb2
Hamish Peebles <hamishpeebles@gmail.com>
2024-08-02 17:09:48 +0100

    Update canisters post release (#6192)

--- backend/canisters/online_users/impl/src/model/principal_to_user_id_map.rs
+++ backend/canisters/online_users/impl/src/model/principal_to_user_id_map.rs
@@ -22,10 +22,6 @@ impl PrincipalToUserIdMap {
     pub fn remove(&mut self, principal: &Principal) -> Option<UserId> {
         self.map.remove(principal).map(|u| u.into())
     }
-
-    pub fn clear(&mut self) {
-        self.map.clear_new()
-    }
 }
 
 fn init_map() -> StableBTreeMap<Principal, Principal, Memory> {
