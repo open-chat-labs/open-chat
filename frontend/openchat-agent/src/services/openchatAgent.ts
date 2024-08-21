2edb54d04e19f218f49c674a2f262c729b4bebc1
megrogan <megrogan@gmail.com>
2024-08-21 15:36:14 +0100

    fix

--- frontend/openchat-agent/src/services/openchatAgent.ts
+++ frontend/openchat-agent/src/services/openchatAgent.ts
@@ -1705,7 +1705,7 @@ export class OpenChatAgent extends EventTarget {
             achievements = current.achievements;
             newAchievements = [];
             chitState = current.chitState;
-            referrals = current.referrals;
+            referrals = current.referrals ?? [];
             walletConfig = current.walletConfig;
 
             if (userResponse.kind === "success") {
