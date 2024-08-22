ae683f40a7a068058105f3fd4454d1fa61da9633
megrogan <megrogan@gmail.com>
2024-08-22 10:54:54 +0100

    Change default WalletConfig to zero balance

--- backend/canisters/user/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user/impl/src/lifecycle/post_upgrade.rs
@@ -1,12 +1,14 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
 use stable_memory::get_reader;
 use tracing::info;
+use types::Timestamped;
 use user_canister::post_upgrade::Args;
+use user_canister::WalletConfig;
 
 #[post_upgrade]
 #[trace]
@@ -20,6 +22,15 @@ fn post_upgrade(args: Args) {
 
     let env = init_env(data.rng_seed);
     init_state(env, data, args.wasm_version);
+
+    // TODO: Delete this after next release
+    mutate_state(|state| {
+        if let WalletConfig::Auto(config) = &state.data.wallet_config.value {
+            if config.min_cents_visible == 100 {
+                state.data.wallet_config = Timestamped::new(WalletConfig::default(), state.env.now());
+            }
+        }
+    });
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
 }
