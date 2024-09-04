4cd52e1334641817104310d76dc07469a335be60
Hamish Peebles <hamishpeebles@gmail.com>
2024-09-04 20:09:19 +0100

    Reduce size of canister wasms

--- backend/canisters/user/impl/src/updates/btc_address.rs
+++ backend/canisters/user/impl/src/updates/btc_address.rs
@@ -3,11 +3,11 @@ use crate::{mutate_state, read_state, run_regular_jobs};
 use canister_tracing_macros::trace;
 use ckbtc_minter_canister::CKBTC_MINTER_CANISTER_ID;
 use ic_cdk::update;
-use user_canister::get_btc_address::{Response::*, *};
+use user_canister::btc_address::{Response::*, *};
 
 #[update(guard = "caller_is_owner")]
 #[trace]
-async fn get_btc_address(_args: Args) -> Response {
+async fn btc_address(_args: Args) -> Response {
     run_regular_jobs();
 
     if let Some(btc_address) = read_state(|state| state.data.btc_address.clone()) {
