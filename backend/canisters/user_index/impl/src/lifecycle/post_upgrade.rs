df66fee794085f28f168d4c46eb7ff4a87182a67
Hamish Peebles <hamishpeebles@gmail.com>
2024-09-10 11:25:34 +0100

    Replay referral reward events in case any were missed

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,9 +1,10 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{jobs, mutate_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
+use local_user_index_canister::{DiamondMembershipPaymentReceived, Event as LocalUserIndexEvent};
 use stable_memory::get_reader;
 use tracing::info;
 use user_index_canister::post_upgrade::Args;
@@ -24,4 +25,36 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    mutate_state(|state| {
+        for user in state.data.users.iter().filter(|u| u.referred_by.is_some()) {
+            let is_lifetime_diamond_member = user.diamond_membership_details.is_lifetime_diamond_member();
+
+            let mut event = None;
+            if let Some(proof) = user.unique_person_proof.as_ref().filter(|_| !is_lifetime_diamond_member) {
+                event = Some(LocalUserIndexEvent::NotifyUniquePersonProof(user.user_id, proof.clone()));
+            } else if let Some(payment) = user.diamond_membership_details.payments().last() {
+                event = Some(LocalUserIndexEvent::DiamondMembershipPaymentReceived(
+                    DiamondMembershipPaymentReceived {
+                        user_id: user.user_id,
+                        timestamp: payment.timestamp,
+                        expires_at: user.diamond_membership_details.expires_at().unwrap(),
+                        token: payment.token.clone(),
+                        amount_e8s: payment.amount_e8s,
+                        block_index: payment.block_index,
+                        duration: payment.duration,
+                        recurring: user.diamond_membership_details.is_recurring(),
+                        send_bot_message: false,
+                    },
+                ));
+            }
+            if let Some(event) = event {
+                if let Some(index) = state.data.local_index_map.get_index_canister(&user.user_id) {
+                    state.data.user_index_event_sync_queue.push(index, event);
+                }
+            }
+        }
+
+        jobs::sync_events_to_local_user_index_canisters::start_job_if_required(state);
+    });
 }
