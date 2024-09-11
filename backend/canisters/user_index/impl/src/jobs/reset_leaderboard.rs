0eed45d334bf1fd528b72a48f5b9dc6132a2bcf9
megrogan <megrogan@gmail.com>
2024-09-11 16:24:46 +0100

    Fix

--- backend/canisters/user_index/impl/src/jobs/reset_leaderboard.rs
+++ backend/canisters/user_index/impl/src/jobs/reset_leaderboard.rs
@@ -13,7 +13,7 @@ pub(crate) fn start_job_if_required(state: &RuntimeState) -> bool {
     if TIMER_ID.get().is_none() {
         let now = state.env.now();
         let start_of_next_month = MonthKey::from_timestamp(now).next().start_timestamp();
-        let timer_id = ic_cdk_timers::set_timer(Duration::from_millis(start_of_next_month), run);
+        let timer_id = ic_cdk_timers::set_timer(Duration::from_millis(start_of_next_month - now), run);
         TIMER_ID.set(Some(timer_id));
         true
     } else {
