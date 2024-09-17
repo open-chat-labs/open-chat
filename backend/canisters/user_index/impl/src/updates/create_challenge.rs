2d35a953de3ec9bf734be6850606e141e6b0db60
Hamish Peebles <hamishpeebles@gmail.com>
2024-09-17 12:41:26 +0100

    Remove `create_challenge`

--- backend/canisters/user_index/impl/src/updates/create_challenge.rs
+++
@@ -1,9 +0,0 @@
-use canister_api_macros::update;
-use canister_tracing_macros::trace;
-use user_index_canister::create_challenge::{Response::*, *};
-
-#[update(msgpack = true)]
-#[trace]
-fn create_challenge(_args: Args) -> Response {
-    NotRequired
-}
