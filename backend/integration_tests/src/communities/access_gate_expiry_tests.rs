95f579f1c714ed45356720e03ec1ec041143acd9
megrogan <megrogan@gmail.com>
2024-09-27 15:55:02 +0100

    Another test

--- backend/integration_tests/src/communities/access_gate_expiry_tests.rs
+++ backend/integration_tests/src/communities/access_gate_expiry_tests.rs
@@ -1,3 +1,4 @@
+use crate::client::user_index;
 use crate::env::ENV;
 use crate::utils::tick_many;
 use crate::{client, CanisterIds, TestEnv, User};
@@ -8,7 +9,9 @@ use std::ops::Deref;
 use std::time::Duration;
 use test_case::test_case;
 use testing::rng::random_string;
-use types::{AccessGate, AccessGateConfig, ChannelId, CommunityId, DiamondMembershipPlanDuration, OptionUpdate};
+use types::{
+    AccessGate, AccessGateConfig, ChannelId, CommunityId, DiamondMembershipPlanDuration, OptionUpdate, TokenBalanceGate,
+};
 
 const DAY_IN_MS: Milliseconds = 24 * 60 * 60 * 1000;
 
@@ -38,9 +41,9 @@ fn diamond_member_lapses_and_rejoins_successfully(channel: bool) {
     });
 
     if channel {
-        update_channel(env, user1.principal, community_id, channel_id, gate_config_update);
+        update_channel_gate(env, user1.principal, community_id, channel_id, gate_config_update);
     } else {
-        update_community(env, user1.principal, community_id, gate_config_update);
+        update_community_gate(env, user1.principal, community_id, gate_config_update);
     }
 
     // User 2 joins the channel
@@ -57,13 +60,7 @@ fn diamond_member_lapses_and_rejoins_successfully(channel: bool) {
     tick_many(env, 10);
 
     // Assert that user2 has lapsed
-    if channel {
-        let summary = client::community::happy_path::channel_summary(env, &user2, community_id, channel_id);
-        assert!(summary.membership.map_or(false, |m| m.lapsed));
-    } else {
-        let summary = client::community::happy_path::summary(env, &user2, community_id);
-        assert!(summary.membership.map_or(false, |m| m.lapsed));
-    }
+    assert_user_lapsed(env, channel, &user2, community_id, channel_id, true);
 
     // Buy Diamond again
     client::upgrade_user(
@@ -85,13 +82,7 @@ fn diamond_member_lapses_and_rejoins_successfully(channel: bool) {
     );
 
     // Assert that user2 is no longer lapsed
-    if channel {
-        let summary = client::community::happy_path::channel_summary(env, &user2, community_id, channel_id);
-        assert!(summary.membership.map_or(false, |m| !m.lapsed));
-    } else {
-        let summary = client::community::happy_path::summary(env, &user2, community_id);
-        assert!(summary.membership.map_or(false, |m| !m.lapsed));
-    }
+    assert_user_lapsed(env, channel, &user2, community_id, channel_id, false);
 }
 
 #[test_case(true)]
@@ -120,9 +111,9 @@ fn remove_gate_unlapses_members(channel: bool) {
     });
 
     if channel {
-        update_channel(env, user1.principal, community_id, channel_id, gate_config_update);
+        update_channel_gate(env, user1.principal, community_id, channel_id, gate_config_update);
     } else {
-        update_community(env, user1.principal, community_id, gate_config_update);
+        update_community_gate(env, user1.principal, community_id, gate_config_update);
     }
 
     // User 2 joins the channel
@@ -139,32 +130,173 @@ fn remove_gate_unlapses_members(channel: bool) {
     tick_many(env, 10);
 
     // Assert that user2 has lapsed
-    if channel {
-        let summary = client::community::happy_path::channel_summary(env, &user2, community_id, channel_id);
-        assert!(summary.membership.map_or(false, |m| m.lapsed));
-    } else {
-        let summary = client::community::happy_path::summary(env, &user2, community_id);
-        assert!(summary.membership.map_or(false, |m| m.lapsed));
-    }
+    assert_user_lapsed(env, channel, &user2, community_id, channel_id, true);
 
     // Remove the gate
     if channel {
-        update_channel(env, user1.principal, community_id, channel_id, OptionUpdate::SetToNone);
+        update_channel_gate(env, user1.principal, community_id, channel_id, OptionUpdate::SetToNone);
     } else {
-        update_community(env, user1.principal, community_id, OptionUpdate::SetToNone);
+        update_community_gate(env, user1.principal, community_id, OptionUpdate::SetToNone);
     }
 
     // Assert that user2 is no longer lapsed
+    assert_user_lapsed(env, channel, &user2, community_id, channel_id, false);
+}
+
+// #[test_case(true, true)]
+// #[test_case(true, false)]
+// #[test_case(false, true)]
+// #[test_case(false, false)]
+// fn extend_or_reduce_expiry_then_member_lapses_when_expected(channel: bool, extend_expiry: bool) {
+//     // Create 2 diamond users, a public community and a public channel
+//     let mut wrapper = ENV.deref().get();
+//     let TestEnv {
+//         env,
+//         canister_ids,
+//         controller,
+//     } = wrapper.env();
+
+//     let TestData {
+//         user1,
+//         user2,
+//         community_id,
+//         channel_id,
+//     } = init_test_data(env, canister_ids, *controller);
+
+//     // Depending on the test branch either update the community or channel
+//     // with an expiring diamond access gate
+//     let gate_config_update = OptionUpdate::SetToSome(AccessGateConfig {
+//         gate: AccessGate::DiamondMember,
+//         expiry: Some(15 * DAY_IN_MS),
+//     });
+
+//     if channel {
+//         update_channel_gate(env, user1.principal, community_id, channel_id, gate_config_update);
+//     } else {
+//         update_community_gate(env, user1.principal, community_id, gate_config_update);
+//     }
+
+//     // User 2 joins the channel
+//     client::local_user_index::happy_path::join_channel(
+//         env,
+//         user2.principal,
+//         canister_ids.local_user_index,
+//         community_id,
+//         channel_id,
+//     );
+
+//     // Move the time forward so that user2's diamond membership expires + the gate expiry
+//     env.advance_time(Duration::from_millis(46 * DAY_IN_MS));
+//     tick_many(env, 10);
+
+//     // Assert that user2 has lapsed
+//     assert_user_lapsed(env, channel, &user2, community_id, channel_id, true);
+// }
+
+#[test_case(true)]
+#[test_case(false)]
+fn member_lapses_from_token_balance_gate_and_rejoins_successfully(channel: bool) {
+    // Create 2 diamond users, a public community and a public channel
+    let mut wrapper = ENV.deref().get();
+    let TestEnv {
+        env,
+        canister_ids,
+        controller,
+    } = wrapper.env();
+
+    let TestData {
+        user1,
+        user2,
+        community_id,
+        channel_id,
+    } = init_test_data(env, canister_ids, *controller);
+
+    // Depending on the test branch either update the community or channel
+    // with a token balance gate
+    let min_balance = 500_000_000;
+    let gate_config_update = OptionUpdate::SetToSome(AccessGateConfig {
+        gate: AccessGate::TokenBalance(TokenBalanceGate {
+            ledger_canister_id: canister_ids.icp_ledger,
+            min_balance,
+        }),
+        expiry: Some(DAY_IN_MS),
+    });
+
     if channel {
-        let summary = client::community::happy_path::channel_summary(env, &user2, community_id, channel_id);
-        assert!(summary.membership.map_or(false, |m| !m.lapsed));
+        update_channel_gate(env, user1.principal, community_id, channel_id, gate_config_update);
     } else {
-        let summary = client::community::happy_path::summary(env, &user2, community_id);
-        assert!(summary.membership.map_or(false, |m| !m.lapsed));
+        update_community_gate(env, user1.principal, community_id, gate_config_update);
+    }
+
+    // User 2 joins the channel
+    client::local_user_index::happy_path::join_channel(
+        env,
+        user2.principal,
+        canister_ids.local_user_index,
+        community_id,
+        channel_id,
+    );
+
+    // Move the time forward so that the gate expires
+    env.advance_time(Duration::from_millis(2 * DAY_IN_MS));
+    tick_many(env, 5);
+
+    // User2 should have sufficient balance so should not be lapsed
+    assert_user_lapsed(env, channel, &user2, community_id, channel_id, false);
+
+    // User2 upgrades to 3 month diamond to reduce their token balance
+    user_index::happy_path::pay_for_diamond_membership(
+        env,
+        user2.principal,
+        canister_ids.user_index,
+        DiamondMembershipPlanDuration::ThreeMonths,
+        false,
+        false,
+    );
+
+    tick_many(env, 4);
+
+    // Move the time forward so that the gate expires
+    env.advance_time(Duration::from_millis(2 * DAY_IN_MS));
+    tick_many(env, 5);
+
+    // Assert that user2 has lapsed
+    assert_user_lapsed(env, channel, &user2, community_id, channel_id, true);
+
+    // Increase token balance
+    client::ledger::happy_path::transfer(env, *controller, canister_ids.icp_ledger, user2.user_id, min_balance);
+
+    // User2 rejoins channel
+    client::local_user_index::happy_path::join_channel(
+        env,
+        user2.principal,
+        canister_ids.local_user_index,
+        community_id,
+        channel_id,
+    );
+
+    // Assert that user2 is no longer lapsed
+    assert_user_lapsed(env, channel, &user2, community_id, channel_id, false);
+}
+
+fn assert_user_lapsed(
+    env: &mut PocketIc,
+    channel: bool,
+    user: &User,
+    community_id: CommunityId,
+    channel_id: ChannelId,
+    lapsed: bool,
+) {
+    if channel {
+        let summary = client::community::happy_path::channel_summary(env, user, community_id, channel_id);
+        assert!(summary.membership.map_or(false, |m| m.lapsed == lapsed));
+    } else {
+        let summary = client::community::happy_path::summary(env, user, community_id);
+        assert!(summary.membership.map_or(false, |m| m.lapsed == lapsed));
     }
 }
 
-fn update_community(
+fn update_community_gate(
     env: &mut PocketIc,
     principal: Principal,
     community_id: CommunityId,
@@ -186,7 +318,7 @@ fn update_community(
     client::community::happy_path::update_community(env, principal, community_id, &args);
 }
 
-fn update_channel(
+fn update_channel_gate(
     env: &mut PocketIc,
     principal: Principal,
     community_id: CommunityId,
