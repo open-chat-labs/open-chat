a895078d3101a5eaabaf8a811f59b3c5aae68bae
Hamish Peebles <hamishpeebles@gmail.com>
2024-09-30 17:10:11 +0100

    One more

--- backend/integration_tests/src/wasms.rs
+++ backend/integration_tests/src/wasms.rs
@@ -34,11 +34,11 @@ lazy_static! {
 }
 
 fn get_canister_wasm(canister_name: &str) -> CanisterWasm {
-    let module = read_file_from_local_bin(&format!("{canister_name}.wasm.gz"));
+    let wasm = read_file_from_local_bin(&format!("{canister_name}.wasm.gz"));
 
     CanisterWasm {
         version: BuildVersion::min(),
-        module,
+        module: wasm.into(),
     }
 }
 
