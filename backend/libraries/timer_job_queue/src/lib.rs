7e4c1fc2621ca97e31c45c1f7e58f0640e986fdf
Hamish Peebles <hamishpeebles@gmail.com>
2024-10-02 11:27:28 +0100

    Better lock handling

--- backend/libraries/timer_job_queue/src/lib.rs
+++ backend/libraries/timer_job_queue/src/lib.rs
@@ -1,8 +1,9 @@
 use ic_cdk_timers::TimerId;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::collections::VecDeque;
+use std::ops::DerefMut;
 use std::rc::Rc;
-use std::sync::{Mutex, MutexGuard};
+use std::sync::Mutex;
 use std::time::Duration;
 
 pub trait TimerJobItem {
@@ -25,29 +26,29 @@ impl<T> TimerJobQueue<T> {
         }
     }
 
-    pub fn set_max_concurrency(&mut self, value: usize) {
-        let mut inner = self.inner();
-        inner.max_concurrency = value;
+    pub fn set_max_concurrency(&self, value: usize) {
+        self.within_lock(|i| i.max_concurrency = value)
     }
 
     pub fn clear(&self) {
-        self.inner().queue.clear()
+        self.within_lock(|i| i.queue.clear())
     }
 
     pub fn len(&self) -> usize {
-        self.inner().queue.len()
+        self.within_lock(|i| i.queue.len())
     }
 
     pub fn is_empty(&self) -> bool {
-        self.inner().queue.is_empty()
+        self.within_lock(|i| i.queue.is_empty())
     }
 
     pub fn in_progress(&self) -> usize {
-        self.inner().in_progress
+        self.within_lock(|i| i.in_progress)
     }
 
-    fn inner(&self) -> MutexGuard<TimerJobQueueInner<T>> {
-        self.inner.lock().unwrap()
+    fn within_lock<F: FnOnce(&mut TimerJobQueueInner<T>) -> R, R>(&self, f: F) -> R {
+        let mut inner = self.inner.try_lock().unwrap();
+        f(inner.deref_mut())
     }
 }
 
@@ -65,11 +66,11 @@ where
     T: TimerJobItem + 'static,
 {
     pub fn start_job_if_required(&self) -> bool {
-        let mut inner = self.inner();
-        if inner.timer_id.is_none() && !inner.queue.is_empty() {
+        let should_start_job = self.within_lock(|i| i.timer_id.is_none() && !i.queue.is_empty());
+        if should_start_job {
             let clone = self.clone();
             let timer_id = ic_cdk_timers::set_timer_interval(Duration::ZERO, move || clone.run());
-            inner.timer_id = Some(timer_id);
+            self.within_lock(|i| i.timer_id = Some(timer_id));
             true
         } else {
             false
@@ -77,42 +78,45 @@ where
     }
 
     pub fn enqueue(&self, item: T) {
-        self.inner().queue.push_back(item);
+        self.within_lock(|i| i.queue.push_back(item));
         self.start_job_if_required();
     }
 
     pub fn enqueue_front(&self, item: T) {
-        self.inner().queue.push_front(item);
+        self.within_lock(|i| i.queue.push_front(item));
         self.start_job_if_required();
     }
 
     pub fn enqueue_many(&self, items: impl Iterator<Item = T>) {
-        let mut inner = self.inner();
-        for item in items {
-            inner.queue.push_back(item);
-        }
+        self.within_lock(|i| {
+            for item in items {
+                i.queue.push_back(item);
+            }
+        });
         self.start_job_if_required();
     }
 
     fn run(&self) {
-        let mut inner = self.inner();
-        if inner.queue.is_empty() {
-            if let Some(timer_id) = inner.timer_id.take() {
-                ic_cdk_timers::clear_timer(timer_id);
-            }
-        }
-
-        let max_to_start = inner.max_concurrency.saturating_sub(inner.in_progress);
         let mut items = Vec::new();
-        while items.len() < max_to_start {
-            if let Some(item) = inner.queue.pop_front() {
-                items.push(item);
-            } else {
-                break;
+
+        self.within_lock(|i| {
+            if i.queue.is_empty() {
+                if let Some(timer_id) = i.timer_id.take() {
+                    ic_cdk_timers::clear_timer(timer_id);
+                }
             }
-        }
-        let count = items.len();
-        inner.in_progress = inner.in_progress.saturating_add(count);
+
+            let max_to_start = i.max_concurrency.saturating_sub(i.in_progress);
+            while items.len() < max_to_start {
+                if let Some(item) = i.queue.pop_front() {
+                    items.push(item);
+                } else {
+                    break;
+                }
+            }
+            let count = items.len();
+            i.in_progress = i.in_progress.saturating_add(count);
+        });
 
         if !items.is_empty() {
             let clone = self.clone();
@@ -127,9 +131,10 @@ where
     async fn process_single(&self, item: T) {
         if let Err(retry) = item.process().await {
             if retry {
-                let mut inner = self.inner();
-                inner.queue.push_front(item);
-                inner.in_progress = inner.in_progress.saturating_sub(1);
+                self.within_lock(|i| {
+                    i.queue.push_front(item);
+                    i.in_progress = i.in_progress.saturating_sub(1);
+                });
                 self.start_job_if_required();
             }
         }
@@ -146,8 +151,7 @@ impl<T> Clone for TimerJobQueue<T> {
 
 impl<T: Serialize> Serialize for TimerJobQueue<T> {
     fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        let inner = self.inner.lock().unwrap();
-        inner.serialize(serializer)
+        self.within_lock(|i| i.serialize(serializer))
     }
 }
 
