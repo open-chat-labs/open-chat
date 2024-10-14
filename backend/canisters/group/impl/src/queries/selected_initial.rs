4c1aaf5aee6e45e841130c6f6c36a78d15f1df1e
Hamish Peebles <hamishpeebles@gmail.com>
2024-10-14 21:07:36 +0100

    Reduce size of responses by only returning UserIds for basic members (#6577)

--- backend/canisters/group/impl/src/queries/selected_initial.rs
+++ backend/canisters/group/impl/src/queries/selected_initial.rs
@@ -1,6 +1,7 @@
 use crate::{read_state, RuntimeState};
 use canister_api_macros::query;
 use group_canister::selected_initial::{Response::*, *};
+use types::{GroupMember, GroupRole};
 
 #[query(candid = true, msgpack = true)]
 fn selected_initial(_args: Args) -> Response {
@@ -14,11 +15,22 @@ fn selected_initial_impl(state: &RuntimeState) -> Response {
         let chat = &state.data.chat;
         let last_updated = chat.details_last_updated();
 
+        let mut members = Vec::new();
+        let mut basic_members = Vec::new();
+        for member in chat.members.iter().map(GroupMember::from) {
+            if matches!(member.role, GroupRole::Participant) && !member.lapsed {
+                basic_members.push(member.user_id);
+            } else {
+                members.push(member);
+            }
+        }
+
         Success(SuccessResult {
             timestamp: last_updated,
             last_updated,
             latest_event_index: chat.events.main_events_reader().latest_event_index().unwrap_or_default(),
-            participants: chat.members.iter().map(|p| p.into()).collect(),
+            participants: members,
+            basic_members,
             blocked_users: chat.members.blocked(),
             invited_users: chat.invited_users.users(),
             pinned_messages: chat.pinned_messages(min_visible_message_index),
