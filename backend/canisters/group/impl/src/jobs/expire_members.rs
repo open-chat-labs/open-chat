d822241d57338ff73b6103c58e8b8d63460e618e
megrogan <megrogan@gmail.com>
2024-10-17 09:35:19 +0100

    Reduce gate expiry delay from 5m->30s in test mode

--- backend/canisters/group/impl/src/jobs/expire_members.rs
+++ backend/canisters/group/impl/src/jobs/expire_members.rs
@@ -18,8 +18,9 @@ const USER_DETAILS_BATCH_SIZE: usize = 1000;
 pub(crate) fn start_job_if_required(state: &RuntimeState) -> bool {
     if TIMER_ID.get().is_none() {
         if let Some(next_expiry) = state.data.expiring_members.next_expiry() {
+            let expiry_delay = if state.data.test_mode { 30 * 1000 } else { MEMBER_ACCESS_EXPIRY_DELAY };
             let timer_id = ic_cdk_timers::set_timer(
-                Duration::from_millis((next_expiry + MEMBER_ACCESS_EXPIRY_DELAY).saturating_sub(state.env.now())),
+                Duration::from_millis((next_expiry + expiry_delay).saturating_sub(state.env.now())),
                 run,
             );
             TIMER_ID.set(Some(timer_id));
