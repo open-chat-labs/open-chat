e12bfaf4aa47bdd8e784283f74a6e7ad23ca484f
megrogan <megrogan@gmail.com>
2024-10-18 15:05:21 +0100

    fmt

--- backend/canisters/airdrop_bot/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/airdrop_bot/impl/src/lifecycle/post_upgrade.rs
@@ -14,7 +14,7 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-let (data, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>) = msgpack::deserialize(reader).unwrap();
+    let (data, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>) = msgpack::deserialize(reader).unwrap();
 
     // TODO: After release change this to
     // let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) = msgpack::deserialize(reader).unwrap();
