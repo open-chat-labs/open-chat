5133b550d3e5c7bef0651b1d3f1c08f2ec557ae3
Matt Grogan <megrogan@gmail.com>
2024-10-18 15:09:56 +0100

    Only deserialize from old log state (#6616)

--- backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
@@ -15,10 +15,11 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
-        msgpack::deserialize(reader).unwrap();
+    let (data, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>) = msgpack::deserialize(reader).unwrap();
 
-    canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
+    // TODO: After release change this to
+    // let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) = msgpack::deserialize(reader).unwrap();
+    canister_logger::init_with_logs(data.test_mode, Vec::new(), logs, traces);
 
     let env = init_env(data.rng_seed);
     init_state(env, data, args.wasm_version);
