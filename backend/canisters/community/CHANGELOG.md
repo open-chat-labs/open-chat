3b240d5b17564991e92624b1155cc9209d9f5d50
Matt Grogan <megrogan@gmail.com>
2024-10-18 09:45:56 +0100

    Add error log with http endpoint to all canisters (#6608)

--- backend/canisters/community/CHANGELOG.md
+++ backend/canisters/community/CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [unreleased]
 
+### Added
+
+- Add an error log with http endpoint ([#6607](https://github.com/open-chat-labs/open-chat/pull/6607))
+
 ### Changed
 
 - Push activity to users using `GroupedTimerJobQueue` ([#6552](https://github.com/open-chat-labs/open-chat/pull/6552))
--- backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
@@ -16,9 +16,17 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (data, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>) = msgpack::deserialize(reader).unwrap();
+    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) = match msgpack::deserialize(reader) {
+        Ok((data, errors, logs, traces)) => (data, errors, logs, traces),
+        Err(_) => {
+            let memory = get_upgrades_memory();
+            let reader = get_reader(&memory);
+            let (data, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>) = msgpack::deserialize(reader).unwrap();
+            (data, Vec::new(), logs, traces)
+        }
+    };
 
-    canister_logger::init_with_logs(data.test_mode, logs, traces);
+    canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
     let env = init_env(data.rng_seed);
     init_state(env, data, args.wasm_version);
