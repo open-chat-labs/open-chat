46e8efd81e24f216ef7dd02c12cb336e71bedf15
Hamish Peebles <hamishpeebles@gmail.com>
2024-10-23 10:35:25 +0100

    Avoid using `heartbeat` to top up the canister pool

--- backend/canisters/local_group_index/impl/src/jobs/mod.rs
+++ backend/canisters/local_group_index/impl/src/jobs/mod.rs
@@ -1,9 +1,11 @@
 use crate::RuntimeState;
 
+pub mod topup_canister_pool;
 pub mod upgrade_communities;
 pub mod upgrade_groups;
 
 pub(crate) fn start(state: &RuntimeState) {
+    topup_canister_pool::start_job_if_required(state);
     upgrade_communities::start_job_if_required(state);
     upgrade_groups::start_job_if_required(state);
 }
