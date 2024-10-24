43dc2d7ca41637849ebb4a128874f5ad68845882
Hamish Peebles <hamishpeebles@gmail.com>
2024-10-24 15:42:38 +0100

    clippy

--- backend/libraries/chat_events/src/hybrid_map.rs
+++ backend/libraries/chat_events/src/hybrid_map.rs
@@ -1,19 +1,18 @@
-use crate::{ChatEventInternal, ChatEventsMap, EventsMap};
+use crate::{ChatEventInternal, EventsMap};
 use serde::{Deserialize, Serialize};
-use std::cell::Cell;
 use std::collections::BTreeMap;
 use std::ops::RangeBounds;
 use types::{EventIndex, EventWrapperInternal, MAX_EVENT_INDEX, MIN_EVENT_INDEX};
 
 #[cfg(test)]
 thread_local! {
-    static LAST_READ_FROM_SLOW: Cell<bool> = Cell::new(false);
+    static LAST_READ_FROM_SLOW: std::cell::Cell<bool> = const { std::cell::Cell::new(false) };
 }
 
-fn set_last_read_from_slow(value: bool) {
+fn set_last_read_from_slow(_value: bool) {
     #[cfg(test)]
     {
-        LAST_READ_FROM_SLOW.set(value);
+        LAST_READ_FROM_SLOW.set(_value);
     }
 }
 
@@ -198,158 +197,164 @@ impl<MSlow: EventsMap> DoubleEndedIterator for Iter<'_, MSlow> {
     }
 }
 
-#[test]
-fn get() {
-    let map = setup_map();
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ChatEventsMap;
 
-    for i in 0u32..100 {
-        let index = i.into();
-        let event = map.get(index).unwrap();
-        assert_eq!(event.index, index);
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+    #[test]
+    fn get() {
+        let map = setup_map();
+
+        for i in 0u32..100 {
+            let index = i.into();
+            let event = map.get(index).unwrap();
+            assert_eq!(event.index, index);
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+        }
+
+        assert!(map.get(100.into()).is_none());
+        assert!(!LAST_READ_FROM_SLOW.get());
     }
 
-    assert!(map.get(100.into()).is_none());
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
+    #[test]
+    fn iter() {
+        let map = setup_map();
 
-#[test]
-fn iter() {
-    let map = setup_map();
-
-    let mut expected = EventIndex::default();
-    for event in map.iter() {
-        assert_eq!(event.index, expected);
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
-        expected = expected.incr();
-    }
-    assert_eq!(expected, 100.into());
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
-
-#[test]
-fn iter_with_removed() {
-    let mut map = setup_map();
-
-    for i in 0u32..10 {
-        map.remove(EventIndex::from(5 + (10 * i)));
-    }
-
-    let mut expected = EventIndex::default();
-    for event in map.iter() {
-        if u32::from(expected) % 10 == 5 {
+        let mut expected = EventIndex::default();
+        for event in map.iter() {
+            assert_eq!(event.index, expected);
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
             expected = expected.incr();
         }
-        assert_eq!(event.index, expected);
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
-        expected = expected.incr();
-    }
-    assert_eq!(expected, 100.into());
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
-
-#[test]
-fn iter_rev() {
-    let map = setup_map();
-
-    let mut expected = EventIndex::from(100);
-    for event in map.iter().rev() {
-        expected = expected.decr();
-        assert_eq!(event.index, expected);
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
-    }
-    assert_eq!(expected, 0.into());
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
-
-#[test]
-fn iter_rev_with_removed() {
-    let mut map = setup_map();
-
-    for i in 0u32..10 {
-        map.remove(EventIndex::from(5 + (10 * i)));
+        assert_eq!(expected, 100.into());
+        assert!(!LAST_READ_FROM_SLOW.get());
     }
 
-    let mut expected = EventIndex::from(100);
-    for event in map.iter().rev() {
-        expected = expected.decr();
-        if u32::from(expected) % 10 == 5 {
-            expected = expected.decr();
+    #[test]
+    fn iter_with_removed() {
+        let mut map = setup_map();
+
+        for i in 0u32..10 {
+            map.remove(EventIndex::from(5 + (10 * i)));
         }
-        assert_eq!(event.index, expected);
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
-    }
-    assert_eq!(expected, 0.into());
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
 
-#[test]
-fn range() {
-    let map = setup_map();
-
-    let start = EventIndex::from(5);
-    let end = EventIndex::from(95);
-    let mut expected = start;
-    for event in map.range(start..=end) {
-        assert_eq!(event.index, expected);
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
-        expected = expected.incr();
-    }
-    assert_eq!(expected, end.incr());
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
-
-#[test]
-fn range_rev() {
-    let map = setup_map();
-
-    let start = EventIndex::from(5);
-    let end = EventIndex::from(95);
-    let mut expected = end;
-    for event in map.range(start..end).rev() {
-        expected = expected.decr();
-        assert_eq!(event.index, expected);
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
-    }
-    assert_eq!(expected, start);
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
-
-#[test]
-fn iter_both_ends() {
-    let map = setup_map();
-
-    let mut iter = map.iter();
-    for i in 0u32..50 {
-        let event = iter.next().unwrap();
-        assert_eq!(event.index, EventIndex::from(i));
-        assert!(LAST_READ_FROM_SLOW.get());
-
-        let event = iter.next_back().unwrap();
-        assert_eq!(event.index, EventIndex::from(99 - i));
-        assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
-    }
-    assert!(iter.next().is_none());
-    assert!(!LAST_READ_FROM_SLOW.get());
-}
-
-fn setup_map() -> HybridMap<ChatEventsMap> {
-    let mut map = HybridMap {
-        fast: BTreeMap::new(),
-        slow: ChatEventsMap::default(),
-        latest_event_index: EventIndex::default(),
-        max_events_in_fast_map: 10,
-    };
-
-    for i in 0u32..100 {
-        map.insert(EventWrapperInternal {
-            index: i.into(),
-            timestamp: i.into(),
-            event: ChatEventInternal::Empty,
-            correlation_id: 0,
-            expires_at: None,
-        });
+        let mut expected = EventIndex::default();
+        for event in map.iter() {
+            if u32::from(expected) % 10 == 5 {
+                expected = expected.incr();
+            }
+            assert_eq!(event.index, expected);
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+            expected = expected.incr();
+        }
+        assert_eq!(expected, 100.into());
+        assert!(!LAST_READ_FROM_SLOW.get());
     }
 
-    map
+    #[test]
+    fn iter_rev() {
+        let map = setup_map();
+
+        let mut expected = EventIndex::from(100);
+        for event in map.iter().rev() {
+            expected = expected.decr();
+            assert_eq!(event.index, expected);
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+        }
+        assert_eq!(expected, 0.into());
+        assert!(!LAST_READ_FROM_SLOW.get());
+    }
+
+    #[test]
+    fn iter_rev_with_removed() {
+        let mut map = setup_map();
+
+        for i in 0u32..10 {
+            map.remove(EventIndex::from(5 + (10 * i)));
+        }
+
+        let mut expected = EventIndex::from(100);
+        for event in map.iter().rev() {
+            expected = expected.decr();
+            if u32::from(expected) % 10 == 5 {
+                expected = expected.decr();
+            }
+            assert_eq!(event.index, expected);
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+        }
+        assert_eq!(expected, 0.into());
+        assert!(!LAST_READ_FROM_SLOW.get());
+    }
+
+    #[test]
+    fn range() {
+        let map = setup_map();
+
+        let start = EventIndex::from(5);
+        let end = EventIndex::from(95);
+        let mut expected = start;
+        for event in map.range(start..=end) {
+            assert_eq!(event.index, expected);
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+            expected = expected.incr();
+        }
+        assert_eq!(expected, end.incr());
+        assert!(!LAST_READ_FROM_SLOW.get());
+    }
+
+    #[test]
+    fn range_rev() {
+        let map = setup_map();
+
+        let start = EventIndex::from(5);
+        let end = EventIndex::from(95);
+        let mut expected = end;
+        for event in map.range(start..end).rev() {
+            expected = expected.decr();
+            assert_eq!(event.index, expected);
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+        }
+        assert_eq!(expected, start);
+        assert!(!LAST_READ_FROM_SLOW.get());
+    }
+
+    #[test]
+    fn iter_both_ends() {
+        let map = setup_map();
+
+        let mut iter = map.iter();
+        for i in 0u32..50 {
+            let event = iter.next().unwrap();
+            assert_eq!(event.index, EventIndex::from(i));
+            assert!(LAST_READ_FROM_SLOW.get());
+
+            let event = iter.next_back().unwrap();
+            assert_eq!(event.index, EventIndex::from(99 - i));
+            assert_eq!(LAST_READ_FROM_SLOW.get(), event.index < EventIndex::from(90));
+        }
+        assert!(iter.next().is_none());
+        assert!(!LAST_READ_FROM_SLOW.get());
+    }
+
+    fn setup_map() -> HybridMap<ChatEventsMap> {
+        let mut map = HybridMap {
+            fast: BTreeMap::new(),
+            slow: ChatEventsMap::default(),
+            latest_event_index: EventIndex::default(),
+            max_events_in_fast_map: 10,
+        };
+
+        for i in 0u32..100 {
+            map.insert(EventWrapperInternal {
+                index: i.into(),
+                timestamp: i.into(),
+                event: ChatEventInternal::Empty,
+                correlation_id: 0,
+                expires_at: None,
+            });
+        }
+
+        map
+    }
 }
