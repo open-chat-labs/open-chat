49a462ac715db444c60aeabe02b817b6366858c8
Hamish Peebles <hamishpeebles@gmail.com>
2024-10-28 15:21:00 +0000

    Fix tests

--- backend/libraries/group_chat_core/src/members.rs
+++ backend/libraries/group_chat_core/src/members.rs
@@ -568,7 +568,7 @@ mod tests {
     #[test]
     fn serialize_with_no_defaults() {
         let mut mentions = Mentions::default();
-        mentions.add(Some(1.into()), 1.into(), 1);
+        mentions.add(Some(1.into()), 1.into(), 1.into(), 1);
 
         let member = GroupMemberInternal {
             user_id: Principal::from_text("4bkt6-4aaaa-aaaaf-aaaiq-cai").unwrap().into(),
@@ -590,7 +590,7 @@ mod tests {
         let member_bytes = msgpack::serialize_then_unwrap(&member);
         let member_bytes_len = member_bytes.len();
 
-        assert_eq!(member_bytes_len, 134);
+        assert_eq!(member_bytes_len, 137);
 
         let _deserialized: GroupMemberInternal = msgpack::deserialize_then_unwrap(&member_bytes);
     }
