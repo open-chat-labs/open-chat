8a8894fe00ddb6e4eca8f7f689daa39c4af3e68d
Julian Jelfs <julian.jelfs@gmail.com>
2024-10-31 16:33:41 +0000

    tests are broken

--- frontend/openchat-client/jest.config.js
+++ frontend/openchat-client/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     testMatch: ["<rootDir>/openchat-client/src/**/*.spec.ts"],
     moduleNameMapper: {
         "openchat-shared": "<rootDir>/openchat-client/src/dummyModule.js",
-        "peerjs": "<rootDir>/openchat-client/src/dummyModule.js",
-        "../utils/notifications": "<rootDir>/src/dummyNotifications.js"
-    }
-}
+        peerjs: "<rootDir>/openchat-client/src/dummyModule.js",
+        "../utils/notifications": "<rootDir>/src/dummyNotifications.js",
+    },
+};
