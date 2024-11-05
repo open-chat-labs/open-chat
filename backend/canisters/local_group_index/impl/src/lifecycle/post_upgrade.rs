5c9bc8c46e383dcd69f79995bc3e826e1bcacecf
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-05 13:15:43 +0000

    Populate queue earlier in `post_upgrade`

--- backend/canisters/local_group_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/local_group_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,6 +1,6 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::{mutate_state, Data};
+use crate::Data;
 use candid::Principal;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
@@ -16,25 +16,21 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
+    let (mut data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
 
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
+
+    data.canisters_pending_events_migration_to_stable_memory = data
+        .local_groups
+        .iter()
+        .map(|(g, _)| Principal::from(*g))
+        .chain(data.local_communities.iter().map(|(c, _)| Principal::from(*c)))
+        .collect();
 
     let env = init_env(data.rng_seed);
     init_cycles_dispenser_client(data.cycles_dispenser_canister_id, data.test_mode);
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
-
-    mutate_state(|state| {
-        state
-            .data
-            .canisters_pending_events_migration_to_stable_memory
-            .extend(state.data.local_groups.iter().map(|(g, _)| Principal::from(*g)));
-        state
-            .data
-            .canisters_pending_events_migration_to_stable_memory
-            .extend(state.data.local_communities.iter().map(|(c, _)| Principal::from(*c)));
-    });
 }
