e4510ff74718ce5bbb37254341e1f2ee67d38241
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-06 12:12:54 +0000

    Flag migrate not complete if any threads pending

--- backend/libraries/chat_events/src/chat_events.rs
+++ backend/libraries/chat_events/src/chat_events.rs
@@ -52,6 +52,10 @@ pub struct ChatEvents {
 }
 
 impl ChatEvents {
+    pub fn thread_messages_to_update_in_stable_memory_len(&self) -> usize {
+        self.thread_messages_to_update_in_stable_memory.len()
+    }
+
     pub fn update_event_in_stable_memory(&mut self, event_key: EventKey) {
         self.main.update_event_in_stable_memory(event_key);
     }
