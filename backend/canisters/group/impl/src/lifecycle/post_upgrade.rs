e5d38a9d2d6f5f3e87a969b71a70ce6714170201
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-07 10:55:01 +0000

    Add comment

--- backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
@@ -23,6 +23,8 @@ fn post_upgrade(args: Args) {
     // Only proceed with removing events from the heap if the stable memory migration is complete
     assert!(data.stable_memory_event_migration_complete);
 
+    // Set the migration flag to false if threads need to be updated, but this can be done purely
+    // in stable memory so no need to abort the upgrade
     if data.chat.events.thread_messages_to_update_in_stable_memory_len() > 0 {
         data.stable_memory_event_migration_complete = false;
     }
