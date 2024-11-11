3d2dcf7a4ac3bc6cbbbd9fcaa8b45c6beea606c1
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-11 11:58:55 +0000

    Fix

--- backend/canisters/local_group_index/impl/src/jobs/mod.rs
+++ backend/canisters/local_group_index/impl/src/jobs/mod.rs
@@ -7,7 +7,7 @@ pub mod upgrade_groups;
 
 pub(crate) fn start(state: &RuntimeState) {
     migrate_events_to_stable_memory::start_job_if_required(state);
-    topup_canister_pool::start_job_if_required(state);
+    topup_canister_pool::start_job_if_required(state, None);
     upgrade_communities::start_job_if_required(state);
     upgrade_groups::start_job_if_required(state);
 }
--- backend/canisters/local_user_index/impl/src/jobs/mod.rs
+++ backend/canisters/local_user_index/impl/src/jobs/mod.rs
@@ -8,6 +8,6 @@ pub mod upgrade_canisters;
 pub(crate) fn start(state: &RuntimeState) {
     migrate_events_to_stable_memory::start_job_if_required(state);
     delete_users::start_job_if_required(state, None);
-    topup_canister_pool::start_job_if_required(state);
+    topup_canister_pool::start_job_if_required(state, None);
     upgrade_canisters::start_job_if_required(state);
 }
