6ac55bca98fac899e2b91965bf0c07a73da12ea7
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-18 13:52:37 +0000

    Improve job to delete files after messages are deleted

--- backend/libraries/storage_bucket_client/src/lib.rs
+++ backend/libraries/storage_bucket_client/src/lib.rs
@@ -1,41 +1,21 @@
-use ic_cdk::api::call::CallResult;
-use std::cell::RefCell;
 use std::collections::HashMap;
 use types::{BlobReference, CanisterId};
+use utils::canister::should_retry_failed_c2c_call;
 
-thread_local! {
-    static PENDING_RETRY: RefCell<Vec<BlobReference>> = RefCell::default();
-}
-
-pub fn retry_failed() {
-    let references = PENDING_RETRY.take();
-    if !references.is_empty() {
-        ic_cdk::spawn(delete_files(references));
-    }
-}
-
-pub async fn delete_files(blob_references: Vec<BlobReference>) {
-    async fn delete_files_inner(
-        canister_id: CanisterId,
-        blob_ids: Vec<u128>,
-    ) -> CallResult<storage_bucket_canister::delete_files::Response> {
+pub async fn delete_files(blob_references: Vec<BlobReference>) -> Vec<BlobReference> {
+    async fn delete_files_inner(canister_id: CanisterId, blob_ids: Vec<u128>) -> Result<(), Vec<BlobReference>> {
         let args = storage_bucket_canister::delete_files::Args {
             file_ids: blob_ids.clone(),
         };
 
-        let response = storage_bucket_canister_c2c_client::delete_files(canister_id, &args).await;
-        if let Err((_, message)) = &response {
-            // If the bucket canister was stopped, mark that the deletion should be retried later.
-            if message.to_uppercase().contains("STOPPED") {
-                let references: Vec<_> = blob_ids
-                    .into_iter()
-                    .map(|blob_id| BlobReference { canister_id, blob_id })
-                    .collect();
-
-                PENDING_RETRY.with_borrow_mut(|r| r.extend(references));
-            }
+        match storage_bucket_canister_c2c_client::delete_files(canister_id, &args).await {
+            Ok(_) => Ok(()),
+            Err((code, message)) if should_retry_failed_c2c_call(code, &message) => Err(blob_ids
+                .into_iter()
+                .map(|blob_id| BlobReference { canister_id, blob_id })
+                .collect()),
+            Err(_) => Err(Vec::new()),
         }
-        response
     }
 
     let mut grouped: HashMap<CanisterId, Vec<u128>> = HashMap::new();
@@ -48,5 +28,9 @@ pub async fn delete_files(blob_references: Vec<BlobReference>) {
         .map(|(canister_id, blob_ids)| delete_files_inner(canister_id, blob_ids))
         .collect();
 
-    futures::future::join_all(futures).await;
+    let results = futures::future::join_all(futures).await;
+    results
+        .into_iter()
+        .flat_map(|res| if let Err(brs) = res { brs } else { Vec::new() })
+        .collect()
 }
