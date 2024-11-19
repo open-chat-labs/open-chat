c223b5d05bfe2e2db040bf9e9e7ed4dd4424f915
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-19 12:35:07 +0000

    Simplify `inspect_message`

--- backend/canisters/local_group_index/impl/src/lifecycle/inspect_message.rs
+++ backend/canisters/local_group_index/impl/src/lifecycle/inspect_message.rs
@@ -2,5 +2,5 @@ use ic_cdk::inspect_message;
 
 #[inspect_message]
 fn inspect_message() {
-    // 'inspect_message' only applies to ingress messages so calls to c2c methods should be rejected
+    // Reject all ingress messages
 }
