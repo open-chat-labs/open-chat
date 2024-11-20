ee565c6bc501a1e97435ad27dfc18bb58aaf3b77
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-20 22:57:09 +0000

    Revert "Modify upgrade job to top up, then migrate events, then upgrade (#6834)"

--- backend/canisters/local_user_index/impl/src/jobs/migrate_events_to_stable_memory.rs
+++ backend/canisters/local_user_index/impl/src/jobs/migrate_events_to_stable_memory.rs
@@ -2,7 +2,6 @@ use crate::{mutate_state, RuntimeState};
 use ic_cdk_timers::TimerId;
 use rand::Rng;
 use std::cell::Cell;
-use std::cmp::min;
 use std::time::Duration;
 use tracing::trace;
 use types::{CanisterId, Empty};
@@ -26,9 +25,7 @@ pub(crate) fn start_job_if_required(state: &RuntimeState) -> bool {
 
 fn run() {
     if let Some(canister_id) = mutate_state(next) {
-        ic_cdk::spawn(async move {
-            migrate_events(canister_id).await;
-        });
+        ic_cdk::spawn(migrate_events(canister_id));
     } else if let Some(timer_id) = TIMER_ID.take() {
         ic_cdk_timers::clear_timer(timer_id);
         trace!("'migrate_events_to_stable_memory' job stopped");
@@ -41,18 +38,16 @@ fn next(state: &mut RuntimeState) -> Option<CanisterId> {
     }
 
     let len = state.data.canisters_pending_events_migration_to_stable_memory.len();
-    let random: usize = state.env.rng().gen_range(0..min(len, 1000));
+    let random: usize = state.env.rng().gen_range(0..len);
 
     state
         .data
         .canisters_pending_events_migration_to_stable_memory
-        .iter()
-        .skip(random)
+        .get(random)
         .copied()
-        .next()
 }
 
-pub(crate) async fn migrate_events(canister_id: CanisterId) -> bool {
+async fn migrate_events(canister_id: CanisterId) {
     if let Ok(true) = user_canister_c2c_client::c2c_migrate_events_to_stable_memory(canister_id, &Empty {}).await {
         mutate_state(|state| {
             state
@@ -60,8 +55,5 @@ pub(crate) async fn migrate_events(canister_id: CanisterId) -> bool {
                 .canisters_pending_events_migration_to_stable_memory
                 .retain(|c| *c != canister_id);
         });
-        true
-    } else {
-        false
     }
 }
