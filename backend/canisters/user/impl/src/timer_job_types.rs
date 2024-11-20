1162f2ffe9706e45424f81353ef78fb34fb1b547
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-20 15:32:29 +0000

    Improve job to delete files after messages are deleted (#6839)

--- backend/canisters/user/impl/src/timer_job_types.rs
+++ backend/canisters/user/impl/src/timer_job_types.rs
@@ -9,7 +9,7 @@ use tracing::error;
 use types::{BlobReference, Chat, ChatId, CommunityId, EventIndex, MessageId, MessageIndex, P2PSwapStatus, UserId};
 use user_canister::{C2CReplyContext, UserCanisterEvent};
 use utils::consts::OPENCHAT_BOT_USER_ID;
-use utils::time::SECOND_IN_MS;
+use utils::time::{MINUTE_IN_MS, SECOND_IN_MS};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub enum TimerJob {
@@ -188,15 +188,8 @@ impl Job for HardDeleteMessageContentJob {
                 if sender == my_user_id {
                     let files_to_delete = content.blob_references();
                     if !files_to_delete.is_empty() {
-                        // If there was already a job queued up to delete these files, cancel it
-                        state.data.timer_jobs.cancel_jobs(|job| {
-                            if let TimerJob::DeleteFileReferences(j) = job {
-                                j.files.iter().all(|f| files_to_delete.contains(f))
-                            } else {
-                                false
-                            }
-                        });
-                        ic_cdk::spawn(storage_bucket_client::delete_files(files_to_delete));
+                        let delete_files_job = DeleteFileReferencesJob { files: files_to_delete };
+                        delete_files_job.execute();
                     }
                     if let MessageContentInternal::P2PSwap(s) = content {
                         if matches!(s.status, P2PSwapStatus::Open) {
@@ -215,7 +208,20 @@ impl Job for HardDeleteMessageContentJob {
 
 impl Job for DeleteFileReferencesJob {
     fn execute(self) {
-        ic_cdk::spawn(storage_bucket_client::delete_files(self.files.clone()));
+        ic_cdk::spawn(async move {
+            let to_retry = storage_bucket_client::delete_files(self.files.clone()).await;
+
+            if !to_retry.is_empty() {
+                mutate_state(|state| {
+                    let now = state.env.now();
+                    state.data.timer_jobs.enqueue_job(
+                        TimerJob::DeleteFileReferences(DeleteFileReferencesJob { files: to_retry }),
+                        now + MINUTE_IN_MS,
+                        now,
+                    );
+                });
+            }
+        });
     }
 }
 
--- backend/integration_tests/src/storage/allocation_exceeded_tests.rs
+++ backend/integration_tests/src/storage/allocation_exceeded_tests.rs
@@ -22,74 +22,80 @@ fn old_files_deleted_when_allocation_exceeded() {
         }],
     );
 
-    let file1 = vec![1u8; 500];
-    let file2 = vec![2u8; 500];
-    let file3 = vec![3u8; 500];
-    let file4 = vec![4u8; 600];
-
-    let allocated_bucket_response1 =
-        client::storage_index::happy_path::allocated_bucket(env, user_id, canister_ids.storage_index, &file1);
-    let bucket1 = allocated_bucket_response1.canister_id;
-    let file_id1 = allocated_bucket_response1.file_id;
-    client::storage_bucket::happy_path::upload_file(env, user_id, bucket1, file_id1, file1, None);
+    let file1 = client::storage_index::happy_path::upload_file(env, user_id, canister_ids.storage_index, 500, Vec::new());
 
     env.advance_time(Duration::from_millis(1));
 
-    let allocated_bucket_response2 =
-        client::storage_index::happy_path::allocated_bucket(env, user_id, canister_ids.storage_index, &file2);
-    let bucket2 = allocated_bucket_response2.canister_id;
-    let file_id2 = allocated_bucket_response2.file_id;
-    client::storage_bucket::happy_path::upload_file(env, user_id, bucket2, file_id2, file2, None);
+    let file2 = client::storage_index::happy_path::upload_file(env, user_id, canister_ids.storage_index, 500, Vec::new());
 
     tick_many(env, 10);
 
     assert!(client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket1, file_id1
+        env,
+        user_id,
+        file1.canister_id,
+        file1.blob_id
     ));
     assert!(client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket2, file_id2
+        env,
+        user_id,
+        file2.canister_id,
+        file2.blob_id
     ));
 
     env.advance_time(Duration::from_millis(1));
 
-    let allocated_bucket_response3 =
-        client::storage_index::happy_path::allocated_bucket(env, user_id, canister_ids.storage_index, &file3);
-    let bucket3 = allocated_bucket_response3.canister_id;
-    let file_id3 = allocated_bucket_response3.file_id;
-    client::storage_bucket::happy_path::upload_file(env, user_id, bucket3, file_id3, file3, None);
+    let file3 = client::storage_index::happy_path::upload_file(env, user_id, canister_ids.storage_index, 500, Vec::new());
 
     tick_many(env, 10);
 
     assert!(!client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket1, file_id1
+        env,
+        user_id,
+        file1.canister_id,
+        file1.blob_id
     ));
     assert!(client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket2, file_id2
+        env,
+        user_id,
+        file2.canister_id,
+        file2.blob_id
     ));
     assert!(client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket3, file_id3
+        env,
+        user_id,
+        file3.canister_id,
+        file3.blob_id
     ));
 
     env.advance_time(Duration::from_millis(1));
 
-    let allocated_bucket_response4 =
-        client::storage_index::happy_path::allocated_bucket(env, user_id, canister_ids.storage_index, &file4);
-    let bucket4 = allocated_bucket_response4.canister_id;
-    let file_id4 = allocated_bucket_response4.file_id;
-    client::storage_bucket::happy_path::upload_file(env, user_id, bucket4, file_id4, file4, None);
+    let file4 = client::storage_index::happy_path::upload_file(env, user_id, canister_ids.storage_index, 600, Vec::new());
 
     tick_many(env, 10);
 
     assert!(!client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket1, file_id1
+        env,
+        user_id,
+        file1.canister_id,
+        file1.blob_id
     ));
     assert!(!client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket2, file_id2
+        env,
+        user_id,
+        file2.canister_id,
+        file2.blob_id
     ));
     assert!(!client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket3, file_id3
+        env,
+        user_id,
+        file3.canister_id,
+        file3.blob_id
     ));
     assert!(client::storage_bucket::happy_path::file_exists(
-        env, user_id, bucket4, file_id4
+        env,
+        user_id,
+        file4.canister_id,
+        file4.blob_id
     ));
 }
