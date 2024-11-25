ca14ecdb996e600e94798a61e569c14ae12c6ba2
megrogan <megrogan@gmail.com>
2024-11-25 17:16:51 +0000

    clippy

--- backend/canisters/registry/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/registry/impl/src/lifecycle/post_upgrade.rs
@@ -1,6 +1,6 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::{mutate_state, Data};
+use crate::Data;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
