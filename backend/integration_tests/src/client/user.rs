9e405bfe0ca91d2ef937c2b6a8d8f8c6926403b8
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-27 09:44:10 +0000

    More

--- backend/integration_tests/src/client/user.rs
+++ backend/integration_tests/src/client/user.rs
@@ -200,7 +200,6 @@ pub mod happy_path {
                 history_visible_to_new_joiners: is_public,
                 permissions: None,
                 rules: Rules::default(),
-                gate: None,
                 gate_config: None,
                 default_channels,
                 default_channel_rules: None,
