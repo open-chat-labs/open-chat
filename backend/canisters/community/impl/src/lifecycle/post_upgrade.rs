86130beaac3ad39119f6a12164fda6e100dcffa8
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-28 12:32:56 +0000

    Populate in post upgrade

--- backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
@@ -36,6 +36,7 @@ fn post_upgrade(args: Args) {
 
     mutate_state(|state| {
         let now = state.env.now();
+        state.data.members.populate_member_channel_links();
         for channel in state.data.channels.iter_mut() {
             let count_removed = channel.chat.members.prune_member_updates(now);
             info!(count_removed, "Removed old member updates");
