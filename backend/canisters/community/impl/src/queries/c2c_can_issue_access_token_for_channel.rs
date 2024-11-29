727ddf6062f7279dcc68a8a0ff0042db12f6093d
Hamish Peebles <hamishpeebles@gmail.com>
2024-11-29 13:10:48 +0000

    More

--- backend/canisters/community/impl/src/queries/c2c_can_issue_access_token_for_channel.rs
+++ backend/canisters/community/impl/src/queries/c2c_can_issue_access_token_for_channel.rs
@@ -3,7 +3,7 @@ use crate::read_state;
 use crate::RuntimeState;
 use canister_api_macros::query;
 use community_canister::c2c_can_issue_access_token_for_channel::*;
-use group_chat_core::{GroupChatCore, GroupMemberInternal};
+use group_chat_core::{GroupChatCore, GroupRoleInternal};
 use types::{AccessTokenType, MessageContentType, VideoCallType};
 
 #[query(guard = "caller_is_local_user_index", msgpack = true)]
@@ -16,13 +16,13 @@ fn c2c_can_issue_access_token_for_channel_impl(args: Args, state: &RuntimeState)
         return false;
     };
 
-    let Some(member) = channel.chat.members.get(&args.user_id) else {
+    let Ok(member) = channel.chat.members.get_verified_member(args.user_id) else {
         return false;
     };
 
     match args.access_type {
         AccessTokenType::StartVideoCallV2(vc) => {
-            can_start_video_call(member, state.data.is_public, vc.call_type, &channel.chat)
+            can_start_video_call(member.role(), state.data.is_public, vc.call_type, &channel.chat)
         }
         AccessTokenType::JoinVideoCall | AccessTokenType::MarkVideoCallAsEnded => true,
         AccessTokenType::BotCommand(c) => channel.chat.members.get_bot(&c.bot).is_some_and(|b| {
@@ -36,12 +36,12 @@ fn c2c_can_issue_access_token_for_channel_impl(args: Args, state: &RuntimeState)
 }
 
 fn can_start_video_call(
-    member: &GroupMemberInternal,
+    member_role: GroupRoleInternal,
     is_public_community: bool,
     call_type: VideoCallType,
     chat: &GroupChatCore,
 ) -> bool {
-    if !member.role().is_permitted(chat.permissions.start_video_call) {
+    if !member_role.is_permitted(chat.permissions.start_video_call) {
         return false;
     }
 
