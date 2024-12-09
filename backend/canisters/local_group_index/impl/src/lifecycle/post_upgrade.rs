c3dab45f76fc6eb46ef879fb8192c545af6b8c3f
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-09 13:47:05 +0000

    Temporarily make Windoge98 community canister logs public (#7019)

--- backend/canisters/local_group_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/local_group_index/impl/src/lifecycle/post_upgrade.rs
@@ -3,10 +3,13 @@ use crate::memory::get_upgrades_memory;
 use crate::Data;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
+use ic_cdk::api::management_canister::main::{CanisterSettings, LogVisibility, UpdateSettingsArgument};
 use ic_cdk::post_upgrade;
 use local_group_index_canister::post_upgrade::Args;
 use stable_memory::get_reader;
+use std::time::Duration;
 use tracing::info;
+use types::CanisterId;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -25,4 +28,18 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    ic_cdk_timers::set_timer(Duration::ZERO, || ic_cdk::spawn(set_windoge_logs_public()));
+}
+
+async fn set_windoge_logs_public() {
+    ic_cdk::api::management_canister::main::update_settings(UpdateSettingsArgument {
+        canister_id: CanisterId::from_text("ow6el-gyaaa-aaaar-av5na-cai").unwrap(),
+        settings: CanisterSettings {
+            log_visibility: Some(LogVisibility::Public),
+            ..Default::default()
+        },
+    })
+    .await
+    .unwrap();
 }
