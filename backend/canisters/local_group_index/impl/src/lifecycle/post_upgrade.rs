4c85373470bf8006083d4da847ef9d0c7ccc3a8f
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-09 15:55:05 +0000

    Increase Windoge community canister's reserved cycles limit (#7022)

--- backend/canisters/local_group_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/local_group_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,15 +1,16 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{mutate_state, read_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
-use ic_cdk::api::management_canister::main::{CanisterSettings, LogVisibility, UpdateSettingsArgument};
+use ic_cdk::api::management_canister::main::{CanisterSettings, UpdateSettingsArgument};
 use ic_cdk::post_upgrade;
 use local_group_index_canister::post_upgrade::Args;
 use stable_memory::get_reader;
 use std::time::Duration;
 use tracing::info;
-use types::CanisterId;
+use types::{CanisterId, CyclesTopUp};
+use utils::canister::deposit_cycles;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -29,17 +30,38 @@ fn post_upgrade(args: Args) {
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
 
-    ic_cdk_timers::set_timer(Duration::ZERO, || ic_cdk::spawn(set_windoge_logs_public()));
+    ic_cdk_timers::set_timer(Duration::ZERO, || {
+        ic_cdk::spawn(increase_windoge_reserved_cycles_limit_public())
+    });
 }
 
-async fn set_windoge_logs_public() {
+async fn increase_windoge_reserved_cycles_limit_public() {
+    let windoge_canister_id = CanisterId::from_text("ow6el-gyaaa-aaaar-av5na-cai").unwrap();
+
+    if read_state(|state| state.data.local_communities.get(&windoge_canister_id.into()).is_none()) {
+        return;
+    }
+
     ic_cdk::api::management_canister::main::update_settings(UpdateSettingsArgument {
-        canister_id: CanisterId::from_text("ow6el-gyaaa-aaaar-av5na-cai").unwrap(),
+        canister_id: windoge_canister_id,
         settings: CanisterSettings {
-            log_visibility: Some(LogVisibility::Public),
+            reserved_cycles_limit: Some(20_000_000_000_000u128.into()),
             ..Default::default()
         },
     })
     .await
     .unwrap();
+
+    let amount = 20_000_000_000_000u128;
+    deposit_cycles(windoge_canister_id, amount).await.unwrap();
+
+    mutate_state(|state| {
+        state.data.local_communities.mark_cycles_top_up(
+            &windoge_canister_id.into(),
+            CyclesTopUp {
+                amount,
+                date: state.env.now(),
+            },
+        );
+    })
 }
