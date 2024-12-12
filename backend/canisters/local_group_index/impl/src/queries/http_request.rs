f1c9c99c0b00f6a23c8262bb88a62b2521b75e23
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-12 15:51:53 +0000

    Expose the cycles top ups of User/Group/Community canisters

--- backend/canisters/local_group_index/impl/src/queries/http_request.rs
+++ backend/canisters/local_group_index/impl/src/queries/http_request.rs
@@ -1,7 +1,8 @@
 use crate::{read_state, RuntimeState};
 use http_request::{build_json_response, encode_logs, extract_route, Route};
 use ic_cdk::query;
-use types::{HttpRequest, HttpResponse, TimestampMillis};
+use std::collections::HashMap;
+use types::{CanisterId, HttpRequest, HttpResponse, TimestampMillis};
 
 #[query]
 fn http_request(request: HttpRequest) -> HttpResponse {
@@ -21,11 +22,24 @@ fn http_request(request: HttpRequest) -> HttpResponse {
         build_json_response(&state.metrics())
     }
 
+    fn get_top_ups(qs: HashMap<String, String>, state: &RuntimeState) -> HttpResponse {
+        let canister_id = CanisterId::from_text(qs.get("canister_id").unwrap()).unwrap();
+
+        if let Some(group) = state.data.local_groups.get(&canister_id.into()) {
+            build_json_response(&group.cycle_top_ups)
+        } else if let Some(community) = state.data.local_communities.get(&canister_id.into()) {
+            build_json_response(&community.cycle_top_ups)
+        } else {
+            HttpResponse::not_found()
+        }
+    }
+
     match extract_route(&request.url) {
         Route::Errors(since) => get_errors_impl(since),
         Route::Logs(since) => get_logs_impl(since),
         Route::Traces(since) => get_traces_impl(since),
         Route::Metrics => read_state(get_metrics_impl),
+        Route::Other(p, qs) if p == "top_ups" => read_state(|state| get_top_ups(qs, state)),
         _ => HttpResponse::not_found(),
     }
 }
