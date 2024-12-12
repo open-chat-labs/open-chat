b60c590284f33138aa63b6ce8edb6042c6049b4a
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-12 17:00:05 +0000

    Expose transfer errors that occur during swaps

--- backend/canisters/escrow/impl/src/queries/http_request.rs
+++ backend/canisters/escrow/impl/src/queries/http_request.rs
@@ -1,6 +1,8 @@
 use crate::{read_state, RuntimeState};
 use http_request::{build_json_response, encode_logs, extract_route, Route};
 use ic_cdk::query;
+use std::collections::HashMap;
+use std::str::FromStr;
 use types::{HttpRequest, HttpResponse, TimestampMillis};
 
 #[query]
@@ -21,11 +23,22 @@ fn http_request(request: HttpRequest) -> HttpResponse {
         build_json_response(&state.metrics())
     }
 
+    fn get_swap_logs(qs: HashMap<String, String>, state: &RuntimeState) -> HttpResponse {
+        let swap_id = u32::from_str(qs.get("swap_id").unwrap()).unwrap();
+
+        if let Some(swap) = state.data.swaps.get(swap_id) {
+            build_json_response(swap)
+        } else {
+            HttpResponse::not_found()
+        }
+    }
+
     match extract_route(&request.url) {
         Route::Errors(since) => get_errors_impl(since),
         Route::Logs(since) => get_logs_impl(since),
         Route::Traces(since) => get_traces_impl(since),
         Route::Metrics => read_state(get_metrics_impl),
+        Route::Other(p, qs) if p == "swap_logs" => read_state(|state| get_swap_logs(qs, state)),
         _ => HttpResponse::not_found(),
     }
 }
