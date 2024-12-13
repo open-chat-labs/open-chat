09e92a0d027e5ff10abf3bf44d45b6c056997128
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-13 11:57:31 +0000

    Fix `RoleChanged` events which were serialized under the wrong name (part 2)

--- backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
@@ -18,8 +18,10 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
+    let (mut data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
+
+    data.events.fix_role_changed_events();
 
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
