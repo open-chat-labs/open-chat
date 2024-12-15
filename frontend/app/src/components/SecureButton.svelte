c68a4490501f4e689c519fb10f870c779658baa9
Julian Jelfs <julian.jelfs@gmail.com>
2024-12-15 16:11:42 +0000

    another attempt to lock down prize messages

---
+++ frontend/app/src/components/SecureButton.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { ButtonProps } from "./Button.svelte";
+    import Button from "./Button.svelte";
+
+    let { children, onClick, ...rest }: ButtonProps & { onClick: (ev: MouseEvent) => void } =
+        $props();
+
+    // real users move, pause and then click. Bots either don't move at all or move and then click immediately
+    const PAUSE_TRESHOLD = 10;
+
+    let lastMoved = Date.now();
+
+    onMount(() => {
+        document.addEventListener("mousemove", mousemove);
+        return () => {
+            document.removeEventListener("mousemove", mousemove);
+        };
+    });
+
+    function mousemove() {
+        lastMoved = Date.now();
+    }
+
+    function probablyBot(ev: MouseEvent): boolean {
+        const pause = Date.now() - lastMoved;
+        console.log("Click Metrics: ", pause, document.activeElement === ev.target);
+        return pause < PAUSE_TRESHOLD || document.activeElement !== ev.target || !ev.isTrusted;
+    }
+
+    function internalOnclick(ev: MouseEvent) {
+        if (probablyBot(ev)) {
+            console.log("Looks like this button was clicked by a bot");
+        } else {
+            onClick(ev);
+        }
+    }
+</script>
+
+<Button on:click={internalOnclick} {...rest}>
+    {@render children?.()}
+</Button>
