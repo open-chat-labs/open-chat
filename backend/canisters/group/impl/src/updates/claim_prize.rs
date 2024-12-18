12cda7c81e7627eebcbae83a0b5894e93efc60d4
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-18 15:29:30 +0000

    Bound the jitter

--- backend/canisters/group/impl/src/updates/claim_prize.rs
+++ backend/canisters/group/impl/src/updates/claim_prize.rs
@@ -87,7 +87,8 @@ fn prepare(args: &Args, state: &mut RuntimeState) -> Result<PrepareResult, Box<R
         };
 
         // Hack to ensure 2 prizes claimed by the same user in the same block don't result in "duplicate transaction" errors.
-        let transaction_time = now_nanos - u32::from(result.message_index) as u64;
+        let jitter = u32::from(result.message_index) as u64 % 1000;
+        let transaction_time = now_nanos - jitter;
 
         let transaction = create_pending_transaction(
             result.token,
