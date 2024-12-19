dc8b33ad751522d80cf095ba8e438148e21844a6
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-19 11:50:05 +0000

    Fix notifications index canisterId in already running job

--- backend/canisters/registry/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/registry/impl/src/lifecycle/post_upgrade.rs
@@ -7,7 +7,6 @@ use ic_cdk::post_upgrade;
 use registry_canister::post_upgrade::Args;
 use stable_memory::get_reader;
 use tracing::info;
-use types::CanisterId;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -16,14 +15,8 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (mut data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
+    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
-
-    if data.test_mode {
-        data.notifications_index_canister_id = CanisterId::from_text("7ekiy-aiaaa-aaaaf-ab2dq-cai").unwrap();
-    } else {
-        data.notifications_index_canister_id = CanisterId::from_text("4glvk-ryaaa-aaaaf-aaaia-cai").unwrap();
-    }
 
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
