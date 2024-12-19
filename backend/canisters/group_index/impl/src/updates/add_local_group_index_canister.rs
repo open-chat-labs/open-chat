fedac21a1f6d4101d5ab4fdbca2a980e04525e28
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-19 14:22:27 +0000

    Add logging + skip steps that have already been completed (#7093)

--- backend/canisters/group_index/impl/src/updates/add_local_group_index_canister.rs
+++ backend/canisters/group_index/impl/src/updates/add_local_group_index_canister.rs
@@ -6,6 +6,7 @@ use canister_api_macros::update;
 use canister_tracing_macros::trace;
 use group_index_canister::add_local_group_index_canister::{Response::*, *};
 use group_index_canister::ChildCanisterType;
+use ic_cdk::api::management_canister::main::{canister_info, CanisterInfoRequest};
 use tracing::info;
 use types::{BuildVersion, CanisterId, CanisterWasm, Hash};
 use utils::canister::{install_basic, set_controllers};
@@ -17,14 +18,31 @@ async fn add_local_group_index_canister(args: Args) -> Response {
         Ok(result) => {
             let wasm_version = result.canister_wasm.version;
 
-            if let Err(error) = set_controllers(args.canister_id, vec![result.this_canister_id]).await {
-                return InternalError(format!("Failed to set controller: {error:?}"));
+            let canister_info = match canister_info(CanisterInfoRequest {
+                canister_id: args.canister_id,
+                num_requested_changes: None,
+            })
+            .await
+            {
+                Ok((info,)) => info,
+                Err(error) => return InternalError(format!("Failed to get canister info: {error:?}")),
+            };
+
+            let controllers = vec![result.this_canister_id];
+            if canister_info.controllers != controllers {
+                if let Err(error) = set_controllers(args.canister_id, controllers).await {
+                    return InternalError(format!("Failed to set controller: {error:?}"));
+                }
+                info!("Updated controllers");
             }
-            if let Err(error) = install_basic(args.canister_id, result.canister_wasm, result.init_args).await {
-                if !error.1.contains("canister is not empty") {
+
+            if canister_info.module_hash != Some(result.canister_wasm_hash.to_vec()) {
+                if let Err(error) = install_basic(args.canister_id, result.canister_wasm, result.init_args).await {
                     return InternalError(format!("Failed to install canister: {error:?}"));
                 }
+                info!("Installed wasm");
             }
+
             if let Err(error) = upgrade_group_wasm_in_local_group_index(
                 args.canister_id,
                 &result.group_canister_wasm,
@@ -56,6 +74,7 @@ async fn add_local_group_index_canister(args: Args) -> Response {
 struct PrepareResult {
     this_canister_id: CanisterId,
     canister_wasm: CanisterWasm,
+    canister_wasm_hash: Hash,
     group_canister_wasm: CanisterWasm,
     group_canister_wasm_hash: Hash,
     community_canister_wasm: CanisterWasm,
@@ -65,26 +84,21 @@ struct PrepareResult {
 
 fn prepare(args: &Args, state: &RuntimeState) -> Result<PrepareResult, Response> {
     if !state.data.local_index_map.contains_key(&args.canister_id) {
-        let canister_wasm = state
-            .data
-            .child_canister_wasms
-            .get(ChildCanisterType::LocalGroupIndex)
-            .wasm
-            .clone();
-        let wasm_version = canister_wasm.version;
+        let canister_wasm = state.data.child_canister_wasms.get(ChildCanisterType::LocalGroupIndex);
 
         let group_canister_wasm = state.data.child_canister_wasms.get(ChildCanisterType::Group);
         let community_canister_wasm = state.data.child_canister_wasms.get(ChildCanisterType::Community);
 
         Ok(PrepareResult {
             this_canister_id: state.env.canister_id(),
-            canister_wasm,
+            canister_wasm: canister_wasm.wasm.clone(),
+            canister_wasm_hash: canister_wasm.wasm_hash,
             group_canister_wasm: group_canister_wasm.wasm.clone(),
             group_canister_wasm_hash: group_canister_wasm.wasm_hash,
             community_canister_wasm: community_canister_wasm.wasm.clone(),
             community_canister_wasm_hash: community_canister_wasm.wasm_hash,
             init_args: local_group_index_canister::init::Args {
-                wasm_version,
+                wasm_version: canister_wasm.wasm.version,
                 user_index_canister_id: state.data.user_index_canister_id,
                 local_user_index_canister_id: args.local_user_index_canister_id,
                 group_index_canister_id: state.env.canister_id(),
