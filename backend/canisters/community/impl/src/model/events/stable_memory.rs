3a8127a0c141d49ca5be7c2244bacc211eeb0f90
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-19 17:28:35 +0000

    clippy

--- backend/canisters/community/impl/src/model/events/stable_memory.rs
+++ backend/canisters/community/impl/src/model/events/stable_memory.rs
@@ -1,4 +1,4 @@
-use crate::model::events::{CommunityEventInternal, CommunityEventInternalOld};
+use crate::model::events::CommunityEventInternal;
 use candid::Deserialize;
 use serde::Serialize;
 use stable_memory_map::{with_map, with_map_mut, CommunityEventKeyPrefix, KeyPrefix};
@@ -10,14 +10,6 @@ pub struct EventsStableStorage {
 }
 
 impl EventsStableStorage {
-    pub fn read_all(&self) -> Vec<EventWrapperInternal<CommunityEventInternalOld>> {
-        with_map(|m| {
-            m.range(self.prefix.create_key(&EventIndex::default())..)
-                .map(|(_, bytes)| msgpack::deserialize_then_unwrap(&bytes))
-                .collect()
-        })
-    }
-
     pub fn insert(&mut self, event: EventWrapperInternal<CommunityEventInternal>) {
         with_map_mut(|m| m.insert(self.prefix.create_key(&event.index), event_to_bytes(event)));
     }
