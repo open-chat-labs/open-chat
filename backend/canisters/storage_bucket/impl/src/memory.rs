8dc93063dbeebb1b4139c3d91f1cdda1dee2540d
Hamish Peebles <hamishpeebles@gmail.com>
2024-12-29 00:08:55 +0000

    Set virtual memory during init/post_upgrade

--- backend/canisters/storage_bucket/impl/src/memory.rs
+++ backend/canisters/storage_bucket/impl/src/memory.rs
@@ -6,6 +6,7 @@ use std::collections::BTreeMap;
 
 const UPGRADES: MemoryId = MemoryId::new(0);
 const BLOBS: MemoryId = MemoryId::new(1);
+const STABLE_MEMORY_MAP: MemoryId = MemoryId::new(2);
 
 pub type Memory = VirtualMemory<DefaultMemoryImpl>;
 
@@ -22,8 +23,12 @@ pub fn get_blobs_memory() -> Memory {
     get_memory(BLOBS)
 }
 
+pub fn get_stable_memory_map_memory() -> Memory {
+    get_memory(STABLE_MEMORY_MAP)
+}
+
 pub fn memory_sizes() -> BTreeMap<u8, u64> {
-    (0u8..=1).map(|id| (id, get_memory(MemoryId::new(id)).size())).collect()
+    (0u8..=2).map(|id| (id, get_memory(MemoryId::new(id)).size())).collect()
 }
 
 fn get_memory(id: MemoryId) -> Memory {
