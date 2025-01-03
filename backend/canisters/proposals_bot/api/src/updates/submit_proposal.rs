5a5cba433c254c839197edc2bfcb53f224eac205
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-03 14:36:57 +0000

    Support submitting proposals directly to ProposalsBot (#7143)

---
+++ backend/canisters/proposals_bot/api/src/updates/submit_proposal.rs
@@ -0,0 +1,33 @@
+use crate::ProposalToSubmit;
+use candid::CandidType;
+use serde::{Deserialize, Serialize};
+use types::{icrc2, CanisterId};
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct Args {
+    pub governance_canister_id: CanisterId,
+    pub proposal: ProposalToSubmit,
+    pub transaction: icrc2::PendingCryptoTransaction,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub enum Response {
+    Success,
+    GovernanceCanisterNotSupported,
+    InsufficientPayment(u128),
+    PaymentFailed(String),
+    Retrying(String),
+    InternalError(String),
+}
+
+impl From<crate::c2c_submit_proposal::Response> for Response {
+    fn from(value: crate::c2c_submit_proposal::Response) -> Self {
+        match value {
+            crate::c2c_submit_proposal::Response::Success => Response::Success,
+            crate::c2c_submit_proposal::Response::GovernanceCanisterNotSupported => Response::Success,
+            crate::c2c_submit_proposal::Response::InsufficientPayment(amount) => Response::InsufficientPayment(amount),
+            crate::c2c_submit_proposal::Response::Retrying(error) => Response::Retrying(error),
+            crate::c2c_submit_proposal::Response::InternalError(error) => Response::InternalError(error),
+        }
+    }
+}
---
+++ backend/canisters/proposals_bot/impl/src/updates/submit_proposal.rs
@@ -0,0 +1,82 @@
+use crate::model::nervous_systems::ValidateSubmitProposalPaymentError;
+use crate::updates::c2c_submit_proposal::{prepare_proposal, submit_proposal};
+use crate::{read_state, RuntimeState};
+use candid::Principal;
+use canister_api_macros::update;
+use canister_tracing_macros::trace;
+use ledger_utils::icrc2::process_transaction;
+use proposals_bot_canister::submit_proposal::{Response::*, *};
+use types::{icrc2, CanisterId, MultiUserChat, SnsNeuronId, UserDetails};
+use user_index_canister_c2c_client::{lookup_user, LookupUserError};
+
+#[update(msgpack = true)]
+#[trace]
+async fn submit_proposal(args: Args) -> Response {
+    let PrepareResult {
+        caller,
+        this_canister_id,
+        user_index_canister_id,
+        neuron_id,
+        chat,
+    } = match read_state(|state| prepare(args.governance_canister_id, &args.transaction, state)) {
+        Ok(ok) => ok,
+        Err(response) => return response,
+    };
+
+    let UserDetails { user_id, username, .. } = match lookup_user(caller, user_index_canister_id).await {
+        Ok(u) => u,
+        Err(LookupUserError::UserNotFound) => panic!("User not found"),
+        Err(LookupUserError::InternalError(error)) => return InternalError(format!("Failed to lookup user: {error}")),
+    };
+
+    match process_transaction(args.transaction.clone(), this_canister_id).await {
+        Ok(Ok(_)) => {}
+        Ok(Err(error)) => return PaymentFailed(error.error_message),
+        Err(error) => return InternalError(format!("{:?}", error)),
+    }
+
+    let proposal = prepare_proposal(args.proposal, user_id, username, chat);
+
+    submit_proposal(
+        user_id,
+        args.governance_canister_id,
+        neuron_id,
+        proposal,
+        args.transaction.ledger,
+        args.transaction.amount,
+        args.transaction.fee,
+    )
+    .await
+    .into()
+}
+
+struct PrepareResult {
+    caller: Principal,
+    this_canister_id: CanisterId,
+    user_index_canister_id: CanisterId,
+    neuron_id: SnsNeuronId,
+    chat: MultiUserChat,
+}
+
+fn prepare(
+    governance_canister_id: CanisterId,
+    transaction: &icrc2::PendingCryptoTransaction,
+    state: &RuntimeState,
+) -> Result<PrepareResult, Response> {
+    use ValidateSubmitProposalPaymentError as E;
+    match state.data.nervous_systems.validate_submit_proposal_payment(
+        &governance_canister_id,
+        transaction.ledger,
+        transaction.amount,
+    ) {
+        Ok(neuron_id) => Ok(PrepareResult {
+            caller: state.env.caller(),
+            this_canister_id: state.env.canister_id(),
+            user_index_canister_id: state.data.user_index_canister_id,
+            neuron_id,
+            chat: state.data.nervous_systems.get_chat_id(&governance_canister_id).unwrap(),
+        }),
+        Err(E::GovernanceCanisterNotSupported | E::IncorrectLedger) => Err(GovernanceCanisterNotSupported),
+        Err(E::InsufficientPayment(min)) => Err(InsufficientPayment(min.into())),
+    }
+}
