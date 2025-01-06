95d62274d336f0d70ded464d10c467aa0550d86d
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-06 17:03:32 +0000

    Switch ProposalsBot over to using MessagePack (#7165)

--- frontend/openchat-agent/src/services/proposalsBot/proposalsBot.client.ts
+++ frontend/openchat-agent/src/services/proposalsBot/proposalsBot.client.ts
@@ -1,20 +1,21 @@
 import type { HttpAgent, Identity } from "@dfinity/agent";
-import { Principal } from "@dfinity/principal";
-import { idlFactory, type ProposalsBotService } from "./candid/idl";
 import { CandidService } from "../candidService";
 import type {
     StakeNeuronForSubmittingProposalsResponse,
     TopUpNeuronResponse,
 } from "openchat-shared";
 import { stakeNeuronForSubmittingProposalsResponse, topUpNeuronResponse } from "./mappers";
+import { principalStringToBytes } from "../../utils/mapping";
+import {
+    ProposalsBotStakeNeuronForSubmittingProposalsArgs,
+    ProposalsBotStakeNeuronForSubmittingProposalsResponse,
+    ProposalsBotTopUpNeuronArgs,
+    ProposalsBotTopUpNeuronResponse,
+} from "../../typebox";
 
 export class ProposalsBotClient extends CandidService {
-    private service: ProposalsBotService;
-
     constructor(identity: Identity, agent: HttpAgent, canisterId: string) {
         super(identity, agent, canisterId);
-
-        this.service = this.createServiceClient<ProposalsBotService>(idlFactory);
     }
 
     stakeNeuronForSubmittingProposals(
@@ -22,21 +23,29 @@ export class ProposalsBotClient extends CandidService {
         stake: bigint,
     ): Promise<StakeNeuronForSubmittingProposalsResponse> {
         const args = {
-            governance_canister_id: Principal.fromText(governanceCanisterId),
+            governance_canister_id: principalStringToBytes(governanceCanisterId),
             stake,
         };
-        return this.handleResponse(
-            this.service.stake_neuron_for_submitting_proposals(args),
-            stakeNeuronForSubmittingProposalsResponse,
+        return this.executeMsgpackUpdate(
+            "stake_neuron_for_submitting_proposals",
             args,
+            stakeNeuronForSubmittingProposalsResponse,
+            ProposalsBotStakeNeuronForSubmittingProposalsArgs,
+            ProposalsBotStakeNeuronForSubmittingProposalsResponse,
         );
     }
 
     topUpNeuron(governanceCanisterId: string, amount: bigint): Promise<TopUpNeuronResponse> {
         const args = {
-            governance_canister_id: Principal.fromText(governanceCanisterId),
+            governance_canister_id: principalStringToBytes(governanceCanisterId),
             amount,
         };
-        return this.handleResponse(this.service.top_up_neuron(args), topUpNeuronResponse);
+        return this.executeMsgpackUpdate(
+            "top_up_neuron",
+            args,
+            topUpNeuronResponse,
+            ProposalsBotTopUpNeuronArgs,
+            ProposalsBotTopUpNeuronResponse,
+        );
     }
 }
