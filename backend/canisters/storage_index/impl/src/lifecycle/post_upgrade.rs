4c366249e68c98fb1029b4990aba036a7da58178
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-07 13:21:18 +0000

    Increase memory limit of another StorageBucket which has reached limit

--- backend/canisters/storage_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/storage_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,12 +1,15 @@
 use crate::lifecycle::{init_cycles_dispenser_client, init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{read_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
+use ic_cdk::api::management_canister::main::{CanisterSettings, UpdateSettingsArgument};
 use ic_cdk::post_upgrade;
 use stable_memory::get_reader;
+use std::time::Duration;
 use storage_index_canister::post_upgrade::Args;
-use tracing::info;
+use tracing::{error, info};
+use types::CanisterId;
 
 #[post_upgrade]
 #[trace]
@@ -28,4 +31,24 @@ fn post_upgrade(args: Args) {
     init_state(env, data, args.wasm_version);
 
     info!(version = %args.wasm_version, "Post-upgrade complete");
+
+    ic_cdk_timers::set_timer(Duration::ZERO, || {
+        ic_cdk::spawn(increase_heap_memory_limit(
+            CanisterId::from_text("6xr54-haaaa-aaaap-qhotq-cai").unwrap(),
+        ))
+    });
+}
+
+async fn increase_heap_memory_limit(canister_id: CanisterId) {
+    const HALF_GB: u128 = 512 * 1024 * 1024;
+
+    ic_cdk::api::management_canister::main::update_settings(UpdateSettingsArgument {
+        canister_id,
+        settings: CanisterSettings {
+            wasm_memory_limit: Some((7 * HALF_GB).into()), // 3.5 GB
+            ..Default::default()
+        },
+    })
+    .await
+    .unwrap()
 }
