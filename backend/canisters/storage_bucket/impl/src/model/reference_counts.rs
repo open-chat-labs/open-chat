960f85ab7882cdb91eba3821da0ff8679e3819c0
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-08 07:57:08 +0000

    Update CHANGELOGs

--- backend/canisters/storage_bucket/impl/src/model/reference_counts.rs
+++ backend/canisters/storage_bucket/impl/src/model/reference_counts.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use stable_memory_map::{with_map, with_map_mut, FileReferenceCountKeyPrefix, KeyPrefix};
+use stable_memory_map::{FileReferenceCountKeyPrefix, StableMemoryMap};
 use types::Hash;
 
 #[derive(Serialize, Deserialize, Default)]
@@ -7,43 +7,52 @@ pub struct ReferenceCountsStableMap {
     prefix: FileReferenceCountKeyPrefix,
 }
 
+impl StableMemoryMap<FileReferenceCountKeyPrefix, u32> for ReferenceCountsStableMap {
+    fn prefix(&self) -> &FileReferenceCountKeyPrefix {
+        &self.prefix
+    }
+
+    fn value_to_bytes(&self, value: u32) -> Vec<u8> {
+        value.to_be_bytes().to_vec()
+    }
+
+    fn bytes_to_value(&self, _key: &Hash, bytes: Vec<u8>) -> u32 {
+        u32::from_be_bytes(bytes.try_into().unwrap())
+    }
+}
+
 impl ReferenceCountsStableMap {
     pub fn incr(&mut self, hash: Hash) -> u32 {
-        let count = self.get(hash).saturating_add(1);
+        let count = self.get(&hash).unwrap_or_default().saturating_add(1);
         self.set(hash, count);
         count
     }
 
     pub fn decr(&mut self, hash: Hash) -> u32 {
-        let count = self.get(hash).saturating_sub(1);
+        let count = self.get(&hash).unwrap_or_default().saturating_sub(1);
         self.set(hash, count);
         count
     }
 
-    fn get(&self, hash: Hash) -> u32 {
-        with_map(|m| m.get(self.prefix.create_key(&hash)))
-            .map(bytes_to_u32)
-            .unwrap_or_default()
-    }
-
     pub fn set(&mut self, hash: Hash, count: u32) {
         if count == 0 {
-            with_map_mut(|m| m.remove(self.prefix.create_key(&hash)));
+            self.remove(&hash);
         } else {
-            with_map_mut(|m| m.insert(self.prefix.create_key(&hash), count.to_be_bytes().to_vec()));
+            self.insert(hash, count);
         }
     }
 
     #[cfg(test)]
     pub fn get_all(&self) -> std::collections::BTreeMap<Hash, u32> {
+        use stable_memory_map::{with_map, KeyPrefix};
+
         with_map(|m| {
             m.range(self.prefix.create_key(&[0; 32])..)
-                .map(|(k, v)| (k.hash(), bytes_to_u32(v)))
+                .map(|(k, v)| {
+                    let hash = k.hash();
+                    (hash, self.bytes_to_value(&hash, v))
+                })
                 .collect()
         })
     }
-}
-
-fn bytes_to_u32(bytes: Vec<u8>) -> u32 {
-    u32::from_be_bytes(bytes.try_into().unwrap())
 }
