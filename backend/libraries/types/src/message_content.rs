03a4fb95f4c02c124797e50710f240642b9fd71b
megrogan <megrogan@gmail.com>
2025-01-08 15:58:02 +0000

    Remove spurious change

--- backend/libraries/types/src/message_content.rs
+++ backend/libraries/types/src/message_content.rs
@@ -734,6 +734,13 @@ pub struct CustomContent {
 }
 
 #[ts_export]
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+pub struct DeletedBy {
+    pub deleted_by: UserId,
+    pub timestamp: TimestampMillis,
+}
+
+#[ts_export]
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct BlobReference {
     pub canister_id: CanisterId,
@@ -754,11 +761,4 @@ impl Debug for ThumbnailData {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("ThumbnailData").field("byte_length", &self.0.len()).finish()
     }
-}
-
-#[ts_export]
-#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
-pub struct DeletedBy {
-    pub deleted_by: UserId,
-    pub timestamp: TimestampMillis,
 }
