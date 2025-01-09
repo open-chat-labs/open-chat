3504f2403cc5e33d1dabefe7e91810c4802ec0a5
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-09 11:38:11 +0000

    Use composite query so that we can call into UserIndex to authorize caller

--- backend/canisters/translations/impl/src/queries/proposed.rs
+++ backend/canisters/translations/impl/src/queries/proposed.rs
@@ -4,7 +4,7 @@ use ic_cdk::query;
 use translations_canister::proposed::{Response::*, *};
 use user_index_canister_c2c_client::{lookup_user, LookupUserError};
 
-#[query]
+#[query(composite = true)]
 #[trace]
 async fn proposed(_args: Args) -> Response {
     let (user_index_canister_id, caller) = read_state(|state| (state.data.user_index_canister_id, state.env.caller()));
