8e8b75900b58dd991445665044835e15b41387e2
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-09 14:40:15 +0000

    Mark as full if heap or stable memory grows beyond limits

--- backend/canisters/storage_index/impl/src/updates/c2c_sync_bucket.rs
+++ backend/canisters/storage_index/impl/src/updates/c2c_sync_bucket.rs
@@ -1,6 +1,7 @@
 use crate::guards::caller_is_bucket;
 use crate::{mutate_state, RuntimeState};
 use canister_tracing_macros::trace;
+use constants::{ONE_GB, ONE_MB};
 use ic_cdk::update;
 use storage_index_canister::c2c_sync_bucket::*;
 
@@ -29,9 +30,9 @@ fn c2c_sync_bucket_impl(args: Args, state: &mut RuntimeState) -> Response {
         b.total_file_bytes = args.total_file_bytes;
     }
 
-    // if args.bytes_remaining <= 0 {
-    //     state.data.buckets.set_full(bucket, true);
-    // }
+    if args.heap_memory_used >= ONE_GB || args.stable_memory_used >= 40 * ONE_GB {
+        state.data.buckets.set_full(bucket, true);
+    }
 
     Response::Success(SuccessResult { files_rejected })
 }
