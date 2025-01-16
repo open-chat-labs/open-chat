41812151c3e32952a47a1a986884092f973a1678
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-16 00:34:48 +0000

    Fix channel Id deserialization

--- backend/libraries/types/src/channel_id.rs
+++ backend/libraries/types/src/channel_id.rs
@@ -1,10 +1,11 @@
 use candid::CandidType;
-use serde::{Deserialize, Serialize};
+use serde::de::{Error, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
 use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 use ts_rs::TS;
 
-#[derive(Serialize, Deserialize, CandidType, TS, Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
-#[serde(from = "u128")]
+#[derive(Serialize, CandidType, TS, Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct ChannelId(u32);
 
 impl Display for ChannelId {
@@ -28,5 +29,42 @@ impl From<u32> for ChannelId {
 impl ChannelId {
     pub fn as_u32(self) -> u32 {
         self.0
+    }
+}
+
+struct ChannelIdVisitor;
+
+impl<'de> Visitor<'de> for ChannelIdVisitor {
+    type Value = ChannelId;
+
+    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        formatter.write_str("a u32, u128 or string")
+    }
+
+    fn visit_u32<E>(self, v: u32) -> Result<Self::Value, E> {
+        Ok(v.into())
+    }
+
+    fn visit_u128<E>(self, v: u128) -> Result<Self::Value, E> {
+        Ok(v.into())
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match u128::from_str(v) {
+            Ok(value) => Ok(value.into()),
+            Err(error) => Err(E::custom(format!("invalid channel id: {v}. Error: {error}"))),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for ChannelId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_any(ChannelIdVisitor)
     }
 }
