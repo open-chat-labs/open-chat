335c6526ea21b01ee93a0f3bce9a8a205da94aec
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-16 17:32:46 +0000

    Fix

--- backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
@@ -18,10 +18,12 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
+    let (mut data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
 
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
+
+    data.members.remove_dangling_member_channel_links();
 
     let env = init_env(data.rng_seed);
     init_state(env, data, args.wasm_version);
@@ -39,7 +41,5 @@ fn post_upgrade(args: Args) {
         state
             .data
             .record_instructions_count(InstructionCountFunctionId::PostUpgrade, now);
-
-        state.data.members.remove_dangling_member_channel_links();
     });
 }
