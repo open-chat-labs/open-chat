601f0d61d3a42f4acf36a3a816453f35519a5ae0
Hamish Peebles <hamishpeebles@gmail.com>
2025-01-16 00:30:49 +0000

    Fix `MessageId` deserialization

--- backend/libraries/types/src/message_id.rs
+++ backend/libraries/types/src/message_id.rs
@@ -1,13 +1,15 @@
 use candid::CandidType;
 use rand::distributions::{Distribution, Standard};
 use rand::Rng;
-use std::cmp::Ordering;
+use serde::de::{Error, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
 use std::fmt::{Display, Formatter};
-use std::hash::{Hash, Hasher};
+use std::hash::Hash;
+use std::str::FromStr;
 use ts_export::ts_export;
 
 #[ts_export]
-#[derive(CandidType, Debug, Default, Clone, Copy, Eq)]
+#[derive(CandidType, Serialize, Debug, Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct MessageId(u64);
 
 impl MessageId {
@@ -40,80 +42,39 @@ impl Display for MessageId {
     }
 }
 
-impl PartialEq<MessageId> for MessageId {
-    fn eq(&self, other: &MessageId) -> bool {
-        self.as_u64() == other.as_u64()
-    }
-}
+struct MessageIdVisitor;
 
-impl PartialOrd<MessageId> for MessageId {
-    fn partial_cmp(&self, other: &MessageId) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+impl<'de> Visitor<'de> for MessageIdVisitor {
+    type Value = MessageId;
 
-impl Ord for MessageId {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.as_u64().cmp(&other.as_u64())
+    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        formatter.write_str("a u64, u128 or string")
     }
-}
 
-impl Hash for MessageId {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.as_u64().hash(state);
-    }
-}
-
-// Serialization
-impl serde::Serialize for MessageId {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        if serializer.is_human_readable() {
-            serializer.serialize_str(&self.0.to_string())
-        } else {
-            serializer.serialize_u64(self.as_u64())
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        match u128::from_str(v) {
+            Ok(value) => Ok(value.into()),
+            Err(error) => Err(E::custom(format!("invalid message id: {v}. Error: {error}"))),
         }
     }
-}
 
-// Deserialization
-mod deserialize {
-    use super::MessageId;
+    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E> {
+        Ok(v.into())
+    }
 
-    pub(super) struct MessageIdVisitor;
-
-    impl serde::de::Visitor<'_> for MessageIdVisitor {
-        type Value = MessageId;
-
-        fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            formatter.write_str("u128 or string")
-        }
-
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            let value = v.parse().map_err(E::custom)?;
-            Ok(MessageId(value))
-        }
-
-        fn visit_u128<E>(self, v: u128) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(v.into())
-        }
+    fn visit_u128<E>(self, v: u128) -> Result<Self::Value, E> {
+        Ok(v.into())
     }
 }
 
-impl<'de> serde::Deserialize<'de> for MessageId {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<MessageId, D::Error> {
-        use serde::de::Error;
-        if deserializer.is_human_readable() {
-            deserializer
-                .deserialize_str(deserialize::MessageIdVisitor)
-                .map_err(D::Error::custom)
-        } else {
-            deserializer.deserialize_u128(deserialize::MessageIdVisitor)
-        }
+impl<'de> Deserialize<'de> for MessageId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_any(MessageIdVisitor)
     }
 }
