d116183d96af15d374eea88867e083d7d560715b
Julian Jelfs <julian.jelfs@gmail.com>
2025-01-20 09:23:25 +0000

    wip

---
+++ frontend/app/src/components/home/communities/CommunityAvatar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import type { AvatarSize, CommunitySummary } from "openchat-client";
+    import Avatar from "../../Avatar.svelte";
+
+    interface Props {
+        selected: boolean;
+        avatarSize: AvatarSize;
+        community: CommunitySummary;
+    }
+
+    let { selected, avatarSize, community }: Props = $props();
+</script>
+
+<Avatar
+    {selected}
+    url={client.communityAvatarUrl(community.id.communityId, community.avatar)}
+    size={avatarSize} />
