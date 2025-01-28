6516a7d6811f247c52d49ed2603e78df6348f0c2
Ivan Jukic <ivan.jukic14@gmail.com>
2025-01-28 10:27:24 +0100

    added missing function reference

--- backend/canisters/identity/api/src/main.rs
+++ backend/canisters/identity/api/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     generate_candid_method!(identity, generate_challenge, update);
     generate_candid_method!(identity, initiate_identity_link, update);
     generate_candid_method!(identity, prepare_delegation, update);
+    generate_candid_method!(identity, remove_identity_link, update);
 
     candid::export_service!();
     std::print!("{}", __export_service());
