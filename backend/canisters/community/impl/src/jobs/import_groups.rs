f7de692298e7e45e9fb4f367bcd7082f697dd69e
megrogan <megrogan@gmail.com>
2025-01-29 09:36:12 +0000

    Update canbench results

--- backend/canisters/community/impl/src/jobs/import_groups.rs
+++ backend/canisters/community/impl/src/jobs/import_groups.rs
@@ -221,8 +221,8 @@ pub(crate) fn finalize_group_import(group_id: ChatId) {
             state.data.channels.add(Channel {
                 id: channel_id,
                 chat,
-                date_imported: None,              // This is only set once everything is complete
-                bot_api_keys: Default::default(), // TODO: Should we import this?
+                date_imported: None, // This is only set once everything is complete
+                bot_api_keys: Default::default(),
             });
 
             state.data.timer_jobs.enqueue_job(
