46df1e9a38d38280d9a4a467766fdfb43e8e5e95
Hamish Peebles <hamishpeebles@gmail.com>
2025-02-10 16:12:11 +0000

    Bump Typebox and skip initial `Clone` when validating (#7389)

--- frontend/openchat-agent/src/utils/typebox.ts
+++ frontend/openchat-agent/src/utils/typebox.ts
@@ -3,5 +3,5 @@ import type { Static, TSchema } from "@sinclair/typebox";
 import { deepRemoveNullishFields } from "./nullish";
 
 export function typeboxValidate<T extends TSchema>(value: unknown, validator: T): Static<T> {
-    return Value.Parse(validator, deepRemoveNullishFields(value));
+    return Value.Parse(["Default", "Convert", "Assert"], validator, deepRemoveNullishFields(value));
 }
