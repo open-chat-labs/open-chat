8d9ff2af47de66044c5d8d432b5a739db39e4f14
Hamish Peebles <hamishpeebles@gmail.com>
2025-02-12 15:57:33 +0000

    Remove the old `start_video_call` and `end_video_call` endpoints (#7399)

--- backend/canisters/group/api/src/updates/start_video_call_v2.rs
+++ backend/canisters/group/api/src/updates/start_video_call_v2.rs
@@ -3,16 +3,6 @@ use serde::{Deserialize, Serialize};
 use types::{MessageId, Milliseconds, UserId, VideoCallType};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
-pub struct ArgsV1 {
-    pub message_id: u128,
-    pub initiator: UserId,
-    pub initiator_username: String,
-    pub initiator_display_name: Option<String>,
-    pub max_duration: Option<Milliseconds>,
-    pub call_type: VideoCallType,
-}
-
-#[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub message_id: MessageId,
     pub initiator: UserId,
@@ -26,17 +16,4 @@ pub struct Args {
 pub enum Response {
     Success,
     NotAuthorized,
-}
-
-impl From<ArgsV1> for Args {
-    fn from(value: ArgsV1) -> Self {
-        Args {
-            message_id: value.message_id.into(),
-            initiator: value.initiator,
-            initiator_username: value.initiator_username,
-            initiator_display_name: value.initiator_display_name,
-            max_duration: value.max_duration,
-            call_type: value.call_type,
-        }
-    }
 }
