9ec66a339347fa005b8387b0662635c5b56bd62b
Hamish Peebles <hamishpeebles@gmail.com>
2025-02-13 16:58:33 +0000

    Expose recent daily CHIT claims

--- backend/canisters/user/impl/src/queries/http_request.rs
+++ backend/canisters/user/impl/src/queries/http_request.rs
@@ -1,8 +1,9 @@
+use crate::model::streak::Streak;
 use crate::{read_state, RuntimeState};
 use http_request::{build_json_response, encode_logs, extract_route, get_document, Route};
 use ic_cdk::query;
 use itertools::Itertools;
-use types::{HttpRequest, HttpResponse, TimestampMillis};
+use types::{ChitEarnedReason, HttpRequest, HttpResponse, TimestampMillis};
 
 #[query]
 fn http_request(request: HttpRequest) -> HttpResponse {
@@ -32,6 +33,17 @@ fn http_request(request: HttpRequest) -> HttpResponse {
         build_json_response(&swaps)
     }
 
+    fn daily_claims(state: &RuntimeState) -> HttpResponse {
+        let (chit_events, _) = state.data.chit_events.events(None, None, 0, 200, false);
+        let claims: Vec<_> = chit_events
+            .into_iter()
+            .filter(|e| matches!(e.reason, ChitEarnedReason::DailyClaim))
+            .map(|c| (Streak::timestamp_to_day(c.timestamp), c))
+            .collect();
+
+        build_json_response(&claims)
+    }
+
     match extract_route(&request.url) {
         Route::Avatar(requested_avatar_id) => read_state(|state| get_avatar_impl(requested_avatar_id, state)),
         Route::Errors(since) => get_errors_impl(since),
@@ -39,6 +51,7 @@ fn http_request(request: HttpRequest) -> HttpResponse {
         Route::Traces(since) => get_traces_impl(since),
         Route::Metrics => read_state(get_metrics_impl),
         Route::Other(path, _) if path == "swaps" => read_state(get_swaps),
+        Route::Other(path, _) if path == "daily_claims" => read_state(daily_claims),
         _ => HttpResponse::not_found(),
     }
 }
