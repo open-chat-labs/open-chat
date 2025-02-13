bcbd18e44ef569974ce0a7ccf8eca04e6bb8dd4a
Hamish Peebles <hamishpeebles@gmail.com>
2025-02-13 09:16:33 +0000

    fmt

--- backend/tools/dupe_deps_detector/src/main.rs
+++ backend/tools/dupe_deps_detector/src/main.rs
@@ -2,10 +2,17 @@ use std::collections::BTreeSet;
 
 fn main() {
     let package = std::env::args().nth(1).unwrap();
-    let output = std::process::Command::new("cargo").args(["tree", "-p", &package]).output().unwrap();
+    let output = std::process::Command::new("cargo")
+        .args(["tree", "-p", &package])
+        .output()
+        .unwrap();
 
     let mut deps = BTreeSet::new();
-    for line in String::from_utf8(output.stdout).unwrap().lines().filter(|line| !line.ends_with("(*)")) {
+    for line in String::from_utf8(output.stdout)
+        .unwrap()
+        .lines()
+        .filter(|line| !line.ends_with("(*)"))
+    {
         let dep = line.chars().skip_while(|c| !c.is_alphabetic()).collect::<String>();
         deps.insert(dep);
     }
