bb4203b0c67e64597c8d78193e48b87fe5501821
Hamish Peebles <hamishpeebles@gmail.com>
2025-02-24 10:10:22 +0000

    Bump PocketIC to pull in wasm code section size limit increase

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -30,7 +30,7 @@ fi
 
 cd backend/integration_tests
 echo "PocketIC download starting"
-curl -Ls https://github.com/dfinity/pocketic/releases/download/${POCKET_IC_SERVER_VERSION}/pocket-ic-x86_64-${PLATFORM}.gz -o pocket-ic.gz || exit 1
+curl -Ls https://download.dfinity.systems/ic/0923aa8f3ea2e2a251e3bfb1525442215b06b2b3/binaries/x86_64-${PLATFORM}/pocket-ic.gz -o pocket-ic.gz || exit 1
 gzip -df pocket-ic.gz
 chmod +x pocket-ic
 echo "PocketIC download completed"
