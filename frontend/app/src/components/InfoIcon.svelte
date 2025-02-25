f36ed953c823e93112c4a95ecb9506e51f4c6dd3
Julian Jelfs <julian.jelfs@gmail.com>
2025-02-25 15:40:31 +0000

    Support ephemeral messages from bots (#7468)

--- frontend/app/src/components/InfoIcon.svelte
+++ frontend/app/src/components/InfoIcon.svelte
@@ -2,11 +2,20 @@
     import TooltipWrapper from "./TooltipWrapper.svelte";
     import TooltipPopup from "./TooltipPopup.svelte";
     import Help from "svelte-material-icons/HelpCircleOutline.svelte";
+    import type { Alignment, Position } from "@src/utils/alignment";
+
+    interface Props {
+        position?: Position;
+        align?: Alignment;
+        color?: string;
+    }
+
+    let { position = "top", align = "end", color }: Props = $props();
 </script>
 
-<TooltipWrapper position={"top"} align={"end"}>
+<TooltipWrapper {position} {align}>
     <div slot="target" on:click class="help">
-        <Help />
+        <Help {color} />
     </div>
     <div let:position let:align slot="tooltip">
         <TooltipPopup {position} {align} textLength={100} longestWord={10}>
