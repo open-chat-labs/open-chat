ee8609865b32b7c260888538ac4868471960bba6
Julian Jelfs <julian.jelfs@gmail.com>
2025-02-26 14:34:47 +0000

    Publish bot by proposal (#7481)

---
+++ frontend/app/src/components/bots/BotPublisher.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { i18nKey, type ExternalBot } from "openchat-client";
+    import ChooseBot from "./ChooseBot.svelte";
+    import Legend from "../Legend.svelte";
+    import BotProperties from "./install/BotProperties.svelte";
+
+    interface Props {
+        selected: ExternalBot | undefined;
+    }
+
+    let { selected = $bindable() }: Props = $props();
+
+    function select(bot: ExternalBot) {
+        selected = bot;
+    }
+</script>
+
+{#if selected === undefined}
+    <Legend large label={i18nKey("proposal.maker.chooseBot")} />
+    <ChooseBot ownedOnly onSelect={select} />
+{:else}
+    <BotProperties padded bot={selected} installing={false}></BotProperties>
+{/if}
+
+<style lang="scss">
+</style>
