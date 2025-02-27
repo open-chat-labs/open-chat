5debab9c8107464c8bfca4c4e4f84fb498252b99
megrogan <megrogan@gmail.com>
2025-02-27 14:57:53 +0000

    On `user::c2c_install_bot` create a direct chat

--- backend/canisters/user/impl/src/updates/c2c_install_bot.rs
+++ backend/canisters/user/impl/src/updates/c2c_install_bot.rs
@@ -2,7 +2,9 @@ use crate::guards::caller_is_local_user_index;
 use crate::{mutate_state, run_regular_jobs, RuntimeState};
 use canister_api_macros::update;
 use canister_tracing_macros::trace;
+use rand::Rng;
 use types::c2c_install_bot::{Response::*, *};
+use types::UserType;
 
 #[update(guard = "caller_is_local_user_index", msgpack = true)]
 #[trace]
@@ -25,6 +27,14 @@ fn c2c_install_bot_impl(args: Args, state: &mut RuntimeState) -> Response {
 
     if !state.data.bots.add(args.bot_id, args.caller, args.granted_permissions, now) {
         return AlreadyAdded;
+    }
+
+    // If there isn't already a direct chat with the bot, create one now
+    if state.data.direct_chats.get(&args.bot_id.into()).is_some() {
+        state
+            .data
+            .direct_chats
+            .create(args.bot_id, UserType::BotV2, state.env.rng().gen(), now);
     }
 
     Success
