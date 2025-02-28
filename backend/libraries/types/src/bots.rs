d476f31959c79f625e062be97965afb0f30724f8
megrogan <megrogan@gmail.com>
2025-02-28 10:14:05 +0000

    serde default

--- backend/libraries/types/src/bots.rs
+++ backend/libraries/types/src/bots.rs
@@ -65,6 +65,7 @@ pub struct StringParam {
     pub max_length: u16,
     #[ts(as = "Vec<BotCommandOptionChoiceString>")]
     pub choices: Vec<BotCommandOptionChoice<String>>,
+    #[serde(default)]
     pub mutli_line: bool,
 }
 
