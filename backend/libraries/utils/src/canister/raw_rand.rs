7f4616e13b6fddc23848855541bce7b4e39cf895
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-02 00:49:31 +0000

    clippy

--- backend/libraries/utils/src/canister/raw_rand.rs
+++ backend/libraries/utils/src/canister/raw_rand.rs
@@ -4,11 +4,11 @@ use ic_cdk::management_canister;
 pub async fn get_random_seed() -> [u8; 32] {
     let raw_rand = match management_canister::raw_rand().await {
         Ok(res) => res,
-        Err(err) => ic_cdk::trap(&format!("failed to get seed: {err}")),
+        Err(err) => ic_cdk::trap(format!("failed to get seed: {err}")),
     };
 
     raw_rand.as_slice().try_into().unwrap_or_else(|_| {
-        ic_cdk::trap(&format!(
+        ic_cdk::trap(format!(
             "when creating seed from raw_rand output, expected raw randomness to be of length 32, got {}",
             raw_rand.len()
         ));
