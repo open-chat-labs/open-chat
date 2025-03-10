13323d326d5185bf89f46b975f3b477a237911aa
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-10 16:24:07 +0000

    Use `unbounded_wait` when installing canisters (#7558)

--- backend/canisters/group_index/CHANGELOG.md
+++ backend/canisters/group_index/CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [unreleased]
 
+## [[2.0.1630](https://github.com/open-chat-labs/open-chat/releases/tag/v2.0.1630-group_index)] - 2025-03-10
+
 ### Added
 
 - Add `versions` to upgrade filter to filter canisters to upgrade by version ([#7531](https://github.com/open-chat-labs/open-chat/pull/7531))
--- backend/libraries/utils/src/canister/start.rs
+++ backend/libraries/utils/src/canister/start.rs
@@ -1,20 +1,27 @@
 use crate::canister::convert_cdk_error;
-use ic_cdk::call::RejectCode;
-use ic_cdk::management_canister::{self, StartCanisterArgs};
+use candid::Principal;
+use ic_cdk::call::{Call, CallResult, RejectCode};
+use ic_cdk::management_canister::StartCanisterArgs;
 use tracing::error;
 use types::CanisterId;
 
 pub async fn start(canister_id: CanisterId) -> Result<(), (RejectCode, String)> {
-    management_canister::start_canister(&StartCanisterArgs { canister_id })
-        .await
-        .map_err(|error| {
-            let (code, msg) = convert_cdk_error(error);
-            error!(
-                %canister_id,
-                error_code = %code,
-                error_message = msg.as_str(),
-                "Error calling start_canister"
-            );
-            (code, msg)
-        })
+    start_canister(&StartCanisterArgs { canister_id }).await.map_err(|error| {
+        let (code, msg) = convert_cdk_error(error);
+        error!(
+            %canister_id,
+            error_code = %code,
+            error_message = msg.as_str(),
+            "Error calling start_canister"
+        );
+        (code, msg)
+    })
+}
+
+// Copied from CDK but modified to use `unbounded_wait`
+async fn start_canister(arg: &StartCanisterArgs) -> CallResult<()> {
+    Ok(Call::unbounded_wait(Principal::management_canister(), "start_canister")
+        .with_arg(arg)
+        .await?
+        .candid()?)
 }
