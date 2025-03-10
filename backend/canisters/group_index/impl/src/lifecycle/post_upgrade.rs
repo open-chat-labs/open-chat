3c86818cee4fae14986ecf773dc8c0ea88000498
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-10 11:03:42 +0000

    Log number of public and private groups active in the last year (#7552)

--- backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,8 +1,9 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::Data;
+use crate::{read_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
+use constants::DAY_IN_MS;
 use group_index_canister::post_upgrade::Args;
 use ic_cdk::post_upgrade;
 use stable_memory::get_reader;
@@ -26,4 +27,27 @@ fn post_upgrade(args: Args) {
 
     let total_instructions = ic_cdk::api::call_context_instruction_counter();
     info!(version = %args.wasm_version, total_instructions, "Post-upgrade complete");
+
+    read_state(|state| {
+        let now = state.env.now();
+        let one_year_ago = now.saturating_sub(365 * DAY_IN_MS);
+        let public_active_in_last_year = state
+            .data
+            .public_groups
+            .iter()
+            .filter(|g| g.has_been_active_since(one_year_ago))
+            .count();
+        let private_active_in_last_year = state
+            .data
+            .private_groups
+            .iter()
+            .filter(|g| g.has_been_active_since(one_year_ago))
+            .count();
+
+        info!(
+            private = private_active_in_last_year,
+            public = public_active_in_last_year,
+            "Number of groups active in the last year"
+        );
+    })
 }
