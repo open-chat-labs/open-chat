334e7b2b1b14cd31805eda8651e0b74644deb542
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-12 13:46:43 +0000

    Use unbounded-wait calls to upload wasm chunks (#7585)

--- backend/libraries/utils/src/canister/chunk_store.rs
+++ backend/libraries/utils/src/canister/chunk_store.rs
@@ -1,6 +1,8 @@
 use crate::canister::convert_cdk_error;
-use ic_cdk::call::RejectCode;
+use candid::Principal;
+use ic_cdk::call::{Call, CallResult, RejectCode};
 use ic_cdk::management_canister::{self, ClearChunkStoreArgs, UploadChunkArgs};
+use ic_management_canister_types::UploadChunkResult;
 use types::{CanisterId, Hash};
 
 const ONE_MB: usize = 1024 * 1024;
@@ -8,7 +10,7 @@ const ONE_MB: usize = 1024 * 1024;
 pub async fn upload_wasm_in_chunks(wasm: &[u8], store_canister_id: CanisterId) -> Result<Vec<Hash>, (RejectCode, String)> {
     let mut chunks = Vec::new();
     for chunk in wasm.chunks(ONE_MB) {
-        let chunk_hash = management_canister::upload_chunk(&UploadChunkArgs {
+        let chunk_hash = upload_chunk(&UploadChunkArgs {
             canister_id: store_canister_id,
             chunk: chunk.to_vec(),
         })
@@ -18,6 +20,13 @@ pub async fn upload_wasm_in_chunks(wasm: &[u8], store_canister_id: CanisterId) -
         chunks.push(chunk_hash.hash.try_into().unwrap());
     }
     Ok(chunks)
+}
+
+pub async fn upload_chunk(arg: &UploadChunkArgs) -> CallResult<UploadChunkResult> {
+    Ok(Call::unbounded_wait(Principal::management_canister(), "upload_chunk")
+        .with_arg(arg)
+        .await?
+        .candid()?)
 }
 
 pub async fn clear_chunk_store(store_canister_id: CanisterId) -> Result<(), (RejectCode, String)> {
