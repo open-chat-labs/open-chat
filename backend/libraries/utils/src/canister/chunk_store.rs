73bacb8da0d81687975ad0d81865e697f2d18329
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-12 13:35:34 +0000

    Fix

--- backend/libraries/utils/src/canister/chunk_store.rs
+++ backend/libraries/utils/src/canister/chunk_store.rs
@@ -10,7 +10,7 @@ const ONE_MB: usize = 1024 * 1024;
 pub async fn upload_wasm_in_chunks(wasm: &[u8], store_canister_id: CanisterId) -> Result<Vec<Hash>, (RejectCode, String)> {
     let mut chunks = Vec::new();
     for chunk in wasm.chunks(ONE_MB) {
-        let chunk_hash = management_canister::upload_chunk(&UploadChunkArgs {
+        let chunk_hash = upload_chunk(&UploadChunkArgs {
             canister_id: store_canister_id,
             chunk: chunk.to_vec(),
         })
