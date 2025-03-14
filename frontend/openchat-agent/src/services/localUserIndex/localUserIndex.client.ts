540d99e12eb4a5cf2e269db6224983d8b2324c20
Julian Jelfs <julian.jelfs@gmail.com>
2025-03-14 17:15:17 +0000

    remove debug

--- frontend/openchat-agent/src/services/localUserIndex/localUserIndex.client.ts
+++ frontend/openchat-agent/src/services/localUserIndex/localUserIndex.client.ts
@@ -440,7 +440,6 @@ export class LocalUserIndexClient extends MsgpackCanisterAgent {
     getAccessToken(accessType: AccessTokenType): Promise<string | undefined> {
         if (accessType.kind === "bot_action_by_command") {
             const args = apiAccessTokenType(accessType);
-            console.log("Mapped access token type: ", args);
             return this.executeMsgpackQuery(
                 "access_token_v2",
                 apiAccessTokenType(accessType),
