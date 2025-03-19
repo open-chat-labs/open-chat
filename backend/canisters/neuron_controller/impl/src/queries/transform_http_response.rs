301756f37eddab4ead15ed90230b7d440c9e9f88
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-19 23:27:43 +0000

    Bump CDK to 0.18.0-alpha.2

--- backend/canisters/neuron_controller/impl/src/queries/transform_http_response.rs
+++ backend/canisters/neuron_controller/impl/src/queries/transform_http_response.rs
@@ -1,8 +1,8 @@
-use ic_cdk::api::management_canister::http_request::{HttpResponse, TransformArgs};
+use ic_cdk::management_canister::{HttpRequestResult, TransformArgs};
 use ic_cdk::query;
 
 #[query]
-fn transform_http_response(args: TransformArgs) -> HttpResponse {
+fn transform_http_response(args: TransformArgs) -> HttpRequestResult {
     let mut response = args.response;
     response.headers.clear();
     response
