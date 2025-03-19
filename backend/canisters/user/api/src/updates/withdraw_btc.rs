db8bde7d796825ede56b09c4f20dd843ec0410bf
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-19 15:48:33 +0000

    Verify PIN when withdrawing BTC (#7629)

--- backend/canisters/user/api/src/updates/withdraw_btc.rs
+++ backend/canisters/user/api/src/updates/withdraw_btc.rs
@@ -2,12 +2,14 @@ use candid::CandidType;
 use oc_error_codes::OCError;
 use serde::{Deserialize, Serialize};
 use ts_export::ts_export;
+use types::{Milliseconds, PinNumberWrapper};
 
 #[ts_export(user, withdraw_btc)]
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub amount: u64,
     pub address: String,
+    pub pin: Option<PinNumberWrapper>,
 }
 
 #[ts_export(user, withdraw_btc)]
@@ -16,6 +18,9 @@ pub enum Response {
     Success(u64), // The block index of the ckBTC burn transaction
     ApproveError(String),
     RetrieveBtcError(String),
+    PinRequired,
+    PinIncorrect(Milliseconds),
+    TooManyFailedPinAttempts(Milliseconds),
     InternalError(String),
     Error(OCError),
 }
