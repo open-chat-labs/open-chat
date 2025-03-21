49105570f6daf69ee6084b2766f0e44221b07589
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-21 09:00:35 +0000

    querystring

--- frontend/openchat-client/src/utils/url.spec.ts
+++ frontend/openchat-client/src/utils/url.spec.ts
@@ -10,8 +10,8 @@ const testCases: { input: string, expectedUrls: string[] }[] = [{
     input: "qwerty http://abc.co.uk! asdfgh",
     expectedUrls: ["http://abc.co.uk"]
 }, {
-    input: `aerugr "https://oc.app/blah-123" and https://oc.app/blah-123;`,
-    expectedUrls: ["https://oc.app/blah-123"]
+    input: `aerugr "https://oc.app/blah-123?p=1234" and https://oc.app/blah-123?p=1234";`,
+    expectedUrls: ["https://oc.app/blah-123?p=1234"]
 }, {
     input: "http://abc.com https://abc.org https://abc.xyz#LINK_REMOVED",
     expectedUrls: ["http://abc.com", "https://abc.org", "https://abc.xyz#LINK_REMOVED"]
