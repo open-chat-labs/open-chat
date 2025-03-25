f45642934ff296297be381fd3ae8a7157f430bdb
Hamish Peebles <hamishpeebles@gmail.com>
2025-03-25 14:44:36 +0000

    Push event on each BTC withdrawal

--- backend/canisters/user/impl/src/updates/withdraw_btc.rs
+++ backend/canisters/user/impl/src/updates/withdraw_btc.rs
@@ -1,10 +1,12 @@
 use crate::guards::caller_is_owner;
 use crate::model::pin_number::VerifyPinError;
+use crate::updates::update_btc_balance::BtcDepositOrWithdrawalEventPayload;
 use crate::{mutate_state, read_state, run_regular_jobs};
 use canister_api_macros::update;
 use canister_tracing_macros::trace;
 use ckbtc_minter_canister::CKBTC_MINTER_CANISTER_ID;
 use constants::{CKBTC_LEDGER_CANISTER_ID, MINUTE_IN_MS, NANOS_PER_MILLISECOND};
+use event_store_producer::EventBuilder;
 use user_canister::withdraw_btc::{Response::*, *};
 
 #[update(guard = "caller_is_owner", msgpack = true)]
@@ -52,7 +54,20 @@ async fn withdraw_btc(args: Args) -> Response {
     )
     .await
     {
-        Ok(Ok(result)) => Success(result.block_index),
+        Ok(Ok(result)) => {
+            mutate_state(|state| {
+                let user_id_string = state.env.canister_id().to_string();
+                let now = state.env.now();
+                state.data.event_store_client.push(
+                    EventBuilder::new("btc_withdrawal", now)
+                        .with_user(user_id_string.clone(), true)
+                        .with_source(user_id_string, true)
+                        .with_json_payload(&BtcDepositOrWithdrawalEventPayload { amount: args.amount })
+                        .build(),
+                );
+            });
+            Success(result.block_index)
+        }
         Ok(Err(error)) => RetrieveBtcError(format!("{error:?}")),
         Err(error) => InternalError(format!("{error:?}")),
     }
