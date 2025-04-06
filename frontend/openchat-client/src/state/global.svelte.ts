4f1fb9da86689874a8f205e09649cf4b5e706272
Julian Jelfs <julian.jelfs@gmail.com>
2025-04-06 10:37:20 +0100

    use getter to make it readonly

--- frontend/openchat-client/src/state/global.svelte.ts
+++ frontend/openchat-client/src/state/global.svelte.ts
@@ -1,7 +1,10 @@
 import { ScreenDimensionState } from "./screenDimensions.svelte";
 
 class GlobalState {
-    screenDimensions = new ScreenDimensionState();
+    #screenDimensions = new ScreenDimensionState();
+    public get screenDimensions(): ScreenDimensionState {
+        return this.#screenDimensions;
+    }
 }
 
 export const globalState = new GlobalState();
