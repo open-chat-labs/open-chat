47cf4763a44dbc82b1fa0ff545a38089b790a7c3
Hamish Peebles <hamishpeebles@gmail.com>
2025-04-10 00:25:43 +0100

    Delete groups that have been dormant for over a year

--- backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group/impl/src/lifecycle/post_upgrade.rs
@@ -1,12 +1,16 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::{get_stable_memory_map_memory, get_upgrades_memory};
+use crate::updates::c2c_delete_group::delete_group;
 use crate::{read_state, Data};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
+use chat_events::Reader;
+use constants::OPENCHAT_BOT_USER_ID;
 use group_canister::post_upgrade::Args;
 use ic_cdk::post_upgrade;
 use instruction_counts_log::InstructionCountFunctionId;
 use stable_memory::get_reader;
+use std::time::Duration;
 use tracing::info;
 
 #[post_upgrade]
@@ -19,6 +23,10 @@ fn post_upgrade(args: Args) {
 
     let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
+
+    if should_delete_group(&data) {
+        ic_cdk_timers::set_timer(Duration::ZERO, || ic_cdk::futures::spawn(delete_group_async()));
+    }
 
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
@@ -34,4 +42,36 @@ fn post_upgrade(args: Args) {
             .data
             .record_instructions_count(InstructionCountFunctionId::PostUpgrade, now)
     });
+}
+
+fn should_delete_group(data: &Data) -> bool {
+    data.chat
+        .events
+        .main_events_reader()
+        .latest_message_event(None)
+        .is_some_and(|m| {
+            m.event.sender == OPENCHAT_BOT_USER_ID
+                && m.event
+                    .content
+                    .text()
+                    .is_some_and(|t| t.contains("This public group has been dormant for over a year"))
+        })
+}
+
+async fn delete_group_async() {
+    let (group_index_canister_id, group_name, members) = read_state(|state| {
+        (
+            state.data.group_index_canister_id,
+            state.data.chat.name.value.clone(),
+            state.data.chat.members.member_ids().iter().copied().collect(),
+        )
+    });
+
+    let args = group_index_canister::c2c_delete_group::Args {
+        deleted_by: OPENCHAT_BOT_USER_ID,
+        group_name,
+        members,
+    };
+
+    delete_group(group_index_canister_id, &args).await;
 }
