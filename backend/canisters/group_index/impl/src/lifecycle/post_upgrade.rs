fd88aefdb7cd4b5f82cf7994e7a0fad01036f0f5
Hamish Peebles <hamishpeebles@gmail.com>
2025-04-15 15:51:55 +0100

    Update GroupIndex post release (#7798)

--- backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,17 +1,13 @@
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::{read_state, Data};
+use crate::Data;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use group_index_canister::post_upgrade::Args;
-use ic_cdk::management_canister::DepositCyclesArgs;
 use ic_cdk::post_upgrade;
 use stable_memory::get_reader;
-use std::time::Duration;
-use tracing::{error, info};
+use tracing::info;
 use utils::cycles::init_cycles_dispenser_client;
-
-const T: u128 = 1_000_000_000_000;
 
 #[post_upgrade]
 #[trace]
@@ -30,29 +26,4 @@ fn post_upgrade(args: Args) {
 
     let total_instructions = ic_cdk::api::call_context_instruction_counter();
     info!(version = %args.wasm_version, total_instructions, "Post-upgrade complete");
-
-    let cycles_balance = ic_cdk::api::canister_liquid_cycle_balance();
-    let excess_cycles = cycles_balance.saturating_sub(200 * T);
-
-    if excess_cycles > 0 {
-        ic_cdk_timers::set_timer(Duration::ZERO, move || {
-            ic_cdk::futures::spawn(transfer_excess_cycles(excess_cycles))
-        });
-    }
-}
-
-async fn transfer_excess_cycles(cycles: u128) {
-    let cycles_dispenser_canister_id = read_state(|state| state.data.cycles_dispenser_canister_id);
-
-    match ic_cdk::management_canister::deposit_cycles(
-        &DepositCyclesArgs {
-            canister_id: cycles_dispenser_canister_id,
-        },
-        cycles,
-    )
-    .await
-    {
-        Ok(_) => info!(cycles, "Transferred excess cycles to CyclesDispenser"),
-        Err(error) => error!(?error, "Failed to transfer excess cycles to CyclesDispenser"),
-    }
 }
