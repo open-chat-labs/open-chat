c6a15c6bb0efa74cdf3c4b5d15677d49e0c0654e
Julian Jelfs <julian.jelfs@gmail.com>
2025-04-22 16:20:21 +0100

    add a few tests

--- frontend/openchat-client/src/state/app.svelte.spec.ts
+++ frontend/openchat-client/src/state/app.svelte.spec.ts
@@ -1,6 +1,12 @@
-import type { CommunityIdentifier, Member } from "openchat-shared";
+import {
+    emptyRules,
+    type ChatIdentifier,
+    type CommunityIdentifier,
+    type Member,
+} from "openchat-shared";
 import { vi } from "vitest";
 import { app } from "./app.svelte";
+import { chatDetailsLocalUpdates } from "./chat_details";
 import { communityLocalUpdates } from "./community_details/local.svelte";
 import { pathState } from "./path.svelte";
 
@@ -25,6 +31,109 @@ describe("app state", () => {
         pathState.setRouteParams(mockContext, {
             kind: "home_route",
             scope: { kind: "group_chat" },
+        });
+    });
+
+    describe("chat state", () => {
+        const communityId: CommunityIdentifier = { kind: "community", communityId: "123456" };
+        const chatId: ChatIdentifier = {
+            kind: "channel",
+            communityId: "123456",
+            channelId: 123456,
+        };
+        beforeEach(() => {
+            pathState.setRouteParams(mockContext, {
+                kind: "selected_channel_route",
+                chatId,
+                communityId,
+                messageIndex: 0,
+                open: false,
+                scope: { kind: "community", id: communityId },
+            });
+        });
+
+        test("selected message context is set", () => {
+            expect(app.selectedMessageContext).toMatchObject({
+                chatId: chatId,
+                threadRootMessageIndex: undefined,
+            });
+
+            pathState.setRouteParams(mockContext, {
+                kind: "selected_channel_route",
+                chatId,
+                communityId,
+                messageIndex: 10,
+                open: true, // simulate opening the thread
+                scope: { kind: "community", id: communityId },
+            });
+
+            expect(app.selectedMessageContext).toMatchObject({
+                chatId: chatId,
+                threadRootMessageIndex: 10,
+            });
+        });
+
+        test("chat list scope is set", () => {
+            expect(app.chatListScope).toMatchObject({ kind: "community", id: communityId });
+            pathState.setRouteParams(mockContext, {
+                kind: "explore_groups_route",
+                scope: { kind: "group_chat" },
+            });
+            expect(app.chatListScope).toMatchObject({ kind: "group_chat" });
+        });
+
+        test("selected chat id is set", () => {
+            expect(app.selectedChatId).toEqual(chatId);
+        });
+
+        function setChatDetails(chatId: ChatIdentifier) {
+            app.setChatDetailsFromServer(
+                chatId,
+                new Map([
+                    [
+                        "user_one",
+                        {
+                            role: "member",
+                            userId: "user_one",
+                            displayName: "User One",
+                            lapsed: false,
+                        },
+                    ],
+                ]),
+                new Set(),
+                new Set(["a", "b", "c"]),
+                new Set(),
+                new Set(),
+                emptyRules(),
+                new Map(),
+                new Map(),
+            );
+        }
+
+        describe("setting chat details", () => {
+            beforeEach(() => setChatDetails(chatId));
+
+            test("make sure local updates are merged", () => {
+                expect(app.selectedChat.members.has("user_one")).toBe(true);
+                const undo = chatDetailsLocalUpdates.removeMember(chatId, "user_one");
+                expect(app.selectedChat.members.has("user_one")).toBe(false);
+                undo();
+                expect(app.selectedChat.members.has("user_one")).toBe(true);
+            });
+
+            test("make sure that only server state is overwritten if chatId doesn't change", () => {
+                app.selectedChat.expandDeletedMessages(new Set([1, 2, 3]));
+                expect(app.selectedChat.expandedDeletedMessages.has(3)).toBe(true);
+                setChatDetails(chatId); // reset the server state for the *same* chatId
+                expect(app.selectedChat.expandedDeletedMessages.has(3)).toBe(true);
+            });
+
+            test("make sure that all state is overwritten if the chatId *does* change", () => {
+                app.selectedChat.expandDeletedMessages(new Set([1, 2, 3]));
+                expect(app.selectedChat.expandedDeletedMessages.has(3)).toBe(true);
+                setChatDetails({ ...chatId, channelId: 654321 }); // reset the server state for a different chatId
+                expect(app.selectedChat.expandedDeletedMessages.has(3)).toBe(false);
+            });
         });
     });
 
