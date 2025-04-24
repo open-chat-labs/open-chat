5feaa9c3c6180965b64a64b244f56e2e3dd80a0f
megrogan <megrogan@gmail.com>
2025-04-24 12:28:55 +0100

    Rationalise routing

--- backend/canisters/community/impl/src/queries/http_request.rs
+++ backend/canisters/community/impl/src/queries/http_request.rs
@@ -1,23 +1,23 @@
 use crate::{RuntimeState, read_state};
-use http_request::{Route, build_json_response, encode_logs, extract_route, get_document};
+use http_request::{AvatarRoute, Route, build_json_response, encode_logs, extract_route, get_document};
 use ic_cdk::query;
-use types::{ChannelId, HttpRequest, HttpResponse, TimestampMillis};
+use types::{HttpRequest, HttpResponse, TimestampMillis};
 
 #[query]
 fn http_request(request: HttpRequest) -> HttpResponse {
-    fn get_avatar_impl(requested_avatar_id: Option<u128>, state: &RuntimeState) -> HttpResponse {
-        get_document(requested_avatar_id, state.data.avatar.as_ref(), "avatar")
-    }
-
-    fn get_channel_avatar_impl(channel_id: ChannelId, requested_avatar_id: Option<u128>, state: &RuntimeState) -> HttpResponse {
-        if let Some(channel) = state.data.channels.get(&channel_id) {
-            get_document(
-                requested_avatar_id,
-                channel.chat.avatar.as_ref(),
-                &format!("channel/{channel_id}/avatar"),
-            )
+    fn get_avatar_impl(route: AvatarRoute, state: &RuntimeState) -> HttpResponse {
+        if let Some(channel_id) = route.channel_id {
+            if let Some(channel) = state.data.channels.get(&channel_id) {
+                get_document(
+                    route.blob_id,
+                    channel.chat.avatar.as_ref(),
+                    &format!("channel/{channel_id}/avatar"),
+                )
+            } else {
+                HttpResponse::not_found()
+            }
         } else {
-            HttpResponse::not_found()
+            get_document(route.blob_id, state.data.avatar.as_ref(), "avatar")
         }
     }
 
@@ -57,10 +57,7 @@ fn http_request(request: HttpRequest) -> HttpResponse {
     }
 
     match extract_route(&request.url) {
-        Route::Avatar(requested_avatar_id) => read_state(|state| get_avatar_impl(requested_avatar_id, state)),
-        Route::ChannelAvatar((channel_id, requested_avatar_id)) => {
-            read_state(|state| get_channel_avatar_impl(channel_id, requested_avatar_id, state))
-        }
+        Route::Avatar(route) => read_state(|state| get_avatar_impl(route, state)),
         Route::Banner(requested_banner_id) => read_state(|state| get_banner_impl(requested_banner_id, state)),
         Route::Errors(since) => get_errors_impl(since),
         Route::Logs(since) => get_logs_impl(since),
