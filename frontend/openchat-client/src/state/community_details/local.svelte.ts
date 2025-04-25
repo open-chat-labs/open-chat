846a43358c01c800695996d244cda7525d9b13da
Julian Jelfs <julian.jelfs@gmail.com>
2025-04-25 15:46:47 +0100

    not doing that any more

--- frontend/openchat-client/src/state/community_details/local.svelte.ts
+++ frontend/openchat-client/src/state/community_details/local.svelte.ts
@@ -41,9 +41,6 @@ export class CommunityLocalState {
     }
 }
 
-// TODO - get rid of this and change CommunityLocalState so that it contains things like:
-// invitedUsers = new CommunityMap<LocalSet<string>>
-// this would be more efficient and simpler, but we need a reactive version of CommunityMap
 export class CommunityLocalStateManager {
     #data = new ReactiveCommunityMap<CommunityLocalState>();
 
