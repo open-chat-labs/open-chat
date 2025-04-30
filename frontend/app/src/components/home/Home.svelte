be8534f455a9d7db09ddc691444b19e0cd8114e4
Julian Jelfs <julian.jelfs@gmail.com>
2025-04-30 10:06:57 +0100

    remove debug

--- frontend/app/src/components/home/Home.svelte
+++ frontend/app/src/components/home/Home.svelte
@@ -31,7 +31,6 @@
         chatSummariesStore,
         defaultChatRules,
         draftMessagesStore,
-        messagesRead,
         nullMembership,
         offlineStore,
         pageRedirect,
@@ -181,9 +180,6 @@
     let showUpgrade: boolean = $state(false);
     let share: Share = { title: "", text: "", url: "", files: [] };
     let messageToForward: Message | undefined = undefined;
-
-    $inspect(messagesRead.messageReadState);
-    $inspect(app.unreadGroupCounts);
 
     onMount(() => {
         const unsubEvents = [
