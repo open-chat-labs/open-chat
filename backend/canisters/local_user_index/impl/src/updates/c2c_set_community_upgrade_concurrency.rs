84154b8f391d03201a96cdcc135b846f27439d8e
Hamish Peebles <hamishpeebles@gmail.com>
2025-05-06 13:31:27 +0100

    Add missing endpoints to support upgrading Groups/Communities

---
+++ backend/canisters/local_user_index/impl/src/updates/c2c_set_community_upgrade_concurrency.rs
@@ -0,0 +1,21 @@
+use crate::guards::caller_is_group_index_canister;
+use crate::mutate_state;
+use canister_api_macros::update;
+use canister_tracing_macros::trace;
+use local_user_index_canister::c2c_set_community_upgrade_concurrency::{Response::*, *};
+use std::cmp::min;
+use tracing::info;
+
+#[update(guard = "caller_is_group_index_canister", msgpack = true)]
+#[trace]
+fn c2c_set_community_upgrade_concurrency(args: Args) -> Response {
+    mutate_state(|state| {
+        let max = state.data.max_concurrent_community_upgrades;
+        state.data.community_upgrade_concurrency = min(args.value, max);
+        if state.data.community_upgrade_concurrency > 0 {
+            crate::jobs::upgrade_communities::start_job_if_required(state);
+        }
+        info!(state.data.community_upgrade_concurrency, "Community upgrade concurrency set");
+        if args.value > max { Capped(max) } else { Success }
+    })
+}
