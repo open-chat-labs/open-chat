3c1830249e15d38b722e709baa7d555bbb62beca
Hamish Peebles <hamishpeebles@gmail.com>
2025-05-07 11:35:46 +0100

    Update CHANGELOG

--- backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,13 +1,11 @@
+use crate::Data;
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::get_upgrades_memory;
-use crate::updates::set_community_or_group_verification::set_community_verification_impl;
-use crate::{Data, mutate_state};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use group_index_canister::post_upgrade::Args;
 use ic_cdk::post_upgrade;
 use stable_memory::get_reader;
-use std::time::Duration;
 use tracing::info;
 use types::CanisterId;
 use utils::cycles::init_cycles_dispenser_client;
@@ -47,7 +45,6 @@ fn post_upgrade(args: Args) {
     data.local_index_map
         .switch_index_canisters(index_replacements.into_iter().collect());
 
-    let test_mode = data.test_mode;
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
     let env = init_env(data.rng_seed);
@@ -56,19 +53,4 @@ fn post_upgrade(args: Args) {
 
     let total_instructions = ic_cdk::api::call_context_instruction_counter();
     info!(version = %args.wasm_version, total_instructions, "Post-upgrade complete");
-
-    if !test_mode {
-        ic_cdk_timers::set_timer(Duration::ZERO, mark_jade8_community_verified);
-    }
-}
-
-fn mark_jade8_community_verified() {
-    let community_id = CanisterId::from_text("siuvd-kiaaa-aaaaf-bm6oa-cai").unwrap().into();
-    let args = group_index_canister::set_community_verification::Args {
-        name: "JADE 8".into(),
-        community_id,
-    };
-
-    let result = mutate_state(|state| set_community_verification_impl(args, state));
-    info!(%community_id, ?result, "Set community verification completed");
 }
