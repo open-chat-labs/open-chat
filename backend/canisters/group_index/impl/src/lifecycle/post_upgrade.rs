0366460ee3b83e322101734aeed8b4f1d3427d3f
Hamish Peebles <hamishpeebles@gmail.com>
2025-05-07 11:32:59 +0100

    Switch to using LocalUserIndexes instead of LocalGroupIndexes

--- backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/group_index/impl/src/lifecycle/post_upgrade.rs
@@ -18,8 +18,34 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
+    let (mut data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
+
+    // Switch the LocalGroupIndex canisterIds to the LocalUserIndex canisterIds, since the
+    // LocalUserIndexes now support all the functionality of the LocalGroupIndexes and we will soon
+    // remove the LocalGroupIndexes
+    let mut index_replacements = Vec::new();
+    if data.test_mode {
+        index_replacements.push((
+            CanisterId::from_text("sbhuw-gyaaa-aaaaf-bfynq-cai").unwrap(),
+            CanisterId::from_text("pecvb-tqaaa-aaaaf-bhdiq-cai").unwrap(),
+        ));
+    } else {
+        index_replacements.push((
+            CanisterId::from_text("suaf3-hqaaa-aaaaf-bfyoa-cai").unwrap(),
+            CanisterId::from_text("nq4qv-wqaaa-aaaaf-bhdgq-cai").unwrap(),
+        ));
+        index_replacements.push((
+            CanisterId::from_text("ainth-qaaaa-aaaar-aaaba-cai").unwrap(),
+            CanisterId::from_text("aboy3-giaaa-aaaar-aaaaq-cai").unwrap(),
+        ));
+        index_replacements.push((
+            CanisterId::from_text("lrqxq-2qaaa-aaaac-aadla-cai").unwrap(),
+            CanisterId::from_text("lyt4m-myaaa-aaaac-aadkq-cai").unwrap(),
+        ));
+    }
+    data.local_index_map
+        .switch_index_canisters(index_replacements.into_iter().collect());
 
     let test_mode = data.test_mode;
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
