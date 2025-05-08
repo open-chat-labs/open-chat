adf7416a1cd0dc23ab502e2f918af6384bbefc62
Julian Jelfs <julian.jelfs@gmail.com>
2025-05-08 12:44:03 +0100

    use image until user interacts

--- frontend/app/src/components/home/YouTubePreview.svelte
+++ frontend/app/src/components/home/YouTubePreview.svelte
@@ -10,6 +10,7 @@
 
     let { pinned, fill, youtubeMatch, intersecting }: Props = $props();
     let rendered = $state(false);
+    let play = $state(false);
 
     $effect(() => {
         if (intersecting && !ui.eventListScrolling && !rendered && !$offlineStore) {
@@ -27,25 +28,58 @@
 
 {#if rendered}
     <div>
-        <iframe
-            class:pinned
-            class:fill
-            width="100%"
-            height="315"
-            src={`https://www.youtube.com/embed/${youtubeCode}?start=${youtubeStartTime}`}
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer;
+        {#if play}
+            <iframe
+                loading="lazy"
+                class:pinned
+                class:fill
+                width="100%"
+                height="315"
+                src={`https://www.youtube.com/embed/${youtubeCode}?autoplay=1&start=${youtubeStartTime}`}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer;
                             autoplay;
                             clipboard-write;
                             encrypted-media;
                             gyroscope;
                             picture-in-picture"
-            allowfullscreen></iframe>
+                allowfullscreen></iframe>
+        {:else}
+            <div class="img-wrapper">
+                <img
+                    class="image"
+                    loading="lazy"
+                    width="100%"
+                    class:pinned
+                    class:fill
+                    src={`https://img.youtube.com/vi/${youtubeCode}/hqdefault.jpg`}
+                    alt="Video thumbnail" />
+                <button aria-label="Play" onclick={() => (play = true)} class="play"></button>
+            </div>
+        {/if}
     </div>
 {/if}
 
 <style lang="scss">
+    .img-wrapper {
+        position: relative;
+    }
+
+    .play {
+        cursor: pointer;
+        background: none;
+        border: none;
+        width: toRem(100);
+        aspect-ratio: 1 / 1;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-image: url(/assets/youtube_play.svg);
+        background-repeat: no-repeat;
+    }
+
     iframe {
         margin-top: $sp3;
     }
