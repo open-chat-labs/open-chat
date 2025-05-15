7cd1a4d50059721d459ec34e2a1b5a896848404a
Julian Jelfs <julian.jelfs@gmail.com>
2025-05-15 16:33:19 +0100

    missed a bit of clean up (#7975)

--- frontend/openchat-client/src/stores/global.ts
+++ frontend/openchat-client/src/stores/global.ts
@@ -1,11 +1,4 @@
 /* eslint-disable no-case-declarations */
-import { chatListScopesEqual, type ChatIdentifier, type ChatListScope } from "openchat-shared";
-import { safeWritable } from "./safeWritable";
+import { type ChatIdentifier, type ChatListScope } from "openchat-shared";
 
 export type PinnedByScope = Map<ChatListScope["kind"], ChatIdentifier[]>;
-
-// This should always be referenced via app.chatListScope where possible - this store only exists for backward compatibility and will be removed
-export const chatListScopeStore = safeWritable<ChatListScope>(
-    { kind: "none" },
-    chatListScopesEqual,
-);
