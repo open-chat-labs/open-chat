434a127289264182b4180d4f1c63bf1eff06d23e
Hamish Peebles <hamishpeebles@gmail.com>
2025-05-18 16:44:47 +0100

    Expose same interface as Svelte stores

--- frontend/openchat-client/src/utils/pausableStore.ts
+++ frontend/openchat-client/src/utils/pausableStore.ts
@@ -1,59 +1,68 @@
-import type { Subscriber, Unsubscriber, Updater } from "svelte/store";
-import { tick } from "svelte";
+import type { Readable, StartStopNotifier, Subscriber, Unsubscriber, Updater, Writable } from "svelte/store";
 
-export class PausableStoreManager {
-    #paused = false;
-    #callbacks: (() => void)[] = [];
+export type ReadableStore<T> = Readable<T> & MaybeDirty;
+export type WritableStore<T> = Writable<T> & MaybeDirty;
+export type EqualityCheck<T> = (a: T, b: T) => boolean;
+type MaybeDirty = { maybeDirty() : boolean };
+type Stores = ReadableStore<unknown> | [ReadableStore<unknown>, ...Array<ReadableStore<unknown>>] | Array<ReadableStore<unknown>>;
+type StoresValues<T> =
+    T extends Readable<infer U> ? U : { [K in keyof T]: T[K] extends Readable<infer U> ? U : never };
 
-    create<T>(initialValue: T, equalityCheck?: (a: T, b: T) => boolean): PausableStore<T> {
-        return new PausableStore(initialValue, equalityCheck ?? ((a, b) => a === b), this);
-    }
+let paused = false;
+let publishesPending: (() => void)[] = [];
 
-    registerCallback(callback: () => void) {
-        this.#callbacks.push(callback);
-    }
-
-    pauseForOneTick() {
-        this.#paused = true;
-        tick().then(() => {
-            this.#paused = false;
-        })
-    }
-
-    get paused() {
-        return this.#paused
-    }
-
-    set paused(value: boolean) {
-        if (!value) {
-            // Flush any dirty values
-            for (const callback of this.#callbacks) {
-                callback();
-            }
-            this.#callbacks = [];
+export function setPaused(value: boolean) {
+    if (paused && !value) {
+        // Publish any dirty values
+        for (const callback of publishesPending) {
+            callback();
         }
-        this.#paused = value;
+        publishesPending = [];
     }
+    paused = value;
 }
 
-export class PausableStore<T> {
+export function writable<T>(value: T, start?: StartStopNotifier<T>, equalityCheck?: EqualityCheck<T>): WritableStore<T> {
+    return new _Writable(value, start, equalityCheck);
+}
+
+export function readable<T>(value: T, start: StartStopNotifier<T>): ReadableStore<T> {
+    const store = writable(value, start);
+    return {
+        subscribe: store.subscribe,
+        maybeDirty: store.maybeDirty,
+    };
+}
+
+export function derived<S extends Stores, T>(_stores: S, _fn: (values: StoresValues<S>) => T, _initial_value?: T | undefined): ReadableStore<T> {
+    throw new Error('not implemented');
+}
+
+class _Writable<T> {
     #subscriptions: Map<symbol, (value: T) => void> = new Map();
     #value: T;
-    #dirtyValue: T | undefined  = undefined;
+    #dirtyValue: T | undefined = undefined;
+    #publishPending: boolean = false;
+    #stop: Unsubscriber | undefined = undefined;
+    readonly #start: StartStopNotifier<T> | undefined;
     readonly #equalityCheck: (a: T, b: T) => boolean;
-    readonly #parent: PausableStoreManager;
 
-    constructor(initValue: T, equalityCheck: (a: T, b: T) => boolean, parent: PausableStoreManager) {
+    constructor(initValue: T, start?: StartStopNotifier<T>, equalityCheck?: (a: T, b: T) => boolean) {
         this.#value = initValue;
-        this.#equalityCheck = equalityCheck;
-        this.#parent = parent;
+        this.#start = start;
+        this.#equalityCheck = equalityCheck ?? ((a, b) => a === b);
     }
 
     subscribe(subscriber: Subscriber<T>): Unsubscriber {
         const id = Symbol();
         this.#subscriptions.set(id, subscriber);
+
+        if (this.#subscriptions.size === 1 && this.#start !== undefined) {
+            this.#stop = this.#start(this.set, this.update) ?? undefined;
+        }
+
         subscriber(this.#value);
-        return () => this.#subscriptions.delete(id);
+        return () => this.#unsubscribe(id);
     }
 
     value(allowDirty = false): T {
@@ -70,9 +79,12 @@ export class PausableStore<T> {
 
         this.#dirtyValue = newValue;
 
-        if (this.#parent.paused) {
-            // Register callback to publish the new value once the store is unpaused
-            this.#parent.registerCallback(() => this.#publish());
+        if (paused) {
+            if (!this.#publishPending) {
+                // Register callback to publish the new value once the store is unpaused
+                publishesPending.push(() => this.#publish());
+                this.#publishPending = true;
+            }
         } else {
             this.#publish();
         }
@@ -84,14 +96,27 @@ export class PausableStore<T> {
         this.set(newValue);
     }
 
+    maybeDirty(): boolean {
+        return this.#dirtyValue !== undefined;
+    }
+
     #publish() {
         if (this.#dirtyValue !== undefined) {
             this.#value = this.#dirtyValue;
-            this.#dirtyValue = undefined
 
             for (const subscription of this.#subscriptions.values()) {
                 subscription(this.#value);
             }
+
+            this.#dirtyValue = undefined
+        }
+        this.#publishPending = false;
+    }
+
+    #unsubscribe(id: symbol) {
+        this.#subscriptions.delete(id);
+        if (this.#subscriptions.size === 0 && this.#stop !== undefined) {
+            this.#stop();
         }
     }
 }
