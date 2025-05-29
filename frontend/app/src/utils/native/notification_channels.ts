ddb77277e061d2fe92e1b2a627a74bcfbd77ecd5
Ivan Jukic <ivan.jukic14@gmail.com>
2025-05-29 12:02:20 +0200

    removed some unused code that was kept for reference

--- frontend/app/src/utils/native/notification_channels.ts
+++ frontend/app/src/utils/native/notification_channels.ts
@@ -1,11 +1,4 @@
-import {
-    createChannel,
-    Importance,
-    Visibility,
-    channels,
-    isPermissionGranted,
-    requestPermission,
-} from "@tauri-apps/plugin-notification";
+import { isPermissionGranted, requestPermission } from "@tauri-apps/plugin-notification";
 import { addPluginListener, PluginListener } from "@tauri-apps/api/core";
 
 const TAURI_PLUGIN_NAME = "oc";
@@ -43,10 +36,6 @@ export async function expectPushNotifications<T>(
         return Promise.reject(new Error("Notification permission not granted"));
     }
 
-    // Create a channel for receiving notifications!
-    // NOTE: This is currently done natively in Kotlin code.
-    // createNotificationChannel();
-
     // Set up the listener for push notifications!
     return addPluginListener(TAURI_PLUGIN_NAME, PUSH_NOTIFICATION_EVENT, handler);
 }
@@ -68,55 +57,4 @@ async function askForPermission(): Promise<boolean> {
     }
 
     return false;
-}
-
-/**
- * Creates a notification channel for messages. This is used to group message
- * notifications, so that they can be managed together in the notification
- * banner.
- *
- * This function checks if the channel already exists before creating it, to avoid
- * duplicate channels; while this is not strictly necessary, it may be useful for
- * debugging purposes or as a micro-optimisation if we want to ensure the channel
- * is created only once - which should be the case anyway even with multiple
- * createChannel calls, as it does not create a duplicate channel if it already
- * exists.
- *
- * NOTE: We are currently doing this natively, in Kotlin code, but keeping
- * this here for future reference in case we want to do it via Tauri.
- */
-async function createNotificationChannel() {
-    try {
-        const channelExists = await checkMessagesNotificationChannelExists();
-        if (channelExists) {
-            console.log("Notification channel 'messages' already exists.");
-            return;
-        }
-
-        await createChannel({
-            id: "messages",
-            name: "Messages",
-            description: "Notifications for new messages",
-            importance: Importance.High,
-            visibility: Visibility.Private,
-            lights: true,
-            lightColor: "#8D2380",
-            vibration: true,
-            sound: "notification_sound",
-        });
-    } catch (error) {
-        console.error("Failed to create notification channel:", error);
-    }
-}
-
-// We can check if the channel exists by listing all channels and checking for
-// the one we want.
-async function checkMessagesNotificationChannelExists() {
-    try {
-        const existingChannels = await channels();
-        return existingChannels.some((channel) => channel.id === "messages");
-    } catch (error) {
-        console.error("Failed to check notification channels:", error);
-        return false;
-    }
 }
