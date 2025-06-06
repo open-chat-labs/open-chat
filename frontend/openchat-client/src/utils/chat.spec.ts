3ab0de805be7ceaf56a4399ff6c1459a0f45133f
Hamish Peebles <hamishpeebles@gmail.com>
2025-06-06 20:02:45 +0100

    Actually fix the test

--- frontend/openchat-client/src/utils/chat.spec.ts
+++ frontend/openchat-client/src/utils/chat.spec.ts
@@ -130,7 +130,7 @@ describe("thread utils", () => {
             },
         );
         const chat = { ...defaultGroupChat };
-        mergeUnconfirmedThreadsIntoSummary(defaultGroupChat);
+        mergeUnconfirmedThreadsIntoSummary(chat);
         expect(chat.membership.latestThreads[0].latestEventIndex).toEqual(4);
         expect(chat.membership.latestThreads[0].latestMessageIndex).toEqual(5);
     });
