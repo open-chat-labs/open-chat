649baf1775e839fa255e80d72d9319357cd4e6cc
Hamish Peebles <hamishpeebles@gmail.com>
2025-06-12 11:30:06 +0100

    Tiny optimisation

--- frontend/app/src/components/home/LinkPreviews.svelte
+++ frontend/app/src/components/home/LinkPreviews.svelte
@@ -53,9 +53,8 @@
     let shouldRenderPreviews = $state(false);
 
     function arraysAreEqual(a: string[], b: string[]) {
-        if (a.length !== b.length) {
-            return false;
-        }
+        if (a === b) return true;
+        if (a.length !== b.length) return false;
 
         for (let i = 0; i < a.length; i++) {
             if (a[i] !== b[i]) {
