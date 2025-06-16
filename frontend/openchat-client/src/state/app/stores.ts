54cacbbe2ce34c7bbf36b1ae257727e6361170d0
Hamish Peebles <hamishpeebles@gmail.com>
2025-06-16 09:34:05 +0100

    Skip recalculating community details derived stores if no updates

--- frontend/openchat-client/src/state/app/stores.ts
+++ frontend/openchat-client/src/state/app/stores.ts
@@ -593,7 +593,7 @@ export const selectedCommunityUserGroupsStore = derived(
         if (updates === undefined) return community.userGroups;
         return updates.apply(community.userGroups);
     },
-    mapsEqualIfEmpty,
+    dequal,
 );
 export const selectedCommunityInvitedUsersStore = derived(
     [selectedServerCommunityStore, communityLocalUpdates.invitedUsers],
@@ -658,7 +658,7 @@ export const selectedChatBlockedUsersStore = derived(
         if (chat === undefined) return new Set() as ReadonlySet<string>;
         return blockedUsers.get(chat.chatId)?.apply(chat.blockedUsers) ?? chat.blockedUsers;
     },
-    setsEqualIfEmpty,
+    setsAreEqual,
 );
 export const selectedChatLapsedMembersStore = derived(selectedServerChatStore, (chat) => {
     return chat?.lapsedMembers ?? (new Set() as ReadonlySet<string>);
