cf75be8a2fd99ef41525dfc788174d02aaffe01e
Julian Jelfs <julian.jelfs@gmail.com>
2025-06-17 10:58:39 +0100

    fix localMap and localSet

--- frontend/openchat-client/src/state/set.spec.ts
+++ frontend/openchat-client/src/state/set.spec.ts
@@ -23,6 +23,54 @@ describe("LocalSet", () => {
         set = new LocalSet(identity);
     });
 
+    test("make sure that the order of operations doesn't cause a problem", () => {
+        // perform two operations
+        const removeUndo = set.remove("123");
+        const addUndo = set.add("123");
+
+        // undo them both
+        removeUndo();
+        addUndo();
+
+        expect(set.added.has("123")).toBe(false);
+        expect(set.removed.has("123")).toBe(false);
+    });
+
+    test("remove trumps add", () => {
+        set.add("123");
+        set.remove("123");
+        set.add("123");
+        set.remove("123");
+
+        const original = new Set<string>();
+        const result = set.apply(original);
+        expect(result.has("123")).toBe(false);
+    });
+
+    test("last mod wins", () => {
+        set.add("123");
+        set.remove("123");
+        set.remove("123");
+        set.add("123");
+
+        const original = new Set<string>();
+        const result = set.apply(original);
+        expect(result.has("123")).toBe(true);
+    });
+
+    test("multiple undos", () => {
+        set.add("123");
+        const u1 = set.remove("123");
+        const u2 = set.remove("123");
+
+        u1();
+        u2();
+
+        const original = new Set<string>();
+        const result = set.apply(original);
+        expect(result.has("123")).toBe(true);
+    });
+
     it("make sure manual undo works", () => {
         const undo = set.add("a");
         expect(set.added.has("a")).toBe(true);
@@ -37,7 +85,7 @@ describe("LocalSet", () => {
         const undo = set.add("a");
 
         expect(set.added.has("a")).toBe(true);
-        expect(set.removed.has("a")).toBe(false);
+        expect(set.removed.has("a")).toBe(true);
 
         undo();
 
