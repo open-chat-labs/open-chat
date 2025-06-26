4de2a80428954ad225f08ba1ccec61e4369f3f33
Hamish Peebles <hamishpeebles@gmail.com>
2025-06-26 16:05:00 +0100

    Increase reserved cycles limit of each LocalUserIndex (#8241)

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -3,10 +3,14 @@ use crate::lifecycle::{init_env, init_state};
 use crate::memory::{get_stable_memory_map_memory, get_upgrades_memory};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
+use constants::T;
 use ic_cdk::post_upgrade;
 use stable_memory::get_reader;
+use std::time::Duration;
 use tracing::info;
+use types::{CanisterId, Cycles};
 use user_index_canister::post_upgrade::Args;
+use utils::canister::set_reserved_cycles_limit;
 use utils::cycles::init_cycles_dispenser_client;
 
 #[post_upgrade]
@@ -23,10 +27,24 @@ fn post_upgrade(args: Args) {
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
     let env = init_env(data.rng_seed, data.oc_key_pair.is_initialised());
+    let local_user_indexes = data.local_index_map.canisters().copied().collect();
 
     init_cycles_dispenser_client(data.cycles_dispenser_canister_id, data.test_mode);
     init_state(env, data, args.wasm_version);
 
     let total_instructions = ic_cdk::api::call_context_instruction_counter();
     info!(version = %args.wasm_version, total_instructions, "Post-upgrade complete");
+
+    ic_cdk_timers::set_timer(Duration::ZERO, || {
+        ic_cdk::futures::spawn(increase_reserved_cycles_limits(local_user_indexes, 20 * T))
+    });
+}
+
+async fn increase_reserved_cycles_limits(canister_ids: Vec<CanisterId>, cycles: Cycles) {
+    let futures: Vec<_> = canister_ids
+        .into_iter()
+        .map(|canister_id| set_reserved_cycles_limit(canister_id, cycles))
+        .collect();
+
+    futures::future::join_all(futures).await;
 }
--- backend/libraries/utils/src/canister/update_settings.rs
+++ backend/libraries/utils/src/canister/update_settings.rs
@@ -2,20 +2,36 @@ use crate::canister::convert_cdk_error;
 use candid::Principal;
 use ic_cdk::management_canister::{self, CanisterSettings, UpdateSettingsArgs};
 use tracing::error;
-use types::{C2CError, CanisterId};
+use types::{C2CError, CanisterId, Cycles};
 
 pub async fn set_controllers(canister_id: CanisterId, controllers: Vec<Principal>) -> Result<(), C2CError> {
-    management_canister::update_settings(&UpdateSettingsArgs {
+    update_settings(
         canister_id,
-        settings: CanisterSettings {
+        CanisterSettings {
             controllers: Some(controllers),
             ..Default::default()
         },
-    })
+    )
     .await
-    .map_err(|e| {
-        let error = convert_cdk_error(canister_id, "update_settings", e);
-        error!(?error, "Error calling update_settings");
-        error
-    })
+}
+
+pub async fn set_reserved_cycles_limit(canister_id: CanisterId, cycles: Cycles) -> Result<(), C2CError> {
+    update_settings(
+        canister_id,
+        CanisterSettings {
+            reserved_cycles_limit: Some(cycles.into()),
+            ..Default::default()
+        },
+    )
+    .await
+}
+
+pub async fn update_settings(canister_id: CanisterId, settings: CanisterSettings) -> Result<(), C2CError> {
+    management_canister::update_settings(&UpdateSettingsArgs { canister_id, settings })
+        .await
+        .map_err(|e| {
+            let error = convert_cdk_error(canister_id, "update_settings", e);
+            error!(?error, "Error calling update_settings");
+            error
+        })
 }
