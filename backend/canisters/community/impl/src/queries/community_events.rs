5be0d46440e8565231b627bbcec725bcacb91aea
megrogan <megrogan@gmail.com>
2025-06-27 15:14:04 +0100

    Ensure `community_events` only accessible to owners

--- backend/canisters/community/impl/src/queries/community_events.rs
+++ backend/canisters/community/impl/src/queries/community_events.rs
@@ -2,17 +2,25 @@ use crate::{RuntimeState, read_state};
 use canister_api_macros::query;
 use community_canister::community_events::Args;
 use community_canister::community_events::{Response::*, *};
+use group_community_common::Member;
+use oc_error_codes::OCErrorCode;
 use types::OCResult;
 
 #[query(candid = true, msgpack = true)]
 fn community_events(args: Args) -> Response {
-    match read_state(|state| events_impl(args, state)) {
+    match read_state(|state| community_events_impl(args, state)) {
         Ok(result) => Success(result),
         Err(error) => Error(error),
     }
 }
 
-fn events_impl(args: Args, state: &RuntimeState) -> OCResult<EventsResponse> {
+fn community_events_impl(args: Args, state: &RuntimeState) -> OCResult<EventsResponse> {
+    // Ensure this endpoint is only accessible to community owners
+    let caller = state.env.caller();
+    if !state.data.members.get_verified_member(caller).map_or(false, |m| m.is_owner()) {
+        return Err(OCErrorCode::InitiatorNotAuthorized.into());
+    }
+
     let events = state.data.events.get_page_events(args);
 
     Ok(EventsResponse {
