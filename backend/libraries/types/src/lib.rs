1447d6f67d2c918b2af346598fbd43cf17aa9b92
Hamish Peebles <hamishpeebles@gmail.com>
2025-07-15 09:02:22 +0100

    Revert file that wasn't meant to be included

--- backend/libraries/types/src/lib.rs
+++ backend/libraries/types/src/lib.rs
@@ -1,6 +1,6 @@
 use crate::nns::Tokens;
 use candid::{CandidType, Principal};
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 
 mod access_tokens;
 mod achievement;
@@ -261,19 +261,4 @@ pub trait Fallback: Sized {
 pub enum CommunityOrGroup {
     Community(CommunityId),
     Group(ChatId),
-}
-
-fn ok_or_default<'de, T: Deserialize<'de> + Default, D: Deserializer<'de>>(d: D) -> Result<T, D::Error> {
-    #[derive(Deserialize)]
-    #[serde(untagged)]
-    enum Temp<T> {
-        Value(T),
-    }
-
-    Ok(Temp::deserialize(d)
-        .map(|e| {
-            let Temp::Value(v) = e;
-            v
-        })
-        .unwrap_or_default())
 }
