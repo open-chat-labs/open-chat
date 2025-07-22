567f1940eb3c99bab83dd8640e5070e8adaee22c
Hamish Peebles <hamishpeebles@gmail.com>
2025-07-22 15:05:16 +0100

    Mark buckets as full if they start having to pay reserved cycles

--- backend/canisters/storage_index/impl/src/jobs/mod.rs
+++ backend/canisters/storage_index/impl/src/jobs/mod.rs
@@ -1,9 +1,11 @@
 use crate::RuntimeState;
 
+mod check_active_buckets;
 pub mod ensure_sufficient_active_buckets;
 pub mod upgrade_buckets;
 
 pub(crate) fn start(state: &RuntimeState) {
+    check_active_buckets::start_job();
     ensure_sufficient_active_buckets::start_job_if_required(state);
     upgrade_buckets::start_job_if_required(state);
 }
