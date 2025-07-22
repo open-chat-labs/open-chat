2cb4a5aeb0601975f73b1d2722586defea1ea105
Hamish Peebles <hamishpeebles@gmail.com>
2025-07-22 14:19:13 +0100

    Fix

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -80,7 +80,7 @@ echo "Canisters created"
     $NNS_INDEX_CANISTER_ID \
     true \
 
-STORAGE_INDEX_CANISTER_ID=$(dfx canister --network $NETWORK id storage_index)
+STORAGE_INDEX_CANISTER_ID=$(dfx canister id storage_index)
 ./scripts/get-test-icp.sh $STORAGE_INDEX_CANISTER_ID $IDENTITY
 dfx --identity $IDENTITY canister call storage_index add_bucket_canister '(record { })'
 
