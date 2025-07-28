38b79591a527414eff2f6131fff55a6f3879c141
Hamish Peebles <hamishpeebles@gmail.com>
2025-07-28 14:28:47 +0100

    Only allow generating account linking code if re-auth'd within 5 mins

--- backend/canisters/identity/api/src/updates/create_account_linking_code.rs
+++ backend/canisters/identity/api/src/updates/create_account_linking_code.rs
@@ -2,13 +2,20 @@ use crate::account_linking_code::AccountLinkingCode;
 use candid::{CandidType, Deserialize};
 use serde::Serialize;
 use ts_export::ts_export;
-use types::Empty;
+use types::SignedDelegation;
 
-pub type Args = Empty;
+#[ts_export(identity, create_account_linking_code)]
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct Args {
+    #[serde(with = "serde_bytes")]
+    pub public_key: Vec<u8>,
+    pub delegation: SignedDelegation,
+}
 
 #[ts_export(identity, create_account_linking_code)]
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub enum Response {
     Success(AccountLinkingCode),
     UserNotFound,
+    DelegationTooOld,
 }
