abd41b70f2f3f17775c60298245bb0d302da91b3
megrogan <megrogan@gmail.com>
2025-07-31 12:49:05 +0100

    Set initial chit balance for all users in LUIs

--- backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/user_index/impl/src/lifecycle/post_upgrade.rs
@@ -1,9 +1,10 @@
-use crate::Data;
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::{get_stable_memory_map_memory, get_upgrades_memory};
+use crate::{Data, mutate_state};
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
+use local_user_index_canister::{ChitBalance, UserIndexEvent};
 use stable_memory::get_reader;
 use tracing::info;
 use user_index_canister::post_upgrade::Args;
@@ -28,4 +29,29 @@ fn post_upgrade(args: Args) {
 
     let total_instructions = ic_cdk::api::call_context_instruction_counter();
     info!(version = %args.wasm_version, total_instructions, "Post-upgrade complete");
+
+    mutate_state(|state| {
+        let users_to_sync: Vec<_> = state
+            .data
+            .users
+            .iter()
+            .filter_map(|user| {
+                let total_chit_earned = user.total_chit_earned();
+                if total_chit_earned != 0 { Some((user.user_id, total_chit_earned)) } else { None }
+            })
+            .collect();
+
+        for (user_id, total_chit_earned) in users_to_sync {
+            state.push_event_to_all_local_user_indexes(
+                UserIndexEvent::UpdateChitBalance(
+                    user_id,
+                    ChitBalance {
+                        total_earned: total_chit_earned,
+                        curr_balance: total_chit_earned, // We don't yet maintain the users total chit balance
+                    },
+                ),
+                None,
+            );
+        }
+    });
 }
