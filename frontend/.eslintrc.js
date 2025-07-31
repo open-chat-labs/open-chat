61fb3fc2bd7cacc920ff19fc02d9b990df658f89
Julian Jelfs <julian.jelfs@gmail.com>
2025-07-31 11:14:57 +0100

    update eslint and use eslint svelte plugin

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
     root: true,
     parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "prettier"],
+    plugins: ["@typescript-eslint", "prettier", "svelte"],
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
     ignorePatterns: ["**/candid/*.ts", "**/candid/*.js"],
     rules: {
         "@typescript-eslint/no-explicit-any": ["error"],
         "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     },
+    overrides: [
+        {
+            files: ["*.svelte"],
+            processor: "svelte/svelte",
+        },
+    ],
 };
