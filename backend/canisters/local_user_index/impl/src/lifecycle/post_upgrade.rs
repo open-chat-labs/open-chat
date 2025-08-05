c9df79728558e53bc96222331e74cda1dac8c9bb
Hamish Peebles <hamishpeebles@gmail.com>
2025-08-05 10:24:56 +0100

    Update canisters post release

--- backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/local_user_index/impl/src/lifecycle/post_upgrade.rs
@@ -6,7 +6,6 @@ use canister_tracing_macros::trace;
 use ic_cdk::post_upgrade;
 use local_user_index_canister::post_upgrade::Args;
 use stable_memory::get_reader;
-use stable_memory_map::StableMemoryMap;
 use tracing::info;
 use utils::cycles::init_cycles_dispenser_client;
 
@@ -18,15 +17,8 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (mut data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
+    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
-
-    for (user_id, blocked_users) in data.blocked_users.collect_all() {
-        for blocked in blocked_users {
-            assert!(data.blocked_users.remove(&(user_id, blocked)).is_some());
-        }
-    }
-    assert!(data.blocked_users.is_empty());
 
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
