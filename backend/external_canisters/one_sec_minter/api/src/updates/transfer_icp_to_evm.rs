8cf9782bb668a52481d339b3ee6626a0a4251016
Hamish Peebles <hamishpeebles@gmail.com>
2025-08-13 12:59:49 +0100

    Add OneSecMinter as an external canister

---
+++ backend/external_canisters/one_sec_minter/api/src/updates/transfer_icp_to_evm.rs
@@ -0,0 +1,20 @@
+use crate::{ErrorMessage, EvmAccount, EvmChain, FetchedBlock, IcpAccount, Token, TransferId};
+use candid::{CandidType, Nat};
+use serde::{Deserialize, Serialize};
+
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+pub struct Args {
+    pub token: Token,
+    pub evm_account: EvmAccount,
+    pub icp_account: IcpAccount,
+    pub evm_chain: EvmChain,
+    pub evm_amount: Option<Nat>,
+    pub icp_amount: Nat,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+pub enum Response {
+    Accepted(TransferId),
+    Fetching(FetchedBlock),
+    Failed(ErrorMessage),
+}
