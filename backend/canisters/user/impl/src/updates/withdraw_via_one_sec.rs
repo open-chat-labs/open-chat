3b1d4e605b695eff8570d15fbda3c59b470c0bb9
Hamish Peebles <hamishpeebles@gmail.com>
2025-08-13 23:53:36 +0100

    clippy

--- backend/canisters/user/impl/src/updates/withdraw_via_one_sec.rs
+++ backend/canisters/user/impl/src/updates/withdraw_via_one_sec.rs
@@ -9,7 +9,6 @@ use local_user_index_canister::UserEvent as LocalUserIndexEvent;
 use oc_error_codes::OCErrorCode;
 use one_sec_minter_canister::{EvmAccount, IcpAccount, Token};
 use serde::Serialize;
-use types::icrc1::Account;
 use types::{EvmChain, OCResult};
 use user_canister::withdraw_via_one_sec::*;
 
@@ -52,10 +51,12 @@ async fn withdraw_via_one_sec_impl(args: Args) -> OCResult {
     )
     .await?
     {
+        one_sec_minter_canister::transfer_icp_to_evm::Response::Accepted(_) => {}
+        // TODO find out what this means
+        one_sec_minter_canister::transfer_icp_to_evm::Response::Fetching(_) => {}
         one_sec_minter_canister::transfer_icp_to_evm::Response::Failed(failed) => {
             return Err(OCErrorCode::TransferFailed.with_message(&failed.error));
         }
-        _ => {}
     }
 
     mutate_state(|state| {
