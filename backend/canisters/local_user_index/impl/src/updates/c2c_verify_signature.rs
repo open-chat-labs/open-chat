3a5959a86924ac23fb212abe782d60dd0cab98aa
Hamish Peebles <hamishpeebles@gmail.com>
2025-08-15 14:47:10 +0100

    clippy

--- backend/canisters/local_user_index/impl/src/updates/c2c_verify_signature.rs
+++ backend/canisters/local_user_index/impl/src/updates/c2c_verify_signature.rs
@@ -3,7 +3,7 @@ use canister_api_macros::update;
 use local_user_index_canister::c2c_verify_signature::*;
 
 #[update(guard = "caller_is_local_child_canister", msgpack = true)]
-fn c2c_verify_signature(args: Args) -> Response {
+fn c2c_verify_signature(_args: Args) -> Response {
     // TODO implement this properly
     // Maybe the Identity canister verifies that the user signed in recently and then calls into
     // other canisters as needed
