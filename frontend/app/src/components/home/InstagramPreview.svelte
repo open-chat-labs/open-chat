d0d5bd7d25d14d2a3be75ef236f4342f5e02d17d
Julian Jelfs <julian.jelfs@gmail.com>
2025-08-15 09:12:36 +0100

    fix instagram preview rendering to prevent overflow

--- frontend/app/src/components/home/InstagramPreview.svelte
+++ frontend/app/src/components/home/InstagramPreview.svelte
@@ -25,6 +25,17 @@
     class="instagram-media"
     data-instgrm-captioned
     data-instgrm-permalink={`https://www.instagram.com/reel/${code}/?utm_source=ig_embed&amp;utm_campaign=loading`}
-    data-instgrm-version="14"
-    style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+    data-instgrm-version="14">
 </blockquote>
+
+<style lang="scss">
+    :global(iframe.instagram-media.instagram-media-rendered) {
+        min-width: unset !important;
+        max-width: unset !important;
+        width: 100%;
+        margin: 0 !important;
+        padding: 0 !important;
+        border-radius: var(--rd) !important;
+        border: var(--bw) solid var(--bd) !important;
+    }
+</style>
