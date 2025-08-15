49b81be6be608333f3c0c41a77d60830c4423ad0
Hamish Peebles <hamishpeebles@gmail.com>
2025-08-15 14:32:21 +0100

    Temporarily remove check that user has recently signed in

--- backend/canisters/local_user_index/impl/src/updates/c2c_verify_signature.rs
+++ backend/canisters/local_user_index/impl/src/updates/c2c_verify_signature.rs
@@ -1,20 +1,11 @@
 use crate::guards::caller_is_local_child_canister;
-use crate::read_state;
 use canister_api_macros::update;
-use constants::MINUTE_IN_MS;
-use identity_utils::verify_signature;
 use local_user_index_canister::c2c_verify_signature::*;
 
 #[update(guard = "caller_is_local_child_canister", msgpack = true)]
 fn c2c_verify_signature(args: Args) -> Response {
-    read_state(|state| {
-        verify_signature(
-            &args.signature,
-            state.data.identity_canister_id,
-            5 * MINUTE_IN_MS,
-            &state.data.ic_root_key,
-            state.env.now(),
-        )
-    })
-    .into()
+    // TODO implement this properly
+    // Maybe the Identity canister verifies that the user signed in recently and then calls into
+    // other canisters as needed
+    Response::Success
 }
