8640e4ce7ca10abfa56b3330ce284c8db5a5a034
Matt Grogan <megrogan@gmail.com>
2025-08-15 16:01:59 +0200

    Support muting of @everyone notifications (#8496)

--- backend/canisters/group/api/src/updates/toggle_mute_notifications.rs
+++ backend/canisters/group/api/src/updates/toggle_mute_notifications.rs
@@ -5,7 +5,8 @@ use types::UnitResult;
 #[ts_export(group, toggle_mute_notifications)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Args {
-    pub mute: bool,
+    pub mute: Option<bool>,
+    pub mute_at_everyone: Option<bool>,
 }
 
 pub type Response = UnitResult;
