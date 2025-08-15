b2b134b38374cc168752394376b680fe0dbfef6a
Julian Jelfs <julian.jelfs@gmail.com>
2025-08-15 14:34:22 +0100

    improve the behaviour when exiting preview

--- frontend/app/src/components/home/PreviewFooter.svelte
+++ frontend/app/src/components/home/PreviewFooter.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import {
+        chatListScopeStore,
         isLocked,
+        mobileWidth,
         type MultiUserChat,
         type OpenChat,
         platformModeratorStore,
@@ -47,13 +49,18 @@
     function cancelPreview() {
         if (previewingCommunity && $selectedCommunitySummaryStore) {
             client.removeCommunity($selectedCommunitySummaryStore.id);
+            page(routeForScope(client.getDefaultScope()));
         } else {
             if (!chat.public) {
                 client.declineInvitation(chat.id);
             }
-            client.removeChat(chat.id);
+            client.removePreviewedChat(chat.id);
+            if ($mobileWidth) {
+                page(routeForScope($chatListScopeStore));
+            } else if (!client.selectDefaultChat(false)) {
+                page(routeForScope($chatListScopeStore));
+            }
         }
-        page(routeForScope(client.getDefaultScope()));
     }
 
     function freeze() {
