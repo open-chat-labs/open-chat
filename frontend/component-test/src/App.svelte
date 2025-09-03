04d6361b689041dbfa4f2d0385728d6a34c6d2f7
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-03 16:28:43 +0100

    made the test page navigation a bit more managable

--- frontend/component-test/src/App.svelte
+++ frontend/component-test/src/App.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-    import { Button, Container, theme as neon } from "component-lib";
+    import {
+        Button,
+        CommonButton,
+        Container,
+        MenuTrigger,
+        theme as neon,
+        Overview,
+    } from "component-lib";
+    import Burger from "svelte-material-icons/Menu.svelte";
+    import MenuItem from "../../component-lib/src/components/menu/MenuItem.svelte";
     import BigButtons from "./BigButtons.svelte";
     import Buttons from "./Buttons.svelte";
     import Colours from "./Colours.svelte";
@@ -21,25 +30,60 @@
         | "menus"
         | "typography";
     let selected = $state<Section>("colours");
+
+    const labels: Record<Section, string> = {
+        colours: "Colours",
+        buttons: "Buttons",
+        big_buttons: "Big buttons",
+        common_buttons: "Common buttons",
+        layout: "Layout",
+        controls: "Controls & indicators",
+        menus: "Menus",
+        typography: "Typography",
+    };
 </script>
 
-{#snippet sectionButton(section: Section, name: string)}
+{#snippet sectionButton(section: Section)}
     <Button
         width={{ kind: "fixed", size: "200px" }}
+        height={{ kind: "fill" }}
         onClick={() => (selected = section)}
-        secondary={selected !== section}>{name}</Button>
+        secondary={selected !== section}>{labels[section]}</Button>
 {/snippet}
 
-<Container height={{ kind: "fill" }} direction={"vertical"}>
-    <Container padding={["zero", "lg"]} gap={"md"}>
-        {@render sectionButton("colours", "Colours")}
-        {@render sectionButton("layout", "Layout")}
-        {@render sectionButton("typography", "Typography")}
-        {@render sectionButton("buttons", "Buttons")}
-        {@render sectionButton("big_buttons", "Big buttons")}
-        {@render sectionButton("common_buttons", "Common buttons")}
-        {@render sectionButton("controls", "Controls & Indicators")}
-        {@render sectionButton("menus", "Context menus")}
+{#snippet sectionMenuItem(section: Section)}
+    <MenuItem onclick={() => (selected = section)}>{labels[section]}</MenuItem>
+{/snippet}
+
+<Container gap={"xl"} height={{ kind: "fill" }} direction={"vertical"}>
+    <Container crossAxisAlignment={"center"} padding={["zero", "lg"]} gap={"xl"}>
+        <MenuTrigger>
+            <CommonButton mode="active" size="large">
+                {#snippet icon(color)}
+                    <Burger {color} />
+                {/snippet}
+                Menu
+            </CommonButton>
+            {#snippet menuItems()}
+                {@render sectionMenuItem("colours")}
+                {@render sectionMenuItem("layout")}
+                {@render sectionMenuItem("typography")}
+                {@render sectionMenuItem("buttons")}
+                {@render sectionMenuItem("big_buttons")}
+                {@render sectionMenuItem("common_buttons")}
+                {@render sectionMenuItem("controls")}
+                {@render sectionMenuItem("menus")}
+            {/snippet}
+        </MenuTrigger>
+        <Overview>{labels[selected]}</Overview>
+        <!-- {@render sectionButton("colours")}
+        {@render sectionButton("layout")}
+        {@render sectionButton("typography")}
+        {@render sectionButton("buttons")}
+        {@render sectionButton("big_buttons")}
+        {@render sectionButton("common_buttons")}
+        {@render sectionButton("controls")}
+        {@render sectionButton("menus")} -->
     </Container>
 
     <Container height={{ kind: "fill" }} direction={"vertical"} padding={["lg"]}>
