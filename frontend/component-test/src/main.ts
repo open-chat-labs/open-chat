11cde134eecb801e7f6c41e73c0a69c1021dda0f
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-03 12:47:54 +0100

    seems like we need the extra div

--- frontend/component-test/src/main.ts
+++ frontend/component-test/src/main.ts
@@ -2,6 +2,6 @@ import { mount } from "svelte";
 import App from "./App.svelte";
 import "./styles.scss";
 
-const app = mount(App, { target: document.body! });
+const app = mount(App, { target: document.getElementById("app")! });
 
 export default app;
