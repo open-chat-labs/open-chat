9bc3e2ddb5436d5603a18b25b2a1dead83a8c833
megrogan <megrogan@gmail.com>
2025-09-05 17:39:03 +0100

    Revert "Promote designated user to owner of biz community (#8539)"
    This reverts commit 8f0d5eb2a53dc060c9b7f646ec5c244cdee6e70a.

--- backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/community/impl/src/lifecycle/post_upgrade.rs
@@ -1,19 +1,14 @@
 use crate::jobs::import_groups::finalize_group_import;
 use crate::lifecycle::{init_env, init_state};
 use crate::memory::{get_stable_memory_map_memory, get_upgrades_memory};
-use crate::model::events::CommunityEventInternal;
-use crate::{Data, mutate_state, read_state};
-use candid::Principal;
+use crate::{Data, read_state};
 use canister_api_macros::post_upgrade;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
-use chat_events::ChatEventInternal;
 use community_canister::post_upgrade::Args;
-use constants::OPENCHAT_BOT_USER_ID;
 use instruction_counts_log::InstructionCountFunctionId;
 use stable_memory::get_reader;
 use tracing::info;
-use types::{CanisterId, CommunityRole, CommunityRoleChanged, GroupRole, RoleChanged, UserId};
 
 #[post_upgrade(msgpack = true)]
 #[trace]
@@ -39,75 +34,6 @@ fn post_upgrade(args: Args) {
 
     let total_instructions = ic_cdk::api::call_context_instruction_counter();
     info!(version = %args.wasm_version, total_instructions, "Post-upgrade complete");
-
-    mutate_state(|state| {
-        // If this is the biz community, promote @AlphaWarden to owner
-        let biz_community_id: CanisterId = Principal::from_text("wowos-hyaaa-aaaar-ar4ca-cai").unwrap();
-        let feff_user_id: UserId = Principal::from_text("wrf4m-qyaaa-aaaaf-aagnq-cai").unwrap().into();
-        let alpha_warden_user_id: UserId = Principal::from_text("2pje3-hqaaa-aaaaf-a2h3q-§cai").unwrap().into();
-
-        if state.env.canister_id() == biz_community_id {
-            let now = state.env.now();
-
-            match state.data.members.change_role(
-                feff_user_id,
-                alpha_warden_user_id,
-                CommunityRole::Owner,
-                &state.data.permissions,
-                false,
-                false,
-                now,
-            ) {
-                Ok(_) => {
-                    let event = CommunityRoleChanged {
-                        user_ids: vec![alpha_warden_user_id],
-                        old_role: CommunityRole::Admin,
-                        new_role: CommunityRole::Owner,
-                        changed_by: OPENCHAT_BOT_USER_ID,
-                    };
-                    state.push_community_event(CommunityEventInternal::RoleChanged(Box::new(event)));
-
-                    // Now promote AlphaWarden to Owner for every public channel where feff is the only owner
-                    for channel in state.data.channels.iter_mut() {
-                        if channel.chat.is_public.value
-                            && channel.chat.members.owners().len() == 1
-                            && channel.chat.members.owners().contains(&feff_user_id)
-                        {
-                            match channel.chat.members.change_role(
-                                feff_user_id,
-                                alpha_warden_user_id,
-                                GroupRole::Owner.into(),
-                                &channel.chat.permissions,
-                                false,
-                                false,
-                                now,
-                            ) {
-                                Ok(old_role) => {
-                                    let event = RoleChanged {
-                                        user_ids: vec![alpha_warden_user_id],
-                                        old_role: old_role.into(),
-                                        new_role: GroupRole::Owner,
-                                        changed_by: OPENCHAT_BOT_USER_ID,
-                                    };
-
-                                    channel
-                                        .chat
-                                        .events
-                                        .push_main_event(ChatEventInternal::RoleChanged(Box::new(event)), now);
-                                }
-                                Err(err) => ic_cdk::println!(
-                                    "Failed to promote AlphaWarden to owner of channel: {}, {:?}",
-                                    channel.chat.name.value,
-                                    err
-                                ),
-                            }
-                        }
-                    }
-                }
-                Err(err) => ic_cdk::println!("Failed to promote AlphaWarden to community owner: {:?}", err),
-            }
-        }
-    });
 
     read_state(|state| {
         let now = state.env.now();
