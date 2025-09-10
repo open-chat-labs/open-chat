db74ba4fdedb2c30f797a1096f311e4f72b79429
Hamish Peebles <hamishpeebles@gmail.com>
2025-09-10 11:09:16 +0100

    Store and regularly update EVM contract addresses

--- backend/canisters/registry/impl/src/jobs/mod.rs
+++ backend/canisters/registry/impl/src/jobs/mod.rs
@@ -4,10 +4,12 @@ mod check_for_sns_updates;
 mod check_for_token_updates;
 mod check_for_updates_to_supported_standards;
 mod update_chat_supply;
+mod update_evm_contract_addresses;
 
 pub(crate) fn start(_state: &RuntimeState) {
     check_for_sns_updates::start_job();
     check_for_token_updates::start_job();
     check_for_updates_to_supported_standards::start_job();
     update_chat_supply::start_job();
+    update_evm_contract_addresses::start_job();
 }
