4c454ec606a4bc4575ff0e4dacde2c6c26ace618
Hamish Peebles <hamishpeebles@gmail.com>
2025-09-10 15:01:03 +0100

    Fix

--- backend/canisters/registry/impl/src/jobs/update_evm_contract_addresses.rs
+++ backend/canisters/registry/impl/src/jobs/update_evm_contract_addresses.rs
@@ -64,13 +64,19 @@ async fn run_async() {
 
         // Add the tokens where the list of EVM contract addresses has changed
         for (ledger_canister_id, previous_addresses) in previous_addresses {
+            let mut updated = false;
             if let Some(new_addresses) = state.data.evm_contract_addresses.get(&ledger_canister_id) {
-                let updated = previous_addresses.len() != new_addresses.len()
-                    || previous_addresses.iter().any(|a| !new_addresses.contains(a));
-
-                if updated {
-                    tokens_with_updates.push(ledger_canister_id);
+                if previous_addresses.len() != new_addresses.len()
+                    || previous_addresses.iter().any(|a| !new_addresses.contains(a))
+                {
+                    updated = true;
                 }
+            } else {
+                updated = true;
+            }
+
+            if updated {
+                tokens_with_updates.push(ledger_canister_id);
             }
         }
 
