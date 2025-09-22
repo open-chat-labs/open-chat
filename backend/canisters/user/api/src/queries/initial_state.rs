249c5730e55fb81e46ef15bbd9b9224e7cc2df43
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-22 13:23:32 +0100

    rejig pinned messages a bit (#8619)

--- backend/canisters/user/api/src/queries/initial_state.rs
+++ backend/canisters/user/api/src/queries/initial_state.rs
@@ -44,6 +44,7 @@ pub struct SuccessResult {
     pub btc_address: Option<String>,
     pub one_sec_address: Option<String>,
     pub premium_items: Vec<u32>,
+    pub pinned_chats: Vec<ChatId>,
 }
 
 #[ts_export(user, initial_state)]
