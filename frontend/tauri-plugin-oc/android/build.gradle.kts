9c5ea92a23f4929880c68fb9419d739ef81510bf
Ivan Jukic <ivan.jukic14@gmail.com>
2025-10-03 13:07:18 +0200

    Set notification priority on the server side --- Notifications pusher will now set the priority for the android notifications to high. On the android side, added priority to the notification when it's created device-side, which may have an effect with some OEMs.

--- frontend/tauri-plugin-oc/android/build.gradle.kts
+++ frontend/tauri-plugin-oc/android/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
@@ -62,11 +62,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.10.2")
     implementation("androidx.browser:browser:1.9.0")
-    implementation("com.google.firebase:firebase-messaging:25.0.0")
+    implementation("com.google.firebase:firebase-messaging:25.0.1")
     implementation("io.coil-kt.coil3:coil:3.2.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
 
-    val roomVersion = "2.7.2"
+    val roomVersion = "2.8.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
