0cea83098d748c77885651ab232e919fba857e21
Julian Jelfs <julian.jelfs@gmail.com>
2025-10-08 09:24:29 +0100

    chat history toggle

--- frontend/app/src/components_mobile/home/createOrUpdateGroup/GeneralSetup.svelte
+++ frontend/app/src/components_mobile/home/createOrUpdateGroup/GeneralSetup.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import { Container } from "component-lib";
+    import { Container, Switch } from "component-lib";
     import { type CandidateGroupChat } from "openchat-client";
+    import Setting from "../../Setting.svelte";
     import DisappearingMessages from "./DisappearingMessages.svelte";
     import GroupCard from "./GroupCard.svelte";
 
@@ -18,6 +19,13 @@
     direction={"vertical"}
     padding={["xxl", "lg", "lg", "lg"]}>
     <GroupCard {candidateGroup} />
+
+    <Setting
+        toggle={() => (candidateGroup.historyVisible = !candidateGroup.historyVisible)}
+        info={"By default new memebers in the group will see all the previous messages that were sent within the group. Enable this option to hide chat history for new members."}
+        title={"Hide chat history for new members"}>
+        <Switch bind:checked={candidateGroup.historyVisible} />
+    </Setting>
 
     <DisappearingMessages bind:candidateGroup />
 </Container>
