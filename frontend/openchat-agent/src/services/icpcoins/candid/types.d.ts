58583128dc8f75e69b83c5123babb19aa57fac48
Julian Jelfs <julian.jelfs@gmail.com>
2025-10-20 09:34:48 +0100

    switch to new icpcoins endpoint (#8638)

--- frontend/openchat-agent/src/services/icpcoins/candid/types.d.ts
+++ frontend/openchat-agent/src/services/icpcoins/candid/types.d.ts
@@ -2,10 +2,76 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export type LatestTokenRow = [[TokenId, TokenId], string, number];
-export type TokenId = bigint;
+export interface CoinWithDetails {
+  'id' : PKKey,
+  'decimals' : number,
+  'deleted' : boolean,
+  'name' : string,
+  'rank' : number,
+  'tags' : Array<string>,
+  'ledger_id' : string,
+  'details_json' : string,
+  'details' : Array<[string, Value]>,
+  'overview_json' : string,
+  'symbol' : string,
+  'unlisted' : boolean,
+}
+export interface Doc {
+  'id' : PKKey,
+  'decimals' : number,
+  'deleted' : boolean,
+  'name' : string,
+  'rank' : number,
+  'tags' : Array<string>,
+  'ledger_id' : string,
+  'details_json' : string,
+  'overview_json' : string,
+  'symbol' : string,
+  'unlisted' : boolean,
+}
+export interface GetCoinFullReq {
+  'id' : PKKey,
+  'select' : [] | [Array<string>],
+}
+export type GetCoinFullResp = [Doc, Array<[string, Value]>];
+export interface GetCoinsByMarketcapReq {
+  'from' : [] | [bigint],
+  'full' : boolean,
+  'select' : [] | [Array<string>],
+}
+export interface GetCoinsByMarketcapResp {
+  'last' : [] | [bigint],
+  'coins' : Array<CoinWithDetails>,
+}
+export interface GetDetailsReq { 'id' : PKKey, 'select' : [] | [Array<string>] }
+export type GetDetailsResp = Array<[string, Value]>;
+export type PKKey = number;
+export type SetDetailsReq = Array<
+  { 'id' : PKKey, 'details' : Array<[string, Value]> }
+>;
+export interface SetReq {
+  'id' : PKKey,
+  'decimals' : number,
+  'deleted' : boolean,
+  'name' : string,
+  'rank' : number,
+  'tags' : Array<string>,
+  'ledger_id' : string,
+  'details_json' : string,
+  'overview_json' : string,
+  'symbol' : string,
+  'unlisted' : boolean,
+}
+export type Value = number;
 export interface _SERVICE {
-  'get_latest' : ActorMethod<[], Array<LatestTokenRow>>,
+  'get_coin_full' : ActorMethod<[GetCoinFullReq], GetCoinFullResp>,
+  'get_coins_by_marketcap' : ActorMethod<
+    [GetCoinsByMarketcapReq],
+    GetCoinsByMarketcapResp
+  >,
+  'get_details' : ActorMethod<[GetDetailsReq], GetDetailsResp>,
+  'set' : ActorMethod<[SetReq], undefined>,
+  'set_details' : ActorMethod<[SetDetailsReq], undefined>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
