fdfe3e02be8475604ecdd9cd6ff82e8c5928763f
Hamish Peebles <hamishpeebles@gmail.com>
2025-11-20 14:15:49 +0000

    Expose partial notification subscription details to aid with debugging

--- backend/canisters/notifications_index/impl/src/queries/http_request.rs
+++ backend/canisters/notifications_index/impl/src/queries/http_request.rs
@@ -1,7 +1,9 @@
 use crate::{RuntimeState, read_state};
+use candid::Principal;
 use http_request::{Route, build_json_response, encode_logs, extract_route};
 use ic_cdk::query;
-use types::{HttpRequest, HttpResponse, TimestampMillis};
+use std::collections::HashMap;
+use types::{HttpRequest, HttpResponse, TimestampMillis, UserId};
 
 #[query]
 fn http_request(request: HttpRequest) -> HttpResponse {
@@ -21,11 +23,28 @@ fn http_request(request: HttpRequest) -> HttpResponse {
         build_json_response(&state.metrics())
     }
 
+    fn get_subscriptions_impl(qs: HashMap<String, String>, state: &RuntimeState) -> HttpResponse {
+        let Some(user_id) = qs.get("user_id").and_then(|u| Principal::from_text(u).ok()).map(UserId::from) else {
+            return HttpResponse::not_found();
+        };
+
+        let endpoints: Vec<_> = state
+            .data
+            .subscriptions
+            .get_by_user(&user_id)
+            .into_iter()
+            .map(|s| s.endpoint)
+            .collect();
+
+        build_json_response(&endpoints)
+    }
+
     match extract_route(&request.url) {
         Route::Errors(since) => get_errors_impl(since),
         Route::Logs(since) => get_logs_impl(since),
         Route::Traces(since) => get_traces_impl(since),
         Route::Metrics => read_state(get_metrics_impl),
+        Route::Other(path, qs) if path == "subscriptions" => read_state(|state| get_subscriptions_impl(qs, state)),
         _ => HttpResponse::not_found(),
     }
 }
