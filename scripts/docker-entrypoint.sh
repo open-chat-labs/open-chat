d0437bc00e864e870fb0eac5a1cd58b0ec3d72bd
Ivan Jukic <ivan.jukic14@gmail.com>
2025-11-28 12:18:41 +0100

    prepared for CI run

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,8 +3,12 @@ set -eux
 
 # Start replica
 dfx start --background --log file --logfile ~/dfx.log
-echo "🚀 DFX replica running!"
+
+# Wait for replica to be ready
+for i in {1..60}; do
+    if dfx ping; then break; fi
+    sleep 1
+done
 
 # Start nginx to serve the UI and proxy requests to dfx replica
 nginx -g "daemon off;"
-echo "🚀 Nginx running!"
