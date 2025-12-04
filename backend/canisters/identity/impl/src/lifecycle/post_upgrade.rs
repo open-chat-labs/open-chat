8154f29a0e757b3e06e6e48bc0990e561823cd27
Hamish Peebles <hamishpeebles@gmail.com>
2025-12-04 12:01:29 +0000

    Disallow new identities via SignInWithEth/Sol as can be automated (#8708)

--- backend/canisters/identity/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/identity/impl/src/lifecycle/post_upgrade.rs
@@ -7,6 +7,7 @@ use ic_cdk::post_upgrade;
 use identity_canister::post_upgrade::Args;
 use stable_memory::get_reader;
 use tracing::info;
+use types::CanisterId;
 use utils::cycles::init_cycles_dispenser_client;
 use utils::env::canister::CanisterEnv;
 
@@ -16,8 +17,20 @@ fn post_upgrade(args: Args) {
     let memory = get_upgrades_memory();
     let reader = get_reader(&memory);
 
-    let (data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
+    let (mut data, errors, logs, traces): (Data, Vec<LogEntry>, Vec<LogEntry>, Vec<LogEntry>) =
         msgpack::deserialize(reader).unwrap();
+
+    for canister_id in [
+        "2notu-qyaaa-aaaar-qaeha-cai", // SignInWithEth
+        "4s357-zaaaa-aaaaf-bjz7q-cai", // SignInWithEthTest
+        "2kpva-5aaaa-aaaar-qaehq-cai", // SignInWithSol
+        "lix6w-ciaaa-aaaaf-bj2aa-cai", // SignInWithSolTest
+    ]
+    .into_iter()
+    .map(|s| CanisterId::from_text(s).unwrap())
+    {
+        data.originating_canisters.remove(&canister_id);
+    }
 
     canister_logger::init_with_logs(data.test_mode, errors, logs, traces);
 
