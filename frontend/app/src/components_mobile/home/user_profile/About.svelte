c1edceccaf93dcba07a1c0ee4f3bb8245ff544f6
Julian Jelfs <julian.jelfs@gmail.com>
2025-12-31 13:23:06 +0000

    open the old landing pages in external browser

--- frontend/app/src/components_mobile/home/user_profile/About.svelte
+++ frontend/app/src/components_mobile/home/user_profile/About.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
     import { i18nKey } from "@src/i18n/i18n";
     import { Body, BodySmall, ColourVars, Container, Logo, Overview } from "component-lib";
+    import type { OpenChat } from "openchat-client";
     import page from "page";
+    import { getContext } from "svelte";
     import ChevronRight from "svelte-material-icons/ChevronRight.svelte";
+    import { openUrl } from "tauri-plugin-oc-api";
     import SlidingPageContent from "../SlidingPageContent.svelte";
+
+    const client = getContext<OpenChat>("client");
 
     //@ts-ignore
     let version = window.OC_WEBSITE_VERSION;
+
+    function goTo(url: string, local: boolean = true) {
+        if (client.isNativeApp()) {
+            openUrl({ url: new URL(url, local ? client.canonicalOrigin() : undefined).toString() });
+        } else {
+            page(url);
+        }
+    }
 </script>
 
 {#snippet menuitem(label: string, route: string)}
-    <Container onClick={() => page(route)} crossAxisAlignment={"center"}>
+    <Container onClick={() => goTo(route)} crossAxisAlignment={"center"}>
         <Body fontWeight={"bold"}>{label}</Body>
         <ChevronRight color={ColourVars.primary} />
     </Container>
@@ -34,7 +47,7 @@
             {@render menuitem("FAQ", "/faq")}
             {@render menuitem("Features", "/features")}
             {@render menuitem("Guidelines", "/guidelines")}
-            {@render menuitem("Metrics", "/metrics")}
+            {@render menuitem("Metrics", "https://tokenterminal.com/explorer/projects/openchat")}
             {@render menuitem("Roadmap", "/roadmap")}
             {@render menuitem("Whitepaper", "/whitepaper")}
         </Container>
