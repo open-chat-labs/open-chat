3d7cfb500b122ae2f85fc8ee2ecc009e160d9ff3
Julian Jelfs <julian.jelfs@gmail.com>
2026-01-06 17:00:32 +0000

    suspended modal

--- frontend/app/src/components_mobile/SuspendedModal.svelte
+++ frontend/app/src/components_mobile/SuspendedModal.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+    import { Body, ColourVars, Column, H2, Row, Sheet } from "component-lib";
     import { currentUserStore } from "openchat-client";
+    import RobotDead from "svelte-material-icons/RobotDeadOutline.svelte";
     import { i18nKey } from "../i18n/i18n";
-    import Markdown from "./home/Markdown.svelte";
-    import ModalContent from "./ModalContent.svelte";
     import Translatable from "./Translatable.svelte";
 
     interface Props {
@@ -11,26 +11,32 @@
 
     let { onClose }: Props = $props();
 
-    function buildNoticeText(): string {
-        const suspensionDetails = $currentUserStore.suspensionDetails!;
-        const actionDate = new Date(Number(suspensionDetails.action.timestamp));
-        const actionText =
-            suspensionDetails.action.kind === "delete_action" ? "deleted" : "unsuspended";
-
-        return `Your account has been suspended.
-
-Reason:
-"${suspensionDetails.reason}"
-
-You can appeal this suspension by sending a direct message to the @OpenChat Twitter account otherwise your account will be ${actionText} on ${actionDate.toLocaleString()}.`;
-    }
+    let suspensionDetails = $derived($currentUserStore.suspensionDetails);
+    let actionDate = $derived(new Date(Number(suspensionDetails?.action?.timestamp)));
+    const actionText = $derived(
+        suspensionDetails?.action?.kind === "delete_action" ? "deleted" : "unsuspended",
+    );
+    let notice = $derived(
+        `You can appeal this suspension by sending a direct message to the @OpenChat Twitter account otherwise your account will be ${actionText} on ${actionDate?.toLocaleString()}.`,
+    );
 </script>
 
-<ModalContent {onClose}>
-    {#snippet header()}
-        <Translatable resourceKey={i18nKey("accountSuspended")} />
-    {/snippet}
-    {#snippet body()}
-        <Markdown text={buildNoticeText()} />
-    {/snippet}
-</ModalContent>
+<Sheet onDismiss={onClose}>
+    <Column crossAxisAlignment={"center"} gap={"xl"} padding={"xl"}>
+        <H2 width={"hug"} fontWeight={"bold"} colour={"primary"}>
+            <Translatable resourceKey={i18nKey("accountSuspended")} />
+        </H2>
+        <Row padding={["lg", "zero"]} mainAxisAlignment={"center"}>
+            <RobotDead color={ColourVars.primary} size={"6rem"} />
+        </Row>
+        <Column>
+            {#if $currentUserStore.suspensionDetails?.reason !== undefined}
+                <Row gap={"sm"}>
+                    <Body width={"hug"} colour={"primary"}>Reason:</Body>
+                    <Body width={"hug"}>{$currentUserStore.suspensionDetails?.reason}</Body>
+                </Row>
+            {/if}
+        </Column>
+        <Body colour={"textSecondary"}>{notice}</Body>
+    </Column>
+</Sheet>
