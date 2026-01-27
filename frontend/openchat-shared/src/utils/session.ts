2552404f24fafae4d103ac0232a7e397ff21a713
Hamish Peebles <hamishpeebles@gmail.com>
2026-01-27 15:36:29 +0000

    Only interact with OC identity storage from within the worker (#8841)

--- frontend/openchat-shared/src/utils/session.ts
+++ frontend/openchat-shared/src/utils/session.ts
@@ -1,17 +1,15 @@
 import type { Identity } from "@icp-sdk/core/agent";
 import type { DelegationIdentity } from "@icp-sdk/core/identity";
 
-export function getTimeUntilSessionExpiryMs(identity: Identity): number {
+export function getSessionExpiryMs(identity: Identity): number {
     if (!("getDelegation" in identity)) {
         return 0;
     }
 
-    const expiryDateTimestampMs = Number(
+    return Number(
         (identity as DelegationIdentity)
             .getDelegation()
             .delegations.map((d) => d.delegation.expiration)
             .reduce((current, next) => (next < current ? next : current)) / BigInt(1_000_000),
     );
-
-    return expiryDateTimestampMs - Date.now();
 }
