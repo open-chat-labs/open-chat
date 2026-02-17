c7126093390430d93acc13e2d67558727f0fc394
Ivan Jukic <ivan.jukic14@gmail.com>
2026-02-17 12:45:48 +0100

    longpress update

--- frontend/component-lib/src/actions/longpress.ts
+++ frontend/component-lib/src/actions/longpress.ts
@@ -2,7 +2,6 @@ import { isTouchDevice, mobileOperatingSystem } from "component-lib";
 
 // how close to the edge can we get for a longpress
 const EDGE_TRESHOLD = 24;
-const TRANSITION_DURATION = 250;
 const SCALE_EFFECT_TARGET = "0.95";
 // Menu activates after 80% of the original delay time has passed.
 const MENU_ACTIVATION_THRESHOLD = 0.8;
@@ -39,9 +38,9 @@ function isEdgeTouch(e: TouchEvent) {
 // Set scaling transitions on the longpress target node, without messing up
 // any other transitions that are already set, though it may colide with any
 // scale values previously set on the node.
-function setTransitions(node: HTMLElement) {
+function setTransitions(node: HTMLElement, scaleDuration: number) {
     const current = window.getComputedStyle(node).transition;
-    const newTransition = `scale ${TRANSITION_DURATION}ms ease-out`;
+    const newTransition = `scale ${scaleDuration}ms ease-out`;
 
     // Check if the current transition is effectively "empty"
     // We look for '0s' which is the constant across all browsers for "no duration"
@@ -70,7 +69,8 @@ type Props = {
     animation?: LongpressAnimation;
     cooldown?: boolean;
     delay?: number;
-    // Helps us prevent longpress-ing again when the menu is already shown!
+    // Helps us prevent longpress-ing again when the menu is already shown, by
+    // reseting a local var when isOpen value changes.
     isOpen?: boolean;
 };
 
@@ -86,12 +86,12 @@ export function longpress(node: HTMLElement, args?: LongpressArg) {
         animation = "none",
         cooldown = false,
         delay = 600,
-        isOpen,
     } = processArgs(args);
 
     let scaleAnimationEnabled = animation === "scale";
     let longPressTimer: number | undefined;
-    let scalePressTimer: number | undefined;
+    let scaleDownTimer: number | undefined;
+    let scaleUpTimer: number | undefined;
     let startX = 0;
     let startY = 0;
     let menuShown = false;
@@ -99,7 +99,9 @@ export function longpress(node: HTMLElement, args?: LongpressArg) {
     const originalScale = window.getComputedStyle(node).scale ?? "1.0";
 
     if (scaleAnimationEnabled) {
-        setTransitions(node);
+        // We divide delay by 4, since scale down will start after half of delay
+        // has passed already, so we have two quarts left to scale down then up.
+        setTransitions(node, delay / 4);
     }
 
     function processArgs(args: LongpressArg) {
@@ -137,10 +139,17 @@ export function longpress(node: HTMLElement, args?: LongpressArg) {
         startY = t.screenY;
         clearLongPressTimer();
 
-        scalePressTimer = window.setTimeout(() => {
+        // Scale down starts when half of the longpress delay has passed. Animation
+        // start does not mean the menu will open.
+        scaleDownTimer = window.setTimeout(() => {
             onpressactive?.();
             shrinkTarget();
         }, delay / 2);
+
+        scaleUpTimer = window.setTimeout(() => {
+            window.clearTimeout(scaleDownTimer); // just in case of overlap
+            restoreTargetScale();
+        }, delay);
 
         // We activate the menu after threshold % of the time had passed. In
         // theory, this should prevent perceived issues with the menu where
@@ -173,8 +182,9 @@ export function longpress(node: HTMLElement, args?: LongpressArg) {
     }
 
     function clearLongPressTimer() {
-        window.clearTimeout(scalePressTimer);
         window.clearTimeout(longPressTimer);
+        window.clearTimeout(scaleDownTimer);
+        window.clearTimeout(scaleUpTimer);
     }
 
     if (isTouchDevice) {
