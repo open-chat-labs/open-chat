8112da585214c8b7a8931c970bbafaddb7144ccc
Hamish Peebles <hamishpeebles@gmail.com>
2026-05-01 12:15:36 +0100

    Fix message entry box being hidden by keyboard after cold start

--- frontend/app/src/components_mobile/home/HomeRoute.svelte
+++ frontend/app/src/components_mobile/home/HomeRoute.svelte
@@ -27,8 +27,8 @@
     .loading {
         width: 100vw;
         height: 100vh;
-        height: calc(var(--vh, 1vh) * 100);
         height: 100dvh; // firefox will ignore this
+        height: calc(var(--vh, 1vh) * 100);
         display: grid;
     }
 
