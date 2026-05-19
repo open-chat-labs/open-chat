6c25c24a938137d21bf773265ab870f07fbd42a5
Hamish Peebles <hamishpeebles@gmail.com>
2026-05-19 14:55:50 +0100

    Check for refund if `notify_deposit` called after swap canceled/expired

--- backend/canisters/escrow/impl/src/updates/notify_deposit.rs
+++ backend/canisters/escrow/impl/src/updates/notify_deposit.rs
@@ -6,28 +6,58 @@ use canister_tracing_macros::trace;
 use escrow_canister::deposit_subaccount;
 use escrow_canister::notify_deposit::{Response::*, *};
 use icrc_ledger_types::icrc1::account::Account;
-use types::CanisterId;
+use types::TokenInfo;
 
 #[update(candid = true, msgpack = true)]
 #[trace]
 async fn notify_deposit(args: Args) -> Response {
-    let PrepareResult {
-        principal,
-        ledger,
-        account,
-        balance_required,
-    } = match mutate_state(|state| prepare(&args, state)) {
-        Ok(ok) => ok,
-        Err(response) => return response,
-    };
+    match mutate_state(|state| prepare(&args, state)) {
+        PrepareResult::Success(success) => {
+            process_swap(
+                args.swap_id,
+                success.principal,
+                success.token_info,
+                success.account,
+                success.balance_required,
+            )
+            .await
+        }
+        PrepareResult::ErrorCheckForRefund(error) => {
+            if let Err(err) = check_for_refund(args.swap_id, error.principal, error.token_info, error.account).await {
+                InternalError(err)
+            } else {
+                error.response
+            }
+        }
+        PrepareResult::Error(response) => response,
+    }
+}
 
-    match icrc_ledger_canister_c2c_client::icrc1_balance_of(ledger, &account)
+async fn process_swap(
+    swap_id: u32,
+    principal: Principal,
+    token_info: TokenInfo,
+    account: Account,
+    balance_required: u128,
+) -> Response {
+    match icrc_ledger_canister_c2c_client::icrc1_balance_of(token_info.ledger, &account)
         .await
         .map(|b| u128::try_from(b.0).unwrap())
     {
         Ok(balance) => mutate_state(|state| {
-            let swap = state.data.swaps.get_mut(args.swap_id).unwrap();
+            let swap = state.data.swaps.get_mut(swap_id).unwrap();
             if balance < balance_required {
+                if balance > token_info.fee {
+                    state.data.pending_payments_queue.push(PendingPayment {
+                        principal,
+                        timestamp: state.env.now(),
+                        amount: balance - token_info.fee,
+                        token_info,
+                        swap_id,
+                        reason: PendingPaymentReason::Refund,
+                    });
+                    crate::jobs::make_pending_payments::start_job_if_required(state);
+                }
                 BalanceTooLow(BalanceTooLowResult {
                     balance,
                     balance_required,
@@ -68,63 +98,125 @@ async fn notify_deposit(args: Args) -> Response {
     }
 }
 
-struct PrepareResult {
+async fn check_for_refund(swap_id: u32, principal: Principal, token_info: TokenInfo, account: Account) -> Result<(), String> {
+    match icrc_ledger_canister_c2c_client::icrc1_balance_of(token_info.ledger, &account)
+        .await
+        .map(|b| u128::try_from(b.0).unwrap())
+    {
+        Ok(balance) => {
+            if balance > token_info.fee {
+                mutate_state(|state| {
+                    state.data.pending_payments_queue.push(PendingPayment {
+                        principal,
+                        timestamp: state.env.now(),
+                        amount: balance - token_info.fee,
+                        token_info,
+                        swap_id,
+                        reason: PendingPaymentReason::Refund,
+                    });
+                    crate::jobs::make_pending_payments::start_job_if_required(state);
+                });
+            }
+            Ok(())
+        }
+        Err(error) => Err(format!("Failed to check balance for refund: {error:?}")),
+    }
+}
+
+enum PrepareResult {
+    Success(PrepareSuccess),
+    ErrorCheckForRefund(PrepareError),
+    Error(Response),
+}
+struct PrepareSuccess {
     principal: Principal,
-    ledger: CanisterId,
+    token_info: TokenInfo,
     account: Account,
     balance_required: u128,
 }
 
-fn prepare(args: &Args, state: &mut RuntimeState) -> Result<PrepareResult, Response> {
-    let now = state.env.now();
-    if let Some(swap) = state.data.swaps.get_mut(args.swap_id) {
-        if swap.cancelled_at.is_some() {
-            Err(SwapCancelled)
-        } else if swap.expires_at < now {
-            Err(SwapExpired)
-        } else {
-            let escrow_canister_id = state.env.canister_id();
-            let principal = args.deposited_by.unwrap_or_else(|| state.env.caller());
+struct PrepareError {
+    principal: Principal,
+    token_info: TokenInfo,
+    account: Account,
+    response: Response,
+}
 
-            if swap.offered_by == principal {
-                if swap.token0_received {
-                    Err(Success(SuccessResult {
-                        complete: swap.token1_received,
-                    }))
-                } else {
-                    Ok(PrepareResult {
-                        principal,
-                        ledger: swap.token0.ledger,
-                        account: Account {
-                            owner: escrow_canister_id,
-                            subaccount: Some(deposit_subaccount(principal, swap.id)),
-                        },
-                        balance_required: swap.amount0 + swap.token0.fee,
-                    })
-                }
-            } else if let Some((accepted_by, _)) = swap.accepted_by {
-                if accepted_by == principal {
-                    Err(Success(SuccessResult {
-                        complete: swap.token0_received,
-                    }))
-                } else {
-                    Err(SwapAlreadyAccepted)
-                }
-            } else if swap.restricted_to.is_none_or(|p| p == principal) {
-                Ok(PrepareResult {
-                    principal,
-                    ledger: swap.token1.ledger,
-                    account: Account {
-                        owner: escrow_canister_id,
-                        subaccount: Some(deposit_subaccount(principal, swap.id)),
-                    },
-                    balance_required: swap.amount1 + swap.token1.fee,
+fn prepare(args: &Args, state: &mut RuntimeState) -> PrepareResult {
+    let Some(swap) = state.data.swaps.get_mut(args.swap_id) else {
+        return PrepareResult::Error(SwapNotFound);
+    };
+
+    let now = state.env.now();
+    let expired = now > swap.expires_at;
+
+    let principal = args.deposited_by.unwrap_or_else(|| state.env.caller());
+    let escrow_canister_id = state.env.canister_id();
+
+    if swap.offered_by == principal {
+        let token_info = swap.token0.clone();
+        let account = Account {
+            owner: escrow_canister_id,
+            subaccount: Some(deposit_subaccount(principal, swap.id)),
+        };
+
+        let response = if expired {
+            SwapExpired
+        } else if swap.cancelled_at.is_some() {
+            SwapCancelled
+        } else if swap.token0_received {
+            Success(SuccessResult {
+                complete: swap.token1_received,
+            })
+        } else {
+            return PrepareResult::Success(PrepareSuccess {
+                principal,
+                account,
+                balance_required: swap.amount0 + swap.token0.fee,
+                token_info,
+            });
+        };
+        PrepareResult::ErrorCheckForRefund(PrepareError {
+            principal,
+            account,
+            token_info,
+            response,
+        })
+    } else {
+        let token_info = swap.token1.clone();
+        let account = Account {
+            owner: escrow_canister_id,
+            subaccount: Some(deposit_subaccount(principal, swap.id)),
+        };
+
+        let response = if expired {
+            SwapExpired
+        } else if swap.cancelled_at.is_some() {
+            SwapCancelled
+        } else if let Some((accepted_by, _)) = swap.accepted_by {
+            if accepted_by == principal {
+                Success(SuccessResult {
+                    complete: swap.token0_received,
                 })
             } else {
-                Err(NotAuthorized)
+                SwapAlreadyAccepted
             }
-        }
-    } else {
-        Err(SwapNotFound)
+        } else if swap.restricted_to.is_none_or(|p| p == principal) {
+            return PrepareResult::Success(PrepareSuccess {
+                principal,
+                account,
+                balance_required: swap.amount1 + token_info.fee,
+                token_info,
+            });
+        } else {
+            NotAuthorized
+        };
+
+        PrepareResult::ErrorCheckForRefund(PrepareError {
+            principal,
+            token_info,
+            account,
+            response,
+        })
     }
 }
--- backend/integration_tests/src/escrow_tests.rs
+++ backend/integration_tests/src/escrow_tests.rs
@@ -2,11 +2,13 @@ use crate::env::ENV;
 use crate::utils::{chat_token_info, icp_token_info, now_millis, tick_many};
 use crate::{TestEnv, client};
 use candid::Principal;
-use constants::DAY_IN_MS;
+use constants::{DAY_IN_MS, HOUR_IN_MS, MINUTE_IN_MS};
 use escrow_canister::deposit_subaccount;
 use icrc_ledger_types::icrc1::account::Account;
 use std::ops::Deref;
 use std::str::FromStr;
+use std::time::Duration;
+use test_case::test_case;
 use types::{Chat, P2PSwapLocation};
 
 #[test]
@@ -148,5 +150,104 @@ fn external_swap_via_escrow_canister_succeeds() {
     assert_eq!(
         client::ledger::happy_path::balance_of(env, canister_ids.icp_ledger, accepter),
         icp_amount
+    );
+}
+
+#[test_case(true)]
+#[test_case(false)]
+fn deposits_refunded_if_swap_no_longer_available(expired: bool) {
+    let mut wrapper = ENV.deref().get();
+    let TestEnv {
+        env,
+        canister_ids,
+        controller,
+    } = wrapper.env();
+
+    let user1 = client::register_user(env, canister_ids);
+    let user2 = client::register_user(env, canister_ids);
+    let now = now_millis(env);
+
+    let icp_amount = 100_000_000_000;
+    let chat_amount = 1_000_000_000_000;
+
+    let swap_id = client::escrow::happy_path::create_swap(
+        env,
+        user1.user_id.into(),
+        canister_ids.escrow,
+        P2PSwapLocation::from_message(Chat::Direct(user2.user_id.into()), None, 0u64.into()),
+        icp_token_info(),
+        icp_amount,
+        None,
+        chat_token_info(),
+        chat_amount,
+        None,
+        now + HOUR_IN_MS,
+    );
+
+    let user1_deposit_account = Account {
+        owner: canister_ids.escrow,
+        subaccount: Some(deposit_subaccount(user1.user_id.into(), swap_id)),
+    };
+
+    client::ledger::happy_path::transfer(
+        env,
+        *controller,
+        canister_ids.icp_ledger,
+        user1_deposit_account,
+        icp_amount + 10_000,
+    );
+
+    let user2_deposit_account = Account {
+        owner: canister_ids.escrow,
+        subaccount: Some(deposit_subaccount(user2.user_id.into(), swap_id)),
+    };
+
+    client::ledger::happy_path::transfer(
+        env,
+        *controller,
+        canister_ids.chat_ledger,
+        user2_deposit_account,
+        chat_amount + 100_000,
+    );
+
+    client::escrow::happy_path::notify_deposit(env, user1.user_id.into(), canister_ids.escrow, swap_id, None);
+
+    if expired {
+        env.advance_time(Duration::from_millis((60 * MINUTE_IN_MS) + 1));
+    } else {
+        client::escrow::happy_path::cancel_swap(env, user1.user_id.into(), canister_ids.escrow, swap_id);
+    }
+
+    let notify_response = client::escrow::notify_deposit(
+        env,
+        user2.user_id.into(),
+        canister_ids.escrow,
+        &escrow_canister::notify_deposit::Args {
+            swap_id,
+            deposited_by: None,
+        },
+    );
+
+    if expired {
+        assert!(matches!(
+            notify_response,
+            escrow_canister::notify_deposit::Response::SwapExpired
+        ));
+    } else {
+        assert!(matches!(
+            notify_response,
+            escrow_canister::notify_deposit::Response::SwapCancelled
+        ));
+    };
+
+    tick_many(env, 10);
+
+    assert_eq!(
+        client::ledger::happy_path::balance_of(env, canister_ids.icp_ledger, user1.user_id),
+        icp_amount
+    );
+    assert_eq!(
+        client::ledger::happy_path::balance_of(env, canister_ids.chat_ledger, user2.user_id),
+        chat_amount
     );
 }
